83a69f8c98e840700e85d11a9780248619e1d27b
sintflut <sintflut@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2002-02-10 18:00:33 +0000

    dirent for Win32 (test)
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@11 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ src/dirent.h
@@ -0,0 +1,41 @@
+#ifndef WIN32
+#include <dirent.h>
+#else
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+struct dirent
+{
+	char d_name[MAX_PATH];
+};
+
+// ich gehe davon aus, das DIR in der orginal-dirent.h ein handle ist, dessen
+// inhalt niemanden interessiert und das es deshalb auch niemanden stört, wenn
+// da ganz was anderes drinsteht.
+
+typedef class Dir DIR;
+class Dir
+{
+	friend DIR* opendir(const char*);
+	friend dirent* readdir(DIR*);
+	friend void closedir(DIR*);
+
+// ich weiss nicht, wie das im original geregelt ist. der fileloc.cpp entnehme
+// ich, dass der rückgabepointer von readfile nicht gelöscht werden muss; da
+// ein globaler "dirent ret;" ziemlich dumm wäre (mit dem DIR-handle kann man
+// mehrere Verzeichnisse parallel durchsuchen, mit einem globalen ret aber nur
+// eine Datei abfragen), löse ich das erstmal so: man kann für jeden verzeich-
+// nisdurchlauf je eine datei betrachten.
+	dirent ret;
+	char* name;
+	HANDLE find;
+	
+	Dir(const char* n);
+	~Dir();
+};
+
+DIR* opendir(const char* name);
+dirent* readdir(DIR* dir);
+void closedir(DIR* dir);
+
+#endif //WIN32
