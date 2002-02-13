48e5aaa6335c4b2d7335d0928fa8d1100624c6cf
sirver <sirver@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2002-02-13 23:18:02 +0000

    Changed disclaimer from 2001 in 2002
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@33 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/ui.cc
+++ src/ui.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001 by Holger Rapp 
+ * Copyright (C) 2002 by Holger Rapp 
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
