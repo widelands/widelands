4d0c43cd0c54b3574cac2f2cd0797464581f22ad
sirver <sirver@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2002-07-07 21:51:49 +0000

    missed some checkins
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@171 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ src/worlddata.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2002 by the Widelands Development Team
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef __S__TRIBEDATA_H
+#define __S__TRIBEDATA_H
+
+#ifndef PACK_VERSION
+#define PACK_VERSION(major, minor) (  (ushort) (((uchar) (major))<<8) | (((uchar) (minor))) ) 
+#endif
+
+#ifndef VERSION_MAJOR
+#define VERSION_MAJOR(vers) ((ushort) (((ushort) (vers)>>8) & 0xff))
+#endif
+
+#ifndef VERSION_MINOR
+#define VERSION_MINOR(vers) ((ushort) (((ushort) (vers)) & 0xff))
+#endif
+
+#define WLWF_VERSION			PACK_VERSION(1,0)
+
+#define TEXTURE_W          64
+#define TEXTURE_H          64
+
+
+#define TERRAIN_DRY        0
+#define TERRAIN_GREEN      1 
+#define TERRAIN_WATER      2
+#define TERRAIN_ACID       3
+#define TERRAIN_MOUNTAIN   4
+
+
+#endif /* __S__TRIBEDATA_H */
