a854569628bf67ec152405da53f4c78bd1885982
ixprefect <ixprefect@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2002-08-27 16:55:52 +0000

    added build script
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@212 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ build/build1.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+echo Build creation, step 1
+echo
+echo Retrieving the latest and greatest from CVS
+
+cvs -z3 -d:pserver:anonymous@cvs.widelands.sourceforge.net:/cvsroot/widelands export -D now widelands
+
+echo
+echo autotools stuff...
+
+cd widelands
+./make_ready.sh
+cd ..
+
+echo
+echo Creating source and binary package file lists
+
+find widelands -maxdepth 1 ! -type d > source.list
+cat >> source.list <<EOF
+widelands/src/*
+widelands/build/*
+widelands/win32/*
+widelands/wffcreate/*
+EOF
+
+cat > binary.list <<EOF
+widelands/AUTHORS
+widelands/COPYING
+widelands/README
+widelands/widelands
+widelands/widelands.exe
+widelands/SDL.dll
+widelands/fonts/*
+widelands/maps/*
+widelands/pics/*
+widelands/tribes/*
+widelands/worlds/*
+EOF
+
+echo
+echo Compiling the source now
+
+cd widelands
+./configure
+make
+cp src/widelands .
+cd ..
+
+cat <<EOF
+
+If everything went okay, you've now got the following steps left to do:
+ 1) Copy widelands.exe and SDL.dll into the widelands directory
+ 2) Build the packages, e.g:
+      tar cjf build-XXX-source.tar.bz2 \`cat source.list\`
+      zip -r build-XXX-binary.zip \`cat binary.list\`
+ 3) Test the packages
+
+ 4) Upload the packages using SourceForge's file system
+ 5) Announce as news item and on widelands-announce
+
+Have fun! :)
+
+EOF
