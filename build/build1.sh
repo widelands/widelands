9c58af42e5e6baeb5c2ba3cb862d7a32d8d7af56
sirver <sirver@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2002-08-31 13:56:10 +0000

    changed export into co
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@228 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- build/build1.sh
+++ build/build1.sh
@@ -5,8 +5,7 @@ echo
 echo Retrieving the latest and greatest from CVS
 
 #cvs -z3 -d:pserver:anonymous@cvs.widelands.sourceforge.net:/cvsroot/widelands export -D now widelands
-cvs -z3 -d:pserver:anonymous@cvs.widelands.sourceforge.net:/cvsroot/widelands e
-xport -dPR widelands
+cvs -z3 -d:pserver:anonymous@cvs.widelands.sourceforge.net:/cvsroot/widelands co -dPR widelands
 
 echo
 echo Creating source and binary package file lists
