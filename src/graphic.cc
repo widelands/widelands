51b579f2e734b3ed429146be2b63eac9ba5806d6
sirver <sirver@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2002-09-01 16:17:11 +0000

    more autoconf changes
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@233 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/graphic.cc
+++ src/graphic.cc
@@ -44,7 +44,7 @@ Graphic *g_graphic = 0;
   it does NOT come from stl in my win32 (i wouldn't have added it, would i?)
   well i'll leave it here, as this part of code will hopefully disappear soon
   */
-#ifdef WIN32
+#if defined(WIN32) && !defined(__GNUC__)
 template <typename T> inline void swap(T& a, T& b)
 {
    T temp = a;
