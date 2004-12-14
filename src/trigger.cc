cdb4061216d42a2e34499adef74f6be73209eac7
sirver <sirver@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2004-12-14 22:28:48 +0000

    - fixed a minor bug occuring when only one trigger is set
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@746 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/trigger.cc
+++ src/trigger.cc
@@ -31,45 +31,45 @@ void Cmd_CheckTrigger::execute (Game* g)
 {
 	trigger_id++;
 	
-	log("Trigger: looking if check is needed for trigger %i\n", trigger_id);
+	// Nolog("Trigger: looking if check is needed for trigger %i\n", trigger_id);
 	
-	if(trigger_id>= g->get_map()->get_number_of_triggers()) {
-    		// either we wrapped around the end of all triggers
-		// if so, restart. if there are no triggers at all,
-		// requeue in about a 10 seconds to check if this state has changed
-		// (a new trigger could be registered) (this should only happen at the beginning
-		// of the game and should not harm at all, and 30seconds means nearly no CPU time
-		// for non trigger games)
-		
-    		if (g->get_map()->get_number_of_triggers())
-			trigger_id=0;
-		else
-			g->enqueue_command (
-			    new Cmd_CheckTrigger(g->get_gametime() + 30000, -1));
-		
-		return;
-	}
+   if(trigger_id >= g->get_map()->get_number_of_triggers()) {
+      // either we wrapped around the end of all triggers
+      // if so, restart. if there are no triggers at all,
+      // requeue in about a 10 seconds to check if this state has changed
+      // (a new trigger could be registered) (this should only happen at the beginning
+      // of the game and should not harm at all, and 30seconds means nearly no CPU time
+      // for non trigger games)
+
+      if (g->get_map()->get_number_of_triggers())
+         trigger_id=0;
+      else {
+         g->enqueue_command (
+               new Cmd_CheckTrigger(g->get_gametime() + 30000, -1));
+         return;
+      }
+   }
 	
 	Trigger* trig=g->get_map()->get_trigger(trigger_id);
 	assert(trig);
-	log("Trigger %s is going to get checked!\n", trig->get_name());
-	
+	// Nolog("Trigger %s is going to get checked!\n", trig->get_name());
+
 	bool trig_state=trig->is_set();
 	trig->check_set_conditions(g);
 	if(trig->is_set()!=trig_state) {
-		log("Trigger has changed state: %s gone to %i\n", trig->get_name(), trig->is_set());
+		// Nolog("Trigger has changed state: %s gone to %i\n", trig->get_name(), trig->is_set());
 
 		for (int i=0; i<g->get_map()->get_number_of_events(); i++) {
 			Event* ev=g->get_map()->get_event(i);
-			log("Checking event: %s\n", ev->get_name());
+			// Nolog("Checking event: %s\n", ev->get_name());
 			bool is_one_timer=false;
 
 			if (ev->check_triggers()) {
 				// This event is ready to run
 				is_one_timer=ev->is_one_time_event();
-				log("Run event!\n");
+				// Nolog("Run event!\n");
 				ev->run(g);
-				log("Run done!\n");
+				// Nolog("Run done!\n");
 				
 				// The event was a one timer and got therefore deleted. we have to make sure that i stays valid
 				if(is_one_timer)
@@ -80,7 +80,9 @@ void Cmd_CheckTrigger::execute (Game* g)
 	
 	// recheck next trigger in the time that all triggers get checked at least once ever 10 seconds
 	int delay=g->get_map()->get_number_of_triggers() ? 1000/g->get_map()->get_number_of_triggers() : 30000;
-	g->enqueue_command (new Cmd_CheckTrigger(g->get_gametime() + delay, trigger_id));
+   // Nolog("Queueing recheck in %i milli-seconds\n", delay);
+
+   g->enqueue_command (new Cmd_CheckTrigger(g->get_gametime() + delay, trigger_id));
 }
 
 #define CMD_CHECK_TRIGGER_VERSION 1
