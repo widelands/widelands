ef3e00454695dcc5651ef29a113869d4524b21c6
sirver <sirver@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2005-12-01 15:51:25 +0000

    - fixed chatwindow typing bug
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@1031 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/ui/ui_basic/ui_multilineeditbox.cc
+++ src/ui/ui_basic/ui_multilineeditbox.cc
@@ -41,7 +41,7 @@ UIMultiline_Editbox::UIMultiline_Editbox(UIPanel *parent, int x, int y, uint w, 
 
    m_needs_update=false;
    m_cur_pos=get_text().size();
-
+   
    set_handle_mouse(true);
    set_can_focus(true);
 	set_think(false);
@@ -174,9 +174,9 @@ void UIMultiline_Editbox::draw(RenderTarget* dst)
    dst->brighten_rect(0,0,get_w(),get_h(),ms_darken_value);
    if (get_text().size()) {
       g_fh->draw_string(dst, m_fontname, m_fontsize, m_fcolor, RGBColor(107,87,55), UIMultiline_Editbox::get_halign(), 0 - m_textpos, get_text().c_str(), m_align, get_eff_w(), m_cache_mode, &m_cache_id, (has_focus() ? (int)m_cur_pos : -1)); //explicit cast is neccessary to avoid a compiler warning
+      m_cache_mode = Widget_Cache_Use;
    }
    UIMultiline_Textarea::draw_scrollbar();
-   m_cache_mode = Widget_Cache_Use;
 }
 
 /*
