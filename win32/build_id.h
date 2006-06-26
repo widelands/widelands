ed7569c2c7f8fa917cff71a1a518477c553247bc
nasenbaer_peter <nasenbaer_peter@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2006-06-26 16:49:14 +0000

    Addition of "Bloodshed's Dev-Cpp-Project-file" This was a big wish by many win32-users. AND it works, as long as the librarys are installed correctly.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@1260 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ win32/build_id.h
@@ -0,0 +1,8 @@
+
+#ifndef BUILD_ID
+#define BUILD_ID cvs-build
+
+const char *g_build_id="cvs-build";
+	
+#endif
+
---
+++ win32/buildcat.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ..
+cd locale && python.exe ../utils/buildcat.py
+cd ..
