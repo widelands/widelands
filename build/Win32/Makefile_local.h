2de9b45189cd9950218928d72a9f8a6af1ff5940
nasenbaer_peter <nasenbaer_peter@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2006-10-31 18:02:13 +0000

    Win32 build-system
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@1736 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ build/Win32/Makefile_local.h
@@ -0,0 +1,17 @@
+# # # # # # # # # # # # # # # # # # # # # # # # #
+#                                               #
+#   !!! !!! THIS IS NO REAL *.h - FILE !!! !!!  #
+#                                               #
+#          PLEASE DEFINE BUILDTYPE HERE         #
+#                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+#### Buildtype ####
+BUILD=release
+#Build=debug-no-parachute
+#Build=debug-slow
+#Build=debug
+
+#### Locales (define to "yes" if locales may be build) ####
+Buildcat:=no
