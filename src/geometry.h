b147b414276d44f91774b9a6dfb365355a5cfacf
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-01-11 13:08:05 +0000

    Add missing operators.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@1909 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/geometry.h
+++ src/geometry.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002-2004, 2006 by the Widelands Development Team
+ * Copyright (C) 2002-2004, 2006-2007 by the Widelands Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -38,6 +38,16 @@ struct Point {
 		{return Point(x + other.x, y + other.y);}
 	Point operator-(const Point other) const
 		{return Point(x - other.x, y - other.y);}
+	Point & operator+=(const Point other) {
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+	Point & operator-=(const Point other) {
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
 
 	int x, y;
 };
