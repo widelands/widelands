ad38298502c6ab54402a2a06c72edf41f4a2547c
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-01-25 15:04:40 +0000

    Add Player_Area.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@1961 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ src/player_area.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2007 by the Widelands Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef __S__PLAYER_AREA_H
+#define __S__PLAYER_AREA_H
+
+#include "geometry.h"
+#include "types.h"
+
+struct Player_Area : public Area {
+	Player_Area() {}
+	Player_Area(const Player_Number pn, const Area area)
+		: Area(area), player_number(pn)
+	{}
+	Player_Number player_number;
+};
+
+#endif // __S__PLAYER_AREA_H
