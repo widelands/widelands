94a999e2d60a599aeb150607f1b2ef3f2c76bcb7
bedouin <bedouin@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-02-18 22:03:47 +0000

    - remove duplication in detect_revision.py
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2045 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- build/scons-tools/detect_revision.py
+++ build/scons-tools/detect_revision.py
@@ -29,34 +29,3 @@ def detect_revision():
 if __name__ == "__main__":
 	print detect_revision()
 
-#!/usr/bin/python -tt
-
-# Tries to find out the repository revision of the current working directory
-# using svn or svk.
-#
-# For svk: must be in checkout's root path, will not work otherwise
-#          (but could be made to)
-
-import os
-
-def detect_revision():
-	revnum='UNKNOWN'
-
-	if os.path.exists('.svn'):
-		has_svn = os.system('svn >/dev/null 2>&1')==0
-		if has_svn:
-			revnum=os.popen('svn info|grep Revision:|cut -d" " -f 2').read()
-
-	has_svk = os.system('svk >/dev/null 2>&1')==0
-	if has_svk:
-		cwd=os.getcwd()
-		is_svk_workdir = os.system('svk co -l|grep '+cwd+' >/dev/null 2>&1')==0
-
-		if is_svk_workdir:
-			revnum=os.popen('svk info|grep Mirrored|cut -d" " -f 5').read()
-
-	return revnum.rstrip()
-
-if __name__ == "__main__":
-	print detect_revision()
-
