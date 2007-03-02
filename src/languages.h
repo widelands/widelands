bf09ed101da0067be75d41398dc481c1d7ad5da6
nasenbaer_peter <nasenbaer_peter@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-03-02 13:06:52 +0000

    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2084 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/languages.h
+++ src/languages.h
@@ -41,7 +41,7 @@ struct Languages {
  * NONE as first entry. Also increase the NR_LANGUAGES variable by one
  */
 #ifdef DEFINE_LANGUAGES  // defined in fullscreen_menu_options.cc
-#define NR_LANGUAGES 14
+#define NR_LANGUAGES 15
 static Languages available_languages[NR_LANGUAGES] = {
    { "Default system language", "" },
 // EXTRACT BEGIN (leaves this line untouched)
@@ -50,6 +50,7 @@ static Languages available_languages[NR_LANGUAGES] = {
    { "English", "en_EN" },
    { "Español", "es_ES" },
    { "Français", "fr_FR" },
+   { "Galego", "gl_ES" },
    { "עברית", "he_HE" },
    { "Magyar", "hu_HU" },
    { "Nederlands", "nl_NL" },
