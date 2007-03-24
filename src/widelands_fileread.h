71a53cbdab0977c9480c51e1f769dbcb2f3e1465
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-03-24 02:16:10 +0000

    Add code for reading/writing a Coords that must be valid or null.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2151 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/widelands_fileread.h
+++ src/widelands_fileread.h
@@ -29,12 +29,18 @@ struct WidelandsFileRead : public FileRead {
 	struct Data_Error                                          {};
 	struct Extent_Exceeded            : public Data_Error      {};
 	struct Width_Exceeded : public Extent_Exceeded {
-		Width_Exceeded(const Uint16 W, const X_Coordinate X) : w(W), x(X) {}
+		Width_Exceeded(const Pos pos, const Uint16 W, const X_Coordinate X) :
+			position(pos), w(W), x(X)
+		{}
+		Pos          position;
 		Uint16       w;
 		X_Coordinate x;
 	};
 	struct Height_Exceeded : public Extent_Exceeded {
-		Height_Exceeded(const Uint16 H, const Y_Coordinate Y) : h(H), y(Y) {}
+		Height_Exceeded(const Pos pos, const Uint16 H, const Y_Coordinate Y) :
+			position(pos), h(H), y(Y)
+		{}
+		Pos          position;
 		Uint16       h;
 		Y_Coordinate y;
 	};
@@ -48,13 +54,35 @@ struct WidelandsFileRead : public FileRead {
 	 */
 	Coords Coords32(const Extent extent);
 
+	/**
+	 * Read Coords from the file. Use this when the result can only be a node
+	 * coordinate or the special value indicating invalidity, as defined by
+	 * Coords::isNull. Unless the read Coords is null, this will throw an
+	 * exception if the width is <= the x coordinate or the height is <= the y
+	 * coordinate. Both coordinates are read from the file before checking and
+	 * possibly throwing, so in case such an exception is thrown, it is
+	 * guaranteed that the whole coordinate pair has been read.
+	 */
+	Coords Coords32_allow_null(const Extent extent);
+
 };
 
 inline Coords WidelandsFileRead::Coords32(const Extent extent) {
 	const Uint16 x = Little16(Deref16(Data(2)));
 	const Uint16 y = Little16(Deref16(Data(2)));
-	if (extent.w <= x) throw Width_Exceeded (extent.w, x);
-	if (extent.h <= y) throw Height_Exceeded(extent.h, y);
+	if (extent.w <= x) throw Width_Exceeded (GetPos() - 4, extent.w, x);
+	if (extent.h <= y) throw Height_Exceeded(GetPrevPos(), extent.h, y);
+	return Coords(x, y);
+}
+
+inline Coords WidelandsFileRead::Coords32_allow_null(const Extent extent) {
+	const Uint16 x = Little16(Deref16(Data(2)));
+	const Uint16 y = Little16(Deref16(Data(2)));
+	const Coords result(x, y);
+	if (not result.isNull()) {
+		if (extent.w <= x) throw Width_Exceeded (GetPos() - 4, extent.w, x);
+		if (extent.h <= y) throw Height_Exceeded(GetPrevPos(), extent.h, y);
+	}
 	return Coords(x, y);
 }
 
