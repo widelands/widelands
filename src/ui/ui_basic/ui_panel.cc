89f258e61663e6697ece61b5b94de9884dbec278
ixprefect <ixprefect@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-08-24 17:12:54 +0000

    Change the comment for Panel::run() in order to clarify that return values may be negative in unusual situations.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2440 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/ui/ui_basic/ui_panel.cc
+++ src/ui/ui_basic/ui_panel.cc
@@ -110,9 +110,11 @@ void Panel::free_children() {while (_fchild) delete _fchild;}
 
 
 /**
-Enters the event loop; all events will be handled by this panel.
-Returns the return code passed to end_modal
-*/
+ * Enters the event loop; all events will be handled by this panel.
+ * \return the return code passed to end_modal. This return code
+ * will be negative when the event loop was quit in an abnormal way
+ * (e.g. the user clicked the window's close button or similar).
+ */
 int Panel::run()
 {
 	WLApplication *app=WLApplication::get();
