fada78311f04921a541657307476bbbdd76c1858
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-08-28 20:42:49 +0000

    Add for and while to the regexp in the previous commit. Combine some other regexps to one.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2520 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- utils/detect_spurious_source_code.sh
+++ utils/detect_spurious_source_code.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 for i in $(find src -name *.h -o -name *.cc); do
-	for regexp in " $" "	$" "[^	]+	" " ," ",[^ \"'<]" "[^\"],\"" "[^	 @]\{" "\{ +[^ /\\]" " \}" "\}[^ ,;}\]" "\( " " \)"  "if\("  "switch\(" "for\(" "while\(" "catch\(" "[^:alpha:_\"]TRUE[^:alpha:_\"]" "[^:alpha:_\"]FALSE[^:alpha:_\"]" "\(void\)" "if \((/[^/*]|[^/])*\) (/[^/*]|[^/])*[^){};\\]$"; do
+	for regexp in " $" "	$" "[^	]+	" " ," ",[^ \"'<]" "[^\"],\"" "[^	 @]\{" "\{ +[^ /\\]" " \}" "\}[^ ,;}\]" "\( " " \)"  "(catch|for|if|switch|while)\(" "[^:alpha:_\"]TRUE[^:alpha:_\"]" "[^:alpha:_\"]FALSE[^:alpha:_\"]" "\(void\)" "(for|if|while) \((/[^/*]|[^/])*\) (/[^/*]|[^/])*[^){};\\]$"; do
 		egrep --color --with-filename --line-number "$regexp" $i;
 	done
 done
