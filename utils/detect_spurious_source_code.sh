0fc37a9ebe70d26ae537a93aec89bcf2c99b9920
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-08-28 20:22:50 +0000

    Add regexp "if \((/[^/*]|[^/])*\) (/[^/*]|[^/])*[^){};\]$" for detecting an "if" statement controlling a single statement, "if (condition) do_something;", that is wrapped, but not between "if (condition)" and "do_something;" (Nicolai does not like those, see revision 2517.)
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2519 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- utils/detect_spurious_source_code.sh
+++ utils/detect_spurious_source_code.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 for i in $(find src -name *.h -o -name *.cc); do
-	for regexp in " $" "	$" "[^	]+	" " ," ",[^ \"'<]" "[^\"],\"" "[^	 @]\{" "\{ +[^ /\\]" " \}" "\}[^ ,;}\]" "\( " " \)"  "if\("  "switch\(" "for\(" "while\(" "catch\(" "[^:alpha:_\"]TRUE[^:alpha:_\"]" "[^:alpha:_\"]FALSE[^:alpha:_\"]" "\(void\)"; do
+	for regexp in " $" "	$" "[^	]+	" " ," ",[^ \"'<]" "[^\"],\"" "[^	 @]\{" "\{ +[^ /\\]" " \}" "\}[^ ,;}\]" "\( " " \)"  "if\("  "switch\(" "for\(" "while\(" "catch\(" "[^:alpha:_\"]TRUE[^:alpha:_\"]" "[^:alpha:_\"]FALSE[^:alpha:_\"]" "\(void\)" "if \((/[^/*]|[^/])*\) (/[^/*]|[^/])*[^){};\\]$"; do
 		egrep --color --with-filename --line-number "$regexp" $i;
 	done
 done
