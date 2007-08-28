148611e46bdabdedf51f7419df0e20b9cd6eb62d
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-08-28 10:34:19 +0000

    Fix Coords IO in Widelands_Map_Attack_Controller_Data_Packet.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2493 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/widelands_map_attack_controller_data_packet.cc
+++ src/widelands_map_attack_controller_data_packet.cc
@@ -19,8 +19,6 @@
 
 #include "attack_controller.h"
 #include "battle.h"
-#include "fileread.h"
-#include "filewrite.h"
 #include "editor_game_base.h"
 #include "error.h"
 #include "geometry.h"
@@ -29,6 +27,8 @@
 #include "map.h"
 #include "soldier.h"
 #include "tribe.h"
+#include "widelands_fileread.h"
+#include "widelands_filewrite.h"
 #include "widelands_map_data_packet_ids.h"
 #include "widelands_map_attack_controller_data_packet.h"
 #include "widelands_map_map_object_loader.h"
@@ -42,7 +42,7 @@
 /* VERSION 1: initial release
 */
 
-#define CURRENT_PACKET_VERSION 1
+#define CURRENT_PACKET_VERSION 2
 
 Widelands_Map_Attack_Controller_Data_Packet::~Widelands_Map_Attack_Controller_Data_Packet() {
 }
@@ -57,17 +57,16 @@ throw (_wexception)
 	if (skip)
 		return;
 
-	FileRead fr;
+	WidelandsFileRead fr;
 	try {
 		fr.Open(fs, "binary/attackcontroller");
 	} catch (...) {
 		// not there, so skip
 		return ;
 	}
-	// First packet version
-	int packet_version=fr.Unsigned16();
-
-	if (packet_version == CURRENT_PACKET_VERSION) {
+	const Uint16 packet_version = fr.Unsigned16();
+	if (1 <= packet_version and packet_version <= CURRENT_PACKET_VERSION) {
+		const Extent extent = egbase->map().extent();
 		uint nrControllers = fr.Unsigned32();
 		for (uint i=0;i<nrControllers;i++) {
 			AttackController* ctrl = egbase->create_attack_controller();
@@ -93,8 +92,38 @@ throw (_wexception)
 				MilitarySite* origin = (MilitarySite*) ol->get_object_by_file_index(fr.Unsigned32());
 				assert(origin);
 
-				Coordinate x = (Coordinate)fr.Unsigned32();
-				Coordinate y = (Coordinate)fr.Unsigned32();
+				Coords battleGround;
+				if (packet_version == 1) {
+					const Uint32 x = fr.Unsigned32();
+					if (extent.w <= x) throw wexception
+						("Widelands_Map_Attack_Controller_Data_Packet::Read: in "
+						 "binary/attackcontroller:%u: battleGround has x "
+						 "coordinate %i, but the map width is only %u",
+						 fr.GetPrevPos(), x, extent.w);
+					const Uint32 y = fr.Unsigned32();
+					if (extent.h <= y) throw wexception
+						("Widelands_Map_Attack_Controller_Data_Packet::Read: in "
+						 "binary/attackcontroller:%u: battleGround has y "
+						 "coordinate %i, but the map height is only %u",
+						 fr.GetPrevPos(), y, extent.h);
+					battleGround = Coords(x, y);
+				} else {
+					try {battleGround = fr.Coords32(extent);}
+					catch (const WidelandsFileRead::Width_Exceeded e) {
+						throw wexception
+							("Widelands_Map_Attack_Controller_Data_Packet::Read: in "
+							 "binary/attackcontroller:%u: battleGround has x "
+							 "coordinate %i, but the map width is only %u",
+							 e.position, e.x, e.w);
+					}
+					catch (const WidelandsFileRead::Height_Exceeded e) {
+						throw wexception
+							("Widelands_Map_Attack_Controller_Data_Packet::Read: in "
+							 "binary/attackcontroller:%u: battleGround has x "
+							 "coordinate %i, but the map width is only %u",
+							 e.position, e.y, e.h);
+					}
+				}
 
 				bool attacker = fr.Unsigned8();
 				bool arrived = fr.Unsigned8();
@@ -102,7 +131,7 @@ throw (_wexception)
 				AttackController::BattleSoldier bs = {
 					soldier,
 					origin,
-					Coords(x, y),
+					battleGround,
 					attacker,
 					arrived,
 					fighting
@@ -137,7 +166,7 @@ void Widelands_Map_Attack_Controller_Data_Packet::Write
  Editor_Game_Base* egbase,
  Widelands_Map_Map_Object_Saver * const os)
 throw (_wexception) {
-	FileWrite fw;
+	WidelandsFileWrite fw;
 
 	// now packet version
 	fw.Unsigned16(CURRENT_PACKET_VERSION);
@@ -174,8 +203,7 @@ throw (_wexception) {
 			assert(os->is_object_known(bs.origin));
 			fw.Unsigned32(os->get_object_file_index(bs.origin));
 
-			fw.Unsigned32(bs.battleGround.x);
-			fw.Unsigned32(bs.battleGround.y);
+			fw.Coords32  (bs.battleGround);
 
 			fw.Unsigned8(bs.attacker);
 			fw.Unsigned8(bs.arrived);
