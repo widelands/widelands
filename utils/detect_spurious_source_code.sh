a9d408cab2b3003f4a3038b4673a00fca2ac7cbc
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-08-28 21:55:55 +0000

    Add regexps to detect const declarations inside casts, which "just makes the code even more annoying to read".
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2522 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- utils/detect_spurious_source_code.sh
+++ utils/detect_spurious_source_code.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 for i in $(find src -name *.h -o -name *.cc); do
-	for regexp in " $" "	$" "[^	]+	" " ," ",[^ \"'<]" "[^\"],\"" "[^	 @]\{" "\{ +[^ /\\]" " \}" "\}[^ ,;}\]" "\( " " \)"  "(catch|for|if|switch|while)\(" "[^:alpha:_\"]TRUE[^:alpha:_\"]" "[^:alpha:_\"]FALSE[^:alpha:_\"]" "\(void\)" "(for|if|while) \((/[^/*]|[^/])*\) (/[^/*]|[^/])*[^){};\\]$"; do
+	for regexp in " $" "	$" "[^	]+	" " ," ",[^ \"'<]" "[^\"],\"" "[^	 @]\{" "\{ +[^ /\\]" " \}" "\}[^ ,;}\]" "\( " " \)"  "(catch|for|if|switch|while)\(" "[^:alpha:_\"]TRUE[^:alpha:_\"]" "[^:alpha:_\"]FALSE[^:alpha:_\"]" "\(void\)" "(for|if|while) \((/[^/*]|[^/])*\) (/[^/*]|[^/])*[^){};\\]$" "_cast<const( +[_a-zA-Z][_a-zA-Z0-9]*)* *>" "_cast<( *[_a-zA-Z][_a-zA-Z0-9]*)* *\* *const *>"; do
 		egrep --color --with-filename --line-number "$regexp" $i;
 	done
 done
