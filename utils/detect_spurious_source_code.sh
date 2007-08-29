1faf11c6573e09f58de9cb49cd74a7a9387f37a2
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-08-29 05:36:14 +0000

    Use "egrep -f" and put the regexps in a separate file, each on its own line.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2542 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- utils/detect_spurious_source_code.sh
+++ utils/detect_spurious_source_code.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 for i in $(find src -name *.h -o -name *.cc); do
-	for regexp in " $" "	$" "[^	]+	" " ," ",[^ \"'<]" "[^\"],\"" "[^	 @]\{" "\{ +[^ /\\]" " \}" "\}[^ ,;}\]" "\( " " \)"  "(catch|for|if|switch|while)\(" "[^:alpha:_\"]TRUE[^:alpha:_\"]" "[^:alpha:_\"]FALSE[^:alpha:_\"]" "\(void\)" "(for|if|while) \((/[^/*]|[^/])*\) (/[^/*]|[^/])*[^){};\\]$" "_cast<const( +[_a-zA-Z][_a-zA-Z0-9]*)* *>" "_cast<( *[_a-zA-Z][_a-zA-Z0-9]*)* *\* *const *>"; do
-		egrep --color --with-filename --line-number "$regexp" $i;
-	done
+	egrep --color --with-filename --line-number -f utils/spurious_source_code_regexps $i;
 done
