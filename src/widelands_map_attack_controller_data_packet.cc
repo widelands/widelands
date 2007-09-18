7de554ece909049fbeb1e5c85c40248f9787a542
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2007-09-18 08:45:13 +0000

    Fix format string. Removu unreachable assert(0). Use else instead of early return.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2737 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/widelands_map_attack_controller_data_packet.cc
+++ src/widelands_map_attack_controller_data_packet.cc
@@ -151,11 +151,10 @@ throw (_wexception)
 		}
 		if (fr.Unsigned32() != 0xffffffff)
 			throw wexception ("Error in Widelands_Attack_Controller_Data_Packet : Couldn't find 0xffffffff.");
-		return; // End of packet, do not run into assert
-	}
-	else
-		throw wexception ("Unkown version of Widelands_Map_Battle_Data_Packet : %d", packet_version);
-	assert(0); // never here
+	} else
+		throw wexception
+			("Unkown version of Widelands_Map_Battle_Data_Packet : %u",
+			 packet_version);
 }
 /*
  * Write Function.
