widelands/widelands

commit acb0167a8c870a7bc10bce69a8b81bdca7f58549

Add notification system.
git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2945 37b2a8de-5219-0410-9f54-a31bc463ab9c

src/player.cc
@@ -741,11 +741,9 @@ const std::vector<uint32_t> * Player::get_ware_production_statistics
 }
 
 
-/**
- * Gain an immovable
- */
-void Player::gain_immovable(PlayerImmovable* imm) {
-	if (upcast(Building const, building, imm)) {
+void Player::receive(const NoteImmovable& note)
+{
+	if (upcast(Building const, building, note.pi)) {
 		upcast(ConstructionSite const, constructionsite, building);
 		const std::string & building_name =
 			constructionsite ?
@@ -760,46 +758,34 @@ void Player::gain_immovable(PlayerImmovable* imm) {
 		std::vector<Building_Stats> & stat =
 			m_building_stats[tribe().get_building_index(building_name.c_str())];
 
-		Building_Stats new_building;
-		new_building.is_constructionsite = constructionsite;
-		new_building.pos = building->get_position();
-		stat.push_back(new_building);
+		if (note.lg == GAIN) {
+			Building_Stats new_building;
+			new_building.is_constructionsite = constructionsite;
+			new_building.pos = building->get_position();
+			stat.push_back(new_building);
+		} else {
+			const Coords building_position = building->get_position();
+			for (uint32_t i = 0; i < stat.size(); ++i) {
+				if (stat[i].pos == building_position) {
+					stat.erase(stat.begin() + i);
+					return;
+				}
+			}
+
+			throw wexception
+				("Interactive_Player::loose_immovable(): A building should be "
+				 "removed at (%i, %i), but nothing is known about this building!",
+				 building_position.x, building_position.y);
+		}
 	}
+
+	NoteSender<NoteImmovable>::send(note);
 }
 
-/**
- * Loose an immovable
- */
-void Player::lose_immovable(PlayerImmovable* imm)
+
+void Player::receive(const NoteField& note)
 {
-	if (upcast(Building const, building, imm)) {
-		upcast(ConstructionSite const, constructionsite, building);
-		const std::string & building_name =
-			constructionsite ?
-			constructionsite->building().name() : building->name();
-
-		const Building_Descr::Index nr_buildings = tribe().get_nrbuildings();
-
-		// Get the valid vector for this
-		if (m_building_stats.size() < nr_buildings)
-			m_building_stats.resize(nr_buildings);
-
-		std::vector<Building_Stats> & stat =
-			m_building_stats[tribe().get_building_index(building_name.c_str())];
-
-		const Coords building_position = building->get_position();
-		for (uint32_t i = 0; i < stat.size(); ++i) {
-			if (stat[i].pos == building_position) {
-				stat.erase(stat.begin() + i);
-				return;
-			}
-		}
-
-		throw wexception
-			("Interactive_Player::loose_immovable(): A building should be "
-			 "removed at (%i, %i), but nothing is known about this building!",
-			 building_position.x, building_position.y);
-	}
+	NoteSender<NoteField>::send(note);
 }
 
 

src/player.h
@@ -24,6 +24,7 @@
 #include "building.h"
 #include "editor_game_base.h"
 #include "mapregion.h"
+#include "notification.h"
 #include "rgbcolor.h"
 
 #include "widelands.h"
@@ -52,7 +53,10 @@ class AttackController;
  * in the long run.
  *                      -- Nicolai
  */
-struct Player {
+struct Player
+	: NoteReceiver<NoteImmovable>, NoteReceiver<NoteField>,
+	  public NoteSender<NoteImmovable>, public NoteSender<NoteField>
+{
 	struct Building_Stats {
 		bool is_constructionsite;
 		Coords pos;
@@ -440,8 +444,9 @@ struct Player {
 	void sample_statistics();
 	void ware_produced(Ware_Index);
 	void next_ware_production_period();
-	void gain_immovable(PlayerImmovable*);
-	void lose_immovable(PlayerImmovable*);
+
+	void receive(const NoteImmovable& note);
+	void receive(const NoteField& note);
 
 private:
 	/**

src/computer_player.cc
@@ -69,6 +69,8 @@ m_game(g), player_number(pid), tribe(0)
 void Computer_Player::late_initialization ()
 {
 	player = game().get_player(player_number);
+	NoteReceiver<NoteImmovable>::connect(*player);
+	NoteReceiver<NoteField>::connect(*player);
 	tribe = &player->tribe();
 
 	log ("ComputerPlayer(%d): initializing\n", player_number);
@@ -1104,6 +1106,21 @@ bool Computer_Player::improve_roads (Flag* flag)
 	return false;
 }
 
+
+void Computer_Player::receive(const NoteImmovable& note)
+{
+	if (note.lg == LOSE)
+		lose_immovable(note.pi);
+	else
+		gain_immovable(note.pi);
+}
+
+void Computer_Player::receive(const NoteField& note)
+{
+	if (note.lg == GAIN)
+		unusable_fields.push_back(note.fc);
+}
+
 // this is called whenever we gain ownership of a PlayerImmovable
 void Computer_Player::gain_immovable (PlayerImmovable* pi)
 {
@@ -1147,15 +1164,6 @@ void Computer_Player::lose_immovable (PlayerImmovable* pi)
 		break;
 	}
 }
-
-// this is called whenever we gain ownership of a field on the map
-void Computer_Player::gain_field (const FCoords& fc)
-{
-	unusable_fields.push_back (fc);
-}
-
-// we don't use this - instead we check or fields regularly, see think()
-void Computer_Player::lose_field (const FCoords &) {}
 
 
 /* CheckStepRoadAI */

src/computer_player.h
@@ -21,6 +21,7 @@
 #define COMPUTER_PLAYER_H
 
 #include "game.h"
+#include "notification.h"
 
 #include <list>
 
@@ -35,22 +36,21 @@ class Road;
 class ProductionSite;
 };
 
-struct Computer_Player {
+struct Computer_Player : Widelands::NoteReceiver<Widelands::NoteImmovable>, Widelands::NoteReceiver<Widelands::NoteField> {
 	Computer_Player(Widelands::Game &, const Widelands::Player_Number);
 	~Computer_Player ();
 
 	void think ();
 
-	void gain_immovable (Widelands::PlayerImmovable *);
-	void lose_immovable (Widelands::PlayerImmovable *);
-
-	void gain_field (Widelands::FCoords const &);
-	void lose_field (Widelands::FCoords const &);
+	void receive(const Widelands::NoteImmovable& note);
+	void receive(const Widelands::NoteField& note);
 
 	Widelands::Game & game() const throw () {return m_game;}
 	Widelands::Player_Number get_player_number() {return player_number;}
 
 private:
+	void gain_immovable (Widelands::PlayerImmovable* pi);
+	void lose_immovable (Widelands::PlayerImmovable* pi);
 	void gain_building (Widelands::Building *);
 	void lose_building (Widelands::Building *);
 

src/editor_game_base.cc
@@ -144,15 +144,16 @@ void Editor_Game_Base::think()
 	// by a given number of milliseconds
 }
 
-void Editor_Game_Base::player_immovable_notification (PlayerImmovable*, losegain_t)
+void Editor_Game_Base::receive(const NoteImmovable& note)
 {
-	//TODO: Get rid of this; replace by general notification system
+	note.pi->get_owner()->receive(note);
 }
 
-void Editor_Game_Base::player_field_notification (const FCoords&, losegain_t)
+void Editor_Game_Base::receive(const NoteField& note)
 {
-	//TODO: Get rid of this; replace by general notification system
+	get_player(note.fc.field->get_owned_by())->receive(note);
 }
+
 
 /*
 ===============
@@ -280,10 +281,10 @@ void Editor_Game_Base::do_conquer_area
 				 or
 				 player(owner).military_influence(index) < new_influence_modified)
 			{
-				if (owner) player_field_notification(mr.location(), LOSE);
+				if (owner) receive(NoteField(mr.location(), LOSE));
 				mr.location().field->set_owned_by(player_area.player_number);
 				inform_players_about_ownership(index, player_area.player_number);
-				player_field_notification (mr.location(), GAIN);
+				receive (NoteField(mr.location(), GAIN));
 			}
 		} else if
 			(not (conquering_player.military_influence(index) -= influence)
@@ -319,10 +320,10 @@ void Editor_Game_Base::do_conquer_area
 				}
 			}
 			if (best_player != player_area.player_number) {
-				player_field_notification (mr.location(), LOSE);
+				receive (NoteField(mr.location(), LOSE));
 				mr.location().field->set_owned_by (best_player);
 				inform_players_about_ownership(index, best_player);
-				if (best_player) player_field_notification (mr.location(), GAIN);
+				if (best_player) receive (NoteField(mr.location(), GAIN));
 			}
 		}
 	} while (mr.advance(map()));

src/editor_game_base.h
@@ -24,6 +24,7 @@
 #include "building.h"
 #include "constants.h"
 #include "map.h"
+#include "notification.h"
 #include "player_area.h"
 
 #include <cassert>
@@ -51,7 +52,7 @@ class Tribe_Descr;
 class Flag;
 class AttackController;
 
-struct Editor_Game_Base {
+struct Editor_Game_Base : NoteReceiver<NoteImmovable>, NoteReceiver<NoteField> {
 	friend struct ::Fullscreen_Menu_LaunchGame;
 	friend struct ::Interactive_Base;
 	friend class Game_Game_Class_Data_Packet;
@@ -138,9 +139,8 @@ struct Editor_Game_Base {
 	void inform_players_about_immovable(Map_Index, Map_Object_Descr const *);
 	void inform_players_about_road     (FCoords,   Map_Object_Descr const *);
 
-	enum losegain_t {LOSE=0, GAIN};
-	virtual void player_immovable_notification (PlayerImmovable*, losegain_t);
-	virtual void player_field_notification (const FCoords&, losegain_t);
+	void receive(const NoteImmovable& note);
+	void receive(const NoteField& note);
 
 	void cleanup_objects() throw () {
 		objects().cleanup(this);

src/game.cc
@@ -550,31 +550,6 @@ void Game::think()
 }
 
 
-void Game::player_immovable_notification (PlayerImmovable* pi, losegain_t lg)
-{
-	for (uint32_t i = 0; i < cpl.size(); ++i)
-		if (cpl[i]->get_player_number()==pi->get_owner()->get_player_number())
-			if (lg==GAIN)
-				cpl[i]->gain_immovable (pi);
-			else
-				cpl[i]->lose_immovable (pi);
-
-	if (lg==GAIN)
-		pi->get_owner()->gain_immovable (pi);
-	else
-		pi->get_owner()->lose_immovable (pi);
-}
-
-void Game::player_field_notification (const FCoords& fc, losegain_t lg)
-{
-	for (uint32_t i = 0; i < cpl.size(); ++i)
-		if (cpl[i]->get_player_number()==fc.field->get_owned_by())
-			if (lg==GAIN)
-				cpl[i]->gain_field (fc);
-			else
-				cpl[i]->lose_field (fc);
-}
-
 /**
  * Cleanup for load
  * \deprecated

src/game.h
@@ -139,9 +139,6 @@ struct Game : public Editor_Game_Base {
 
 	bool get_allow_cheats();
 
-	virtual void player_immovable_notification (PlayerImmovable*, losegain_t);
-	virtual void player_field_notification (const FCoords&, losegain_t);
-
 	void enqueue_command (Command * const);
 
 	void send_player_command (Widelands::PlayerCommand *);

commit 43674b4b355922f6bae6c2fd31ffa8d8364e708b

Fix bug 1906380 (regression from r2945)
Notifications about removed buildings are sent on.

This bug was a typical case of refactoring code and not seeing a non-local
control statement (in this case, a return statement from within a for-loop)
while trying to cram everything into a single function instead of separating
the code into different functions.


git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@2957 37b2a8de-5219-0410-9f54-a31bc463ab9c

src/player.cc
@@ -739,43 +739,52 @@ const std::vector<uint32_t> * Player::get_ware_production_statistics
 }
 
 
-void Player::receive(const NoteImmovable& note)
+/**
+ * Add or remove the given building from building statistics.
+ * Only to be called by \ref receive
+ */
+void Player::update_building_statistics(Building* building, losegain_t lg)
 {
-	if (upcast(Building const, building, note.pi)) {
-		upcast(ConstructionSite const, constructionsite, building);
-		const std::string & building_name =
-			constructionsite ?
-			constructionsite->building().name() : building->name();
+	upcast(ConstructionSite const, constructionsite, building);
+	const std::string & building_name =
+		constructionsite ?
+		constructionsite->building().name() : building->name();
 
-		const Building_Descr::Index nr_buildings = tribe().get_nrbuildings();
+	const Building_Descr::Index nr_buildings = tribe().get_nrbuildings();
 
 		// Get the valid vector for this
-		if (m_building_stats.size() < nr_buildings)
-			m_building_stats.resize(nr_buildings);
+	if (m_building_stats.size() < nr_buildings)
+		m_building_stats.resize(nr_buildings);
 
-		std::vector<Building_Stats> & stat =
-			m_building_stats[tribe().get_building_index(building_name.c_str())];
+	std::vector<Building_Stats> & stat =
+		m_building_stats[tribe().get_building_index(building_name.c_str())];
 
-		if (note.lg == GAIN) {
-			Building_Stats new_building;
-			new_building.is_constructionsite = constructionsite;
-			new_building.pos = building->get_position();
-			stat.push_back(new_building);
-		} else {
-			const Coords building_position = building->get_position();
-			for (uint32_t i = 0; i < stat.size(); ++i) {
-				if (stat[i].pos == building_position) {
-					stat.erase(stat.begin() + i);
-					return;
-				}
+	if (lg == GAIN) {
+		Building_Stats new_building;
+		new_building.is_constructionsite = constructionsite;
+		new_building.pos = building->get_position();
+		stat.push_back(new_building);
+	} else {
+		const Coords building_position = building->get_position();
+		for (uint32_t i = 0; i < stat.size(); ++i) {
+			if (stat[i].pos == building_position) {
+				stat.erase(stat.begin() + i);
+				return;
 			}
-
-			throw wexception
-				("Interactive_Player::loose_immovable(): A building should be "
-				 "removed at (%i, %i), but nothing is known about this building!",
-				 building_position.x, building_position.y);
 		}
+
+		throw wexception
+			("Interactive_Player::loose_immovable(): A building should be "
+			 "removed at (%i, %i), but nothing is known about this building!",
+			 building_position.x, building_position.y);
 	}
+}
+
+
+void Player::receive(const NoteImmovable& note)
+{
+	if (upcast(Building, building, note.pi))
+		update_building_statistics(building, note.lg);
 
 	NoteSender<NoteImmovable>::send(note);
 }

src/player.h
@@ -442,6 +442,8 @@ struct Player :
 	void receive(const NoteField& note);
 
 private:
+	void update_building_statistics(Building* building, losegain_t lg);
+
 	/**
 	 * Called when a node becomes seen (not only the first time) before the
 	 * vision counter is incremented. Discovers the node and those of the 6