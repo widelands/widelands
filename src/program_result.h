6cd7d3702bf96e08eb1fac9c2b214d100bb2f7bc
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2008-10-09 16:12:16 +0000

    Add missing file.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@3375 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ src/program_result.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2002-2004, 2006-2008 by the Widelands Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef PROGRAM_RESULT_H
+#define PROGRAM_RESULT_H
+
+namespace Widelands {
+enum Program_Result {None = 0, Failed = 1, Completed = 2, Skipped = 3};
+enum Program_Failure_Handling_Method {Fail, Repeat, Ignore};
+}
+
+#endif
