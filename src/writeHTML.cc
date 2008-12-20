c905b407615b7916f5d29598b755435c3ea52fa6
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2008-12-20 15:32:48 +0000

    Set the encoding of generated HTML-files to the same as all po-files. This fixes bug #2454252 without converting any strings when writing them.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@3631 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/writeHTML.cc
+++ src/writeHTML.cc
@@ -191,7 +191,7 @@ void Tribe_Descr::referenceWare
 }
 
 #define HTML_FILE_BEGIN                                                       \
-   "<?xml version=\"1.0\" encoding=\"us-ascii\"?>\n"                          \
+   "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"                             \
    "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "              \
    "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"                 \
    "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"                          \
