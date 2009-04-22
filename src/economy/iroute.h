0bcefa251314dd728e2ff32095634f7d33b2d2d0
sf_azagtoth <sf_azagtoth@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2009-04-22 19:31:15 +0000

    Whitespace fixes
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@3885 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/economy/iroute.h
+++ src/economy/iroute.h
@@ -20,29 +20,27 @@
 #ifndef S__IROUTE_H
 #define S__IROUTE_H
 
-namespace Widelands { 
+namespace Widelands {
 class RoutingNode;
 
 /**
- * This class represents a Route Interface, a virtual base class 
- * for a Route used for routing of wares for workers. 
+ * This class represents a Route Interface, a virtual base class
+ * for a Route used for routing of wares for workers.
  *
  * This class was introduced to hide the implementation of route
  * and hide some ugly helper functions. It is therefore not a complete
- * interface, it only shows what was needed for refactoring. 
+ * interface, it only shows what was needed for refactoring.
  *
  * Most parts of the code still use the Route class directly and not this
  * interface
  **/
 class IRoute {
 public:
-    virtual ~IRoute() { };
+	virtual ~IRoute() {};
 
-	virtual void init( int32_t ) = 0;
-    virtual void insert_as_first(RoutingNode* node) = 0;
+	virtual void init(int32_t) = 0;
+	virtual void insert_as_first(RoutingNode * node) = 0;
 };
 
 }
-#endif 
-
-
+#endif
