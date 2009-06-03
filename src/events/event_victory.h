f1ee6b727395c139be87420daa24672af0092a09
dwarik <dwarik@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2009-06-03 16:01:36 +0000

    no victory, forgot to add the new files
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@4218 37b2a8de-5219-0410-9f54-a31bc463ab9c

---
+++ src/events/event_victory.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2008-2009 by the Widelands Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef EVENT_VICTORY_H
+#define EVENT_VICTORY_H
+
+#include "event.h"
+
+namespace Widelands {
+
+struct Event_Victory : Event {
+	Event_Victory(char const * Name, State const S)
+		: Event(Name, S)
+		{}
+	Event_Victory(Section &, Editor_Game_Base &);
+	void Write(Section &, Editor_Game_Base &) const;
+
+	State run(Game &);
+	int32_t option_menu(Editor_Interactive &);
+protected:
+	bool victorious;
+
+};
+
+};
+
+#endif
