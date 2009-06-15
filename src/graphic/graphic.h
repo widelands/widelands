4b3a4a3457408079df75062fd0b077d84f1edd85
dwarik <dwarik@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2009-06-15 11:22:29 +0000

    use tabs for indentation
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@4264 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/graphic/graphic.h
+++ src/graphic/graphic.h
@@ -77,7 +77,7 @@ struct Graphic {
 	void refresh(bool force = true);
 
 	void flush(PicMod module);
-  PictureID & get_picture(PicMod module, const std::string & fname);
+	PictureID & get_picture(PicMod module, const std::string & fname);
 	//__attribute__ ((pure));
 	PictureID get_picture
 		(PicMod module, Surface &, const std::string & name = "");
