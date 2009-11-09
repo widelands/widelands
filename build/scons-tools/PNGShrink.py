fe9770b13b4951b52d5380aceca97571d190a071
sigra <sigra@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2009-11-09 19:01:11 +0000

    Attempt to introduce another step in PNG shrinking. Shrink started, but failed with "scons: *** [temp] Source `temp.png\' not found, needed by target `temp\'.". Further investigation required. PNGs that it shrunk before it failed are included in this commit.
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@4622 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- build/scons-tools/PNGShrink.py
+++ build/scons-tools/PNGShrink.py
@@ -16,7 +16,7 @@ def move_temp_png(dest):
 def shrink_command(source, target, env, for_signature):
 	commands=[]
 
-	if (env['PNGREWRITE']==None) or (env['OPTIPNG']==None):
+	if (env['PNGREWRITE']==None) or (env['OPTIPNG']==None) or (env['ADVDEF']==None):
 		return env.Action(complain_pngshrink)
 
 	s=source[0].path
@@ -24,6 +24,7 @@ def shrink_command(source, target, env, for_signature):
 	commands.append(env.Action('$PNGREWRITE '+s+' temp.png'))
 	commands.append(env.Action(move_temp_png(s)))
 	commands.append(env.Action('$OPTIPNG $OPTIPNGFLAGS '+s))
+	commands.append(env.Action('$ADVDEF $ADVDEFFLAGS '+s))
 
 	return commands
 
@@ -32,6 +33,9 @@ def generate(env):
 
 	env['OPTIPNG']=find_optipng(env)
 	env['OPTIPNGFLAGS']='-q -zc1-9 -zm1-9 -zs0-3 -f0-5'
+
+	env['ADVDEF']=find_advdef(env)
+	env['ADVDEFFLAGS']='-z4'
 
 	env['BUILDERS']['PNGShrink']=SCons.Builder.Builder(generator=shrink_command, single_source=1)
 
@@ -51,7 +55,15 @@ def find_optipng(env):
 		print 'Searching for optipng:    ', b
 	return b
 
+def find_advdef(env):
+	b=env.WhereIs('advdef')
+	if b==None:
+		print 'Searching for advdef:      not found. PNG compatification disabled.'
+	else:
+		print 'Searching for advdef:     ', b
+	return b
+
 def exists(env):
-	if (find_optipng(env)==None) or (find_pngrewrite(env)==None):
+	if (find_advdef(env)==None) or (find_optipng(env)==None) or (find_pngrewrite(env)==None):
 		return 0
 	return 1
