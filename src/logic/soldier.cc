a1f59539e0c395e5c379ea2aa4d4e2f9ef0cca5f
nasenbaer_peter <nasenbaer_peter@37b2a8de-5219-0410-9f54-a31bc463ab9c>
2010-01-08 17:48:15 +0000

    implement FR #2768019 - draw a clearly visible light playercolor in energy bar of the soldiers
    git-svn-id: https://widelands.svn.sourceforge.net/svnroot/widelands/trunk@4946 37b2a8de-5219-0410-9f54-a31bc463ab9c

--- src/logic/soldier.cc
+++ src/logic/soldier.cc
@@ -565,20 +565,7 @@ void Soldier::draw
 		// Draw the actual bar
 		assert(m_hp_max);
 		const float fraction = static_cast<float>(m_hp_current) / m_hp_max;
-		//  FIXME Draw bar in playercolor, should be removed when soldier is
-		//  FIXME correctly painted.
-		RGBColor color
-			(owner().get_playercolor()->r(),
-			 owner().get_playercolor()->g(),
-			 owner().get_playercolor()->b());
-#if 0
-		if (fraction <= 0.15)
-			color = RGBColor(255, 0, 0);
-		else if (fraction <= 0.5)
-			color = RGBColor(255, 255, 0);
-		else
-			color = RGBColor(17, 192, 17);
-#endif
+		RGBColor color(owner().get_playercolor()[3]);
 		assert(2 <= r.w);
 		assert(2 <= r.h);
 		dst.fill_rect
