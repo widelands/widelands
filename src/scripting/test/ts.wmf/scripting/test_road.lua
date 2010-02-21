a70453ae5a2ad868ea3857ab263338e0d3803816
Holger Rapp <SirVer@gmx.de>
2010-02-21 17:56:45 +0100

    One more test case and a fixed comment

--- src/scripting/test/ts.wmf/scripting/test_road.lua
+++ src/scripting/test/ts.wmf/scripting/test_road.lua
@@ -12,12 +12,13 @@ function road_tests:setup()
 
    self.start_flag = self.p:place_flag(self.f)
    self.r = self.p:place_road(self.start_flag, "r", "r")
+   self.end_flag = wl.map.Field(12,10).immovable
 end
 function road_tests:teardown()
    pcall(function()
       f1 = self.r.start_flag
       f2 = self.r.end_flag
-      f1:remove() -- should remove road as well
+      f1:remove() -- removes road as well
       f2:remove() 
    end)
 end
@@ -30,3 +31,8 @@ function road_tests:test_deletion_of_flag()
    self.r.start_flag:remove()
    assert_error("Road vanished!", function() self.r:length() end)
 end
+function road_tests:test_access_of_flags()
+   assert_equal(self.end_flag, self.r.end_flag)
+   assert_equal(self.start_flag, self.r.start_flag)
+end
+
