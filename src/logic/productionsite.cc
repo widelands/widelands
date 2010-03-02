8658d28a061b8a121fe8cfb191da417d8dae15e5
Nasenbaer <Nasenbaer@drehatlas.de>
2010-03-02 23:07:56 +0100

    get rid of UP and DOWN and instead use + and - as we used in many translations for quite some time - this is at least as intuitive as the UP and DOWN, but isn't that disturbing as those big texts.

--- src/logic/productionsite.cc
+++ src/logic/productionsite.cc
@@ -1,4 +1,4 @@
-/*
+// /*
  * Copyright (C) 2002-2004, 2006-2010 by the Widelands Development Team
  *
  * This program is free software; you can redistribute it and/or
@@ -310,7 +310,7 @@ void ProductionSite::calc_statistics()
 	uint32_t const lastPercOk = (lastOk * 100) / (STATISTICS_VECTOR_LENGTH / 2);
 
 	const std::string trend =
-		lastPercOk > percOk ? _("UP") : lastPercOk < percOk ? _("DOWN") : "=";
+		lastPercOk > percOk ? "+" : lastPercOk < percOk ? "-" : "=";
 
 	if (0 < percOk and percOk < 100)
 		snprintf
