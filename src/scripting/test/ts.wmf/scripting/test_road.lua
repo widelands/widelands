01bc466ee35f83d1328ac3178e655cca46db4dfe
Holger Rapp <SirVer@gmx.de>
2010-04-08 22:03:35 +0200

    Roads can now be created, but no carriers are on them

--- src/scripting/test/ts.wmf/scripting/test_road.lua
+++ src/scripting/test/ts.wmf/scripting/test_road.lua
@@ -30,89 +30,89 @@ function road_construction_tests:teardown()
    end)
 end
 function road_construction_tests:test_simple()
-   r = self.p:place_road(self.start_flag,"r","br","r")
+   r = self.p:place_road(self.start_flag, "r", "br", "r")
    self.flags[#self.flags+1] = self.f.rn.brn.rn.immovable
    assert_equal(3, r.length)
 end
--- function road_construction_tests:test_too_short()
---    assert_error("can't place flag", function()
---       self.p:place_road(self.start_flag,"r")
---    end)
--- end
--- function road_construction_tests:test_connect_two_flags()
---    self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn)
---    r = self.p:place_road(self.start_flag,"r", "r")
---    assert_equal(2, r.length)
--- end
--- function road_construction_tests:test_road_crosses_another()
---    self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn.rn.rn)
---    r = self.p:place_road(self.start_flag,"r", "r", "r", "r")
---    assert_equal(4, r.length)
---    self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn.tln.tln)
---    self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn.brn.brn)
---    assert_error("Immovable in the way", function()
---       r = self.p:place_road(self.flags[3],"br", "br", "br", "br")
---    end)
--- end
--- function road_construction_tests:test_road_is_closed_loop()
---    assert_error("Cannot build closed loop", function()
---       self.p:place_road(self.start_flag,"r", "r", "tl", "tl", "bl", "bl")
---    end)
--- end
--- function road_construction_tests:test_road_crosses_itself()
---    assert_error("The road crosses itself", function()
---       self.p:place_road(self.start_flag,"r", "r", "r", "tl", "bl", "bl")
---    end)
--- end
---
--- -- ============
--- -- Usage tests
--- -- ============
--- road_tests = lunit.TestCase("road tests")
--- function road_tests:setup()
---    self.f = wl.map.Field(10,10)
---    self.p = wl.game.Player(1)
---    self.p:conquer(f, 6)
---
---    self.start_flag = self.p:place_flag(self.f)
---
---    self.r = self.p:place_road(self.start_flag, "r", "r")
---    self.end_flag = self.f.rn.rn.immovable
--- end
--- function road_tests:teardown()
---    flags = {self.start_flag, self.end_flag}
---    for temp, f in ipairs(flags) do
---       pcall(function() f:remove() end)
---    end
--- end
---
--- function road_tests:test_length()
---    assert_equal(2, self.r.length)
--- end
--- function road_tests:test_roadtype()
---    assert_equal("normal", self.r.road_type)
--- end
--- function road_tests:test_type()
---    assert_equal("road", self.r.type)
--- end
--- function road_tests:test_name()
---    assert_equal("road", self.r.name)
--- end
---
--- function road_tests:test_deletion_of_flag()
---    self.r.start_flag:remove()
---    assert_error("Road vanished!", function() self.r:length() end)
--- end
--- function road_tests:test_access_of_flags()
---    assert_equal(self.end_flag, self.r.end_flag)
---    assert_equal(self.start_flag, self.r.start_flag)
--- end
--- function road_tests:test_road_upcasting()
---    i = self.f.rn.immovable
---    assert_equal(i, self.r)
---    assert_equal(2, i.length)
--- end
---
+function road_construction_tests:test_too_short()
+   assert_error("can't place flag", function()
+      self.p:place_road(self.start_flag, "r")
+   end)
+end
+function road_construction_tests:test_connect_two_flags()
+   self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn)
+   r = self.p:place_road(self.start_flag,"r", "r")
+   assert_equal(2, r.length)
+end
+function road_construction_tests:test_road_crosses_another()
+   self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn.rn.rn)
+   r = self.p:place_road(self.start_flag,"r", "r", "r", "r")
+   assert_equal(4, r.length)
+   self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn.tln.tln)
+   self.flags[#self.flags+1] = self.p:place_flag(self.f.rn.rn.brn.brn)
+   assert_error("Immovable in the way", function()
+      r = self.p:place_road(self.flags[3],"br", "br", "br", "br")
+   end)
+end
+function road_construction_tests:test_road_is_closed_loop()
+   assert_error("Cannot build closed loop", function()
+      self.p:place_road(self.start_flag,"r", "r", "tl", "tl", "bl", "bl")
+   end)
+end
+function road_construction_tests:test_road_crosses_itself()
+   assert_error("The road crosses itself", function()
+      self.p:place_road(self.start_flag,"r", "r", "r", "tl", "bl", "bl")
+   end)
+end
+
+-- ============
+-- Usage tests
+-- ============
+road_tests = lunit.TestCase("road tests")
+function road_tests:setup()
+   self.f = wl.map.Field(10,10)
+   self.p = wl.game.Player(1)
+   self.p:conquer(f, 6)
+
+   self.start_flag = self.p:place_flag(self.f)
+
+   self.r = self.p:place_road(self.start_flag, "r", "r")
+   self.end_flag = self.f.rn.rn.immovable
+end
+function road_tests:teardown()
+   flags = {self.start_flag, self.end_flag}
+   for temp, f in ipairs(flags) do
+      pcall(function() f:remove() end)
+   end
+end
+
+function road_tests:test_length()
+   assert_equal(2, self.r.length)
+end
+function road_tests:test_roadtype()
+   assert_equal("normal", self.r.road_type)
+end
+function road_tests:test_type()
+   assert_equal("road", self.r.type)
+end
+function road_tests:test_name()
+   assert_equal("road", self.r.name)
+end
+
+function road_tests:test_deletion_of_flag()
+   self.r.start_flag:remove()
+   assert_error("Road vanished!", function() self.r:length() end)
+end
+function road_tests:test_access_of_flags()
+   assert_equal(self.end_flag, self.r.end_flag)
+   assert_equal(self.start_flag, self.r.start_flag)
+end
+function road_tests:test_road_upcasting()
+   i = self.f.rn.immovable
+   assert_equal(i, self.r)
+   assert_equal(2, i.length)
+end
+
 -- -- ====================
 -- -- Creating of carrier
 -- -- ====================
