6ab73ad7d72967478cdbba8b09178806663f6774
Nasenbaer <Nasenbaer@drehatlas.de>
2010-04-17 13:18:54 +0200

    As requested by the chieftain ;) - here is a new map for the new excellent tutorial

--- campaigns/tutorial01.wmf/scripting/texts.lua
+++ campaigns/tutorial01.wmf/scripting/texts.lua
@@ -78,7 +78,7 @@ lumberjack_message_01 = {
    body = rt(p(_
 [[There you go. I will explain about all those symbols in a minute. First, let
  me show you how to make a lumberjack's hut and how to connect it. There is a
- sweet sport for a lumberjack right next to those trees.]]
+ sweet spot for a lumberjack right next to those trees.]]
       )
    )
 }
