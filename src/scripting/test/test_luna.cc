a4cb599157072e772b312ff11a4885e20786ef23
Jari Hautio <jari.hautio@iki.fi>
2010-06-06 22:05:57 +0300

    Added unit tests for lua/luna properties. Crash when built on vs2008 or gcc-4.3.3.

--- src/scripting/test/test_luna.cc
+++ src/scripting/test/test_luna.cc
@@ -22,6 +22,11 @@ public:
 		lua_pushuint32(L, x);
 		return 1;
 	}
+	virtual int get_prop1(lua_State*L)
+	{
+		lua_pushuint32(L, x*2);
+		return 1;
+	}
 	virtual void __persist(lua_State * L) {}
 	virtual void __unpersist(lua_State * L) {}
 };
@@ -31,6 +36,7 @@ const MethodType<L_Class> L_Class::Methods[] = {
 	{0, 0},
 };
 const PropertyType<L_Class> L_Class::Properties[] = {
+	PROP_RO(L_Class, prop1),
 	{0, 0, 0},
 };
 
@@ -90,23 +96,14 @@ const static struct luaL_reg wl [] = {
 	{0, 0}
 };
 
-BOOST_AUTO_TEST_CASE(test_luna)
+BOOST_AUTO_TEST_CASE(test_luna_simple)
 {
 	const char * script1 =
 		"print( 'test_luna simple class' )\n"
 		"t = wl.test.Class()\n"
 		"x = t:test()\n"
-		"print( x )\n";
-	const char * script2 =
-		"print( 'test_luna simple inheritance' )\n"
-		"s = wl.test.SubClass()\n"
-		"sx = s:test()\n"
-		"print( sx )\n";
-	const char * script3 =
-		"print( 'test_luna virtual inheritance' )\n"
-		"v = wl.test.VirtualClass()\n"
-		"vx = v:test()\n"
-		"print( vx )\n";
+		"p = t.prop1\n"
+		"print( 'test(): ' .. x .. ' prop1: ' .. p )\n";
 
 	shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
 	lua_State*L = L_ptr.get();
@@ -118,22 +115,89 @@ BOOST_AUTO_TEST_CASE(test_luna)
 	lua_pop(L, 1); // pop the table from the stack
 	register_class<L_Class>(L, "test");
 
+	BOOST_CHECK_EQUAL(0, luaL_loadbuffer(L, script1, strlen(script1), "testscript1"));
+	BOOST_CHECK_EQUAL(0, lua_pcall(L, 0, 1, 0));
+	
+}
+
+BOOST_AUTO_TEST_CASE(test_luna_inheritance)
+{
+	const char * script2 =
+		"print( 'test_luna simple inheritance' )\n"
+		"t = wl.test.SubClass()\n"
+		"x = t:test()\n"
+		"p = t.prop1\n"
+		"print( 'test(): ' .. x .. ' prop1: ' .. p )\n";
+
+	shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
+	lua_State*L = L_ptr.get();
+	luaL_openlibs(L);
+
+	luaL_register(L, "wl", wl);
+	lua_pop(L, 1); // pop the table from the stack
+	luaL_register(L, "wl.test", wltest);
+	lua_pop(L, 1); // pop the table from the stack
+
 	// single inheritance
 	register_class<L_SubClass>(L, "test", true);
 	add_parent<L_SubClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
+	
+	BOOST_CHECK_EQUAL(0, luaL_loadbuffer(L, script2, strlen(script2), "testscript2"));
+	BOOST_CHECK_EQUAL(0, lua_pcall(L, 0, 1, 0));
+
+}
+
+
+BOOST_AUTO_TEST_CASE(test_luna_virtualbase_method)
+{
+	const char * script3 =
+		"print( 'test_luna virtual inheritance method call' )\n"
+		"t = wl.test.VirtualClass()\n"
+		"x = t:test()\n"
+		"print( 'test(): ' .. x )\n";
+
+	shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
+	lua_State*L = L_ptr.get();
+	luaL_openlibs(L);
+
+	luaL_register(L, "wl", wl);
+	lua_pop(L, 1); // pop the table from the stack
+	luaL_register(L, "wl.test", wltest);
+	lua_pop(L, 1); // pop the table from the stack
 
 	// virtual base class 
 	register_class<L_VirtualClass>(L, "test", true);
 	add_parent<L_VirtualClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
 
-	BOOST_CHECK_EQUAL(0, luaL_loadbuffer(L, script1, strlen(script1), "testscript1"));
-	BOOST_CHECK_EQUAL(0, lua_pcall(L, 0, 1, 0));
-	
-	BOOST_CHECK_EQUAL(0, luaL_loadbuffer(L, script2, strlen(script2), "testscript2"));
-	BOOST_CHECK_EQUAL(0, lua_pcall(L, 0, 1, 0));
-
 	BOOST_CHECK_EQUAL(0, luaL_loadbuffer(L, script3, strlen(script3), "testscript3"));
 	BOOST_CHECK_EQUAL(0, lua_pcall(L, 0, 1, 0));
 }
+
+BOOST_AUTO_TEST_CASE(test_luna_virtualbase_property)
+{
+	const char * script4 =
+		"print( 'test_luna virtual inheritance properties' )\n"
+		"t = wl.test.VirtualClass()\n"
+		"p = t.prop1\n"
+		"print( 'prop1: ' .. p )\n";
+
+	shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
+	lua_State*L = L_ptr.get();
+	luaL_openlibs(L);
+
+	luaL_register(L, "wl", wl);
+	lua_pop(L, 1); // pop the table from the stack
+	luaL_register(L, "wl.test", wltest);
+	lua_pop(L, 1); // pop the table from the stack
+
+	// virtual base class 
+	register_class<L_VirtualClass>(L, "test", true);
+	add_parent<L_VirtualClass, L_Class>(L);
+	lua_pop(L, 1); // Pop the meta table
+
+	BOOST_CHECK_EQUAL(0, luaL_loadbuffer(L, script4, strlen(script4), "testscript4"));
+	BOOST_CHECK_EQUAL(0, lua_pcall(L, 0, 1, 0));
+}
+
