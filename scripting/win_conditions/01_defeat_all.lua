e6a4e7f47d0b876a2cff50dee93cab03c9cc9725
Nasenbaer <Nasenbaer@drehatlas.de>
2010-08-11 20:08:48 +0200

    Give defeated players of autocrat games full vision

--- scripting/win_conditions/01_defeat_all.lua
+++ scripting/win_conditions/01_defeat_all.lua
@@ -36,7 +36,7 @@ return {
       end
 
       -- Iterate all players, if one is defeated, remove him
-      -- from the list and send him a defeated message
+      -- from the list, send him a defeated message and give him full vision
       repeat
          sleep(5000)
          for idx,p in ipairs(plrs) do
@@ -44,6 +44,7 @@ return {
                p:send_message(_ "You lost!",
                   _ "Sorry, you have lost this game!",
                   { popup = true })
+               p.see_all = 1
                table.remove(plrs, idx)
                break
             end
