016f1058f6eef52a1df4899505c6fcd97d14bd0a
Nasenbaer <Nasenbaer@drehatlas.de>
2010-08-31 19:41:28 +0200

    Finally it works as it should

--- scripting/win_conditions/03_territorial_lord.lua
+++ scripting/win_conditions/03_territorial_lord.lua
@@ -11,10 +11,10 @@ set_textdomain("win_conditions")
 return {
 	name = _ "Territorial Lord",
 	description = _ (
-"Each player or team tries to obtain more than half of the maps' area. " .. 
-"The winner will be the player or the team that is able to keep that area " ..
-"for at least 20 minutes."
-),
+		"Each player or team tries to obtain more than half of the maps' " ..
+		"area. The winner will be the player or the team that is able to keep " ..
+		"that area for at least 20 minutes."
+	),
 	func = function()
 
 		-- Get the size of the map
@@ -26,50 +26,77 @@ return {
 		-- of the map's area
 		local currentcandidate = "" -- Name of Team or Player
 		local candidateisteam = false
-		local remaining_time = 10 -- time in seconds, once == 0, the game ends
+		local remaining_time = 10 -- (dummy) -- time in secs, if == 0 -> victory
 
 		-- Find all valid players
 		local plrs = {}
 		valid_players(plrs)
 
+		-- Find all valid teams
+		local teamnumbers = {} -- array with team numbers
+		for idx,p in ipairs(plrs) do
+			local team = p.team
+			if team > 0 then
+				local found = false
+				for idy,t in ipairs(teamnumbers) do
+					if t == team then
+						found = true
+						break
+					end
+				end
+				if not found then
+					teamnumbers[#teamnumbers+1] = team
+				end
+			end
+		end
+
 		function _calc_points()
-			local stats = wl.game.get_general_statistics()
-			local teams = {} -- points of teams
+			local teampoints = {}     -- points of teams
 			local maxplayerpoints = 0 -- the highest points of a player without team
 			local maxpointsplayer = 0 -- the player
+			local foundcandidate = false
 			for idx, p in ipairs(plrs) do
 				local team = p.team
+				local landsize = p.get_current_land_size(p)
 				if team == 0 then
-					if maxplayerpoints < stats[p:player_number()].land_size[#stats[p:player_number()].land_size] then
-						maxplayerpoints = stats[p:player_number()].land_size[#stats[p:player_number()].land_size]
+					if maxplayerpoints < landsize then
+						maxplayerpoints = landsize
 						maxpointsplayer = p
 					end
 				else
-					teams[team] = teams[team] + stats[p:player_number()].land_size[#stats[p:player_number()].land_size]
+					if not teampoints[team] then -- init the value
+						teampoints[team] = 0
+					end
+					teampoints[team] = teampoints[team] + landsize
 				end
 			end
-			if maxplayerspoints > ( mapsize / 2 ) then
+			if maxplayerpoints > ( mapsize / 2 ) then
 				-- player owns more than half of the map's area
-				if candidateisteam == false and currentcandidate == maxpointsplayer.get_name() then
-					remaining_time = remaining_time - 10
+				foundcandidate = true
+				if candidateisteam == false and currentcandidate == maxpointsplayer.name then
+					remaining_time = remaining_time - 30
 				else
-					currentcandidate = maxpointsplayer.get_name()
+					currentcandidate = maxpointsplayer.name
 					candidateisteam = false
 					remaining_time = 20 * 60 -- 20 minutes
 				end
 			else
-				for idx, teampoints in ipairs(teams) do
-					if teampoints > ( mapsize / 2 ) then
-						-- team owns more than half of the map's area
-						if candidateisteam == true and currentcandidate == _("Team %i"):format(idx) then
-							remaining_time = remaining_time - 10
+				for idx, t in ipairs(teamnumbers) do
+					if teampoints[t] > ( mapsize / 2 ) then
+						-- this team owns more than half of the map's area
+						foundcandidate = true
+						if candidateisteam == true and currentcandidate == _("Team %i"):format(t) then
+							remaining_time = remaining_time - 30
+						else
+							currentcandidate = _("Team %i"):format(t)
+							candidateisteam = true
+							remaining_time = 20 * 60 -- 20 minutes
 						end
-					else
-						currentcandidate = _("Team %i"):format(idx)
-						candidateisteam = true
-						remaining_time = 20 * 60 -- 20 minutes
 					end
 				end
+			end
+			if not foundcandidate then
+				remaining_time = 10
 			end
 		end
 
@@ -80,11 +107,11 @@ return {
 
 			local msg2 = _("You own more than half of the maps area.")
 			msg2 = msg2 .. "\n"
-			msg2 = msg2 .. _("Keep it for %i more minutes win the game."):format(remaining_time / 60)
+			msg2 = msg2 .. _("Keep it for %i more minutes to win the game."):format(remaining_time / 60)
 
 			for idx, p in ipairs(plrs) do
 				if candidateisteam and currentcandidate == _("Team %i"):format(p.team)
-					or not candidateisteam and currentcandidate == p.get_name() then
+					or not candidateisteam and currentcandidate == p.name then
 					p:send_message(_ "Status", msg2)
 				else
 					p:send_message(_ "Status", msg1)
@@ -96,22 +123,34 @@ return {
 
 		-- here is the main loop!!!
 		while true do
-			sleep(10 * 1000) -- Sleep 10 seconds
+			-- Sleep 30 seconds == STATISTICS_SAMPLE_TIME
+			sleep(30000)
 
+			-- Check if a player or team is a candidate and update variables
 			_calc_points()
 
-			if remaining_time % 600 == 0 then
-				_send_state()
-			end
-
+			-- Check whether one of the players got defeated and handle defeats
 			check_player_defeated(plrs, _ "You are defeated!",
 				_ "You have nothing to command left. If you want, you may " ..
 				  "continue as spectator.")
 
-			-- Game ended?
+			-- Do this stuff, if the game is over
 			if remaining_time == 0 then
-				-- TODO: send victory and loser messages and show full map to all
+				for idx, p in ipairs(plrs) do
+					p.see_all = 1
+					if candidateisteam and currentcandidate == _("Team %i"):format(p.team)
+						or not candidateisteam and currentcandidate == p.name then
+						p:send_message(_"You won!", _"You are the winner!")
+					else
+						p:send_message(_"You lost", _"You've lost this game!")
+					end
+				end
 				break
+			end
+
+			-- If there is a candidate, check whether we have to send an update
+			if remaining_time % 300 == 0 then -- every 5 minutes (5 * 60 )
+				_send_state()
 			end
 		end
 	end
--- src/scripting/lua_game.cc
+++ src/scripting/lua_game.cc
@@ -102,10 +102,12 @@ const MethodType<L_Player> L_Player::Methods[] = {
 	METHOD(L_Player, set_frontier_style),
 	METHOD(L_Player, get_suitability),
 	METHOD(L_Player, allow_workers),
+	METHOD(L_Player, get_current_land_size),
 	{0, 0},
 };
 const PropertyType<L_Player> L_Player::Properties[] = {
 	PROP_RO(L_Player, number),
+	PROP_RO(L_Player, name),
 	PROP_RO(L_Player, allowed_buildings),
 	PROP_RO(L_Player, objectives),
 	PROP_RO(L_Player, defeated),
@@ -113,7 +115,7 @@ const PropertyType<L_Player> L_Player::Properties[] = {
 	PROP_RW(L_Player, retreat_percentage),
 	PROP_RW(L_Player, changing_retreat_percentage_allowed),
 	PROP_RO(L_Player, inbox),
-	PROP_RO(L_Player, team),
+	PROP_RW(L_Player, team),
 	PROP_RO(L_Player, tribe),
 	PROP_RW(L_Player, see_all),
 	{0, 0, 0},
@@ -148,6 +150,19 @@ int L_Player::get_number(lua_State * L) {
 	lua_pushuint32(L, m_pl);
 	return 1;
 }
+
+/* RST
+.. attribute:: name
+
+		(RO) The name of this Player.
+*/
+int L_Player::get_name(lua_State * L) {
+	Game & game = get_game(L);
+	Player & p = get(L, game);
+	lua_pushstring(L, p.get_name());
+	return 1;
+}
+
 /* RST
 	.. attribute:: allowed_buildings
 
@@ -294,9 +309,16 @@ int L_Player::get_tribe(lua_State *L) {
 /* RST
 	.. attribute:: team
 
-		(RO) The team number of this player (0 means player is not in a team)
+		(RW) The team number of this player (0 means player is not in a team)
+
+		normally only reading should be enough, however it's a nice idea to have
+		a modular scenario, where teams form during the game.
 */
-int L_Player::get_team(lua_State *L) {
+int L_Player::set_team(lua_State * L) {
+	get(L, get_egbase(L)).set_team_number(luaL_checkinteger(L, -1));
+	return 0;
+}
+int L_Player::get_team(lua_State * L) {
 	lua_pushinteger(L, get(L, get_egbase(L)).team_number());
 	return 1;
 }
@@ -1139,6 +1161,29 @@ int L_Player::allow_workers(lua_State * L) {
 		}
 	}
 	return 0;
+}
+
+
+/* RST
+	.. method:: get_current_land_size()
+
+	returns the current landsize of the player
+
+*/
+int L_Player::get_current_land_size(lua_State * L) {
+	Game & game      = get_game(L);
+	Player_Number pn = get(L, game).player_number();
+	const Game::General_Stats_vector & stats = game.get_general_statistics();
+
+	if (stats[pn - 1].land_size.empty()) {
+		lua_pushuint32(L, 0);
+		log("No statistics for this player. Perhaps the game just started\n");
+		return 1;
+	}
+
+	lua_pushuint32
+			(L, stats[pn - 1].land_size[stats[pn - 1].land_size.size() - 1]);
+	return 1;
 }
 
 /*
