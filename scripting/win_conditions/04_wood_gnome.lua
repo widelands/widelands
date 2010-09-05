37e01cd1ba511f18b6c147487b9f33be7f3d0535
Holger Rapp <SirVer@gmx.de>
2010-09-05 12:27:50 +0200

    Added statistics to woodgnome win condition

--- scripting/win_conditions/04_wood_gnome.lua
+++ scripting/win_conditions/04_wood_gnome.lua
@@ -9,125 +9,135 @@ use("aux", "win_condition_functions")
 set_textdomain("win_conditions")
 
 local wc_name = _ "Wood Gnome"
-local wc_desc = _ (
-	"As wood gnome you like big forests, so your task is, to have more " ..
-	"trees on your territory than any other player. The game will end after 4 " ..
-	"hours of playing. The one with the most trees at that point will win " ..
-	"the game."
-)
+local wc_desc = _
+[[As wood gnome you like big forests, so your task is, to have more trees on
+your territory than any other player. The game will end after 4 hours of
+playing. The one with the most trees at that point will win the game.]]
+
 return {
 	name = wc_name,
 	description = wc_desc,
 	func = function()
-		-- Find all valid players
-		local plrs = {}
-		valid_players(plrs)
+	local plrs = {}
+	valid_players(plrs)
 
-		-- send a message with the game type to all players
-		for idx, p in ipairs(plrs) do
-			p:send_message(wc_name, wc_desc)
+	-- send a message with the game type to all players
+	broadcast(plrs, wc_name, wc_desc)
+
+	local remaining_time = 4 * 60 -- 4 hours
+	
+	-- The function to calculate the current points.
+	local _last_time_calculated = -100000
+	local _plrpoints = {}
+	local function _calc_points()
+		if _last_time_calculated > wl.game.get_time() - 5000 then
+			return _plrpoints
 		end
 
-		local remaining_time = 4 * 60 -- 4 hours
+		-- init the playerpoints for each player
+		for idx,plr in ipairs(plrs) do
+			_plrpoints[plr.number] = 0
+		end
 
-		local points = {}
-		local playerpoints = {}
-		function _calc_points()
-			-- init the playerpoints for each player
-			for idx,plr in ipairs(plrs) do
-				playerpoints[plr.number] = 0
-			end
-			local mapwidth  = wl.map.get_width()
-			local mapheight = wl.map.get_height()
-			for x=0,mapwidth-1 do
-				for y=0,mapheight-1 do
-					local f = wl.map.Field(x,y)
-					if f then
-						-- check if field is owned by a player
-						local owner = 0
-						if f.owners[1] then
-							owner = f.owners[1].number
-							-- check if field has an immovable
-							local imm = f.immovable
-							if imm then
-								-- check if immovable is a tree
-								if imm:has_attribute("tree") then
-								playerpoints[owner] = playerpoints[owner] + 1
-								end
-							end
+		for x=0,wl.map.get_width()-1 do
+			for y=0,wl.map.get_height()-1 do
+				local f = wl.map.Field(x,y)
+				-- check if field is owned by a player
+				local owner = 0
+				if f.owners[1] then
+					owner = f.owners[1].number
+					-- check if field has an immovable
+					local imm = f.immovable
+					if imm then
+						-- check if immovable is a tree
+						if imm:has_attribute("tree") then
+						_plrpoints[owner] = _plrpoints[owner] + 1
 						end
 					end
 				end
 			end
-			for idx,plr in ipairs(plrs) do
-				points[#points + 1] = { plr, playerpoints[plr.number] }
-			end
+		end
+		_last_time_calculated = wl.game.get_time()
+		return _plrpoints
+	end
+
+	local function _send_state()
+		local playerpoints = _calc_points()
+		local msg = _("The game will end in %i minutes."):format(remaining_time)
+		msg = msg .. "\n\n"
+		msg = msg .. _ ("Player overview:")
+		for idx,plr in ipairs(plrs) do
+			msg = msg .. "\n"
+			msg = msg .. _ ("%s has "):format(plr.name)
+			msg = msg .. _ ("%i trees at the moment ."):format(playerpoints[plr.number])
 		end
 
-		function _send_state()
-			_calc_points()
-			local msg = _("The game will end in %i minutes."):format(remaining_time)
-			msg = msg .. "\n\n"
-			msg = msg .. _ ("Player overview:")
-			for idx,plr in ipairs(plrs) do
-				msg = msg .. "\n"
-				msg = msg .. _ ("%s has "):format(plr.name)
-				msg = msg .. _ ("%i trees at the moment ."):format(playerpoints[plr.number])
-			end
-
-			-- reset variables
-			points = {}
-			playerpoints = {}
-
-			for idx, p in ipairs(plrs) do
-				p:send_message(_ "Status", msg)
-			end
-		end
-
-		-- main loop
-		while true do
-			local runs = 0
-			repeat
-				sleep(5000)
-				check_player_defeated(plrs, _ "You are defeated!",
-					_ ("You have nothing to command left. If you want, you may " ..
-					   "continue as spectator."))
-				runs = runs + 1
-			until runs >= 60 -- 60 * 5000ms = 300000 ms = 5 minutes
-
-			remaining_time = remaining_time - 5
-
-			-- at the beginning send remaining time message only each 30 minutes
-			-- if only 30 minutes or less are left, send each 5 minutes
-			if (remaining_time < 30 or remaining_time % 30 == 0) and remaining_time > 0 then
-				_send_state()
-			end
-
-			-- Do this, as soon as the 4 hours are over
-			if remaining_time <= 0 then
-				_calc_points()
-				local msg = "\n\n"
-				msg = msg .. _ ("Player overview:")
-				for idx,plr in ipairs(plrs) do
-					msg = msg .. "\n"
-					msg = msg .. _ ("%s had "):format(plr.name)
-					msg = msg .. _ ("%i trees."):format(playerpoints[plr.number])
-				end
-				table.sort(points, function(a,b) return a[2] < b[2] end)
-				msg = msg .. "\n\n"
-				msg = msg .. _ ("The winner is %s "):format(points[#points][1].name)
-				msg = msg .. _ ("with %i trees."):format(playerpoints[points[#points][1].number])
-				local privmsg = ""
-				for i=1,#points-1 do
-					privmsg = _ ("You have lost this game!")
-					privmsg = privmsg .. msg
-					points[i][1]:send_message(_"You lost!", privmsg, {popup = true})
-				end
-				privmsg = _ ("You are the winner of this game!")
-				privmsg = privmsg .. msg
-				points[#points][1]:send_message(_"You won!", privmsg, {popup = true})
-				break
-			end
+		-- reset variables
+		for idx, p in ipairs(plrs) do
+			p:send_message(_ "Status", msg)
 		end
 	end
+
+	-- Start a new coroutine that checks for defeated players
+	run(function()
+		sleep(5000)
+		check_player_defeated(plrs, _ "You are defeated!",
+			_ ("You have nothing to command left. If you want, you may " ..
+				"continue as spectator."))
+	end)
+
+	-- Install statistics hook
+	if hooks == nil then hooks = {} end
+	hooks.custom_statistic = {
+		name = _ "Trees owned",
+		pic = "pics/genstats_trees.png",
+		calculator = function(p) 
+			local pts = _calc_points(p)
+			return pts[p.number]
+		end,
+	}
+
+	-- main loop
+	while true do
+		sleep(300000) -- 5 Minutes
+		remaining_time = remaining_time - 5
+
+		-- at the beginning send remaining time message only each 30 minutes
+		-- if only 30 minutes or less are left, send each 5 minutes
+		if (remaining_time < 30 or remaining_time % 30 == 0)
+				and remaining_time > 0 then
+			_send_state()
+		end
+
+		if remaining_time <= 0 then break end
+	end
+
+	-- Game has ended
+	local playerpoints = _calc_points()
+	local points = {}
+	for idx,plr in ipairs(plrs) do
+		points[#points + 1] = { plr, playerpoints[plr.number] }
+	end
+	table.sort(points, function(a,b) return a[2] < b[2] end)
+
+	local msg = "\n\n"
+	msg = msg .. _ ("Player overview:")
+	for idx,plr in ipairs(plrs) do
+		msg = msg .. "\n"
+		msg = msg .. _ ("%s had "):format(plr.name)
+		msg = msg .. _ ("%i trees."):format(playerpoints[plr.number])
+	end
+	msg = msg .. "\n\n"
+	msg = msg .. _ ("The winner is %s "):format(points[#points][1].name)
+	msg = msg .. _ ("with %i trees."):format(playerpoints[points[#points][1].number])
+	local privmsg = ""
+	for i=1,#points-1 do
+		privmsg = _ ("You have lost this game!")
+		privmsg = privmsg .. msg
+		points[i][1]:send_message(_"You lost!", privmsg, {popup = true})
+	end
+	privmsg = _ ("You are the winner of this game!")
+	privmsg = privmsg .. msg
+	points[#points][1]:send_message(_"You won!", privmsg, {popup = true})
+end
 }
