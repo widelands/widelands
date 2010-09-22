3499a3efa920dd8f42846a47e1bc920ae491c005
Holger Rapp <SirVer@gmx.de>
2010-09-22 20:25:45 +0200

    Already the first bug in my last commit found & fixed

--- tribes/barbarians/scripting/sc01_citadel_village.lua
+++ tribes/barbarians/scripting/sc01_citadel_village.lua
@@ -12,7 +12,7 @@ return {
 
    plr:allow_workers("all")
    
-   local sf = wl.Game().map.player_slots[p.number].starting_field
+   local sf = wl.Game().map.player_slots[plr.number].starting_field
    local h = plr:place_building("citadel", sf, false, true)
 
    h:set_soldiers{[{0,0,0,0}] = 12}
