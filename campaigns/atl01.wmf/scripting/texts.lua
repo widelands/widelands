f91148a45c3fcd90c0971308511bf2e2a25aed89
Holger Rapp <SirVer@gmx.de>
2010-10-02 20:01:02 +0200

    First part of atlantean story

--- campaigns/atl01.wmf/scripting/texts.lua
+++ campaigns/atl01.wmf/scripting/texts.lua
@@ -41,10 +41,10 @@ function speech(img, clr, g_title, g_text)
       title = nil
       text = g_title
    end
-     
+
    local s = ""
    if title then
-      s = rt("<p font-size=20 font-weight=bold font-face=FreeSerif " .. 
+      s = rt("<p font-size=20 font-weight=bold font-face=FreeSerif " ..
          ("font-color=%s>"):format(clr) .. title ..
          "</p><p font-size=8> <br></p>"
       )
@@ -64,7 +64,7 @@ function obj_text(heading, body)
 end
 
 -- Append an objective text to a dialog box in a nice fashion.
-function new_objectives(...) 
+function new_objectives(...)
    local s = rt(
    "<p font=FreeSerif font-size=18 font-weight=bold font-color=D1D1D1>"
    .. _"New Objectives" .. "</p>")
@@ -76,7 +76,7 @@ function new_objectives(...)
 end
 
 -- =======================================================================
---                                Objectives                                
+--                                Objectives
 -- =======================================================================
 obj_ensure_build_wares_production = {
    name = _ "obj_build_wares",
@@ -95,36 +95,77 @@ obj_build_environment = {
 }
 
 -- =======================================================================
---                                  Texts                                  
+--                                  Texts
 -- =======================================================================
 
 -- This is an array or message descriptions
 initial_messages = {
--- Message 01
 {
-   title = _ "Though speaked: It shall be light",
+   title = _ "Proudest to the death",
    body = rt(
       h1(_"This is a caption. And it is translated") ..
       p(_
-[[This is one paragraph. It is also translated and it spans
-multiple lines. Every newline is converted to a space, which
-is what we usually want. To keep it readable, we reformat the paragraphs
-to be < 80 chars in the Lua source code. SirVer can do this, do not
-bother too much about it.]]
+[[On the hidden and lost island of Atlantis, a proud tribe settled since the
+ world was very young. Ruled by the bloodline of King Ajanthul - the first human
+ to be empowered by the sea god Lutas to breath above the sea level - and the
+ wise clerics keeping the connection to Lutas they prospered and became
+ civilized.]]
       ) .. p(
-[[This is one more paragraph. It is *not* translated, because the _ is missing
-in front.  In other words a string like this _ "Hi" is translated, one without
-_ is not. Capice?]]
+[[This story happens during the regency of King Askandor, the 43th successor of
+ King Ajanthul. He has been a good king, ruling Atlantis with wisdom and
+ foresight. But with age, he became afraid of dying and so he began looking for
+ a cure to death even though most clerics warned him. Some said, endless life
+ was only for the gods and the seek for it was forbidden.]]
       )
    ),
 },
--- Message 02, direct speech now
 {
-   title = _ "And now I speak!",
-   body = princess(_ "The princess speaks", _
-[[Let's sink this ship!]]
-   ) .. new_objectives(obj_be_a_smarty)
-}
+   title = _ "The god's disgrace",
+   body = rt(
+      h1("The god's punishment") ..
+      p(_
+[[But all seemed well, and only the horses seemed to feel something.
+In the nights, the went crazy and were full of fear. Not before long, the
+horsebreeder Xydra figured out what was wrong with them: The sea level before
+their stable was rising in an ever accelerating speed.]]
+   ) .. p(_
+[[The clerics went into their meditation and the reason for the rising water
+ was soon to be found: The god lost faith in the atlanteans because of the
+ boldness of their king. He decided to withdraw the rights that were
+ granted to King Ajanthul and his children. And so, he calls them now back
+ below the sea.]]
+   )
+)
+},
+{
+   title = _ "Uproar and confusion",
+   body = rt(
+      h1("Chaos emerges...") ..
+      p(_
+[[Guilt ridden, the king suicided. Without monarch, the people turned to the
+ clerics. But those had no substantial help to offer: most accepted their
+ fate, others tried to change the god's mind by offering animals in his
+ temple. But to no avail...]]
+   ) .. p(_
+[[Jundlina, the late king's daughter and the highest priestess of the god 
+ was the most determined cleric. As countless offerings didn't change the
+ situation, she convinced herself, that the god needed an offer of big 
+ personal value to be soothed. So she offered him her most beloved: 
+ her husband, father of her only child.]]
+   ) .. p(_
+[[But not even this changed the mind of the god. The water kept on rising.
+ Nearly driven crazy by guilt, pain and anger, Jundlina became a heretic:
+ Secretly, she gathered people of the common folk who were not in line with the
+ decision of the clerics to accept the god's will. Together with them, she set
+ the temple on fire and stole a ship to flee from the god's influence of
+ Atlantis. This small group started praying to Satul, the fire god and the worst
+ enemy of Lutas.]]
+   ) .. p(_
+[[Leaving the dying Atlantis and their past behind, they started a quest to
+ find a place sheltered by the fire and protected from the sea.]]
+   )
+)
+},
 } -- end of initial messages.
 
 first_briefing_messages = {
@@ -138,7 +179,7 @@ first_briefing_messages = {
 
 
 -- =======================================================================
---                         Leftover buildings found                         
+--                         Leftover buildings found
 -- =======================================================================
 first_leftover_building_found = {
 {
