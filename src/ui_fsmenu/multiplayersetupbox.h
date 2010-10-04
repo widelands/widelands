f980e1ce2407fe84529d3a26f12d31102c981a9b
Nasenbaer <Nasenbaer@drehatlas.de>
2010-10-04 23:14:40 +0200

    copyright update and multiplayer parts

--- src/ui_fsmenu/multiplayersetupbox.h
+++ src/ui_fsmenu/multiplayersetupbox.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002, 2006, 2008, 2010 by the Widelands Development Team
+ * Copyright (C) 2010 by the Widelands Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -17,8 +17,8 @@
  *
  */
 
-#ifndef PLAYERDESCRGROUP_H
-#define PLAYERDESCRGROUP_H
+#ifndef MULTIPLAYERSETUPBOX_H
+#define MULTIPLAYERSETUPBOX_H
 
 #include "constants.h"
 #include "ui_basic/panel.h"
@@ -27,33 +27,36 @@
 
 struct GameSettingsProvider;
 
-struct PlayerDescriptionGroupImpl;
+struct MultiPlayerSetupBoxImpl;
 
-/** class PlayerDescriptionGroup
+/** class MultiPlayerSetupBox
  *
  * - checkbox to enable/disable player
  * - button to switch between: Human, Remote, AI
  */
-struct PlayerDescriptionGroup : public UI::Panel {
-	PlayerDescriptionGroup
+struct MultiPlayerSetupBox : public UI::Panel {
+	MultiPlayerSetupBox
 		(UI::Panel * parent,
 		 int32_t x, int32_t y, int32_t w, int32_t h,
 		 GameSettingsProvider * settings,
 		 uint32_t plnum,
 		 std::string const & fname = UI_FONT_NAME,
 		 uint32_t fsize = UI_FONT_SIZE_SMALL);
-	~PlayerDescriptionGroup();
+	~MultiPlayerSetupBox();
 
 	void refresh();
+	void enable_pdg       (bool enable = true);
+	void show_tribe_button(bool show   = true);
 
 private:
 	void enable_player(bool);
+	void ready_player(bool);
 	void toggle_playertype();
 	void toggle_playertribe();
 	void toggle_playerinit();
 	void toggle_playerteam();
 
-	PlayerDescriptionGroupImpl * d;
+	MultiPlayerSetupBoxImpl * d;
 	std::map<std::string,std::string> m_tribenames;
 };
 
