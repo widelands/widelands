19e8d0ea486b3c213a3fd6eb4d8cd726400332ef
Timo Wingender <timo.wingender@gmx.de>
2010-10-17 22:04:55 +0200

    fix spectator mode for netclient.

--- src/network/netclient.cc
+++ src/network/netclient.cc
@@ -181,9 +181,13 @@ void NetClient::run ()
 		d->game = &game;
 		game.set_game_controller(this);
 		// If our player is in shared kingdom mode - set the iabase accordingly.
-		uint8_t const pn =
+		uint8_t pn;
+		if (d->playernum < d->settings.players.size()) {
+		pn =
 			(d->settings.players.at(d->playernum).partner > 0) ?
 			 d->settings.players.at(d->playernum).partner : d->playernum + 1;
+		} else
+			pn = 0;
 		Interactive_GameBase * igb;
 		if (pn > 0)
 			igb =
