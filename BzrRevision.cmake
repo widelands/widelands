d5512c2bdda680262165e9e87610ad11c7c90a71
Jens Beyer (Qcumber-some) <qcumber-some@buerotiger.de>
2010-11-03 22:23:40 +0100

    do not reconfigure config.h during make

--- BzrRevision.cmake
+++ BzrRevision.cmake
@@ -7,7 +7,6 @@ execute_process (
 
 file (WRITE ${BINARY_DIR}/VERSION "${WL_VERSION}")
 
-configure_file (${SOURCE_DIR}/src/config.h.cmake ${BINARY_DIR}/src/config.h)
 configure_file (${SOURCE_DIR}/src/build_info.cc.cmake ${BINARY_DIR}/src/build_info.cc)
 
 message (STATUS "Version of Widelands Build is ${WL_VERSION}(${CMAKE_BUILD_TYPE})")
