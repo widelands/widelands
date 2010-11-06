b4e7b1907cc42a67e9bf0f2151fc02ffbc434b8f
Erik Sigra <esigra@gmail.com>
2010-11-06 17:00:31 +0100

    Fix const correctness and style.

--- src/scripting/test/test_luna.cc
+++ src/scripting/test/test_luna.cc
@@ -26,44 +26,39 @@
 #include "scripting/luna_impl.h"
 
 #ifndef BEGIN_LUNA_PROPERTIES
-#define BEGIN_LUNA_PROPERTIES(klass) \
-	const PropertyType<klass> klass::Properties[] = {
+#define BEGIN_LUNA_PROPERTIES(klass)                                          \
+   PropertyType<klass> const klass::Properties[] = {                          \
 
 #define END_LUNA_PROPERTIES() {0, 0, 0}};
 #endif
 
-class L_Class : public LunaClass
-{
-	int x;
-	int prop;
-public:
+struct L_Class : public LunaClass {
 	LUNA_CLASS_HEAD(L_Class);
 	const char * get_modulename() {return "test";}
-	L_Class() :x(123), prop(246) {}
+	L_Class() : x(123), prop(246) {}
 	virtual ~L_Class() {}
-	L_Class(lua_State *L) :x(124), prop(248) {}
-	virtual int test(lua_State *L) 
-	{
+	L_Class(lua_State * const L) : x(124), prop(248) {}
+	virtual int test(lua_State * const L) {
 		lua_pushuint32(L, x);
 		return 1;
 	}
-	virtual int get_prop1(lua_State*L)
-	{
+	virtual int get_prop1(lua_State * const L) {
 		lua_pushint32(L, prop);
 		return 1;
 	}
-	virtual int get_propr(lua_State*L)
-	{
+	virtual int get_propr(lua_State * const L) {
 		lua_pushint32(L, 1);
 		return 1;
 	}
-	virtual int set_prop1(lua_State*L)
-	{
+	virtual int set_prop1(lua_State * const L) {
 		prop = lua_tointeger(L, -1);
 		return 0;
 	}
-	virtual void __persist(lua_State * L) {}
-	virtual void __unpersist(lua_State * L) {}
+	virtual void __persist(lua_State *) {}
+	virtual void __unpersist(lua_State *) {}
+private:
+	int x;
+	int prop;
 };
 const char L_Class::className[] = "Class";
 const MethodType<L_Class> L_Class::Methods[] = {
@@ -75,20 +70,18 @@ BEGIN_LUNA_PROPERTIES(L_Class)
 	PROP_RW(L_Class, prop1),
 END_LUNA_PROPERTIES()
 
-class L_SubClass : public L_Class
-{
-	int y;
-public:
+struct L_SubClass : public L_Class {
 	LUNA_CLASS_HEAD(L_SubClass);
-	L_SubClass() :y(1230) {}
-	L_SubClass(lua_State *L) : L_Class(L), y(1240) {}
-	virtual int subtest(lua_State *L) 
-	{
+	L_SubClass() : y(1230) {}
+	L_SubClass(lua_State * const L) : L_Class(L), y(1240) {}
+	virtual int subtest(lua_State * const L) {
 		lua_pushuint32(L, y);
 		return 1;
 	}
-	virtual void __persist(lua_State * L) {}
-	virtual void __unpersist(lua_State * L) {}
+	virtual void __persist  (lua_State *) {}
+	virtual void __unpersist(lua_State *) {}
+private:
+	int y;
 };
 const char L_SubClass::className[] = "SubClass";
 const MethodType<L_SubClass> L_SubClass::Methods[] = {
@@ -98,20 +91,18 @@ const MethodType<L_SubClass> L_SubClass::Methods[] = {
 BEGIN_LUNA_PROPERTIES(L_SubClass)
 END_LUNA_PROPERTIES()
 
-class L_VirtualClass : public virtual L_Class
-{
-	int z;
-public:
+struct L_VirtualClass : public virtual L_Class {
 	LUNA_CLASS_HEAD(L_VirtualClass);
-	L_VirtualClass() :z(12300) {}
-	L_VirtualClass(lua_State *L) : L_Class(L), z(12400) {}
-	virtual int virtualtest(lua_State *L) 
-	{
+	L_VirtualClass() : z(12300) {}
+	L_VirtualClass(lua_State * const L) : L_Class(L), z(12400) {}
+	virtual int virtualtest(lua_State * const L) {
 		lua_pushuint32(L, z);
 		return 1;
 	}
-	virtual void __persist(lua_State * L) {}
-	virtual void __unpersist(lua_State * L) {}
+	virtual void __persist  (lua_State *) {}
+	virtual void __unpersist(lua_State *) {}
+private:
+	int z;
 };
 const char L_VirtualClass::className[] = "VirtualClass";
 const MethodType<L_VirtualClass> L_VirtualClass::Methods[] = {
@@ -121,31 +112,26 @@ const MethodType<L_VirtualClass> L_VirtualClass::Methods[] = {
 BEGIN_LUNA_PROPERTIES(L_VirtualClass)
 END_LUNA_PROPERTIES()
 
-class L_Second
-{
-public:
-	int get_second(lua_State *L) {lua_pushint32(L, 2001); return 1;}
-	virtual int multitest(lua_State *L) 
-	{
+struct L_Second {
+	int get_second(lua_State * const L) {lua_pushint32(L, 2001); return 1;}
+	virtual int multitest(lua_State * const L) {
 		lua_pushint32(L, 2002);
 		return 1;
 	}
 };
 
-class L_MultiClass : public L_Class, public L_Second
-{
-	int z;
-public:
+struct L_MultiClass : public L_Class, public L_Second {
 	LUNA_CLASS_HEAD(L_MultiClass );
-	L_MultiClass () :z(12300) {}
-	L_MultiClass (lua_State *L) : L_Class(L), z(12400) {}
-	virtual int virtualtest(lua_State *L) 
-	{
+	L_MultiClass () : z(12300) {}
+	L_MultiClass (lua_State * const L) : L_Class(L), z(12400) {}
+	virtual int virtualtest(lua_State * const L) {
 		lua_pushuint32(L, z);
 		return 1;
 	}
-	virtual void __persist(lua_State * L) {}
-	virtual void __unpersist(lua_State * L) {}
+	virtual void __persist(lua_State *) {}
+	virtual void __unpersist(lua_State *) {}
+private:
+	int z;
 };
 const char L_MultiClass::className[] = "MultiClass";
 const MethodType<L_MultiClass> L_MultiClass::Methods[] = {
@@ -172,7 +158,7 @@ int test_check_int(lua_State *L) {
 	return 0;
 }
 
-void InitLuaTests(lua_State *L)
+void InitLuaTests(lua_State * const L)
 {
 	luaL_openlibs(L);
 
@@ -190,7 +176,7 @@ void InitLuaTests(lua_State *L)
 
 BOOST_AUTO_TEST_CASE(test_luna_simple)
 {
-	const char * script1 =
+	char const * const script1 =
 		"t = wl.test.Class()\n"
 		"wl.test.CheckInt(248,t.prop1)\n"
 		"wl.test.CheckInt(124,t:test())\n"
@@ -198,11 +184,12 @@ BOOST_AUTO_TEST_CASE(test_luna_simple)
 		"wl.test.CheckInt(999,t.prop1)\n";
 
 	boost::shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
-	lua_State *L = L_ptr.get();
+	lua_State * const L = L_ptr.get();
 	InitLuaTests(L);
 	register_class<L_Class>(L, "test");
 
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script1, strlen(script1), "testscript1"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script1, strlen(script1), "testscript1"));
 	BOOST_REQUIRE_EQUAL(0, lua_pcall(L, 0, 1, 0));
 
 	
@@ -219,20 +206,21 @@ BOOST_AUTO_TEST_CASE(test_luna_property_ro)
 	InitLuaTests(L);
 	register_class<L_Class>(L, "test");
 
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script1, strlen(script1), "testscript1"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script1, strlen(script1), "testscript1"));
 	// Should get LUA runtime error instead of for example crashing
 	BOOST_REQUIRE_EQUAL(LUA_ERRRUN, lua_pcall(L, 0, 1, 0));
 }
 
 BOOST_AUTO_TEST_CASE(test_luna_inheritance)
 {
-	const char * script2 =
+	char const * const script2 =
 		"t = wl.test.SubClass()\n"
 		"wl.test.CheckInt(124, t:test())\n"
 		"wl.test.CheckInt(248, t.prop1)\n";
 
 	boost::shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
-	lua_State*L = L_ptr.get();
+	lua_State * const L = L_ptr.get();
 	InitLuaTests(L);
 
 	// single inheritance
@@ -240,19 +228,20 @@ BOOST_AUTO_TEST_CASE(test_luna_inheritance)
 	add_parent<L_SubClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
 	
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script2, strlen(script2), "testscript2"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script2, strlen(script2), "testscript2"));
 	BOOST_REQUIRE_EQUAL(0, lua_pcall(L, 0, 1, 0));
 
 }
 
 BOOST_AUTO_TEST_CASE(test_luna_virtualbase_method)
 {
-	const char * script3 =
+	char const * const script3 =
 		"t = wl.test.VirtualClass()\n"
 		"wl.test.CheckInt(124, t:test())\n";
 
 	boost::shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
-	lua_State*L = L_ptr.get();
+	lua_State * const L = L_ptr.get();
 	InitLuaTests(L);
 
 	// virtual base class 
@@ -260,18 +249,19 @@ BOOST_AUTO_TEST_CASE(test_luna_virtualbase_method)
 	add_parent<L_VirtualClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
 
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script3, strlen(script3), "testscript3"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script3, strlen(script3), "testscript3"));
 	BOOST_REQUIRE_EQUAL(0, lua_pcall(L, 0, 1, 0));
 }
 
 BOOST_AUTO_TEST_CASE(test_luna_virtualbase_property)
 {
-	const char * script4 =
+	char const * const script4 =
 		"t = wl.test.VirtualClass()\n"
 		"wl.test.CheckInt(248, t.prop1)\n";
 
 	boost::shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
-	lua_State*L = L_ptr.get();
+	lua_State * const L = L_ptr.get();
 	InitLuaTests(L);
 
 	// virtual base class 
@@ -279,7 +269,8 @@ BOOST_AUTO_TEST_CASE(test_luna_virtualbase_property)
 	add_parent<L_VirtualClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
 
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script4, strlen(script4), "testscript4"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script4, strlen(script4), "testscript4"));
 	BOOST_REQUIRE_EQUAL(0, lua_pcall(L, 0, 1, 0));
 }
 
@@ -299,45 +290,48 @@ BOOST_AUTO_TEST_CASE(test_luna_multibase_method)
 	add_parent<L_MultiClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
 
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script5, strlen(script5), "testscript5"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script5, strlen(script5), "testscript5"));
 	BOOST_REQUIRE_EQUAL(0, lua_pcall(L, 0, 1, 0));
 }
 
 BOOST_AUTO_TEST_CASE(test_luna_multibase_property_get)
 {
-	const char * script6 =
+	char const * const script6 =
 		"t = wl.test.MultiClass()\n"
 		"wl.test.CheckInt(248, t.prop1)\n"
 		"wl.test.CheckInt(2001, t.second)\n";
 
 	boost::shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
-	lua_State*L = L_ptr.get();
+	lua_State * const L = L_ptr.get();
 	InitLuaTests(L);
 
 	register_class<L_MultiClass>(L, "test", true);
 	add_parent<L_MultiClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
 
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script6, strlen(script6), "testscript6"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script6, strlen(script6), "testscript6"));
 	BOOST_REQUIRE_EQUAL(0, lua_pcall(L, 0, 1, 0));
 }
 
 BOOST_AUTO_TEST_CASE(test_luna_multibase_property_set)
 {
-	const char * script6 =
+	char const * const script6 =
 		"t = wl.test.MultiClass()\n"
 		"wl.test.CheckInt(248, t.prop1)\n"
 		"t.prop1 = 111\n"
 		"wl.test.CheckInt(111, t.prop1)\n";
 
 	boost::shared_ptr<lua_State> L_ptr(lua_open(),&lua_close);
-	lua_State*L = L_ptr.get();
+	lua_State * const L = L_ptr.get();
 	InitLuaTests(L);
 
 	register_class<L_MultiClass>(L, "test", true);
 	add_parent<L_MultiClass, L_Class>(L);
 	lua_pop(L, 1); // Pop the meta table
 
-	BOOST_REQUIRE_EQUAL(0, luaL_loadbuffer(L, script6, strlen(script6), "testscript6"));
+	BOOST_REQUIRE_EQUAL
+		(0, luaL_loadbuffer(L, script6, strlen(script6), "testscript6"));
 	BOOST_REQUIRE_EQUAL(0, lua_pcall(L, 0, 1, 0));
 }
