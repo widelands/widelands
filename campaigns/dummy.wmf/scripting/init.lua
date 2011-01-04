0642e25fa0ee373ab0ac74904e68ad21d66d8722
Nasenbaer <Nasenbaer@drehatlas.de>
2011-01-04 13:17:44 +0100

    Add dummy campaign map to show the players, that there are planned further maps, but they are not yet implemented.

---
+++ campaigns/dummy.wmf/scripting/init.lua
@@ -0,0 +1,17 @@
+-- =======================================================================
+--                           Dummy Mission Map                        
+-- =======================================================================
+set_textdomain("scenario_dummy.wmf")
+
+use("aux", "ui")
+
+p1 = wl.Game().players[1]
+dummy_msg = {
+   title=_ "Not yet implemented",
+   body=_ "Sorry, this map is not yet implemented.",
+   width=500,
+   height=450,
+   posy=1,
+}
+p1:message_box(dummy_msg.title, dummy_msg.body, dummy_msg)
+
