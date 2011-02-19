86bc8523728673adff44bfab4c1e31e2cf888919
Nicolai Hähnle <prefect_@gmx.net>
2011-02-20 00:37:18 +0100

    Remove cruft in FontHandler

--- src/graphic/font_handler.h
+++ src/graphic/font_handler.h
@@ -25,7 +25,6 @@
 #include "rgbcolor.h"
 #include "graphic.h"
 #include "picture_id.h"
-#include "ui_basic/widget_cache.h"
 
 #include <SDL_ttf.h>
 
@@ -38,12 +37,11 @@
 struct RenderTarget;
 
 namespace UI {
-struct Text_Block;
+
 struct TextStyle;
 
-/** class Font_Handler
- *
- * This class generates font Pictures out of strings and returns them
+/**
+ * Main class for string rendering. Manages the cache of pre-rendered strings.
  */
 struct Font_Handler {
 	Font_Handler();
@@ -76,100 +74,20 @@ struct Font_Handler {
 		 const std::string & text,
 		 uint32_t & w, uint32_t & h,
 		 uint32_t wrap = std::numeric_limits<uint32_t>::max());
-	int32_t calc_linewidth(TTF_Font &, const std::string & text);
 	uint32_t get_fontheight(std::string const & name, int32_t size);
-	std::string word_wrap_text
-		(TTF_Font &,
-		 const std::string & unwrapped_text,
-		 int32_t max_width);
-	std::string word_wrap_text
-		(std::string const & font, int32_t size,
-		 std::string const & unwrapped_text, int32_t max_width);
 	void do_align
 		(Align, int32_t & dstx, int32_t & dsty, int32_t w, int32_t h);
 	// This deletes all cached pictures, it is called
 	// from the graphics code before the graphics are flushed,
 	// to make sure that everything is forgotten
 	void flush_cache();
-	void draw_richtext
-		(RenderTarget &,
-		 RGBColor bg,
-		 Point dstpoint,
-		 std::string text,
-		 int32_t wrap,
-		 Widget_Cache widget_cache = Widget_Cache_None,
-		 PictureID * widget_cache_id = 0,
-		 bool transparent = true);
-	void get_size_from_cache
-		(PictureID widget_cache_id, uint32_t & w, uint32_t & h);
 
 private:
-	struct _Cache_Infos {
-		PictureID picture_id;
-		std::string str;
-		const TTF_Font * f;
-		RGBColor fg;
-		RGBColor bg;
-		int32_t caret;
-		uint32_t      w;
-		uint32_t      h;
-
-		bool operator== (_Cache_Infos const & who) const {
-			return
-				str == who.str &&
-				f   == who.f   &&
-				fg  == who.fg  &&
-				bg  == who.bg &&
-				caret == who.caret;
-		}
-	};
-
-private:
-	static const uint32_t CACHE_ARRAY_SIZE = 500;
-
-	std::list<_Cache_Infos> m_cache;
-
 	struct Data;
 	boost::scoped_ptr<Data> d;
 
 private:
-	PictureID convert_sdl_surface
-		(SDL_Surface &, const RGBColor bg, bool transparent = false);
-	SDL_Surface * draw_string_sdl_surface
-		(std::string const & fontname, int32_t fontsize,
-		 RGBColor fg, RGBColor bg,
-		 const std::string & text,
-		 Align,
-		 uint32_t            wrap        = std::numeric_limits<uint32_t>::max(),
-		 int32_t style = TTF_STYLE_NORMAL,
-		 uint32_t            linespacing = 0);
-	SDL_Surface * create_sdl_text_surface
-		(TTF_Font &, RGBColor fg, RGBColor bg,
-		 std::string const & text,
-		 Align,
-		 uint32_t            wrap        = std::numeric_limits<uint32_t>::max(),
-		 uint32_t            linespacing = 0);
-	SDL_Surface * create_static_long_text_surface
-		(TTF_Font &, RGBColor fg, RGBColor bg,
-		 std::string const & text,
-		 Align,
-		 uint32_t            wrap        = std::numeric_limits<uint32_t>::max(),
-		 uint32_t            linespacing = 0);
-	SDL_Surface * create_single_line_text_surface
-		(TTF_Font &, RGBColor fg, RGBColor bg,
-		 std::string text, Align);
-	SDL_Surface * create_empty_sdl_surface(uint32_t w, uint32_t h);
-	SDL_Surface * join_sdl_surfaces
-		(uint32_t w, uint32_t h,
-		 const std::vector<SDL_Surface *> & surfaces,
-		 RGBColor bg,
-		 Align align        = Align_Left,
-		 int32_t spacing        = 0,
-		 bool vertical      = false,
-		 bool keep_surfaces = false);
-	SDL_Surface * load_image(std::string file);
-	SDL_Surface * render_space
-		(Text_Block &, RGBColor bg, int32_t style = TTF_STYLE_NORMAL);
+	//SDL_Surface * load_image(std::string file); // TODO deprecate
 
 	void draw_caret
 		(RenderTarget &,
