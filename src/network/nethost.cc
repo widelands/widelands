055e5ae01d33e1dc6ea7be46f3cae3b816396f08
Nicolai Hähnle <prefect_@gmx.net>
2011-02-24 18:58:35 +0100

    Sanitize richtext in chat messages on the client side
    The previous server-side approach was at least partial snake oil, because
    a malicious game host could easily remove those checks.
    
    The current checks still do not eliminate richtext entirely from system
    messages, because formatting is used in the context of the meta server and
    dedicated server, but it should mitigate the scope of potentially accessible
    bugs (not that there are any known problematic bugs in the richtext renderer
    in the first place, but better safe than sorry).

--- src/network/nethost.cc
+++ src/network/nethost.cc
@@ -856,15 +856,6 @@ void NetHost::send(ChatMessage msg)
 	if (msg.msg.empty())
 		return;
 
-	// Make sure that msg is free of richtext formation tags. Such tags could not
-	// just be abused by the user, but could also break the whole text formation.
-	//  FIXME It would be better to escape < as &lt; and then render that as <
-	//  FIXME instead of replacing < with braces in chat messages.
-	if (msg.playern != -2) // System messages may have special formations
-		container_iterate(std::string, msg.msg, i)
-			if (*i.current == '<')
-				*i.current = '{';
-
 	if (msg.recipient.empty()) {
 		SendPacket s;
 		s.Unsigned8(NETCMD_CHAT);
