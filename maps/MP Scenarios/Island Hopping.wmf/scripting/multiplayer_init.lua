bcdd91117f1ec86527ff59910f25ac922bd341b8
Holger Rapp <SirVer@gmx.de>
2011-03-13 00:05:24 +0100

    Some more TODOs

--- maps/MP Scenarios/Island Hopping.wmf/scripting/multiplayer_init.lua
+++ maps/MP Scenarios/Island Hopping.wmf/scripting/multiplayer_init.lua
@@ -12,6 +12,9 @@ use("aux", "set")
 -- ==========
 -- TODO: parse the scripts and create a new catalog for it
 -- set_textdomain("scenario_atl01.wmf")
+-- TODO: transfer of horses does not work, because of missing scripting of roads?
+-- TODO: bug report: position of players in multiplayer scenarios is fixed to blue or red
+-- TODO: endless game is always starting, also in scenarios
 game = wl.Game()
 map = game.map
 
