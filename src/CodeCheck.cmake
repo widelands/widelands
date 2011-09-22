42df49d8a74c3899cd21c46f54a69fbadab6d268
Shevonar <>
2011-09-23 01:29:00 +0200

    CodeCheck fix (for MinGW/MSYS)

--- src/CodeCheck.cmake
+++ src/CodeCheck.cmake
@@ -1,5 +1,5 @@
 execute_process(
-  COMMAND "${WL_SOURCE_CHECKER}" -c "${sourcefile}"
+  COMMAND "${PYTHON_EXECUTABLE}" "${WL_SOURCE_CHECKER}" -c "${sourcefile}"
   OUTPUT_VARIABLE checkresult
 #  OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/codecheck-stamps/${stamp}
   OUTPUT_STRIP_TRAILING_WHITESPACE
