fdf1df0c638232dffa7ba5793313d624dbc41a6c
Shevonar <>
2011-09-23 10:49:44 +0200

    fix for WIN32 only

--- src/CodeCheck.cmake
+++ src/CodeCheck.cmake
@@ -1,9 +1,18 @@
-execute_process(
-  COMMAND "${PYTHON_EXECUTABLE}" "${WL_SOURCE_CHECKER}" -c "${sourcefile}"
-  OUTPUT_VARIABLE checkresult
-#  OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/codecheck-stamps/${stamp}
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if (WIN32)
+  execute_process(
+    COMMAND "${PYTHON_EXECUTABLE}" "${WL_SOURCE_CHECKER}" -c "${sourcefile}"
+    OUTPUT_VARIABLE checkresult
+#   OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/codecheck-stamps/${stamp}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+else (WIN32)
+  execute_process(
+    COMMAND "${WL_SOURCE_CHECKER}" -c "${sourcefile}"
+    OUTPUT_VARIABLE checkresult
+#   OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/codecheck-stamps/${stamp}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+endif (WIN32)
 
 if (checkresult STREQUAL "")
   execute_process(
