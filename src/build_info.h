409c8726fe667b6d29f4fd0e70a77b0fddaaf82e
Shevonar <infomh@anmaruco.de>
2012-02-21 21:56:52 +0100

    updated copyright in build_info.h

--- src/build_info.h
+++ src/build_info.h
@@ -1,11 +1,11 @@
 #ifndef BUILD_ID_H
 #define BUILD_ID_H
-#define WLCR "(C) 2002-2011 "
+#define WLCR "(C) 2002-2012 "
 
 #include <string>
 
 ///\return the build id which is automagically created from the checkout's
-//revision number or the VERSION file
+///revision number or the VERSION file
 const std::string & build_id();
 
 ///\return the build type, which is set during compile time (either manually
