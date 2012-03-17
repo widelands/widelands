cb92bbfcab24b83c3b7102f20b310fa75addf453
Venatrix <elisabeth.jaeger@gmail.com>
2012-03-17 23:22:38 +0100

    Removed the orange text in the missing houses, too.

--- tribes/barbarians/citadel/help.lua
+++ tribes/barbarians/citadel/help.lua
@@ -4,6 +4,6 @@ use("aux", "format_help")
 set_textdomain("tribe_barbarians")
 
 return {
-	text = rt(p("font-weight=bold font-size=14 font-color=ff7700",_"Please note that the in-game help is incomplete and under active development.")) ..
+	text =
 		rt("image=tribes/barbarians/constructionsite/idle_with_worker_00.png",p(_"The help text of this building is under construction."))
 }
