76206795dcab41e467c22988dbf444fb6fa101ef
Nasenbaer <Nasenbaer@drehatlas.de>
2013-03-10 14:36:47 +0100

    Revert part of revision 6515 - the change checked if a port buildspace is valid before saving it - this sounds like a good idea for the editor, but removes port buildspaces in a running game if e.g. a tree is growing nearby or a building is constructed on the field or nearby

--- src/map_io/widelands_map_port_spaces_data_packet.cc
+++ src/map_io/widelands_map_port_spaces_data_packet.cc
@@ -70,28 +70,21 @@ void Map_Port_Spaces_Data_Packet::Write(FileSystem & fs, Editor_Game_Base & egba
 	throw (_wexception)
 {
 	Profile prof;
-	prof.create_section("global").set_int("packet_version", CURRENT_PACKET_VERSION);
+	Section & s1 = prof.create_section("global");
+	s1.set_int("packet_version", CURRENT_PACKET_VERSION);
 
 	Map & map = egbase.map();
 	std::vector<Coords> port_spaces = map.get_port_spaces();
-	uint32_t count = 0;
+	const uint16_t num = port_spaces.size();
+	char buf[8]; // there won't be that many port spaces... definitely!
+	s1.set_int("number_of_port_spaces", num);
+
 	Section & s2 = prof.create_section("port_spaces");
-	for (uint32_t i = 0; i < port_spaces.size(); ++i) {
-		const FCoords f = map.get_fcoords(port_spaces[i]);
-		// Perform some implicit cleanup while saving
-		if (f.field->get_caps() & BUILDCAPS_PORT) {
-			char buf[8];
-			snprintf(buf, sizeof(buf), "%u", count);
-			s2.set_Coords(buf, f);
-			++count;
-		}
+	for (uint16_t i = 0; i < num; ++i) {
+		snprintf(buf, sizeof(buf), "%u", i);
+		s2.set_Coords(buf, port_spaces.at(i));
 	}
-
-	// Note that we do not keep a pointer to the global section around
-	// because it may have become invalid by creating the "port_spaces" section
-	prof.get_section("global")->set_int("number_of_port_spaces", count);
-
 	prof.write("port_spaces", false, fs);
 }
 
-} // namespace Widelands
+}
