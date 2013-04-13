879bd66addd5fab83cbdb6b389aca144fc83c544
Nicolai Hähnle <nhaehnle@gmail.com>
2013-04-13 16:21:27 +0200

    Hack on alternative spritemap generation (via rectangle coverings)

--- utils/make_spritemap.py
+++ utils/make_spritemap.py
@@ -3,14 +3,18 @@
 
 from glob import glob
 from itertools import chain, combinations, permutations
+import collections
 import re
 import argparse
+import math
 import os
 import sys
 
 import Image
 import numpy as np
 from scipy import ndimage
+
+OriginalFrame = collections.namedtuple('OriginalFrame', ('pic', 'pc_pic', 'anim', 'idx'))
 
 # Set partition: http://code.activestate.com/recipes/576795/
 def set_partition(iterable, chain=chain, map=map):
@@ -194,14 +198,13 @@ class Packer(object):
                 offsets[id] = (x, y)
         return offsets, img, pc_img
 
-# TODO: Merge in crop_animations.py script. It does not matter so much anymore though.
 def load_animations(anims):
     rv = []
     rv_pc = []
 
     def _load(fn, seen_shape):
         img = np.asarray(Image.open(fn))
-        if seen_shape and seen_shape != rv[0][0].shape:
+        if seen_shape and seen_shape != img.shape:
             print "This file has different dimensions than the others before. Terminating."
             sys.exit(-1)
         return img
@@ -209,30 +212,34 @@ def load_animations(anims):
     for anim in anims:
         seen_shape = None
         for idx, fn in enumerate(sorted(glob(anim + "??.png"))):
-            rv.append((_load(fn, seen_shape), anim, idx))
+            pic = _load(fn, seen_shape)
             if seen_shape is None:
-                seen_shape = rv[0][0].shape
+                seen_shape = pic.shape
             pc_fn = os.path.splitext(fn)[0] + "_pc.png"
             if os.path.exists(pc_fn):
-                rv_pc.append(_load(pc_fn, seen_shape))
+                pc_pic = _load(pc_fn, seen_shape)
+            else:
+                pc_pic = None
+
+            rv.append(OriginalFrame(pic, pc_pic, anim, idx))
 
     # Crop the Images.
-    line_x_all_alpha = lambda x,idx: (rv[idx][0][x,:,-1] == 0).all()
-    col_x_all_alpha = lambda x,idx: (rv[idx][0][:,x,-1] == 0).all()
-    while all(line_x_all_alpha(0,i) for i in range(len(rv))):
-        rv = [(img[1:], anim, idx) for img, anim, idx in rv]
-        rv_pc = [img[1:] for img in rv_pc]
-    while all(line_x_all_alpha(-1,i) for i in range(len(rv))):
-        rv = [(img[:-1], anim, idx) for img, anim, idx in rv]
-        rv_pc = [img[:-1] for img in rv_pc]
-    while all(col_x_all_alpha(0,i) for i in range(len(rv))):
-        rv = [(img[:,1:], anim, idx) for img, anim, idx in rv]
-        rv_pc = [img[:,1:] for img in rv_pc]
-    while all(col_x_all_alpha(-1,i) for i in range(len(rv))):
-        rv = [(img[:,:-1], anim, idx) for img, anim, idx in rv]
-        rv_pc = [img[:,:-1] for img in rv_pc]
+    #line_x_all_alpha = lambda x,idx: (rv[idx][0][x,:,-1] == 0).all()
+    #col_x_all_alpha = lambda x,idx: (rv[idx][0][:,x,-1] == 0).all()
+    #while all(line_x_all_alpha(0,i) for i in range(len(rv))):
+        #rv = [(img[1:], anim, idx) for img, anim, idx in rv]
+        #rv_pc = [img[1:] for img in rv_pc]
+    #while all(line_x_all_alpha(-1,i) for i in range(len(rv))):
+        #rv = [(img[:-1], anim, idx) for img, anim, idx in rv]
+        #rv_pc = [img[:-1] for img in rv_pc]
+    #while all(col_x_all_alpha(0,i) for i in range(len(rv))):
+        #rv = [(img[:,1:], anim, idx) for img, anim, idx in rv]
+        #rv_pc = [img[:,1:] for img in rv_pc]
+    #while all(col_x_all_alpha(-1,i) for i in range(len(rv))):
+        #rv = [(img[:,:-1], anim, idx) for img, anim, idx in rv]
+        #rv_pc = [img[:,:-1] for img in rv_pc]
 
-    return rv, rv_pc
+    return rv
 
 def merge_overlapping(regions):
     for i in range(len(regions)):
@@ -309,23 +316,265 @@ def prepare_animations(anims):
             pics_to_fit.append(ImageWrapper(subimg, pc_subimg, r, id))
     return pics_to_fit, regions, base_pic.shape[1], base_pic.shape[0]
 
+# Consider an additional fragment/rectangle to be beneficial if it saves this many pixels in image data
+FRAGMENT_COST = 32
 
-def pack_animations(anim_sets):
-    pics_to_fit = []
-    dimensions = {}
-    regions = {}
-    for anims in anim_sets:
-        new_pics_to_fit, anim_regions, w, h = prepare_animations(anims)
-        pics_to_fit.extend(new_pics_to_fit)
-        for anim in anims:
-            dimensions[anim] = (w, h)
-            regions[anim] = anim_regions
+def average_cost_lower_bounds(tiles, tileshape):
+    cumulative = tiles.cumsum(0).cumsum(1)
+    print cumulative
 
-    pics_to_fit.sort(reverse=True)
-    p = Packer()
-    p.fit(pics_to_fit)
-    offsets_by_id, result_img, pc_result_img = p.get_result()
-    return regions, dimensions, offsets_by_id, result_img, pc_result_img
+    indices = np.indices(tiles.shape * 2)
+    print indices
+
+
+def macr_exact_bruteforce(bitmask, lower_range=None, upper_range=None, FRAGMENT_COST=FRAGMENT_COST):
+    """
+    Compute a Minimum Average Cost Rectangle among all rectangles with lower left corner in lower_range
+    and upper right corner in upper_range, by computing the cost of all possible rectangles.
+
+    Returns (cost, rectangle)
+
+    Note: Returns a rectangle with cost strictly greater than FRAGMENT_COST + 1 if bitmask contains
+    no set pixels.
+    """
+    lower_range = lower_range or ((0, 0), bitmask.shape)
+    upper_range = upper_range or ((0, 0), bitmask.shape)
+    lower_ext = tuple(np.subtract(lower_range[1], lower_range[0]))
+    upper_ext = tuple(np.subtract(upper_range[1], upper_range[0]))
+    cum0 = bitmask.cumsum(0)
+    cum1 = bitmask.cumsum(1)
+    cum01 = cum0.cumsum(1)
+
+    tr = cum01[upper_range[0][0]:upper_range[1][0], upper_range[0][1]:upper_range[1][1]]
+    tr = np.tile(tr.reshape((1,1) + upper_ext), lower_ext + (1,1))
+    tl = (cum01 - cum0)[upper_range[0][0]:upper_range[1][0], lower_range[0][1]:lower_range[1][1]]
+    tl = np.tile(tl.transpose().reshape((1, lower_ext[1], upper_ext[0], 1)), (lower_ext[0], 1, 1, upper_ext[1]))
+    br = (cum01 - cum1)[lower_range[0][0]:lower_range[1][0], upper_range[0][1]:upper_range[1][1]]
+    br = np.tile(br.reshape((lower_ext[0], 1, 1, upper_ext[1])), (1, lower_ext[1], upper_ext[0], 1))
+    bl = (cum01 - cum0 - cum1 + bitmask)[lower_range[0][0]:lower_range[1][0], lower_range[0][1]:lower_range[1][1]]
+    bl = np.tile(bl.reshape(lower_ext + (1,1)), (1,1) + upper_ext)
+
+    indices = np.indices(lower_ext + upper_ext)
+    covered = (tr - tl - br + bl) * ((indices[2] >= indices[0]) & (indices[3] >= indices[1]))
+
+    cost = (
+        ((upper_range[0][0] - lower_range[0][0]) + indices[2] - indices[0] + 1) *
+        ((upper_range[0][1] - lower_range[0][1]) + indices[3] - indices[1] + 1)
+    )
+    cost = FRAGMENT_COST + np.fmax(cost, 1)
+    #print cost
+
+    avg_cost = cost / np.fmax(covered, 0.1)
+    #print avg_cost
+    argmin_local = np.unravel_index(np.argmin(avg_cost), avg_cost.shape)
+    argmin = (
+        argmin_local[0] + lower_range[0][0],
+        argmin_local[1] + lower_range[0][1],
+        argmin_local[2] + upper_range[0][0] + 1,
+        argmin_local[3] + upper_range[0][1] + 1
+    )
+    return avg_cost[argmin_local], argmin
+
+def minimum_average_cost_rectangle(bitmask, FRAGMENT_COST=FRAGMENT_COST):
+    """
+    Compute a rectangle that minimizes (FRAGMENT_COST + Area) / (Pixels Covered in bitmask)
+
+    Returns (cost, rectangle)
+    """
+    return macr_exact_bruteforce(bitmask, FRAGMENT_COST=FRAGMENT_COST)
+
+    # Setup tiling hierarchy
+    MAX_TILES = 16
+    AVG_COST_INF = FRAGMENT_COST + 2
+
+    tileshapes = []
+    remaindershape = bitmask.shape
+    while remaindershape[0] > MAX_TILES or remaindershape[1] > MAX_TILES:
+        tiles = tuple([
+            int(math.ceil(rs ** (1.0 / int(math.ceil(math.log(rs, MAX_TILES))))))
+            for rs in remaindershape
+        ])
+        tileshapes.append(tiles)
+        remaindershape = tuple([
+            (rs + ts - 1) / ts
+            for ts, rs in zip(tiles, remaindershape)
+        ])
+    tileshapes.append(remaindershape)
+    shape = tuple(np.prod(tileshapes, 0))
+    print 'original:', bitmask.shape, 'shapes:', tileshapes, 'total shape:', shape
+    if shape != bitmask.shape:
+        extend = np.zeros(shape)
+        extend[:bitmask.shape[0], :bitmask.shape[1]] = bitmask
+        bitmask = extend
+
+def minimum_average_cost_grow(bitmask, rectangle):
+    """
+    Find the best way to grow the given rectangle in one direction only,
+    in terms of (Increased Area) / (Additionally Covered Pixels in bitmask).
+
+    Returns (cost, rectangle), where cost is None if no additional pixels can be covered.
+    """
+    best_cost = None
+    best_rectangle = rectangle
+    height = rectangle[2] - rectangle[0]
+    width = rectangle[3] - rectangle[1]
+
+    # grow positive in axis 1
+    additional = bitmask[rectangle[0]:rectangle[2], rectangle[3]:].sum(0).cumsum()
+    if additional.shape[0]:
+        cost = np.arange(height, (additional.shape[0] + 1) * height, height, np.float)
+        inv = additional / cost # avoid division by zero
+        best = inv.argmax()
+        if inv[best] > 0:
+            best_cost = 1.0 / inv[best]
+            best_rectangle = (rectangle[0], rectangle[1], rectangle[2], rectangle[3] + 1 + best)
+
+    # grow negative in axis 1
+    additional = bitmask[rectangle[0]:rectangle[2], :rectangle[1]].sum(0)[::-1].cumsum()
+    if additional.shape[0]:
+        cost = np.arange(height, (additional.shape[0] + 1) * height, height, np.float)
+        inv = additional / cost # avoid division by zero
+        best = inv.argmax()
+        if inv[best] > 0 and (best_cost is None or best_cost > 1.0 / inv[best]):
+            best_cost = 1.0 / inv[best]
+            best_rectangle = (rectangle[0], rectangle[1] - 1 - best, rectangle[2], rectangle[3])
+
+    # grow positive in axis 0
+    additional = bitmask[rectangle[2]:, rectangle[1]:rectangle[3]].sum(1).cumsum()
+    if additional.shape[0]:
+        cost = np.arange(width, (additional.shape[0] + 1) * width, width, np.float)
+        inv = additional / cost # avoid division by zero
+        best = inv.argmax()
+        if inv[best] > 0 and (best_cost is None or best_cost > 1.0 / inv[best]):
+            best_cost = 1.0 / inv[best]
+            best_rectangle = (rectangle[0], rectangle[1], rectangle[2] + 1 + best, rectangle[3])
+
+    # grow negative in axis 1
+    additional = bitmask[:rectangle[0], rectangle[1]:rectangle[3]].sum(1)[::-1].cumsum()
+    if additional.shape[0]:
+        cost = np.arange(width, (additional.shape[0] + 1) * width, width, np.float)
+        inv = additional / cost # avoid division by zero
+        best = inv.argmax()
+        if inv[best] > 0 and (best_cost is None or best_cost > 1.0 / inv[best]):
+            best_cost = 1.0 / inv[best]
+            best_rectangle = (rectangle[0] - 1 - best, rectangle[1], rectangle[2], rectangle[3])
+
+    return (best_cost, best_rectangle)
+
+def draw_bitmask(bitmask):
+    for y in range(bitmask.shape[0]):
+        print ''.join([(' ', '*')[cell] for cell in bitmask[y]])
+
+def bitmask_from_rectangle(shape, rectangle):
+    idx = np.indices(shape)
+    return np.logical_and(
+        np.logical_and(idx[0] >= rectangle[0], idx[0] < rectangle[2]),
+        np.logical_and(idx[1] >= rectangle[1], idx[1] < rectangle[3])
+    )
+
+def draw_rectangles_over_bitmask(bitmask, rectangles):
+    coverage = np.max(
+        [np.where(bitmask_from_rectangle(bitmask.shape, rect), num+1, 0) for num, rect in enumerate(rectangles)],
+        axis=0
+    )
+    for bmrow, crow in zip(bitmask, coverage):
+        print ''.join([str(c) if c > 0 else '*' if b else ' ' for b, c in zip(bmrow, crow)])
+
+def rectangle_cost(rectangle, FRAGMENT_COST=FRAGMENT_COST):
+    print 'cost of rectangle', rectangle, 'at', FRAGMENT_COST, 'is',
+    cost = FRAGMENT_COST + (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1])
+    print cost
+    return cost
+
+def compute_rectangle_covering(bitmask, FRAGMENT_COST=FRAGMENT_COST):
+    """
+    Given a bitmask of pixels, find a list of rectangles that covers all pixels of the mask that are set to true,
+    with a goal of minimizing Total Area of Covering Rectangles + FRAGMENT_COST * Number of Covering Rectangles.
+
+    Returns (cost, list of rectangles)
+    """
+    rectangles = []
+    indices = np.indices(bitmask.shape)
+    remainder = bitmask
+    while np.count_nonzero(remainder) != 0:
+        new_rectangle_score, new_rectangle = minimum_average_cost_rectangle(
+            remainder,
+            FRAGMENT_COST=FRAGMENT_COST
+        )
+        best_score = new_rectangle_score
+        best_rectangles = rectangles + [new_rectangle]
+
+        for idx, rect in enumerate(rectangles):
+            grow_score, grow_rect = minimum_average_cost_grow(remainder, rect)
+            if grow_score <= best_score:
+                best_score = grow_score
+                best_rectangles = rectangles[:idx] + rectangles[idx + 1:] + [grow_rect]
+
+        print 'best score', best_score
+        print 'rectangles:', best_rectangles
+
+        rectangles = best_rectangles
+        rectangles.append(new_rectangle)
+        remainder = np.logical_and(
+            remainder,
+            np.logical_or(
+                np.logical_or(indices[0] < new_rectangle[0], indices[0] >= new_rectangle[2]),
+                np.logical_or(indices[1] < new_rectangle[1], indices[1] >= new_rectangle[3])
+            )
+        )
+    return sum([rectangle_cost(rect, FRAGMENT_COST=FRAGMENT_COST) for rect in rectangles]), rectangles
+
+def build_frame_group(frames):
+    """
+    Given a list of frames, find a combination of shared base picture plus individual deltas
+    """
+    shape = frames[0].pic.shape
+    all_opaque_mask = np.all([frame.pic[:,:,3] == 255 for frame in frames], 0)
+    base_pic_mask = frames[0].pic[:,:,3] != 0
+    for frame in frames[1:]:
+        base_pic_mask &= np.all(frame.pic == frames[0].pic, -1) | all_opaque_mask
+    p = np.argwhere(base_pic_mask)
+    base_pic_min = np.min(p, 0)
+    base_pic_max = np.max(p, 0)
+    base_pic_pixels = (base_pic_max[0] - base_pic_min[0] + 1) * (base_pic_max[1] - base_pic_min[0] + 1)
+    base_pic = np.where(np.reshape(base_pic_mask, (shape[0], shape[1], 1)), frames[0].pic, 0)
+
+    for frame in frames:
+        delta_mask = (frame.pic[:,:,3] != 0) & np.any(frame.pic != base_pic, -1)
+        #cost, covering = compute_rectangle_covering(delta_mask)
+        print delta_mask
+        print 'number pix', np.count_nonzero(delta_mask)
+    return delta_mask
+
+def pack_animations(anims):
+    frames = load_animations(anims)
+    shape = frames[0].pic.shape
+
+    pixelcounts = [np.count_nonzero(frame.pic[:,:,3]) for frame in frames]
+    commoncounts = [[np.count_nonzero(np.all(a.pic == b.pic, -1) & (a.pic[:,:,3] != 0)) for a in frames] for b in frames]
+    commonfrac = [
+        [math.sqrt(float(cc)**2 / (float(pca) * float(pcb))) for cc, pca in zip(row, pixelcounts)]
+        for row, pcb in zip(commoncounts, pixelcounts)
+    ]
+    threshold = 0.8
+    for row in commonfrac:
+        print ''.join([[' ', '*'][int(frac >= threshold)] for frac in row])
+
+    #pics_to_fit = []
+    #dimensions = {}
+    #regions = {}
+    #for anims in anim_sets:
+        #new_pics_to_fit, anim_regions, w, h = prepare_animations(anims)
+        #pics_to_fit.extend(new_pics_to_fit)
+        #for anim in anims:
+            #dimensions[anim] = (w, h)
+            #regions[anim] = anim_regions
+
+    #pics_to_fit.sort(reverse=True)
+    #p = Packer()
+    #p.fit(pics_to_fit)
+    #offsets_by_id, result_img, pc_result_img = p.get_result()
+    #return regions, dimensions, offsets_by_id, result_img, pc_result_img
 
 def output_results(anim, img_name, dimensions, regions, offsets_by_id, args):
     with open("conf", "a") as f:
@@ -391,6 +640,8 @@ def main():
 
     best = 10000**2
     best_result = None
+    pack_animations(args.anim)
+    sys.exit()
     for anim_sets in set_partitions_all_permutation(args.anim):
         print "Trying: ", anim_sets
         regions, dimensions, offsets_by_id, result_img, pc_result_img = pack_animations(anim_sets)
