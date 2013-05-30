f249d59efa1c4b72dc5ae075a949345d28e6b19e
Holger Rapp <SirVer@gmx.de>
2013-05-30 18:12:53 +0200

    Load maps with faulty allowed_worker_types_data_packet. Also change defaults for unknown or unmentioned workers: they are allowed by default (otherwise the shipwrighth is forbidden on all old maps).

--- src/logic/player.cc
+++ src/logic/player.cc
@@ -89,7 +89,7 @@ Player::Player
 	m_allow_retreat_change(false),
 	m_retreat_percentage  (50),
 	m_fields            (0),
-	m_allowed_worker_types  (tribe_descr.get_nrworkers  (), false),
+	m_allowed_worker_types  (tribe_descr.get_nrworkers  (), true),
 	m_allowed_building_types(tribe_descr.get_nrbuildings(), true),
 	m_ai(""),
 	m_current_produced_statistics(tribe_descr.get_nrwares    ()),
