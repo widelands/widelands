f9374a0ace6cc7156d2434e3ae5e45ceda3738d7
charlyghislain@gmail.com <>
2013-08-01 17:43:56 +0200

    Make Panel::die() method virtual, and use it to destroy windows

--- src/ui_basic/unique_window.cc
+++ src/ui_basic/unique_window.cc
@@ -42,7 +42,7 @@ void UniqueWindow::Registry::create() {
 */
 void UniqueWindow::Registry::destroy() {
 	if (window) {
-		delete window;
+		window->die();
 	}
 }
 
@@ -51,7 +51,7 @@ void UniqueWindow::Registry::destroy() {
 */
 void UniqueWindow::Registry::toggle() {
 	if (window) {
-		delete window;
+		window->die();
 	} else {
 		constr();
 	}
