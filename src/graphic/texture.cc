da5306a185fca4a751af08944665f0d018aa79cc
Holger Rapp <SirVer@gmx.de>
2013-09-23 22:30:35 +0200

    Fixed minimap rendering bug in SDL mode.

--- src/graphic/texture.cc
+++ src/graphic/texture.cc
@@ -48,6 +48,9 @@ Texture::Texture(const string& fnametmpl, uint32_t frametime, const SDL_PixelFor
 		is_32bit   (format.BytesPerPixel == 4),
 		m_was_animated(false)
 {
+	// TODO(sirver): There is no 16bit mode anymore. Kill is_32bit and replace through true.
+	assert(is_32bit);
+
 	// Load the images one by one
 	char fname[256];
 
@@ -218,6 +221,7 @@ Uint32 Texture::get_minimap_color(char shade) {
 		return m_mmap_color[128 + shade];
 
 	uint8_t clr = m_pixels[0]; // just use the top-left pixel
+
 	uint32_t table = static_cast<uint8_t>(shade);
 	return
 		is_32bit ?
