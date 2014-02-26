adf45b0b7e4e72a1fee9f87fe541c095c89cf80b
GunChleoc <fios@foramnagaidhlig.net>
2014-02-26 16:52:26 +0000

    Proofread English strings and improved formatting for map_plateau.wmf.pot

--- maps/Plateau.wmf/scripting/texts.lua
+++ maps/Plateau.wmf/scripting/texts.lua
@@ -2,6 +2,8 @@
 --                     TEXTS - No logic inside this file
 -- =======================================================================
 
+use("aux", "formatting")
+
 -- Removes whitespace at the beginning or the end and translates
 -- newlines into spaces.
 function reflow(s)
@@ -22,22 +24,22 @@ end
 
 obj_capture_ancient_castle = {
    name = "capture_ancient_castle",
-   title =_ "Capture the ancient castle",
-   body = _"* Defeat Lanissa and capture the ancient castle.",
+   title =_ "Capture the Ancient Castle",
+   body = [[• ]] .. _
+[[Defeat Lanissa and capture the ancient castle.]],
 }
 
 obj_defeat_erwyn = {
    name = "defeat_erwyn",
    title =_ "Defeat Erwyn",
-   body = _
-[[* Defeat Erwyn. He commands the strongest opposing soldiers left on this
-island.]],
+   body = [[• ]] .. _
+[[Defeat Erwyn. He commands the strongest opposing military force left on this island.]],
 }
 obj_defeat_jomo = {
    name = "defeat_jomo",
    title =_ "Defeat Jomo",
-   body = _
-      "* Defeat Jomo. He takes care about the soldiers food supply."
+   body = [[• ]] .. _
+[[Defeat Jomo. He will take care of the soldiers’ food supply.]]
    ,
 }
 
@@ -51,7 +53,7 @@ function _msg(title, text, ...)
    "<rt><p line-spacing=3 font-size=12>" .. reflow(text)
    objs = {...}
    if #objs > 0 then
-      s = s ..  "<br><br>" ..  _"-- NEW OBJECTIVES --" .. "<br>"
+      s = s .. "<br><br></p></rt>" .. rt(h2(_"New Objectives")) .. "<rt><p line-spacing=3 font-size=12>"
       local i = 1
       while i < #objs do
          s = s .. reflow(objs[i].body) .. "<br>"
@@ -70,12 +72,12 @@ briefing_1_the_forbidden_island = {
    width = 400, 
    height = 300, 
    posy = 1,
-   body = _msg( _"The forbidden island", _
-[[Finally! Our first step to victory was just done! Last night, we landed on
-the forbidden island and defeated the few guards that sentineled the north
-western part of the island. I am quite sure, that none of the other warlords
-has detected us so far, so we should keep quiet and build up our
-infrastructure. Soon we will be strong enough to raid their positions.]]),
+   body = _msg( _"The Forbidden Island", _(
+[[Finally! We have just taken our first step towards victory! ]] ..
+[[Last night, we landed on the forbidden island and defeated the few guards that were watching the ]] ..
+[[north-western part of the island. ]] ..
+[[I am quite sure that none of the other warlords has detected us so far, ]] ..
+[[so we should keep quiet and build up our infrastructure. Soon we will be strong enough to raid their positions.]])),
 }
 
 briefing_2_found_ancient_castle = { 
@@ -83,14 +85,13 @@ briefing_2_found_ancient_castle = {
    width = 400, 
    height = 300,
    posy = 1,
-   body = _msg(_"An ancient castle", _
-[[By the Gods! One of our scouts found a mighty castle at the middle of the old
-plateau. The castle must be quite old and seems to be build in a foreign
-style. It's quite obvious, that this is not barbarian craft.<br>Let's hope
-Lanissa – the warlord holding command over that castle – still has not observed
-us. Perhaps we have a chance to conquer that mighty building without a bigger
-fight!<br>However we really have to capture it. It will be our key for our
-reign over this island!]],
+   body = _msg(_"An Ancient Castle", _(
+[[By the Gods! One of our scouts has discovered a mighty castle at the center of the old plateau. ]] ..
+[[The castle must be quite old and seems to have been built in a foreign style. ]] ..
+[[It’s quite obvious that this is not barbarian craft.]]) .. "<br><br>" ..
+_([[Let’s hope that Lanissa – the warlord holding sway over that castle – has not discovered our movements yet. ]] ..
+[[Perhaps we have a chance of conquering that mighty building without a bigger fight! ]] ..
+[[However, it is essential that we capture it. It will be the key to our reign over this island!]]),
    obj_capture_ancient_castle),
 }
 
@@ -99,17 +100,16 @@ briefing_3_captured_ancient_castle = {
    width = 400,
    height = 300,
    posy = 1,
-   body = _msg(_"Ancient castle captured", _
-[[Wonderful! Our troops finally defeated Lanissa and her soldiers. The ancient
-castle is ours!<br> It is amazing, how far one can see from the highest tower
-of the castle. We can watch the whole island. So now I wonder, why Lanissa has
-not seen us and thus did not prepare. But well, some things will never come to
-the light of day. The only important things at the moment are the left opposing
-troops. We observed enemy's positions from Erwyn north and eastwards of the
-castle. He is known to be a strong warlord and surely commands the strongest
-warriors. In the south we caught sight of some guarded food infrastructures of Jomo -
-a younger warlord – we should take care about that infrastructures and cut of
-the food supply.]], obj_defeat_erwyn, obj_defeat_jomo),
+   body = _msg(_"Ancient Castle Captured", _(
+[[Wonderful! Our troops have finally defeated Lanissa and her soldiers. The ancient castle is ours!]]) .. "<br><br>" ..
+_([[It is amazing how far one can see from the highest tower of the castle. We can watch the whole island. ]] ..
+[[So now I wonder why Lanissa did not see us and thus did not prepare. ]] ..
+[[Be that as it may, some things will never come to the light of day. ]] ..
+[[The only important issue at the moment are the opposing troops still left. ]] ..
+[[We have discovered enemy positions held by Erwyn to the north and east of the castle. ]] ..
+[[He is known to be a strong warlord and surely commands the strongest warriors. ]] ..
+[[In the south, we caught sight of some food infrastructures guarded by Jomo – a younger warlord – ]] ..
+[[and we should take care of those infrastructures so we can take our cut of the food supply.]]), obj_defeat_erwyn, obj_defeat_jomo),
 }
 
 briefing_erwyn_defeated = {
@@ -117,9 +117,8 @@ briefing_erwyn_defeated = {
    width = 400,
    height = 300,
    posy = 1,
-   body = _msg( _"Erwyn defeated", _
-[[Great! Erwyn gave up, when he saw his last buildings burning down. One strong
-warlord less on this island!]]), 
+   body = _msg( _"Erwyn Defeated", _
+[[Great! Erwyn gave up when he saw his last buildings burning down. That’s one less strong warlord on this island!]]), 
 }
 
 briefing_jomo_defeated = {
@@ -127,9 +126,8 @@ briefing_jomo_defeated = {
    width = 400, 
    height = 300, 
    posy = 1, 
-   body= _msg( _"Jomo defeated", _
-[[Great! Jomo gave up, when he saw his last buildings burning down. One warlord
-less on this island!]]), 
+   body= _msg( _"Jomo Defeated", _
+[[Great! Jomo gave up when he saw his last buildings burning down. That’s one less warlord on this island!]]), 
 }
 
 last_briefing_victory = {
@@ -138,9 +136,8 @@ last_briefing_victory = {
    height=300,
    posy=1,
    body = _msg( _"Victory!", _
-[[Finally! The complete island is ours. Now we just have to defend it better than
-the warlords did.]] .. "<br><br>" .. 
-_"Congratulations! You mastered this scenario. You may play on if you like!"
+[[Finally! The island is completely ours. Now we just have to defend it better than the warlords did.]] .. "<br><br>" ..
+_"Congratulations! You have mastered this scenario. You may play on if you like!"
 ),
 }
 
