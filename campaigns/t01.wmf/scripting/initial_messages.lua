edba5b8155e7fa92c4d59b24764fdb146bd5a86b
GunChleoc <fios@foramnagaidhlig.net>
2014-02-26 20:52:29 +0000

    Proofread English strings in the Barbarian (tutorial) campaign and hid formatting characters from translators

--- campaigns/t01.wmf/scripting/initial_messages.lua
+++ campaigns/t01.wmf/scripting/initial_messages.lua
@@ -12,18 +12,21 @@ end
 
 function briefing_messages() 
 -- Briefing message
-title = _"The story begins"
+title = _"The Story Begins"
 msg = thron(_"Thron sighs...",
-_[[It's been months and we are still hiding where the forests are old and dark.<br> My warriors hunt at day and lie awake at night – listening to the sounds of the cruel slaughter echoing from afar amongst the ancient trees.]]) 
+_[[It’s been months, and we are still hiding where the forests are old and dark.]] .. "<br><br>" ..
+_[[My warriors hunt at day and lie awake at night – listening to the sounds of the cruel slaughter echoing from afar amongst the ancient trees.]]) 
 show_story_box(title, msg)
 
 msg = 
 thron(
-_[[We can see the raging flames that swallow Al'thunran from here, miles away.<br> The red lights flash in the darkness and dance to the rhythm of the wardrums that haunt me even in my nightmares.]])
+_[[We can see the raging flames that swallow Al’thunran from here, miles away.]] .. "<br><br>" ..
+_[[The red lights flash in the darkness and dance to the rhythm of the war drums that haunt me even in my nightmares.]])
 show_story_box(title, msg, al_thunran)
 
 msg = thron(
-_[[My father's bones rest peacefully in the grounds on which he once ended the senseless bloodspilling that has risen among us. It pains me that his peace only endured for one generation.<br> Boldreth, my loyal companion and friend is a source of peace and comfort to me in these dark times. He keeps my spirits high and those of my warriors awake, not to let greed or despair destroy the bonds between us as well.]])
+_[[My father’s bones rest peacefully in the ground on which he once ended the senseless spilling of blood that had arisen amongst us. It pains me that his peace only endured for one generation.]] .. "<br><br>" ..
+_[[Boldreth, my loyal companion and friend is a source of peace and comfort to me in these dark times. He keeps my spirits high and those of my warriors awake, preventing greed or despair from destroying the bonds between us as well.]])
 show_story_box(title, msg, grave)
 
 msg = thron(
