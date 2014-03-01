750903adab542908356c6bf8346e142dcbd60ca6
Holger Rapp <SirVer@gmx.de>
2014-03-01 13:24:11 +0100

    Started to sketch some ideas for configuration of a single world.

---
+++ worlds/world.lua
@@ -0,0 +1,58 @@
+
+
+wl.world.add_resource({
+   name = "coal",
+   desname = _ "Coal",
+   max_amount = 20,
+   editor_pictures = {
+      [5] = "world/pics/coal1.png",
+      [10] = "world/pics/coal2.png",
+      [15] = "world/pics/coal3.png",
+      [1000] = "world/pics/coal4.png",
+   }
+})
+
+
+
+-- [coal]
+-- name=_ Coal
+-- editor_pic=coal1.png 5
+-- editor_pic=coal2.png 10
+-- editor_pic=coal3.png 15
+-- editor_pic=coal4.png
+-- max_amount=20
+
+-- [gold]
+-- name=_ Gold
+-- editor_pic=gold1.png 5
+-- editor_pic=gold2.png 10
+-- editor_pic=gold3.png 15
+-- editor_pic=gold4.png
+-- max_amount=20
+
+-- [iron]
+-- name=_ Iron
+-- editor_pic=iron1.png 5
+-- editor_pic=iron2.png 10
+-- editor_pic=iron3.png 15
+-- editor_pic=iron4.png
+-- max_amount=20
+
+-- [granit]
+-- name=_ Granite
+-- editor_pic=granit1.png 5
+-- editor_pic=granit2.png 10
+-- editor_pic=granit3.png 15
+-- editor_pic=granit4.png
+-- max_amount=20
+
+-- [water]
+-- name=_ Water
+-- editor_pic=water_resource.png
+-- max_amount=50
+
+-- [fish]
+-- name=_ Fish
+-- detectable=false
+-- max_amount=20
+-- editor_pic=fish_resource.png
