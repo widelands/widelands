c01e3eb1ed57c20f62e99b47175d8224631f408a
GunChleoc <fios@foramnagaidhlig.net>
2014-03-06 19:41:44 +0000

    Replaced some placeholders with string literals in help files to avoid grammar problems.

--- tribes/barbarians/gamekeepers_hut/help.lua
+++ tribes/barbarians/gamekeepers_hut/help.lua
@@ -40,5 +40,5 @@ return {
 		text_line(_"Experience levels:","n/a") ..
 	--Production Section
 		rt(h2(_"Production")) ..
-		text_line(_"Performance:", _"The %1$s pauses %2$s before going to work again.":bformat(_"Gamekeeper","52.5s"))
+		text_line(_"Performance:", _"The gamekeeper pauses 52.5s before going to work again.")
 }
