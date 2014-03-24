052f432871bca9e58d13b87048b0cc61bb32b4bd
Holger Rapp <SirVer@gmx.de>
2014-03-24 21:22:24 +0100

    Fixed a few NOCOMMITs from hjd.

--- world/critters/brownbear/init.lua
+++ world/critters/brownbear/init.lua
@@ -3,9 +3,6 @@ dirname = path.dirname(__file__)
 animations = {
    idle = {
       pictures = path.glob(dirname, "brownbear_idle_se_*.png"),
-      -- NOCOM: I took the liberty of using the idle animation so that things placed in the editor are not completely static.
-      -- (For instance the trees are moving, and I do believe some animals do as well.
-      -- pictures = {dirname .. "/menu.png" },
       player_color_masks = {},
       hotspot = {21, 16},
       fps = 20
@@ -23,6 +20,3 @@ world:new_critter_type{
    },
    animations = animations,
 }
-
--- NOCOM Credits? #graphics: Toralf Bethke
-
--- world/immovables/manmade/snowman/init.lua
+++ world/immovables/manmade/snowman/init.lua
@@ -3,8 +3,7 @@ dirname = path.dirname(__file__)
 world:new_immovable_type{
    name = "snowman",
    descname = _ "Snowman",
-   -- NOCOM: I don't know if these are correct values for size, attributes and programs, but the editor refused to start without these fields.
-   size = "small",
+   size = "none",
    attributes = {},
    programs = {},
    animations = {
@@ -12,7 +11,7 @@ world:new_immovable_type{
          pictures = { dirname .. "/idle.png" },
          player_color_masks = {},
          hotspot = { 9, 24 },
-         fps = 1,
+         fps = 0,
       },
    }
 }
