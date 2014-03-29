dae3d3728057df6e26b34e363b2b2aaed4ebd411
Holger Rapp <SirVer@gmx.de>
2014-03-29 15:19:58 +0100

    Converted wisent.

---
+++ world/critters/wisent/init.lua
@@ -0,0 +1,22 @@
+dirname = path.dirname(__file__)
+
+animations = {
+   idle = {
+      pictures = path.glob(dirname, "wisent_idle_\\d+.png"),
+      player_color_masks = {},
+      hotspot = { 14, 27 },
+      fps = 20,
+   },
+}
+add_walking_animations(animations, dirname, "wisent_walk", {24, 32}, 20)
+
+world:new_critter_type{
+   name = "wisent",
+   descname = _ "Wisent",
+   swimming = false,
+   attributes = { "eatable" },
+   programs = {
+      remove = { "remove" },
+   },
+   animations = animations,
+}
