0d88407c37711971e611ddf47bf75f9732a9631e
Holger Rapp <SirVer@gmx.de>
2014-03-29 13:42:49 +0100

    Converted reindeer.

---
+++ world/critters/reindeer/init.lua
@@ -0,0 +1,22 @@
+dirname = path.dirname(__file__)
+
+animations = {
+   idle = {
+      pictures = path.glob(dirname, "reindeer_idle_\\d+.png"),
+      player_color_masks = {},
+      hotspot = { 23, 21 },
+      fps = 20,
+   },
+}
+add_walking_animations(animations, dirname, "reindeer_walk", {25, 30}, 20)
+
+world:new_critter_type{
+   name = "reindeer",
+   descname = _ "Reindeer",
+   swimming = false,
+   attributes = { "eatable" },
+   programs = {
+      remove = { "remove" },
+   },
+   animations = animations,
+}
