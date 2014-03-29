08cb844358459003fc132222d9a87bcf958453c3
Holger Rapp <SirVer@gmx.de>
2014-03-29 14:07:27 +0100

    gfps -> fps.

--- world/init.lua
+++ world/init.lua
@@ -30,13 +30,13 @@ include "world/immovables/trees/aspen_summer_old/init.lua"
 -- Adds 6 animations for each walking direction into 'table'. The pictures are
 -- searched for in 'dirname'. All files should look like this
 -- 'basename'_(e|ne|se|sw|w|nw)_\d+.png. 'hotspot' is the hotspot for blitting.
-function add_walking_animations(table, dirname, basename, hotspot, gfps)
+function add_walking_animations(table, dirname, basename, hotspot, fps)
    for idx, dir in ipairs{ "ne", "e", "se", "sw", "w", "nw" } do
       table["walk_" .. dir] = {
          pictures = path.glob(dirname, basename .. "_" .. dir .. "_\\d+.png"),
          player_color_masks = {},
          hotspot = hotspot,
-         fps = gfps,
+         fps = fps,
       }
    end
 end
