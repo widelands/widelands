53d5760fabcc1fd544854494f5ed5d10a31e279b
Holger Rapp <SirVer@gmx.de>
2014-03-29 15:33:23 +0100

    Converted marten. All critters are now converted.

---
+++ world/critters/marten/init.lua
@@ -0,0 +1,22 @@
+dirname = path.dirname(__file__)
+
+animations = {
+   idle = {
+      pictures = path.glob(dirname, "marten_idle_\\d+.png"),
+      player_color_masks = {},
+      hotspot = { 11, 11 },
+      fps = 20,
+   },
+}
+add_walking_animations(animations, dirname, "marten_walk", {15, 14}, 20)
+
+world:new_critter_type{
+   name = "marten",
+   descname = _ "Marten",
+   swimming = false,
+   attributes = { "eatable" },
+   programs = {
+      remove = { "remove" },
+   },
+   animations = animations,
+}
