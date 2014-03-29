50802ebc737e5dd38efc67fcf6f5984e8afd2c75
Holger Rapp <SirVer@gmx.de>
2014-03-29 13:48:57 +0100

    Converted Wolf to Lua.

---
+++ world/critters/wolf/init.lua
@@ -0,0 +1,22 @@
+dirname = path.dirname(__file__)
+
+animations = {
+   idle = {
+      pictures = path.glob(dirname, "wolf_idle_\\d+.png"),
+      player_color_masks = {},
+      hotspot = { 8, 15 },
+      fps = 10,
+   },
+}
+add_walking_animations(animations, dirname, "wolf_walk", {19, 19}, 20)
+
+world:new_critter_type{
+   name = "wolf",
+   descname = _ "Wolf",
+   swimming = false,
+   attributes = { "eatable" },
+   programs = {
+      remove = { "remove" },
+   },
+   animations = animations,
+}
