6f8d4fba8a6d1301dd116c25c3aaf5ecf093126f
Hans Joachim Desserud <>
2014-03-31 19:58:16 +0200

    Moved skeleton3

---
+++ world/immovables/skeleton3/init.lua
@@ -0,0 +1,16 @@
+dirname = path.dirname(__file__)
+
+world:new_immovable_type{
+   name = "skeleton3",
+   descname = _ "Skeleton 3",
+   size = "none",
+   attributes = {},
+   programs = {},
+   animations = {
+      idle = {
+         pictures = { dirname .. "/idle.png" },
+         player_color_masks = {},
+         hotspot = { 47, 69 },
+      },
+   }
+}
