edc3c17f13f414ce506aff2502eca0786ffa5870
Holger Rapp <SirVer@gmx.de>
2014-04-07 07:50:42 +0200

    Converted grass1.

--- world/init.lua
+++ world/init.lua
@@ -24,11 +24,12 @@ world:new_editor_category{
 include "world/resources/init.lua"
 include "world/terrains/init.lua"
 
+include "world/immovables/grass1/init.lua"
+include "world/immovables/manmade/snowman/init.lua"
 include "world/immovables/skeleton1/init.lua"
 include "world/immovables/skeleton2/init.lua"
 include "world/immovables/skeleton3/init.lua"
 include "world/immovables/skeleton4/init.lua"
-include "world/immovables/manmade/snowman/init.lua"
 include "world/immovables/trees/aspen_summer_old/init.lua"
 
 -- Adds 6 animations for each walking direction into 'table'. The pictures are
