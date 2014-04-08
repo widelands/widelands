1e86cbecb5db1185cf6ae414bcd90990f0a3eb51
GunChleoc <fios@foramnagaidhlig.net>
2014-04-08 16:40:13 +0100

    Converted mushroom1 + mushroom2

---
+++ world/immovables/mushroom2/init.lua
@@ -0,0 +1,16 @@
+dirname = path.dirname(__file__)
+
+world:new_immovable_type{
+   name = "mushroom2",
+   descname = _ "Mushroom",
+   size = "none",
+   attributes = {},
+   programs = {},
+   animations = {
+      idle = {
+         pictures = { dirname .. "/idle.png" },
+         player_color_masks = {},
+         hotspot = { 5, 7 },
+      },
+   }
+}
