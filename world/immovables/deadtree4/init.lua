01b9cc01e845475fe8c2ad5babd21819364cc0bb
GunChleoc <fios@foramnagaidhlig.net>
2014-04-08 17:15:33 +0100

    Converted deadtree1 - deadtree6

---
+++ world/immovables/deadtree4/init.lua
@@ -0,0 +1,19 @@
+dirname = path.dirname(__file__)
+
+world:new_immovable_type{
+   name = "deadtree4",
+   descname = _ "Dead Tree",
+   size = "none",
+   attributes = {},
+   programs = {
+      animate=idle 20000
+      remove=18
+   },
+   animations = {
+      idle = {
+         pictures = { dirname .. "/idle.png" },
+         player_color_masks = {},
+         hotspot = { 23, 61 },
+      },
+   }
+}
