cd9b110158b905a20ab7025ce67c13fb5ab8e2bf
GunChleoc <fios@foramnagaidhlig.net>
2014-04-15 03:26:49 +0100

    Converted help text for deep_coalmine and added TODO comments.

--- tribes/barbarians/coalmine/help.lua
+++ tribes/barbarians/coalmine/help.lua
@@ -14,36 +14,36 @@ Wood may serve for a household fire and to keep you warm, but when it comes to w
 
 	--General Section
 	building_help_general_string("barbarians", building_description, "coal",
-		_"A %s exploits only %s of the resource. <br> From there on, it will only have a 5%% chance of finding %s.":format(_"coal mine","1/3",_"coal"),
+		_"A %1$s exploits only %2$s of the resource. <br> From there on, it will only have a 5%% chance of finding 3$%s.":bformat(_"coal mine","1/3",_"coal"),
 		_"Dig %s out of the ground in mountain terrain.":format(_"coal"),
 		"2", "--") ..
-		--text_line(_"Vision range:", "4") ..
+
 	--Dependencies
-		rt(h2(_"Dependencies")) ..
-		rt(h3(_"Incoming:")) ..
+	rt(h2(_"Dependencies")) ..
+	rt(h3(_"Incoming:")) ..
 
-		building_help_dependencies_ware_building("barbarians", {"tavern", "ration", "coalmine"}, "ration", "tavern") ..
-		building_help_dependencies_ware_building("barbarians", {"inn", "ration", "coalmine"}, "ration", "inn") ..
-		building_help_dependencies_ware_building("barbarians", {"big_inn", "ration", "coalmine"}, "ration", "big_inn") ..
+	building_help_dependencies_ware_building("barbarians", {"tavern", "ration", "coalmine"}, "ration", "tavern") ..
+	building_help_dependencies_ware_building("barbarians", {"inn", "ration", "coalmine"}, "ration", "inn") ..
+	building_help_dependencies_ware_building("barbarians", {"big_inn", "ration", "coalmine"}, "ration", "big_inn") ..
 
-		rt(h3(_"Outgoing:")) ..
-		building_help_dependencies_resi("barbarians", {"resi_coal2", "coalmine", "coal"}, "coal") ..
-		building_help_dependencies_building("barbarians",{"coal","axfactory"}, "axfactory") ..
-		building_help_dependencies_building("barbarians",{"coal","warmill"}, "warmill") ..
-		building_help_dependencies_building("barbarians",{"coal","helmsmithy"}, "helmsmithy") ..
-		building_help_dependencies_building("barbarians",{"coal","smelting_works"}, "smelting_works") ..
-		building_help_dependencies_building("barbarians",{"coal","lime_kiln"}, "lime_kiln") ..
+	rt(h3(_"Outgoing:")) ..
+	building_help_dependencies_resi("barbarians", {"resi_coal2", "coalmine", "coal"}, "coal") ..
+	building_help_dependencies_building("barbarians",{"coal","axfactory"}, "axfactory") ..
+	building_help_dependencies_building("barbarians",{"coal","warmill"}, "warmill") ..
+	building_help_dependencies_building("barbarians",{"coal","helmsmithy"}, "helmsmithy") ..
+	building_help_dependencies_building("barbarians",{"coal","smelting_works"}, "smelting_works") ..
+	building_help_dependencies_building("barbarians",{"coal","lime_kiln"}, "lime_kiln") ..
 
 	--Building Section
 	building_help_building_section("barbarians", building_description) ..
-		rt(h2(_"Building")) ..
-		text_line(_"Upgraded from:", "n/a") ..
-		text_line(_"Upgradeable to:",_"Deep Coal Mine","tribes/barbarians/deep_coalmine/menu.png") ..
+	rt(h2(_"Building")) ..
+	text_line(_"Upgraded from:", "n/a") ..
+	text_line(_"Upgradeable to:",_"Deep Coal Mine","tribes/barbarians/deep_coalmine/menu.png") ..
 	--Workers Section
-		building_help_crew_string("barbarians", "coalmine", "miner", "pick") ..
+	building_help_crew_string("barbarians", "coalmine", "miner", "pick") ..
 
 	--Production Section
-		rt(h2(_"Production")) ..
-		text_line(_"Performance:", _"If the food supply is steady, this mine can produce %s in %s on average.":format(_"Coal","32.5s"))
+	rt(h2(_"Production")) ..
+	text_line(_"Performance:", _"If the food supply is steady, this mine can produce %s in %s on average.":format(_"Coal","32.5s"))
    end
 }
--- tribes/barbarians/deep_coalmine/help.lua
+++ tribes/barbarians/deep_coalmine/help.lua
@@ -4,58 +4,63 @@ include "scripting/format_help.lua"
 set_textdomain("tribe_barbarians")
 
 return {
-	text =
+   func = function(building_description)
+	return
+
 		--rt(h1(_"The Barbarian Deep Coal Mine")) ..
 	--Lore Section
-		rt(h2(_"Lore")) ..
-		rt("image=tribes/barbarians/deep_coalmine/deep_coalmine_i_00.png", p(--text identical to coalmine
-			_([[Ages ago, the Barbarians learned to delve into mountainsides for that black material that feeds their furnaces.<br>
-]] ..
-			[[Wood may serve for a household fire and to keep you warm, but when it comes to working with iron or gold, there is no way around coal.]])
-		)) ..
+	--text identical to coalmine
+	building_help_lore_string("barbarians", "deep_coalmine",_[[Ages ago, the Barbarians learned to delve into mountainsides for that black material that feeds their furnaces. <br>
+Wood may serve for a household fire and to keep you warm, but when it comes to working with iron or gold, there is no way around coal.]]) ..
+
 	--General Section
-		rt(h2(_"General")) ..
-		rt(p(_"A %1$s exploits only %2$s of the resource. From there on out, it will only have a 5%% chance of finding %3$s.":bformat(_"Deep Coal Mine","2/3",_"Coal"))) ..
-		rt(h3(_"Purpose:")) ..
-		image_line("tribes/barbarians/coal/menu.png", 1, p(_"Dig %s out of the ground in mountain terrain.":format(_"Coal"))) ..
-		text_line(_"Working radius:", "2") ..
-		text_line(_"Conquer range:", "n/a") ..
-		text_line(_"Vision range:", "4") ..
+	-- TODO the last 2 parameters should be reqad from C++. "coal" as well?
+	building_help_general_string("barbarians", building_description, "coal",
+		_"A %1$s exploits only %2$s of the resource. From there on out, it will only have a 5%% chance of finding %3$s.":bformat(_"deep coal mine","2/3",_"coal"),
+		_"Dig %s out of the ground in mountain terrain.":format(_"coal"),
+		"2", "--") ..
+
 	--Dependencies
-		rt(h2(_"Dependencies")) ..
-		rt(h3(_"Incoming:")) ..
-		dependencies({"tribes/barbarians/inn/menu.png","tribes/barbarians/snack/menu.png","tribes/barbarians/deep_coalmine/menu.png"}, p(_"%s from an Inn":format(_"Snack"))) ..
-		dependencies({"tribes/barbarians/big_inn/menu.png","tribes/barbarians/snack/menu.png","tribes/barbarians/deep_coalmine/menu.png"}, p(_"%s from a Big Inn":format(_"Snack"))) ..
-		rt(h3(_"Outgoing:")) ..
-		dependencies({"tribes/barbarians/resi_coal2/resi_00.png","tribes/barbarians/deep_coalmine/menu.png","tribes/barbarians/coal/menu.png"}, p(_"Coal")) ..
-		dependencies({"tribes/barbarians/coal/menu.png","tribes/barbarians/axfactory/menu.png"}, p(_"Axfactory")) ..
-		dependencies({"tribes/barbarians/coal/menu.png","tribes/barbarians/warmill/menu.png"}, p(_"War Mill")) ..
-		dependencies({"tribes/barbarians/coal/menu.png","tribes/barbarians/helmsmithy/menu.png"}, p(_"Helm Smithy")) ..
-		dependencies({"tribes/barbarians/coal/menu.png","tribes/barbarians/smelting_works/menu.png"}, p(_"Smelting Works")) ..
-		dependencies({"tribes/barbarians/coal/menu.png","tribes/barbarians/lime_kiln/menu.png"}, p(_"Lime Kiln")) ..
+	rt(h2(_"Dependencies")) ..
+	rt(h3(_"Incoming:")) ..
+	-- TODO read dependencies from C++ and automate
+	building_help_dependencies_ware_building("barbarians", {"inn", "snack", "deep_coalmine"}, "snack", "inn") ..
+	building_help_dependencies_ware_building("barbarians", {"big_inn", "snack", "deep_coalmine"}, "snack", "big_inn") ..
+
+	rt(h3(_"Outgoing:")) ..
+	building_help_dependencies_resi("barbarians", {"resi_coal2", "deep_coalmine", "coal"}, "coal") ..
+	building_help_dependencies_building("barbarians",{"coal","axfactory"}, "axfactory") ..
+	building_help_dependencies_building("barbarians",{"coal","warmill"}, "warmill") ..
+	building_help_dependencies_building("barbarians",{"coal","helmsmithy"}, "helmsmithy") ..
+	building_help_dependencies_building("barbarians",{"coal","smelting_works"}, "smelting_works") ..
+	building_help_dependencies_building("barbarians",{"coal","lime_kiln"}, "lime_kiln") ..
+
 	--Building Section
-		rt(h2(_"Building")) ..
-		text_line(_"Space required:",_"Mine plot","pics/mine.png") ..
-		text_line(_"Upgraded from:",_"Coal mine","tribes/barbarians/coalmine/menu.png") ..
-		rt(h3(_"Upgrade cost:")) ..
-		help_building_line("barbarians", "raw_stone", ngettext("%i Raw Stone", "%i Raw Stones", 2), 2) ..
-		help_building_line("barbarians", "log", ngettext("%i Log", "%i Logs", 4), 4) ..
-		rt(h3(_"Cost cumulative:")) ..
-		help_building_line("barbarians", "raw_stone", ngettext("%i Raw Stone", "%i Raw Stones", 4), 4) ..
-		help_building_line("barbarians", "log", ngettext("%i Log", "%i Logs", 8), 8) ..
-		rt(h3(_"Dismantle yields:")) ..
-		help_building_line("barbarians", "raw_stone", ngettext("%i Raw Stone", "%i Raw Stones", 2), 2) ..
-		help_building_line("barbarians", "log", ngettext("%i Log", "%i Logs", 4), 4) ..
-		text_line(_"Upgradeable to:",_"Deeper Coal Mine","tribes/barbarians/deeper_coalmine/menu.png") ..
+	building_help_building_section("barbarians", building_description) ..
+
+	-- TODO read from C++
+	--text_line(_"Upgraded from:",_"Coal mine","tribes/barbarians/coalmine/menu.png") ..
+	-- add this line at end of building section
+	text_line(_"Upgradeable to:",_"Deeper Coal Mine","tribes/barbarians/deeper_coalmine/menu.png") ..
+
+	-- TODO this is empty
+	--rt(h3(_"Cost cumulative:")) ..
+	--help_building_line("barbarians", "raw_stone", ngettext("%i Raw Stone", "%i Raw Stones", 4), 4) ..
+	--help_building_line("barbarians", "log", ngettext("%i Log", "%i Logs", 8), 8) ..
+	--rt(h3(_"Dismantle yields:")) ..
+	-- TODO: this needs to be cumulative
+	--help_building_line("barbarians", "raw_stone", ngettext("%i Raw Stone", "%i Raw Stones", 2), 2) ..
+	--help_building_line("barbarians", "log", ngettext("%i Log", "%i Logs", 4), 4) ..
+	-- TODO - already in format_help, but value is missing
+	--text_line(_"Upgraded from:",_"Coal mine","tribes/barbarians/coalmine/menu.png") ..
+
 	--Workers Section
-		rt(h2(_"Workers")) ..
-		rt(h3(_"Crew required:")) ..
-		image_line("tribes/barbarians/chief-miner/menu.png", 1, p(_"%s or better":format(_"Chief Miner"))) ..
-		image_line("tribes/barbarians/miner/menu.png", 1, p(_"%s or better":format(_"Miner"))) ..
-		text_line(_"Workers use:",_"Pick","tribes/barbarians/pick/menu.png") ..
-		rt(h3(_"Experience levels:")) ..
-		rt("text-align=right", p(_"%s to %s (%s EP)":format(_"Miner",_"Chief Miner","19") .. "<br>" .. _"%s to %s (%s EP)":format(_"Chief Miner",_"Master Miner","28"))) ..
+	-- TODO this duplicates the tool etc. - get worker list from C++ and handle in format_help
+	building_help_crew_string("barbarians", "coalmine", "chief-miner", "pick") ..
+	building_help_crew_string("barbarians", "coalmine", "miner", "pick") ..
+
 	--Production Section
-		rt(h2(_"Production")) ..
-		text_line(_"Performance:", _"If the food supply is steady, this mine can produce %1$s in %2$s on average.":bformat(_"Coal","19.5s"))
+	rt(h2(_"Production")) ..
+	text_line(_"Performance:", _"If the food supply is steady, this mine can produce %1$s in %2$s on average.":bformat(_"Coal","19.5s"))
+   end
 }
