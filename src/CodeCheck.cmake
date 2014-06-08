da83b82f12c15332f7c7e2931263ed9b4cc78b57
Holger Rapp <SirVer@gmx.de>
2014-06-08 20:39:11 +0200

    A crashing codecheck is a failed codecheck too.

--- src/CodeCheck.cmake
+++ src/CodeCheck.cmake
@@ -1,13 +1,14 @@
 execute_process(
   COMMAND "${PYTHON_EXECUTABLE}" "${WL_SOURCE_CHECKER}" -c "${SRC}"
   OUTPUT_VARIABLE checkresult
+  RESULT_VARIABLE exitcode
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-if (checkresult STREQUAL "")
+if (checkresult STREQUAL "" AND NOT exitcode)
   execute_process(
     COMMAND cmake -E touch ${OUTPUT_FILE}
   )
-else (checkresult STREQUAL "")
+else (checkresult STREQUAL "" AND NOT exitcode)
   message ("${checkresult}")
-endif (checkresult STREQUAL "")
+endif (checkresult STREQUAL "" AND NOT exitcode)
