4f863487a92741533df810d49c2936982c5ea63a
Holger Rapp <SirVer@gmx.de>
2014-06-09 21:51:19 +0200

    Converted winterland map generation.

--- world/map_generation.lua
+++ world/map_generation.lua
@@ -535,126 +535,191 @@ desert = {
    } -- land resources
 }
 
--- NOCOM(#sirver): temporary
-return desert
 
 -- -------------------------
 -- --  Former winterland  --
 -- -------------------------
--- [areas]
--- water=water,water_iceflows,water_ice
--- land=land
--- wasteland=wasteland
--- mountains=mountains
 
+winterland = {
+   heights = {
+      ocean = 10,
+      shelf = 10,
+      shallow = 10,
+      coast = 11,
+      upperland = 14,
+      mountainfoot = 15,
+      mountain = 16,
+      snow = 39,
+      summit = 45,
+   },
+   areas = {
+      water = {
+         {
+            name = "water",
+            weight = 1,
+            ocean_terrains = { "winter_water" },
+            shelf_terrains = { "winter_water" },
+            shallow_terrains = { "winter_water" },
+         },
+         {
+            name = "ice_floes",
+            weight = 1,
+            ocean_terrains = { "ice_floes", "ice_floes2" },
+            shelf_terrains = { "ice_floes", "ice_floes2" },
+            shallow_terrains = { "ice_floes", "ice_floes2" },
+         },
+         {
+            name = "ice",
+            weight = 1,
+            ocean_terrains = { "ice" },
+            shelf_terrains = { "ice" },
+            shallow_terrains = { "ice" },
+         }
+      },
+      land = {
+         {
+            name = "land",
+            weight = 1,
+            coast_terrains = { "winter_beach" },
+            land_terrains = { "tundra", "tundra2", "tundra3" },
+            upper_terrains = { "tundra", "tundra2", "tundra3" },
+         },
+      },
+      wasteland = {
+         {
+            name = "wasteland",
+            weight = 1,
+            inner_terrains = { "tundra_taiga" },
+            outer_terrains = { "tundra" },
+         },
+      },
+      mountains = {
+         {
+            name = "mountains",
+            weight = 1,
+            mountainfoot_terrains = { "tundra_taiga" },
+            mountain_terrains = {
+               "winter_mountain1", "winter_mountain2",
+               "winter_mountain3", "winter_mountain4"
+            },
+            snow_terrains = { "snow" },
+         }
+      }
+   }, -- areas
+   bob_categories = {
+      {
+         name = "trees",
+         immovables = {
+            "aspen_summer_old", "oak_summer_old",
+            "spruce_summer_old", "maple_winter_old",
+            "birch_summer_old", "beech_summer_old",
+            "larch_summer_old", "rowan_summer_old",
+         },
+         critters = { "deer", "elk", "lynx", "reindeer", "wolf" },
+      },
+      {
+         name = "bushes",
+         immovables = { "bush1", "bush2", "bush3", "bush4", "bush5" },
+         critters = {
+            "wisent", "wolf", "bunny", "elk", "fox", "lynx",
+            "wolf", "marten", "sheep"
+         },
+      },
+      {
+         name = "grasses",
+         immovables = { "grass1", "grass2", "grass3" },
+         critters = {
+            "wisent", "wolf", "bunny", "elk", "fox", "lynx",
+            "wolf", "marten", "sheep"
+         },
+      },
+      {
+         name = "standing_stones",
+         immovables = {
+            "standing_stone1_winter",
+            "standing_stone2_winter",
+            "standing_stone3_winter",
+            "standing_stone4_winter",
+            "standing_stone5_winter",
+         },
+         critters = {},
+      },
+      {
+         name = "stones",
+         immovables = {
+            "winterland_stones1", "winterland_stones2",
+            "winterland_stones3", "winterland_stones4",
+            "winterland_stones5", "winterland_stones6",
+         },
+         critters = {},
+      },
+      {
+         name = "wasteland_doodads",
+         immovables = {
+            "skeleton1", "skeleton2", "skeleton3", "deadtree1",
+            "deadtree2", "deadtree3", "deadtree4"
+         },
+         critters = {},
+      }
+   }, -- bob_categories
+   land_resources = {
+      {
+         name = "forest",
+         weight = 6,
+         immovable_density = 100,
+         critter_density = 20,
+         land_coast_bobs = "bushes",
+         land_inner_bobs = "trees",
+         land_upper_bobs = "bushes",
+         wasteland_inner_bobs = "wasteland_doodads",
+         wasteland_outer_bobs = "bushes",
+      },
+      {
+         name = "bushland",
+         weight = 2,
+         immovable_density = 50,
+         critter_density = 20,
+         land_coast_bobs = "grasses",
+         land_inner_bobs = "bushes",
+         land_upper_bobs = "grasses",
+         wasteland_inner_bobs = "wasteland_doodads",
+         wasteland_outer_bobs = "grasses",
+      },
+      {
+         name = "meadow",
+         weight = 2,
+         immovable_density = 10,
+         critter_density = 20,
+         land_coast_bobs = "grasses",
+         land_inner_bobs = "grasses",
+         land_upper_bobs = "grasses",
+         wasteland_inner_bobs = "wasteland_doodads",
+         wasteland_outer_bobs = "grasses",
+      },
+      {
+         name = "stone",
+         weight = 4,
+         immovable_density = 100,
+         critter_density = 0,
+         land_coast_bobs = "",
+         land_inner_bobs = "stones",
+         land_upper_bobs = "",
+         wasteland_inner_bobs = "stones",
+         wasteland_outer_bobs = "stones",
+      },
+      {
+         name = "standing_stones",
+         weight = 2,
+         immovable_density = 100,
+         critter_density = 0,
+         land_coast_bobs = "",
+         land_inner_bobs = "standing_stones",
+         land_upper_bobs = "",
+         wasteland_inner_bobs = "standing_stones",
+         wasteland_outer_bobs = "standing_stones",
+      }
+   } -- land resources
+}
 
-
-
--- [water]
--- ocean_terrains=water
--- shelf_terrains=water
--- shallow_terrains=water
-
--- [water_iceflows]
--- ocean_terrains=ice_flows,ice_flows2
--- shelf_terrains=ice_flows,ice_flows2
--- shallow_terrains=ice_flows,ice_flows2
-
--- [water_ice]
--- ocean_terrains=ice
--- shelf_terrains=ice
--- shallow_terrains=ice
-
--- [land]
--- coast_terrains=strand
--- land_terrains=tundra,tundra2,tundra3
--- upper_terrains=tundra,tundra2,tundra3
-
--- [mountains]
--- mountainfoot_terrains=tundra_taiga
--- mountain_terrains=mountain1,mountain2,mountain3,mountain4
--- snow_terrains=snow
-
--- [wasteland]
--- inner_terrains=tundra_taiga
--- outer_terrains=tundra
-
-
-
--- [bobs]
--- land_resources=forest,bushland,meadow,stone,standing_stones
--- categories=trees,bushes,grasses,stones,standing_stones,wasteland_doodads
-
--- [forest]
--- weight=6
--- immovable_density=100
--- critter_density=20
--- land_coast_bobs=bushes
--- land_inner_bobs=trees
--- land_upper_bobs=bushes
--- wasteland_inner_bobs=wasteland_doodads
--- wasteland_outer_bobs=bushes
-
--- [bushland]
--- weight=2
--- immovable_density=50
--- critter_density=20
--- land_coast_bobs=grasses
--- land_inner_bobs=bushes
--- land_upper_bobs=grasses
--- wasteland_inner_bobs=wasteland_doodads
--- wasteland_outer_bobs=grasses
-
--- [meadow]
--- weight=2
--- immovable_density=10
--- critter_density=20
--- land_coast_bobs=grasses
--- land_inner_bobs=grasses
--- land_upper_bobs=grasses
--- wasteland_inner_bobs=wasteland_doodads
--- wasteland_outer_bobs=grasses
-
--- [stone]
--- weight=4
--- immovable_density=100
--- critter_density=0
--- land_coast_bobs=
--- land_inner_bobs=stones
--- land_upper_bobs=
--- wasteland_inner_bobs=stones
--- wasteland_outer_bobs=stones
-
--- [standing_stones]
--- weight=2
--- immovable_density=100
--- critter_density=0
--- land_coast_bobs=
--- land_inner_bobs=standing_stones
--- land_upper_bobs=
--- wasteland_inner_bobs=standing_stones
--- wasteland_outer_bobs=standing_stones
-
--- [trees]
--- immovables=tree1,tree2,tree3,tree4,tree5,tree6,tree7,tree8
--- critters=deer,elk,lynx,reindeer,wolf
-
--- [bushes]
--- immovables=bush1,bush2,bush3,bush4,bush5
--- critters=wisent,wolf,bunny,elk,fox,lynx,wolf,marten,sheep,sheep
-
--- [grasses]
--- immovables=grass1,grass2,grass3
--- critters=bunny,elk,fox,lynx,wolf,marten,sheep,sheep
-
--- [standing_stones]
--- immovables=sstones1,sstones2,sstones3,sstones4,sstones5,sstones6,sstones7
--- critters=
-
--- [stones]
--- immovables=stones1,stones2,stones3,stones4,stones5,stones5,stones6,stones6,stones6
--- critters=
-
--- [wasteland_doodads]
--- immovables=skeleton1,skeleton2,skeleton3,deadtree1,deadtree2,deadtree3,deadtree4
--- critters=
+-- NOCOM(#sirver): temporary
+return winterland
