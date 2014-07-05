996425175d78389296bcb3b38a82a0d7401f3cc1
Holger Rapp <SirVer@gmx.de>
2014-07-05 13:47:52 +0200

    Color no longer extends from SDL_Color.

--- src/graphic/color.h
+++ src/graphic/color.h
@@ -22,8 +22,7 @@
 
 #include <SDL.h>
 
-// TODO(sirver): Do not inherit from SDL_Color if possible.
-struct RGBColor : public SDL_Color {
+struct RGBColor {
 	RGBColor(uint8_t R, uint8_t G, uint8_t B);
 
 	// Initializes the color to black.
@@ -36,6 +35,8 @@ struct RGBColor : public SDL_Color {
 	void set(SDL_PixelFormat * fmt, uint32_t clr);
 
 	bool operator == (const RGBColor& other) const;
+
+	uint8_t r, g, b;
 };
 
 struct RGBAColor {
