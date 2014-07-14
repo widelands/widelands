fc07a5de95c544a7012e92b58382fa6024f7e154
Jens Beyer (Qcumber-some) <qcumber-some@buerotiger.de>
2014-07-14 16:40:58 +0200

    review comments: consistency and comments

--- cmake/WlFunctions.cmake
+++ cmake/WlFunctions.cmake
@@ -1,4 +1,6 @@
-macro(_parse_common_args ARGS)
+# Utilize cmake_parse_arguments standard function to parse for common arguments.
+include(CMakeParseArguments)
+macro(wl_parse_common_args ARGS)
   set(OPTIONS
     THIRD_PARTY  # Is a third party lib. Less warnings, no codecheck.
     C_LIBRARY # Pure C library. No CXX flags.
@@ -20,24 +22,30 @@ macro(_parse_common_args ARGS)
   cmake_parse_arguments(ARG "${OPTIONS}" "${ONE_VALUE_ARG}" "${MULTI_VALUE_ARGS}"
     ${ARGS}
   )
-endmacro(_parse_common_args)
+endmacro(wl_parse_common_args)
 
-# Define the directory structure for installation - will be hardcoded in WL bin
-# (through config.h).
-macro(_set_if_unset VAR VALUE)
+# Set variable VAR to VALUE if it is not set or empty. Does nothing if already set.
+macro(wl_set_if_unset VAR VALUE)
   if (NOT ${VAR} OR ${VAR} STREQUAL "")
 	  set (${VAR} ${VALUE})
   endif()
-endmacro(_set_if_unset)
+endmacro(wl_set_if_unset)
 
-function(_include_directories TARGET DIR)
+# Add DIR as include directores for TARGET. Depending on cmake version the way how
+# it works is different.
+function(wl_include_directories TARGET DIR)
   _include_directories_internal(${TARGET} ${DIR} FALSE)
-endfunction(_include_directories TARGET DIR)
-function(_include_system_directories TARGET DIR)
-  _include_directories_internal(${TARGET} ${DIR} TRUE)
-endfunction(_include_system_directories TARGET_DIR)
+endfunction(wl_include_directories TARGET DIR)
 
-macro(_common_compile_tasks)
+# Add DIR as SYSTEM include directores for TARGET. Depending on cmake version the way how
+# it works is different. SYSTEM includes silence warnings for included headers etc.
+function(wl_include_system_directories TARGET DIR)
+  _include_directories_internal(${TARGET} ${DIR} TRUE)
+endfunction(wl_include_system_directories TARGET_DIR)
+
+# Add common compile tasks, like includes and libraries to link against for third party
+# libraries, and codecheck hook for sources.
+macro(wl_common_compile_tasks)
   if (NOT ARG_C_LIBRARY)
     set(TARGET_COMPILE_FLAGS "${TARGET_COMPILE_FLAGS} ${WL_GENERIC_CXX_FLAGS}")
 
@@ -53,7 +61,7 @@ macro(_common_compile_tasks)
     set(TARGET_COMPILE_FLAGS "${TARGET_COMPILE_FLAGS} -w")
   else()
     foreach(SRC ${ARG_SRCS})
-      run_codecheck(${NAME} ${SRC})
+      wl_run_codecheck(${NAME} ${SRC})
     endforeach(SRC)
 
     set(TARGET_COMPILE_FLAGS "${TARGET_COMPILE_FLAGS} ${WL_COMPILE_DIAGNOSTICS}")
@@ -63,60 +71,60 @@ macro(_common_compile_tasks)
 
   if(NOT ARG_THIRD_PARTY)
     # src/ is the base for all of our includes. The binary one is for generated files.
-    _include_directories(${NAME} ${CMAKE_SOURCE_DIR}/src)
-    _include_directories(${NAME} ${CMAKE_BINARY_DIR}/src)
+    wl_include_directories(${NAME} ${CMAKE_SOURCE_DIR}/src)
+    wl_include_directories(${NAME} ${CMAKE_BINARY_DIR}/src)
 
     # Boost is practically the standard library, so we always add a search path
     # to include it easily. Except for third party.
-    _include_system_directories(${NAME} ${Boost_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${Boost_INCLUDE_DIR})
   endif()
 
   if(ARG_USES_ZLIB)
-    _include_system_directories(${NAME} ${ZLIB_INCLUDE_DIRS})
+    wl_include_system_directories(${NAME} ${ZLIB_INCLUDE_DIRS})
     target_link_libraries(${NAME} ${ZLIB_LIBRARY})
   endif()
 
   # OpenGL and GLEW are one thing for us. If you use the one, you also use the
   # other.
   if(ARG_USES_OPENGL)
-    _include_system_directories(${NAME} ${GLEW_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${GLEW_INCLUDE_DIR})
     target_link_libraries(${NAME} ${GLEW_LIBRARY})
     target_link_libraries(${NAME} ${OPENGL_gl_LIBRARY})
     add_definitions(${GLEW_EXTRA_DEFINITIONS})
   endif()
 
   if(ARG_USES_PNG)
-    _include_system_directories(${NAME} ${PNG_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${PNG_INCLUDE_DIR})
     target_link_libraries(${NAME} ${PNG_LIBRARY})
   endif()
 
   if(ARG_USES_SDL)
-    _include_system_directories(${NAME} ${SDL_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${SDL_INCLUDE_DIR})
     target_link_libraries(${NAME} ${SDL_LIBRARY})
   endif()
 
   if(ARG_USES_SDL_MIXER)
-    _include_system_directories(${NAME} ${SDLMIXER_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${SDLMIXER_INCLUDE_DIR})
     target_link_libraries(${NAME} ${SDLMIXER_LIBRARY})
   endif()
 
   if(ARG_USES_SDL_NET)
-    _include_system_directories(${NAME} ${SDLNET_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${SDLNET_INCLUDE_DIR})
     target_link_libraries(${NAME} ${SDLNET_LIBRARY})
   endif()
 
   if(ARG_USES_SDL_IMAGE)
-    _include_system_directories(${NAME} ${SDLIMAGE_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${SDLIMAGE_INCLUDE_DIR})
     target_link_libraries(${NAME} ${SDLIMAGE_LIBRARY})
   endif()
 
   if(ARG_USES_SDL_GFX)
-    _include_system_directories(${NAME} ${SDLGFX_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${SDLGFX_INCLUDE_DIR})
     target_link_libraries(${NAME} ${SDLGFX_LIBRARY})
   endif()
 
   if(ARG_USES_SDL_TTF)
-    _include_system_directories(${NAME} ${SDLTTF_INCLUDE_DIR})
+    wl_include_system_directories(${NAME} ${SDLTTF_INCLUDE_DIR})
     target_link_libraries(${NAME} ${SDLTTF_LIBRARY})
   endif()
 
@@ -124,7 +132,7 @@ macro(_common_compile_tasks)
     # libintl is not used on all systems, so only include it, when we actually
     # found it.
     if (INTL_FOUND)
-      _include_system_directories(${NAME} ${INTL_INCLUDE_DIR})
+      wl_include_system_directories(${NAME} ${INTL_INCLUDE_DIR})
       target_link_libraries(${NAME} ${INTL_LIBRARY})
     endif()
   endif()
@@ -136,12 +144,11 @@ macro(_common_compile_tasks)
   foreach(DEPENDENCY ${ARG_DEPENDS})
     target_link_libraries(${NAME} ${DEPENDENCY})
   endforeach(DEPENDENCY)
-endmacro(_common_compile_tasks)
+endmacro(wl_common_compile_tasks)
 
-# See
-# http://www.cmake.org/cmake/help/v3.0/module/CMakeParseArguments.html
+# Common library target definition.
 function(wl_library NAME)
-  _parse_common_args("${ARGN}")
+  wl_parse_common_args("${ARGN}")
 
   add_library(${NAME}
     STATIC
@@ -153,11 +160,12 @@ function(wl_library NAME)
   # TODO remove this once cycling dependencies are history in widelands
   set_target_properties(${NAME} PROPERTIES LINK_INTERFACE_MULTIPLICITY 5)
 
-  _common_compile_tasks()
+  wl_common_compile_tasks()
 endfunction()
 
+# Common test target definition.
 function(wl_test NAME)
-  _parse_common_args("${ARGN}")
+  wl_parse_common_args("${ARGN}")
 
   add_executable(${NAME} ${ARG_SRCS})
 
@@ -173,7 +181,7 @@ function(wl_test NAME)
   list(REMOVE_ITEM SDL_LIBRARY_TEMP ${SDLMAIN_LIBRARY})
   target_link_libraries(${NAME} ${SDL_LIBRARY_TEMP})
 
-  _common_compile_tasks()
+  wl_common_compile_tasks()
 
   add_test(${NAME} ${NAME})
   add_dependencies(wl_tests ${NAME})
@@ -184,7 +192,7 @@ endfunction()
 # target for the codecheck will be added as a dependency to 'NAME' for debug
 # builds, but always for the target 'codecheck', so that make codecheck checks
 # all source code.
-function(run_codecheck NAME SRC)
+function(wl_run_codecheck NAME SRC)
   get_filename_component(ABSOLUTE_SRC ${SRC} ABSOLUTE)
 
   # If the file does not exist, it is probably auto-generated. In that case, it
@@ -219,9 +227,10 @@ function(run_codecheck NAME SRC)
       add_dependencies(${NAME} see_if_codecheck_needs_to_run_${CHECKSUM})
     endif(CMAKE_BUILD_TYPE STREQUAL Debug)
   endif(EXISTS ${ABSOLUTE_SRC})
-endfunction(run_codecheck)
+endfunction(wl_run_codecheck)
 
-function(add_flag VAR_NAME FLAG)
+# Add a compiler flag VAR_NAME=FLAG. If VAR_NAME is already set, FLAG is appended.
+function(wl_add_flag VAR_NAME FLAG)
   if (${VAR_NAME})
     set(${VAR_NAME} "${${VAR_NAME}} ${FLAG}" PARENT_SCOPE)
   else()
@@ -229,8 +238,9 @@ function(add_flag VAR_NAME FLAG)
   endif()
 endfunction()
 
+# Common binary target definition.
 function(wl_binary NAME)
-  _parse_common_args("${ARGN}")
+  wl_parse_common_args("${ARGN}")
 
   if (ARG_WIN32)
     add_executable(${NAME}
@@ -243,5 +253,5 @@ function(wl_binary NAME)
     )
   endif()
 
-  _common_compile_tasks()
+  wl_common_compile_tasks()
 endfunction()
