1146fd58ffc43f10725781f1b6d0a6d43c015f30
Ferdinand Thiessen <rpm@fthiessen.de>
2014-07-20 15:39:34 +0200

    Some fixes (mission complete when player completly_defeated()), seems now to work.

--- campaigns/t03.wmf/scripting/mission_thread.lua
+++ campaigns/t03.wmf/scripting/mission_thread.lua
@@ -252,10 +252,6 @@ function check_player_completly_defeated(dp)
     if #dp:get_buildings("barrier")   > 0 then return false end
     if #dp:get_buildings("sentry")    > 0 then return false end
     if #dp:get_buildings("warehouse") > 0 then return false end
-    print("Besiegt: BAR:")
-    print(#dp:get_buildings("donjon"))
-    print("SEN:")
-    print(#dp:get_buildings("sentry"))
     return true
 end
 
@@ -277,7 +273,9 @@ function kalitath()
    send_msg(order_msg_7_destroy_kalitaths_army)
    local o = add_obj(obj_destroy_kalitaths_army)
 
-   while not check_player_completly_defeated(p2) do sleep(7837) end
+   while not check_player_completly_defeated(p2) do
+        sleep(7837)
+   end
    o.done = true
 end
 
@@ -326,7 +324,10 @@ end
 
 function mission_complete()
    local map = wl.Game().map
-   while not (p2.defeated and p3.defeated and p4.defeated) do
+   while not (
+      check_player_completly_defeated(p2) and
+      check_player_completly_defeated(p3) and
+      check_player_completly_defeated(p4)) do
       sleep(8923)
    end
 
