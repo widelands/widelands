b5849b9544de911f86aa7d1483a1c82301114294
GunChleoc <fios@foramnagaidhlig.net>
2014-09-11 18:46:18 +0100

    Fixed codecheck

--- src/logic/cmd_delete_message.h
+++ src/logic/cmd_delete_message.h
@@ -17,8 +17,8 @@
  *
  */
 
-#ifndef WL_LOGIC_CMD_EXPIRE_MESSAGE_H
-#define WL_LOGIC_CMD_EXPIRE_MESSAGE_H
+#ifndef WL_LOGIC_CMD_DELETE_MESSAGE_H
+#define WL_LOGIC_CMD_DELETE_MESSAGE_H
 
 #include <memory>
 
@@ -51,4 +51,4 @@ private:
 
 }
 
-#endif  // end of include guard: WL_LOGIC_CMD_EXPIRE_MESSAGE_H
+#endif  // end of include guard: WL_LOGIC_CMD_DELETE_MESSAGE_H
