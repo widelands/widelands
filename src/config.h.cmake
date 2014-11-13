6a4f9d048d5045024f8576cfafe22ea3120fd8ea
GunChleoc <fios@foramnagaidhlig.net>
2014-11-13 11:25:03 +0000

    Fixed idiomatic expression in a comment

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -5,7 +5,7 @@
 // path (i.e. starting with a /), it will be interpreted as such. If it is a
 // relative path it will be interpreted to be relative to the executable. For a
 // standard Linux installation, this should be
-// "/usr/local/share/games/widelands" or something along the lines.
+// "/usr/local/share/games/widelands" or something along these lines.
 #define INSTALL_DATADIR "@WL_INSTALL_DATADIR@"
 
 // True if getenv has been found.
