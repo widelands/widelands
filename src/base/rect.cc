2891ad2cae02dd459a97a836cdb16ecbf0e42920
Holger Rapp <SirVer@gmx.de>
2014-12-29 23:11:23 +0100

    Now working, but hacky test cases still.

--- src/base/rect.cc
+++ src/base/rect.cc
@@ -17,30 +17,35 @@
  *
  */
 
+#include "base/rect.h"
+
 #include <algorithm>
-#include <cassert>
 #include <set>
 #include <vector>
 
 #include "base/log.h"
-#include "base/rect.h"
 
-// NOCOM(#sirver): figure this out
-// NOCOM(#sirver): document that I used their nomeclature.
+// This implements the Paper:
 // An Optimal Rectangle-Intersection Algorithm Using Linear Arrays Only by Frank Devai1, Laszlo Neumann.
+//
+// I mirrored it here: http://www.widelands.org/~sirver/wl/141229_devail_rects.pdf.
+//
+// The code uses the nomenclature used in the Paper up to some differences. We
+// did not use intervals, but instead use the rectangles directly.
 
 namespace  {
 
-struct Edge {
-	Edge(const int init_rectangle_index, const float init_value)
-	   : rectangle_index(init_rectangle_index), value(init_value) {
+// A left or right edge of one of the rectangles. Sort in ascending x order.
+struct VerticalEdge {
+	VerticalEdge(const int init_corresponding_rect_in_H, const float init_value)
+	   : corresponding_rect_in_H(init_corresponding_rect_in_H), value(init_value) {
 	}
 
-	inline bool operator<(const Edge& other) const {
+	inline bool operator<(const VerticalEdge& other) const {
 		return value < other.value;
 	}
 
-	int rectangle_index;
+	int corresponding_rect_in_H;
 	float value;
 };
 
@@ -49,53 +54,55 @@ struct RectWrapper {
 	   : original_index(init_original_index), r(init_r) {
 	}
 
-	inline float lower_x() const {
+	inline float left() const {
 		return r->x;
 	}
 
-	inline float higher_x() const {
+	inline float right() const {
 		return r->x + r->w;
 	}
 
-	inline float lower_y() const {
+	inline float top() const {
 		return r->y;
 	}
 
-	inline float higher_y() const {
+	inline float bottom() const {
 		return r->y + r->h;
 	}
 
 	// Sorts by the lower edge of the contained rectangle.
 	inline bool operator<(const RectWrapper& other) const {
-		return higher_y() < other.higher_y();
+		return top() < other.top();
 	}
 
 	size_t original_index;
 	const FloatRect* r;
 };
 
-static std::set<std::pair<int, int>> pairs;
+using ResultType = std::set<std::pair<size_t, size_t>>;
 
-void report_pair(const int a, const int b) {
-	log("#sirver a: %d,b: %d\n", a, b);
-	pairs.insert(std::make_pair(std::min(a, b), std::max(a, b)));
+void report_pair(const size_t a, const size_t b, ResultType* result) {
+	result->insert(std::make_pair(std::min(a, b), std::max(a, b)));
 }
 
-void stab(const std::vector<RectWrapper>& H, const std::vector<int>& A, const std::vector<int>& B) {
+void stab(const std::vector<RectWrapper>& H,
+          const std::vector<size_t>& A,
+          const std::vector<size_t>& B,
+          ResultType* result) {
 	size_t i = 0;
 	size_t j = 0;
 	while (i < A.size() && j < B.size()) {
-		if (H[A[i]].lower_y() < H[B[j]].lower_y()) {
+		if (H[A[i]].top() < H[B[j]].top()) {
 			size_t k = j;
-			while (k < B.size() && H[B[k]].lower_y() < H[A[i]].higher_y()) {
-				report_pair(H[A[i]].original_index, H[B[k]].original_index);
+			while (k < B.size() && H[B[k]].top() < H[A[i]].bottom()) {
+				report_pair(H[A[i]].original_index, H[B[k]].original_index, result);
 				++k;
 			}
 			++i;
 		} else {
 			size_t k = i;
-			while (k < A.size() && H[A[k]].lower_y() < H[B[j]].higher_y()) {
-				report_pair(H[B[j]].original_index, H[A[k]].original_index);
+			while (k < A.size() && H[A[k]].top() < H[B[j]].bottom()) {
+				report_pair(H[B[j]].original_index, H[A[k]].original_index, result);
 				++k;
 			}
 			++j;
@@ -103,114 +110,93 @@ void stab(const std::vector<RectWrapper>& H, const std::vector<int>& A, const st
 	}
 }
 
-void detect(const std::vector<Edge>& V, int lower_index, int higher_index, const std::vector<RectWrapper>& H, const std::string indent) {
-	log("#sirver higher_index: %d,lower_index: %d\n", higher_index, lower_index);
-	if (higher_index - lower_index < 2) {
+void detect(const std::vector<VerticalEdge>& V,
+            size_t low_index,
+            size_t high_index,
+            const std::vector<RectWrapper>& H,
+            ResultType* result) {
+	if (high_index - low_index < 2) {
 		return;
 	}
 
 	std::vector<int> count1(H.size());
 	std::vector<int> count2(H.size());
 
-	std::vector<RectWrapper> H1, H2;
-	// NOCOM(#sirver): index into H
-	std::vector<int> S11, S22, S12, S21;
 
-	const float min_value = V[lower_index].value;
-	const float max_value = V[higher_index - 1].value;
+	const float min_value = V[low_index].value;
+	const float max_value = V[high_index - 1].value;
 
-	for (int i = lower_index; i < (lower_index + higher_index) / 2; ++i) {
-		++count1[V[i].rectangle_index];
+	const size_t mid_index = (high_index + low_index) / 2;
+	for (size_t i = low_index; i < mid_index; ++i) {
+		++count1[V[i].corresponding_rect_in_H];
 	}
-	for (int i = (lower_index + higher_index) / 2; i < higher_index; ++i) {
-		++count2[V[i].rectangle_index];
+	for (size_t i = mid_index; i < high_index; ++i) {
+		++count2[V[i].corresponding_rect_in_H];
 	}
 
+	std::vector<size_t> S11, S22, S12, S21;
 	for (size_t i = 0; i < H.size(); ++i) {
 		if (count1[i] == 0 && count2[i] == 0) {
 			continue;
 		}
-		assert(count1[i] + countl2[i] <= 2);
 
 		if (count1[i] == 2 && count2[i] == 0) {
 			S11.push_back(i);
 		} else if (count1[i] == 0 && count2[i] == 2) {
 			S22.push_back(i);
-		} else if (count1[i] == 1 && count2[i] == 1) {
-				// S12.push_back(i);
-				// S21.push_back(i);
-			// NOCOM(#sirver): what here?
 		} else if (count1[i] == 1 && count2[i] == 0) {
-			// If the right edge is over the highest value, we span V2.
-			if (H[i].higher_x() >= max_value) {
+			// If the right edge is over the highest value, we span V2, otherwise
+			// we don't.
+			if (H[i].right() > max_value) {
 				S12.push_back(i);
 			} else {
 				S11.push_back(i);
 			}
 		} else if (count1[i] == 0 && count2[i] == 1) {
-			// If the left edge is lower than the lowest value, we do span V1.
-			if (H[i].lower_x() <= min_value) {
+			// If the left edge is lower than the lowest value, we do span V1
+			// otherwise we don't.
+			if (H[i].left() < min_value) {
 				S21.push_back(i);
 			} else {
 				S22.push_back(i);
 			}
-		} else {
-			assert(false);
 		}
 	}
-	log("#sirver S11: ");
-	for (const auto& i : S11) { log("%d ", i); }
-	log("\n");
-	log("#sirver S12: ");
-	for (const auto& i : S12) { log("%d ", i); }
-	log("\n");
-	log("#sirver S22: ");
-	for (const auto& i : S22) { log("%d ", i); }
-	log("\n");
-	log("#sirver S21: ");
-	for (const auto& i : S21) { log("%d ", i); }
-	log("\n");
 
-	stab(H, S12, S22);
-	stab(H, S21, S11);
-	stab(H, S12, S21);
+	stab(H, S12, S22, result);
+	stab(H, S21, S11, result);
+	stab(H, S12, S21, result);
 
-	detect(V, lower_index, (lower_index + higher_index) / 2, H, indent + " ");
-	detect(V, (lower_index + higher_index) / 2, higher_index, H, indent + " ");
+	detect(V, low_index, (low_index + high_index) / 2, H, result);
+	detect(V, (low_index + high_index) / 2, high_index, H, result);
 }
 
 }  // namespace
 
-std::vector<std::vector<int>> report(const std::vector<FloatRect>& H) {
-	std::vector<RectWrapper> rects;
-	rects.reserve(H.size());
+std::vector<std::vector<int>> report(const std::vector<FloatRect>& rectangles) {
+	std::vector<RectWrapper> H;
+	H.reserve(rectangles.size());
+	for (size_t i = 0; i < rectangles.size(); ++i) {
+		H.emplace_back(i, &rectangles[i]);
+	}
+	std::sort(H.begin(), H.end());
 
+
+	std::vector<VerticalEdge> V;
 	for (size_t i = 0; i < H.size(); ++i) {
-		rects.emplace_back(i, &H[i]);
-	}
-
-	std::sort(rects.begin(), rects.end());
-
-	for (int i = 0; i < rects.size(); ++i) {
-		log("#sirver %f %f\n", rects[i].r->x, rects[i].r->y);
-	}
-
-	std::vector<Edge> V;
-	for (size_t i = 0; i < rects.size(); ++i) {
-		const FloatRect& rect = *rects[i].r;
+		const FloatRect& rect = *H[i].r;
 		V.emplace_back(i, rect.x);
 		V.emplace_back(i, rect.x + rect.w);
 	}
-
 	std::sort(V.begin(), V.end());
 
-	detect(V, 0, V.size(), rects, "");
+	ResultType result;
+	detect(V, 0, V.size(), H, &result);
 
 	std::vector<std::vector<int>> rv;
-	rv.resize(H.size());
+	rv.resize(rectangles.size());
 
-	for (const auto& p : pairs) {
-		log("#sirver intersection: %d %d\n", p.first, p.second);
+	for (const auto& p : result) {
 		rv[p.first].push_back(p.second);
 	}
 	return rv;
