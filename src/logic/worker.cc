b3115d5a48af8fab5984f529bbc3c12a0ee9426a
Teppo <tm@iki.fi>
2015-01-14 17:29:06 +0200

    nits..

--- src/logic/worker.cc
+++ src/logic/worker.cc
@@ -846,20 +846,26 @@ bool Worker::run_plant(Game & game, State & state, const Action & action)
 
 	// Each candidate is weighted by its probability to grow.
 	double total_weight = 0.0;
-	for (auto bsii : best_suited_immovables_index)
+	for (const auto bsii : best_suited_immovables_index)
 	{
 		double weight = std::get<0>(bsii);
 		total_weight += weight * weight;
 	}
 
-	double choice = logic_rand_as_double(&game) * total_weight + std::numeric_limits<double>::epsilon();
+	double choice = logic_rand_as_double(&game) * total_weight + std::numeric_limits<double>::denorm_min();
 
-	for (auto bsii : best_suited_immovables_index)
-	if (0 < choice)
+	for (const auto bsii : best_suited_immovables_index)
 	{
-		double weight = std::get<0>(bsii);
-		state.ivar2 = std::get<1>(bsii);
-		choice -= weight * weight;
+		if (0 < choice)
+		{
+			double weight = std::get<0>(bsii);
+			state.ivar2 = std::get<1>(bsii);
+			choice -= weight * weight;
+		}
+		else
+		{
+			break;
+		}
 	}
 
 	Immovable& newimm =
