bb5104d2ec1e1f570b03e4fd727aa74b4e661ad0
GunChleoc <fios@foramnagaidhlig.net>
2015-05-14 08:59:14 +0100

    Added hook for translators into developers.json.

--- txts/AUTHORS.lua
+++ txts/AUTHORS.lua
@@ -1,6 +1,5 @@
 include "scripting/formatting.lua"
 include "txts/developers.lua"
-include "txts/translators/translators.lua"
 
 set_textdomain("texts")
 
@@ -17,15 +16,11 @@ function p_authors(person, image)
 	return "<rt image=" .. image .. " text-align=left image-align=left><p font-size=12>" .. person .. "</p></rt>"
 end
 
-
--- Uses structured data to format developers and inserts translators before packagers
+-- Uses structured data to format authors
 function list_authors()
 	local developers = developers()
 	local result = ""
 	for i, category in ipairs(developers) do
-		if (category["heading"] == _"Packagers") then -- Translators come from a separate list
-			result = result .. list_translators(translators())
-		end
 		result = result .. "<rt>" .. h1_authors(category["heading"]) .. "</rt>"
 		for j, entry in ipairs(category["entries"])  do
 			if (entry["subheading"] ~= nil) then
@@ -38,19 +33,6 @@ function list_authors()
 		result = result .. rt("<p font-size=10> <br></p>")
 	end
 	return result
-end
-
--- Uses structured data to format translators
-function list_translators(translators)
-		local result = ""
-		result = result .. "<rt>" .. h1_authors(_"Translators") .. "</rt>"
-		for i, entry in ipairs(translators) do
-			result = result .. h2_authors(entry[1])
-			for i, person in ipairs(entry[2])  do
-			   result = result .. p_authors(person, "pics/fsel_editor_set_height.png")
-		   end
-		end
-		return result
 end
 
 -- Main script
