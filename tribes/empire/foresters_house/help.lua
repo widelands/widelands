059f0c717b99475fd26ac9e043865b9d1e3bc01b
GunChleoc <fios@foramnagaidhlig.net>
2015-07-30 06:55:02 +0100

    Fixed typo in Forester's House.

--- tribes/empire/foresters_house/help.lua
+++ tribes/empire/foresters_house/help.lua
@@ -15,7 +15,7 @@ return {
 	--General Section
 	building_help_general_string("empire", building_description,
 		_"Plants trees in the surrounding area.",
-		_"The foresters’s house needs free space within the work area to plant the trees.") ..
+		_"The forester’s house needs free space within the work area to plant the trees.") ..
 
 	--Dependencies
 	building_help_dependencies_production("empire", building_description) ..
