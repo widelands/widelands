4a426a6e0996b5a489a44cd4d4b7430e1b109d12
GunChleoc <fios@foramnagaidhlig.net>
2015-09-28 19:47:57 +0100

    Implemented line wrapping for Japanese into fh1.

--- src/graphic/text/bidi.h
+++ src/graphic/text/bidi.h
@@ -23,14 +23,20 @@
 #include <string>
 #include <vector>
 
+#include <unicode/uchar.h>
+
 #include "graphic/text/font_set.h"
 
 // BiDi support for RTL languages
 namespace i18n {
 	std::string make_ligatures(const char* input);
 	std::string line2bidi(const char* input);
+	std::vector<std::string> split_cjk_word(const char* input);
 	bool has_rtl_character(const char* input);
 	bool has_rtl_character(std::vector<std::string> input);
+	bool has_cjk_character(const char* input);
+	bool cannot_start_line(const UChar& c);
+	bool cannot_end_line(const UChar& c);
 
 } // namespace UI
 
