c7695547311d1b3701fb1ca2e24dfb0b485b0ce6
wl-zocker <>
2015-10-24 18:21:37 +0200

    Added line break to editor category Artifacts

--- world/init.lua
+++ world/init.lua
@@ -14,10 +14,7 @@ world:new_editor_immovable_category{
 
 world:new_editor_immovable_category{
    name = "artifacts",
-	-- NOCOM: A newline would be nice, but neither \n nor </p><p ...> work.
-   descname = _ "Artifacts." .. " " ..
-	--TRANSLATORS: Make sure you translate “Artifacts” consistently
-	_"These immovables are used by the win condition “Artifacts”.",
+   descname = _ "Artifacts" .. "<br>" .. _ "These immovables are used by the win condition “Artifacts”.",
    picture = "world/immovables/manmade/artifacts/artifact00/idle.png",
 }
 
