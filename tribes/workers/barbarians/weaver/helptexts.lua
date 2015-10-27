730d82d395f020cf071ef167d77e8badb076fbba
GunChleoc <fios@foramnagaidhlig.net>
2015-10-27 16:41:18 +0000

    Corrected helptext for Barbarian Weaver.

--- tribes/workers/barbarians/weaver/helptexts.lua
+++ tribes/workers/barbarians/weaver/helptexts.lua
@@ -1,4 +1,4 @@
 function worker_helptext()
 	-- TRANSLATORS: Helptext for a worker: Weaver
-	return pgettext("barbarians_worker", "Produces cloth for ships and soldiers.")
+	return pgettext("barbarians_worker", "Produces cloth for ships’ sails.")
 end
