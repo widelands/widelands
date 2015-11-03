32e6063c147d97b66c7a93a32a70ff5d9c0fb524
GunChleoc <fios@foramnagaidhlig.net>
2015-11-03 18:17:49 +0000

    Shifted NumberGlob from Animations to lua_path. Replaced path.list_directory with path.list_files, which uses NumberGlob to be more efficient. Updated the test.

--- tribes/buildings/productionsites/barbarians/lumberjacks_hut/init.lua
+++ tribes/buildings/productionsites/barbarians/lumberjacks_hut/init.lua
@@ -18,18 +18,15 @@ tribes:new_productionsite_type {
 
    animations = {
       idle = {
-         template = "idle_??",
-         directory = dirname,
+         pictures = path.list_files(dirname .. "idle_??.png"),
          hotspot = { 43, 45 },
       },
       build = {
-         template = "build_??",
-         directory = dirname,
+         pictures = path.list_files(dirname .. "build_??.png"),
          hotspot = { 43, 45 },
       },
       unoccupied = {
-         template = "unoccupied_??",
-         directory = dirname,
+         pictures = path.list_files(dirname .. "unoccupied_??.png"),
          hotspot = { 43, 45 },
       },
    },
