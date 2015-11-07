a462204b4401dc80ec0ba454d307c9e97726a076
GunChleoc <fios@foramnagaidhlig.net>
2015-11-07 08:45:50 +0000

    Consistent phrasing for building purpose help texts. Added helptexts for Ports.

--- tribes/buildings/warehouses/empire/warehouse/helptexts.lua
+++ tribes/buildings/warehouses/empire/warehouse/helptexts.lua
@@ -13,7 +13,7 @@ end
 
 function building_helptext_purpose()
    -- TRANSLATORS: Purpose helptext for a building
-   return pgettext("empire_building", "Warehouses store soldiers, wares and tools.")
+   return pgettext("empire_building", "Stores soldiers, wares and tools.")
 end
 
 function building_helptext_note()
