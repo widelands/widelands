2afc116c803a0466aeb4bfe77fb3b2cf7c94faf5
GunChleoc <fios@foramnagaidhlig.net>
2015-11-08 17:06:27 +0000

    Found an obsolete term.

--- tribes/wares/spidercloth/helptexts.lua
+++ tribes/wares/spidercloth/helptexts.lua
@@ -1,7 +1,7 @@
 function ware_helptext(tribe)
    local helptext = {
       -- TRANSLATORS: Helptext for a ware: Spidercloth
-      atlanteans = pgettext("atlanteans_ware", "Spidercloth is made out of spideryarn in a weaving mill. It is used in the toolsmithy and the shipyard. Also some higher developed buildings need spidercloth for their construction.")
+      atlanteans = pgettext("atlanteans_ware", "Spidercloth is made out of spider silk in a weaving mill. It is used in the toolsmithy and the shipyard. Also some higher developed buildings need spidercloth for their construction.")
    }
    local result = ""
    if tribe then
