cb8000e2d3b9d61b25aa551a40c8c6969cb54e4b
GunChleoc <fios@foramnagaidhlig.net>
2015-11-27 15:02:40 +0000

    Fixed a copyright header.

--- src/logic/queue_cmd_factory.cc
+++ src/logic/queue_cmd_factory.cc
@@ -1,10 +1,10 @@
 /*
- * Copyright(C) 2002-2004, 2008-2010 by the Widelands Development Team
+ * Copyright (C) 2002-2004, 2008-2010, 2015 by the Widelands Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License, or(at your option) any later version.
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
