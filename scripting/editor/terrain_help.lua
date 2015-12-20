bbc7d9b64fa0d41c2b9814de54b17d6aa0080b13
wl-zocker <>
2015-12-20 11:49:11 +0100

    Trees and terrains are now sorted by percentage

--- scripting/editor/terrain_help.lua
+++ scripting/editor/terrain_help.lua
@@ -1,4 +1,4 @@
--- NOCOM Sort by percentage and localize
+-- NOCOM localize
 include "scripting/formatting.lua"
 
 function picture_li(imagepath, text)
@@ -21,14 +21,29 @@ return {
 
 		local result = picture_li(terrain.representative_image, "")
 
-      local tree_string = ""
+      local tree_list = {}
       for i, tree_name in ipairs(world:immovable_descriptions("tree")) do
 			local probability = terrain:probability_to_grow(tree_name)
-			local tree = wl.Editor():get_immovable_description(tree_name)
 			if (probability > 0.01) then
-				tree_string = tree_string .. picture_li(tree.representative_image, tree.basename .. ("<br>Probability to grow: %2.1f%%"):bformat(100 * probability))
+				-- sort the trees by percentage
+				i = 1
+				while (tree_list[i] and (tree_list[i].probability_ > probability)) do
+					i = i + 1
+				end
+
+				for j = #tree_list, i, -1 do
+					tree_list[j+1] = tree_list[j]
+				end
+				tree_list[i] = {tree_name_ = tree_name, probability_ = probability}
 			end
 		end
+
+		local tree_string = ""
+		for k,v in ipairs(tree_list) do
+			local tree = wl.Editor():get_immovable_description(v.tree_name_)
+			tree_string = tree_string .. picture_li(tree.representative_image, tree.basename .. ("<br>Probability to grow: %2.1f%%"):bformat(100 * v.probability_))
+		end
+
 		if (tree_string ~="") then
 			result = result .. rt(h2("Trees that may grow")) .. tree_string
 		else
--- scripting/editor/tree_help.lua
+++ scripting/editor/tree_help.lua
@@ -1,4 +1,4 @@
--- NOCOM Sort by percentage and localize
+-- NOCOM localize
 include "scripting/formatting.lua"
 
 function picture_li(imagepath, text)
@@ -21,12 +21,30 @@ return {
       end
       ]]
       result = result .. rt(h2("Preferred terrains"))
+		terrain_list = {}
       for i, terrain_name in ipairs(world:terrain_descriptions()) do
 			local terrain = wl.Editor():get_terrain_description(terrain_name)
 			local probability = terrain:probability_to_grow(tree_name)
 			if (probability > 0.01) then
-				-- NOCOM result = result .. rt(h3(terrain.descname))
-				result = result .. picture_li(terrain.representative_image, (_"%1% (%2%)"):bformat(terrain.descname, terrain.editor_category.descname) .. "<br>" .. ("%2.1f%%"):bformat(100 * probability))
+				---- NOCOM result = result .. rt(h3(terrain.descname))
+				-- sort the terrains by percentage
+				i = 1
+				while (terrain_list[i] and (terrain_list[i].probability_ > probability)) do
+					i = i + 1
+				end
+
+				for j = #terrain_list, i, -1 do
+					terrain_list[j+1] = terrain_list[j]
+				end
+				terrain_list[i] = {terrain_name_ = terrain_name, probability_ = probability}
+			end
+		end
+
+		for k,v in ipairs(terrain_list) do
+			local terrain = wl.Editor():get_terrain_description(v.terrain_name_)
+			result = result .. picture_li(terrain.representative_image, (_"%1% (%2%)"):bformat(terrain.descname, terrain.editor_category.descname) .. "<br>" .. ("%2.1f%%"):bformat(100 * v.probability_))
+		end
+
 				--[[ NOCOM
 				result = result .. picture_li(terrain.representative_image,
 							"Fertility:" .. " " .. ("%2.1f%%"):bformat(100 * terrain.fertility) .. "<br>" ..
@@ -34,8 +52,6 @@ return {
 							"Temperature:" .. " " .. terrain.temperature)
 				result = result .. rt(p(("Probability to grow: %2.1f%%"):bformat(100 * probability)))
 				]]
-			end
-		end
       return result
    end
 }
