d38d410890627be4042c381806e7806f9cf7a5c2
Holger Rapp <SirVer@gmx.de>
2016-01-23 15:54:44 +0100

    Correctly crop also scaled rectangles.

--- src/graphic/texture_cache.cc
+++ src/graphic/texture_cache.cc
@@ -24,7 +24,6 @@
 #include <SDL.h>
 
 #include "graphic/texture.h"
-#include "base/log.h" // NOCOM(#sirver): remove again
 
 // The implementation took inspiration from
 // http://timday.bitbucket.org/lru.html, but our use case here is a little
