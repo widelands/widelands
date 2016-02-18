01d4820184c9950637c23283f26829696335b181
GunChleoc <fios@foramnagaidhlig.net>
2016-02-18 14:12:23 +0000

    Refactored struct variable names in ai_help_structs. BuildingObserver::Type is now an enum class.

--- src/ai/ai_help_structs.cc
+++ src/ai/ai_help_structs.cc
@@ -36,7 +36,7 @@ bool FindNodeWithFlagOrRoad::accept(const Map&, FCoords fc) const {
 
 bool CheckStepRoadAI::allowed(
    Map& map, FCoords start, FCoords end, int32_t, CheckStep::StepId const id) const {
-	uint8_t endcaps = player_->get_buildcaps(end);
+	uint8_t endcaps = player->get_buildcaps(end);
 
 	// we should not cross fields with road or flags (or any other immovable)
 	if ((map.get_immovable(start)) && !(id == CheckStep::stepFirst)) {
@@ -44,13 +44,13 @@ bool CheckStepRoadAI::allowed(
 	}
 
 	// Calculate cost and passability
-	if (!(endcaps & movecaps_))
+	if (!(endcaps & movecaps))
 		return false;
 
 	// Check for blocking immovables
 	if (BaseImmovable const* const imm = map.get_immovable(end))
 		if (imm->get_size() >= BaseImmovable::SMALL) {
-			if (id != CheckStep::stepLast && !open_end_)
+			if (id != CheckStep::stepLast && !open_end)
 				return false;
 
 			if (dynamic_cast<Flag const*>(imm))
@@ -66,8 +66,8 @@ bool CheckStepRoadAI::allowed(
 bool CheckStepRoadAI::reachable_dest(Map& map, FCoords const dest) const {
 	NodeCaps const caps = dest.field->nodecaps();
 
-	if (!(caps & movecaps_)) {
-		if (!((movecaps_ & MOVECAPS_SWIM) && (caps & MOVECAPS_WALK)))
+	if (!(caps & movecaps)) {
+		if (!((movecaps & MOVECAPS_SWIM) && (caps & MOVECAPS_WALK)))
 			return false;
 
 		if (!map.can_reach_by_water(dest))
--- src/ai/ai_help_structs.h
+++ src/ai/ai_help_structs.h
@@ -70,19 +70,15 @@ enum class SchedulerTaskId : uint8_t {
 
 struct CheckStepRoadAI {
 	CheckStepRoadAI(Player* const pl, uint8_t const mc, bool const oe)
-	   : player_(pl), movecaps_(mc), open_end_(oe) {
-	}
-
-	void set_openend(bool const oe) {
-		open_end_ = oe;
+		: player(pl), movecaps(mc), open_end(oe) {
 	}
 
 	bool allowed(Map&, FCoords start, FCoords end, int32_t dir, CheckStep::StepId) const;
 	bool reachable_dest(Map&, FCoords dest) const;
 
-	Player* player_;
-	uint8_t movecaps_;
-	bool open_end_;
+	Player* player;
+	uint8_t movecaps;
+	bool open_end;
 };
 
 // We are looking for fields we can walk on
@@ -96,8 +92,7 @@ struct FindNodeEnemy {
 	Player* player;
 	Game& game;
 
-	FindNodeEnemy(Player* p, Game& g) : player(p), game(g) {
-	}
+	FindNodeEnemy(Player* p, Game& g) : player(p), game(g) {}
 };
 
 // We are looking for buildings owned by hostile player
@@ -113,8 +108,7 @@ struct FindNodeEnemiesBuilding {
 	Player* player;
 	Game& game;
 
-	FindNodeEnemiesBuilding(Player* p, Game& g) : player(p), game(g) {
-	}
+	FindNodeEnemiesBuilding(Player* p, Game& g) : player(p), game(g) {}
 };
 
 // When looking for unowned terrain to acquire, we are actually
@@ -124,32 +118,30 @@ struct FindNodeUnowned {
 	bool accept(const Map&, const FCoords& fc) const {
 		return (fc.field->nodecaps() & MOVECAPS_WALK) &&
 		       ((fc.field->get_owned_by() == 0) ||
-		        player_->is_hostile(*game.get_player(fc.field->get_owned_by()))) &&
-		       (!only_enemies_ || (fc.field->get_owned_by() != 0));
+				  player->is_hostile(*game.get_player(fc.field->get_owned_by()))) &&
+				 (!only_enemies || (fc.field->get_owned_by() != 0));
 	}
 
-	Player* player_;
+	Player* player;
 	Game& game;
-	bool only_enemies_;
+	bool only_enemies;
 
-	FindNodeUnowned(Player* p, Game& g, bool oe = false) : player_(p), game(g), only_enemies_(oe) {
-	}
+	FindNodeUnowned(Player* p, Game& g, bool oe = false) : player(p), game(g), only_enemies(oe) {}
 };
 
 // Sometimes we need to know how many nodes our allies owns
 struct FindNodeAllyOwned {
 	bool accept(const Map&, const FCoords& fc) const {
 		return (fc.field->nodecaps() & MOVECAPS_WALK) && (fc.field->get_owned_by() != 0) &&
-		       (fc.field->get_owned_by() != pn) &&
-		       !player_->is_hostile(*game.get_player(fc.field->get_owned_by()));
+				 (fc.field->get_owned_by() != player_number) &&
+				 !player->is_hostile(*game.get_player(fc.field->get_owned_by()));
 	}
 
-	Player* player_;
+	Player* player;
 	Game& game;
-	PlayerNumber pn;
+	PlayerNumber player_number;
 
-	FindNodeAllyOwned(Player* p, Game& g, PlayerNumber n) : player_(p), game(g), pn(n) {
-	}
+	FindNodeAllyOwned(Player* p, Game& g, PlayerNumber n) : player(p), game(g), player_number(n) {}
 };
 
 // When looking for unowned terrain to acquire, we must
@@ -157,15 +149,13 @@ struct FindNodeAllyOwned {
 // Fields should be completely unowned
 struct FindNodeUnownedMineable {
 	bool accept(const Map&, const FCoords& fc) const {
-
 		return (fc.field->nodecaps() & BUILDCAPS_MINE) && (fc.field->get_owned_by() == 0);
 	}
 
-	Player* player_;
+	Player* player;
 	Game& game;
 
-	FindNodeUnownedMineable(Player* p, Game& g) : player_(p), game(g) {
-	}
+	FindNodeUnownedMineable(Player* p, Game& g) : player(p), game(g) {}
 };
 
 // Unowned but walkable fields nearby
@@ -174,10 +164,10 @@ struct FindNodeUnownedWalkable {
 		return (fc.field->nodecaps() & MOVECAPS_WALK) && (fc.field->get_owned_by() == 0);
 	}
 
-	Player* player_;
+	Player* player;
 	Game& game;
 
-	FindNodeUnownedWalkable(Player* p, Game& g) : player_(p), game(g) {}
+	FindNodeUnownedWalkable(Player* p, Game& g) : player(p), game(g) {}
 };
 
 // Looking only for mines-capable fields nearby
@@ -217,14 +207,14 @@ struct FindNodeWithFlagOrRoad {
 
 struct NearFlag {
 	Flag const* flag;
-	int32_t cost_;
-	int32_t distance_;
+	int32_t cost;
+	int32_t distance;
 
-	NearFlag(const Flag& f, int32_t const c, int32_t const d) : flag(&f), cost_(c), distance_(d) {
+	NearFlag(const Flag& f, int32_t const c, int32_t const d) : flag(&f), cost(c), distance(d) {
 	}
 
 	bool operator<(const NearFlag& f) const {
-		return cost_ > f.cost_;
+		return cost > f.cost;
 	}
 
 	bool operator == (Flag const* const f) const {
@@ -234,25 +224,25 @@ struct NearFlag {
 
 struct CompareDistance {
 	bool operator()(const NearFlag& a, const NearFlag& b) const {
-		return a.distance_ < b.distance_;
+		return a.distance < b.distance;
 	}
 };
 
 // ordering nearflags by biggest reduction
 struct CompareShortening {
 	bool operator()(const NearFlag& a, const NearFlag& b) const {
-		return (a.cost_ - a.distance_) > (b.cost_ - b.distance_);
+		return (a.cost - a.distance) > (b.cost - b.distance);
 	}
 };
 
 struct WalkableSpot {
 	Coords coords;
-	bool has_flag_;
+	bool has_flag;
 
-	int32_t cost_;
+	int32_t cost;
 	void* eco;
 
-	int16_t from_;
+	int16_t from;
 	int16_t neighbours[6];
 };
 }
@@ -260,63 +250,63 @@ struct WalkableSpot {
 struct BuildableField {
 	Widelands::FCoords coords;
 
-	uint32_t field_info_expiration_;
+	uint32_t field_info_expiration;
 
-	bool preferred_;
-	bool enemy_nearby_;
+	bool preferred;
+	bool enemy_nearby;
 
-	uint8_t unowned_land_nearby_;
+	uint8_t unowned_land_nearby;
 	// to identify that field is too close to border and no production building should be built there
-	bool near_border_;
-	uint8_t unowned_mines_spots_nearby_;
-	uint8_t trees_nearby_;
-	uint8_t rocks_nearby_;
-	int16_t water_nearby_;
-	int16_t distant_water_;
-	int8_t fish_nearby_;
-	int8_t critters_nearby_;
-	int8_t ground_water_;  // used by wells
-	uint8_t space_consumers_nearby_;
-	uint8_t rangers_nearby_;
+	bool near_border;
+	uint8_t unowned_mines_spots_nearby;
+	uint8_t trees_nearby;
+	uint8_t rocks_nearby;
+	int16_t water_nearby;
+	int16_t distant_water;
+	int8_t fish_nearby;
+	int8_t critters_nearby;
+	int8_t ground_water;  // used by wells
+	uint8_t space_consumers_nearby;
+	uint8_t rangers_nearby;
 	// to manage the military better following variables exists:
 	// capacity of nearby buildings:
-	int16_t area_military_capacity_;
+	int16_t area_military_capacity;
 	// distance to near buldings:
-	int16_t military_loneliness_;
+	int16_t military_loneliness;
 	// count of military buildings in construction
 	// when making decision on new mulitary buildings it considers also
 	// unowned fields and mines, but this information is not quite correct as there
 	// are construction sites that will change this once they are built
-	int16_t military_in_constr_nearby_;
+	int16_t military_in_constr_nearby;
 	// actual count of soldiers in nearby buldings
-	int16_t area_military_presence_;
+	int16_t area_military_presence;
 	// stationed (manned) military buildings nearby
-	int16_t military_stationed_;
+	int16_t military_stationed;
 	// stationed (manned) military buildings nearby
-	int16_t military_unstationed_;
-	bool is_portspace_;
-	bool port_nearby_;  // to increase priority if a port is nearby,
+	int16_t military_unstationed;
+	bool is_portspace;
+	bool port_nearby;  // to increase priority if a port is nearby,
 	// especially for new colonies
-	Widelands::ExtendedBool portspace_nearby_;  // prefer military buildings closer to the portspace
-	int32_t max_buildcap_nearby_;
+	Widelands::ExtendedBool portspace_nearby;  // prefer military buildings closer to the portspace
+	int32_t max_buildcap_nearby;
 	// it is not necessary to check resources (stones, fish...) too frequently as they do not change fast
 	// this stores time of last check
-	uint32_t last_resources_check_time_;
+	uint32_t last_resources_check_time;
 
-	std::vector<uint8_t> consumers_nearby_;
-	std::vector<uint8_t> producers_nearby_;
+	std::vector<uint8_t> consumers_nearby;
+	std::vector<uint8_t> producers_nearby;
 	// and for rangers, fishbreeders:
-	std::vector<uint8_t> supporters_nearby_;
+	std::vector<uint8_t> supporters_nearby;
 
 	BuildableField(const Widelands::FCoords& fc)
 	   : coords(fc),
-	     field_info_expiration_(20000),
-	     preferred_(false),
-	     enemy_nearby_(0),
-	     unowned_land_nearby_(0),
-	     near_border_(false),
-	     unowned_mines_spots_nearby_(0),
-	     trees_nearby_(0),
+		  field_info_expiration(20000),
+		  preferred(false),
+		  enemy_nearby(0),
+		  unowned_land_nearby(0),
+		  near_border(false),
+		  unowned_mines_spots_nearby(0),
+		  trees_nearby(0),
 	     // explanation of starting values
 	     // this is done to save some work for AI (CPU utilization)
 	     // base rules are:
@@ -324,59 +314,56 @@ struct BuildableField {
 	     // is recalculated only when previous count is positive
 	     // count of water fields are stable, so if the current count is
 	     // non-negative, water is not recaldulated
-	     rocks_nearby_(1),
-	     water_nearby_(-1),
-	     distant_water_(0),
-	     fish_nearby_(-1),
-	     critters_nearby_(-1),
-	     ground_water_(1),
-	     space_consumers_nearby_(0),
-	     rangers_nearby_(0),
-	     area_military_capacity_(0),
-	     military_loneliness_(1000),
-	     military_in_constr_nearby_(0),
-	     area_military_presence_(0),
-	     military_stationed_(0),
-	     military_unstationed_(0),
-	     is_portspace_(false),
-	     port_nearby_(false),
-	     portspace_nearby_(Widelands::ExtendedBool::kUnset),
-	     max_buildcap_nearby_(0),
-	     last_resources_check_time_(0) {
+		  rocks_nearby(1),
+		  water_nearby(-1),
+		  distant_water(0),
+		  fish_nearby(-1),
+		  critters_nearby(-1),
+		  ground_water(1),
+		  space_consumers_nearby(0),
+		  rangers_nearby(0),
+		  area_military_capacity(0),
+		  military_loneliness(1000),
+		  military_in_constr_nearby(0),
+		  area_military_presence(0),
+		  military_stationed(0),
+		  military_unstationed(0),
+		  is_portspace(false),
+		  port_nearby(false),
+		  portspace_nearby(Widelands::ExtendedBool::kUnset),
+		  max_buildcap_nearby(0),
+		  last_resources_check_time(0) {
 	}
 
 	int32_t own_military_sites_nearby_() {
-		return military_stationed_ + military_unstationed_;
+		return military_stationed + military_unstationed;
 	}
 };
 
 struct MineableField {
 	Widelands::FCoords coords;
-
-	uint32_t field_info_expiration_;
-
-	bool preferred_;
-
-	int32_t mines_nearby_;
+	uint32_t field_info_expiration;
+	bool preferred;
+	int32_t mines_nearby;
 	// this is to provide that a mine is not built on the edge of mine area
-	int32_t same_mine_fields_nearby_;
+	int32_t same_mine_fields_nearby;
 
 	MineableField(const Widelands::FCoords& fc)
 	   : coords(fc),
-	     field_info_expiration_(20000),
-	     preferred_(false),
-	     mines_nearby_(0),
-	     same_mine_fields_nearby_(0) {
+		  field_info_expiration(20000),
+		  preferred(false),
+		  mines_nearby(0),
+		  same_mine_fields_nearby(0) {
 	}
 };
 
 struct EconomyObserver {
 	Widelands::Economy& economy;
 	std::list<Widelands::Flag const*> flags;
-	int32_t dismantle_grace_time_;
+	int32_t dismantle_grace_time;
 
 	EconomyObserver(Widelands::Economy& e) : economy(e) {
-		dismantle_grace_time_ = std::numeric_limits<int32_t>::max();
+		dismantle_grace_time = std::numeric_limits<int32_t>::max();
 	}
 };
 
@@ -385,107 +372,107 @@ struct BuildingObserver {
 	Widelands::DescriptionIndex id;
 	Widelands::BuildingDescr const* desc;
 
-	enum {
-		BORING,
-		CONSTRUCTIONSITE,
-		PRODUCTIONSITE,
-		MILITARYSITE,
-		WAREHOUSE,
-		TRAININGSITE,
-		MINE
+	enum class Type {
+		kBoring,
+		kConstructionsite,
+		kProductionsite,
+		kMilitarysite,
+		kWarehouse,
+		kTrainingsite,
+		kMine
 	} type;
 
-	bool plants_trees_;
-	bool recruitment_;  // is "producing" workers?
-	Widelands::BuildingNecessity new_building_;
-	uint32_t new_building_overdue_;
-	int32_t primary_priority_;
-	bool is_buildable_;
-	bool need_trees_;   // lumberjack = true
-	bool need_rocks_;  // quarry = true
-	bool mines_water_;  // wells
-	bool need_water_;   // fisher, fish_breeder = true
-	bool is_hunter_;    // need to identify hunters
-	bool is_fisher_;    // need to identify fishers
-	bool is_port_;
-	bool is_shipyard_;
-	bool space_consumer_;       // farm, vineyard... = true
-	bool expansion_type_;       // military building used that can be used to control area
-	bool fighting_type_;        // military building built near enemies
-	bool mountain_conqueror_;   // military building built near mountains
-	uint32_t prohibited_till_;  // do not build before (ms)
-	uint32_t forced_after_;     // do not wait until ware is needed
-	TrainingSiteType trainingsite_type_;
+	bool plants_trees;
+	bool recruitment;  // is "producing" workers?
+	Widelands::BuildingNecessity new_building;
+	uint32_t new_building_overdue;
+	int32_t primary_priority;
+	bool is_buildable;
+	bool need_trees;   // lumberjack = true
+	bool need_rocks;  // quarry = true
+	bool mines_water;  // wells
+	bool need_water;   // fisher, fish_breeder = true
+	bool is_hunter;    // need to identify hunters
+	bool is_fisher;    // need to identify fishers
+	bool is_port;
+	bool is_shipyard;
+	bool space_consumer;       // farm, vineyard... = true
+	bool expansion_type;       // military building used that can be used to control area
+	bool fighting_type;        // military building built near enemies
+	bool mountain_conqueror;   // military building built near mountains
+	uint32_t prohibited_till;  // do not build before (ms)
+	uint32_t forced_after;     // do not wait until ware is needed
+	TrainingSiteType trainingsite_type;
 
-	uint16_t unconnected_count_;  // to any warehouse (count of such buildings)
+	uint16_t unconnected_count;  // to any warehouse (count of such buildings)
 
-	int32_t mines_;           // type of resource it mines_
-	uint16_t mines_percent_;  // % of res it can mine
-	uint32_t current_stats_;
+	int32_t mines;           // type of resource it mines_
+	uint16_t mines_percent;  // % of res it can mine
+	uint32_t current_stats;
 
-	std::vector<int16_t> inputs_;
-	std::vector<int16_t> outputs_;
-	std::vector<Widelands::DescriptionIndex> critical_built_mat_;
+	std::vector<int16_t> inputs;
+	std::vector<int16_t> outputs;
+	std::vector<Widelands::DescriptionIndex> critical_building_material;
 
-	bool built_mat_producer_;
+	bool produces_building_material;
 
 	// an enhancement to this building:
 	// produces all wares as current building, and perhaps more
-	bool upgrade_substitutes_;
+	bool upgrade_substitutes;
 	// produces some additional wares
-	bool upgrade_extends_;
+	bool upgrade_extends;
 
 	// It seems that fish and meat are subsitutes (for trainingsites), so
 	// when testing if a trainingsite is supplied enough
 	// we count the wares together
-	std::unordered_set<Widelands::DescriptionIndex> substitute_inputs_;
-	int32_t substitutes_count_;
+	std::unordered_set<Widelands::DescriptionIndex> substitute_inputs;
+	int32_t substitutes_count;
 
-	int16_t production_hint_;
+	int16_t production_hint;
 
 	// information needed for decision on new building construction
-	int16_t max_preciousness_;
-	int16_t max_needed_preciousness_;
+	int16_t max_preciousness;
+	int16_t max_needed_preciousness;
 
-	int32_t cnt_built_;
-	int32_t cnt_under_construction_;
-	int32_t cnt_target_;  // number of buildings as target
-	int32_t cnt_limit_by_aimode_; // limit imposed by weak or normal AI mode
+	int32_t cnt_built;
+	int32_t cnt_under_construction;
+	int32_t cnt_target;  // number of buildings as target
+	int32_t cnt_limit_by_aimode; // limit imposed by weak or normal AI mode
 
 	// used to track amount of wares produced by building
-	uint32_t stocklevel_;
+	uint32_t stocklevel;
 	uint32_t stocklevel_time;  // time when stocklevel_ was last time recalculated
-	uint32_t last_dismantle_time_;
-	uint32_t construction_decision_time_;
-	uint32_t last_building_built_;
+	uint32_t last_dismantle_time;
+	uint32_t construction_decision_time;
+	uint32_t last_building_built;
 
-	uint32_t unoccupied_count_;
+	uint32_t unoccupied_count;
 
-	bool build_material_shortage_;
+	bool build_material_shortage;
 
 	int32_t total_count() const {
-		return cnt_built_ + cnt_under_construction_;
+		return cnt_built + cnt_under_construction;
 	}
 	Widelands::AiModeBuildings aimode_limit_status() {
-		if (total_count() > cnt_limit_by_aimode_) {
+		if (total_count() > cnt_limit_by_aimode) {
 			return Widelands::AiModeBuildings::kLimitExceeded;
-		} else if (total_count() == cnt_limit_by_aimode_) {
+		} else if (total_count() == cnt_limit_by_aimode) {
 			return Widelands::AiModeBuildings::kOnLimit;
 		} else {
 			return Widelands::AiModeBuildings::kAnotherAllowed;
 		}
 	}
-	bool buildable(Widelands::Player& player_) {
-		return is_buildable_ && player_.is_building_type_allowed(id);
+	bool buildable(Widelands::Player& p) {
+		return is_buildable && p.is_building_type_allowed(id);
 	}
 };
 
 struct ProductionSiteObserver {
 	Widelands::ProductionSite* site;
-	uint32_t built_time_;
-	uint32_t unoccupied_till_;
-	uint8_t stats_zero_;
-	uint32_t no_resources_since_;
+	uint32_t built_time;
+	uint32_t unoccupied_till;
+	uint8_t stats_zero;
+	uint32_t no_resources_since;
 	BuildingObserver* bo;
 };
 
@@ -495,7 +482,7 @@ struct MilitarySiteObserver {
 	uint8_t checks;
 	// when considering attack most military sites are inside territory and should be skipped during
 	// evaluation
-	bool enemies_nearby_;
+	bool enemies_nearby;
 };
 
 struct TrainingSiteObserver {
@@ -510,8 +497,8 @@ struct WarehouseSiteObserver {
 
 struct ShipObserver {
 	Widelands::Ship* ship;
-	Widelands::Coords expedition_start_point_;
-	std::unordered_set<uint32_t> visited_spots_;
+	Widelands::Coords expedition_start_point;
+	std::unordered_set<uint32_t> visited_spots;
 
 	// a ship circumvents all islands in the same direction, the value
 	// is assigned only once
@@ -521,9 +508,9 @@ struct ShipObserver {
 };
 
 struct WareObserver {
-	uint8_t producers_;
-	uint8_t consumers_;
-	uint8_t preciousness_;
+	uint8_t producers;
+	uint8_t consumers;
+	uint8_t preciousness;
 };
 
 // Computer player does not get notification messages about enemy militarysites
@@ -533,7 +520,7 @@ struct WareObserver {
 // Also AI test more such targets when considering attack and calculated score is
 // is stored in the observer
 struct EnemySiteObserver {
-	bool warehouse_;
+	bool is_warehouse;
 	int32_t attack_soldiers_strength;
 	int32_t defenders_strength;
 	uint8_t stationed_soldiers;
@@ -544,7 +531,7 @@ struct EnemySiteObserver {
 	int16_t no_attack_counter;
 
 	EnemySiteObserver()
-	   : warehouse_(false),
+		: is_warehouse(false),
 	     attack_soldiers_strength(0),
 	     defenders_strength(0),
 	     stationed_soldiers(0),
