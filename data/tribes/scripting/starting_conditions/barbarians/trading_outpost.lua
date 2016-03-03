53a2e1d0e1daed53f3f209f27e2528b8ed294db9
GunChleoc <fios@foramnagaidhlig.net>
2016-03-03 08:42:18 +0000

    Added sawmill / wood hardener and fixed Lua tabs.

--- data/tribes/scripting/starting_conditions/barbarians/trading_outpost.lua
+++ data/tribes/scripting/starting_conditions/barbarians/trading_outpost.lua
@@ -11,97 +11,104 @@ return {
    tooltip = _"Never run out of important wares for long",
    func = function(player, shared_in_start)
 
-   local sf = wl.Game().map.player_slots[player.number].starting_field
-   if shared_in_start then
-      sf = shared_in_start
-   else
-      player:allow_workers("all")
-   end
+      local sf = wl.Game().map.player_slots[player.number].starting_field
+      if shared_in_start then
+         sf = shared_in_start
+      else
+         player:allow_workers("all")
+      end
 
-   hq = prefilled_buildings(player, { "barbarians_headquarters", sf.x, sf.y,
-      wares = {
-         ax = 5,
-         bread_paddle = 2,
-         blackwood = 32,
-         cloth = 5,
-         coal = 12,
-         felling_ax = 4,
-         fire_tongs = 2,
-         fish = 6,
-         fishing_rod = 2,
-         gold = 14,
-         grout = 12,
-         hammer = 12,
-         hunting_spear = 2,
-         iron = 12,
-         iron_ore = 5,
-         kitchen_tools = 4,
-         meal = 4,
-         meat = 6,
-         pick = 8,
-         barbarians_bread = 8,
-         ration = 12,
-         granite = 40,
-         scythe = 6,
-         shovel = 4,
-         snack = 3,
-         thatch_reed = 24,
-         log = 80,
-      },
-      workers = {
-         barbarians_blacksmith = 2,
-         barbarians_blacksmith_master = 1,
-         barbarians_brewer = 1,
-         barbarians_brewer_master = 1,
-         barbarians_builder = 10,
-         barbarians_charcoal_burner = 1,
-         barbarians_carrier = 40,
-         barbarians_gardener = 1,
-         barbarians_geologist = 4,
-         barbarians_lime_burner = 1,
-         barbarians_lumberjack = 3,
-         barbarians_miner = 4,
-         barbarians_ranger = 1,
-         barbarians_stonemason = 2,
-         barbarians_ox = 5,
-      },
-      -- Max health: 3, Max attack: 5, Max defense: 0, Max evade: 2
-      soldiers = {
-         [{0,0,0,0}] = 35,
-         [{1,0,0,1}] = 5,
-         [{1,1,0,0}] = 5,
-      }
-   })
-   place_building_in_region(player, "barbarians_tower", sf:region(13), {
-      soldiers = {
-         [{0,0,0,0}] = 1,
-      },
+      hq = prefilled_buildings(player, { "barbarians_headquarters", sf.x, sf.y,
+         wares = {
+            ax = 5,
+            bread_paddle = 2,
+            blackwood = 32,
+            cloth = 5,
+            coal = 12,
+            felling_ax = 4,
+            fire_tongs = 2,
+            fish = 6,
+            fishing_rod = 2,
+            gold = 14,
+            grout = 12,
+            hammer = 12,
+            hunting_spear = 2,
+            iron = 12,
+            iron_ore = 5,
+            kitchen_tools = 4,
+            meal = 4,
+            meat = 6,
+            pick = 8,
+            barbarians_bread = 8,
+            ration = 12,
+            granite = 40,
+            scythe = 6,
+            shovel = 4,
+            snack = 3,
+            thatch_reed = 24,
+            log = 80,
+         },
+         workers = {
+            barbarians_blacksmith = 2,
+            barbarians_blacksmith_master = 1,
+            barbarians_brewer = 1,
+            barbarians_brewer_master = 1,
+            barbarians_builder = 10,
+            barbarians_charcoal_burner = 1,
+            barbarians_carrier = 40,
+            barbarians_gardener = 1,
+            barbarians_geologist = 4,
+            barbarians_lime_burner = 1,
+            barbarians_lumberjack = 3,
+            barbarians_miner = 4,
+            barbarians_ranger = 1,
+            barbarians_stonemason = 2,
+            barbarians_ox = 5,
+         },
+         -- Max health: 3, Max attack: 5, Max defense: 0, Max evade: 2
+         soldiers = {
+            [{0,0,0,0}] = 35,
+            [{1,0,0,1}] = 5,
+            [{1,1,0,0}] = 5,
+         }
       })
 
-   place_building_in_region(player, "barbarians_metal_workshop", sf:region(11), {
-      wares = {
-         iron = 8,
-         log = 8
-      }
-   })
+      place_building_in_region(player, "barbarians_tower", sf:region(13), {
+         soldiers = {
+            [{0,0,0,0}] = 1,
+         },
+      })
 
-   place_building_in_region(player, "barbarians_hunters_hut", sf:region(11), {
-      wares = {}
-   })
+      place_building_in_region(player, "barbarians_metal_workshop", sf:region(11), {
+         wares = {
+            iron = 8,
+            log = 8
+         }
+      })
 
-   -- Get all warehouse types
-    local plr = wl.Game().players[player.number]
-    local warehouse_types = {}
-    for i, building_name in ipairs(wl.Game():get_tribe_description(plr.tribe_name).buildings) do
+      place_building_in_region(player, "barbarians_wood_hardener", sf:region(11), {
+         wares = {
+            log = 8
+         }
+      })
+
+      place_building_in_region(player, "barbarians_hunters_hut", sf:region(11), {
+         wares = {}
+      })
+
+      -- Get all warehouse types
+      local plr = wl.Game().players[player.number]
+      local warehouse_types = {}
+      for i, building_name in ipairs(wl.Game():get_tribe_description(plr.tribe_name).buildings) do
       if (wl.Game():get_building_description(building_name).type_name == "warehouse") then
          table.insert(warehouse_types, building_name)
       end
-    end
+      end
 
-    -- index of a warehouse we will add to. Used to 'rotate' warehouses
-    local idx = 1
+      -- index of a warehouse we will add to. Used to 'rotate' warehouses
+      local idx = 1
 
-    for i=1,100000 do
+      for i=1,100000 do
       sleep(300000)
 
       -- collect all ~warehouses and pick one to insert the wares
@@ -117,52 +124,52 @@ return {
             idx = 1
          end
 
-          local wh = warehouses[idx]
-          local added = 0
+         local wh = warehouses[idx]
+         local added = 0
 
-          if plr:get_wares("water") < 40 + #warehouses * 10 then
-              wh:set_wares("water", wh:get_wares("water") + 20 + #warehouses * 2)
-              added = added + 1
-          end
-          if plr:get_wares("log") < 40 + #warehouses * 10 then
-              wh:set_wares("log", wh:get_wares("log") + 20)
-              added = added + 1
-          end
-          if plr:get_wares("granite") < 30 + #warehouses * 10 then
-              wh:set_wares("granite", wh:get_wares("granite") +  10 + #warehouses * 2)
-              added = added + 1
-          end
-          if plr:get_wares("coal") < 70 + #warehouses * 10 then
-              wh:set_wares("coal", wh:get_wares("coal") + 25 + #warehouses * 5)
-              added = added + 1
-          end
-          if plr:get_wares("iron_ore") < 30 + #warehouses * 10 then
-              wh:set_wares("iron_ore", wh:get_wares("iron_ore") + 10 + #warehouses * 2)
-              added = added + 1
-          end
-          if wh:get_wares("fish") < 40 + #warehouses * 5 then
-              wh:set_wares("fish", wh:get_wares("fish") + 10)
+         if plr:get_wares("water") < 40 + #warehouses * 10 then
+            wh:set_wares("water", wh:get_wares("water") + 20 + #warehouses * 2)
             added = added + 1
-          end
-          if plr:get_wares("gold") < 20 + #warehouses * 5 then
-              wh:set_wares("gold", wh:get_wares("gold") + 3)
-              added = added + 1
-          end
-          if plr:get_wares("wheat") < 60 + #warehouses * 10 then
-              wh:set_wares("wheat", wh:get_wares("wheat") + 15 + #warehouses * 2)
-              added = added + 1
-          end
-          if plr:get_wares("barbarians_bread") < 40 + #warehouses * 5 then
-              wh:set_wares("barbarians_bread", wh:get_wares("barbarians_bread") + 8 + #warehouses * 2)
-              added = added + 1
-          end
+         end
+         if plr:get_wares("log") < 40 + #warehouses * 10 then
+            wh:set_wares("log", wh:get_wares("log") + 20)
+            added = added + 1
+         end
+         if plr:get_wares("granite") < 30 + #warehouses * 10 then
+            wh:set_wares("granite", wh:get_wares("granite") +  10 + #warehouses * 2)
+            added = added + 1
+         end
+         if plr:get_wares("coal") < 70 + #warehouses * 10 then
+            wh:set_wares("coal", wh:get_wares("coal") + 25 + #warehouses * 5)
+            added = added + 1
+         end
+         if plr:get_wares("iron_ore") < 30 + #warehouses * 10 then
+            wh:set_wares("iron_ore", wh:get_wares("iron_ore") + 10 + #warehouses * 2)
+            added = added + 1
+         end
+         if wh:get_wares("fish") < 40 + #warehouses * 5 then
+            wh:set_wares("fish", wh:get_wares("fish") + 10)
+            added = added + 1
+         end
+         if plr:get_wares("gold") < 20 + #warehouses * 5 then
+            wh:set_wares("gold", wh:get_wares("gold") + 3)
+            added = added + 1
+         end
+         if plr:get_wares("wheat") < 60 + #warehouses * 10 then
+            wh:set_wares("wheat", wh:get_wares("wheat") + 15 + #warehouses * 2)
+            added = added + 1
+         end
+         if plr:get_wares("barbarians_bread") < 40 + #warehouses * 5 then
+            wh:set_wares("barbarians_bread", wh:get_wares("barbarians_bread") + 8 + #warehouses * 2)
+            added = added + 1
+         end
 
          if (added > 0) then
             print (player.number..": "..added.." types of ware added to warehouse "..idx.." of "..#warehouses.." (cheating mode)")
          end
 
-          idx = idx + 1
+         idx = idx + 1
       end
-    end
+   end
 end
 }
--- data/tribes/scripting/starting_conditions/empire/trading_outpost.lua
+++ data/tribes/scripting/starting_conditions/empire/trading_outpost.lua
@@ -11,78 +11,78 @@ return {
    tooltip = _"Never run out of important wares for long",
    func = function(player, shared_in_start)
 
-   local sf = wl.Game().map.player_slots[player.number].starting_field
+      local sf = wl.Game().map.player_slots[player.number].starting_field
 
-   if shared_in_start then
-      sf = shared_in_start
-   else
-      player:allow_workers("all")
-   end
+      if shared_in_start then
+         sf = shared_in_start
+      else
+         player:allow_workers("all")
+      end
 
-   prefilled_buildings(player, { "empire_headquarters", sf.x, sf.y,
-      wares = {
-         armor_helmet = 4,
-         spear_wooden = 5,
-         felling_ax = 6,
-         bread_paddle = 2,
-         basket = 2,
-         empire_bread = 8,
-         cloth = 5,
-         coal = 12,
-         fire_tongs = 2,
-         fish = 6,
-         fishing_rod = 2,
-         flour = 4,
-         gold = 14,
-         grape = 4,
-         hammer = 14,
-         hunting_spear = 2,
-         iron = 12,
-         iron_ore = 5,
-         kitchen_tools = 4,
-         marble = 25,
-         marble_column = 6,
-         meal = 4,
-         meat = 6,
-         pick = 8,
-         ration = 12,
-         saw = 2,
-         scythe = 5,
-         shovel = 6,
-         granite = 40,
-         log = 30,
-         water = 12,
-         wheat = 4,
-         wine = 8,
-         planks = 45,
-         wool = 2,
-      },
-      workers = {
-         empire_armorsmith = 1,
-         empire_brewer = 1,
-         empire_builder = 10,
-         empire_carrier = 40,
-         empire_charcoal_burner = 1,
-         empire_geologist = 4,
-         empire_lumberjack = 3,
-         empire_miner = 4,
-         empire_stonemason = 2,
-         empire_toolsmith = 2,
-         empire_weaponsmith = 1,
-         empire_donkey = 5,
-      },
-      -- Max health: 4, Max attack: 4, Max defense: 0, Max evade: 2
-      soldiers = {
-         [{0,0,0,0}] = 35,
-         [{1,0,0,1}] = 5,
-         [{1,1,0,0}] = 5,
-      }
-   })
+      prefilled_buildings(player, { "empire_headquarters", sf.x, sf.y,
+         wares = {
+            armor_helmet = 4,
+            spear_wooden = 5,
+            felling_ax = 6,
+            bread_paddle = 2,
+            basket = 2,
+            empire_bread = 8,
+            cloth = 5,
+            coal = 12,
+            fire_tongs = 2,
+            fish = 6,
+            fishing_rod = 2,
+            flour = 4,
+            gold = 14,
+            grape = 4,
+            hammer = 14,
+            hunting_spear = 2,
+            iron = 12,
+            iron_ore = 5,
+            kitchen_tools = 4,
+            marble = 25,
+            marble_column = 6,
+            meal = 4,
+            meat = 6,
+            pick = 8,
+            ration = 12,
+            saw = 2,
+            scythe = 5,
+            shovel = 6,
+            granite = 40,
+            log = 30,
+            water = 12,
+            wheat = 4,
+            wine = 8,
+            planks = 45,
+            wool = 2,
+         },
+         workers = {
+            empire_armorsmith = 1,
+            empire_brewer = 1,
+            empire_builder = 10,
+            empire_carrier = 40,
+            empire_charcoal_burner = 1,
+            empire_geologist = 4,
+            empire_lumberjack = 3,
+            empire_miner = 4,
+            empire_stonemason = 2,
+            empire_toolsmith = 2,
+            empire_weaponsmith = 1,
+            empire_donkey = 5,
+         },
+         -- Max health: 4, Max attack: 4, Max defense: 0, Max evade: 2
+         soldiers = {
+            [{0,0,0,0}] = 35,
+            [{1,0,0,1}] = 5,
+            [{1,1,0,0}] = 5,
+         }
+      })
 
       place_building_in_region(player, "empire_outpost", sf:region(13), {
-      soldiers = {
-         [{0,0,0,0}] = 1,
-      },
+         soldiers = {
+            [{0,0,0,0}] = 1,
+         },
       })
 
       place_building_in_region(player, "empire_toolsmithy", sf:region(11), {
@@ -92,24 +92,30 @@ return {
          }
       })
 
+      place_building_in_region(player, "empire_sawmill", sf:region(11), {
+         wares = {
+            log = 8
+         }
+      })
+
       place_building_in_region(player, "empire_hunters_house", sf:region(11), {
          wares = {}
       })
 
       -- Get all warehouse types
-    local plr = wl.Game().players[player.number]
-    local warehouse_types = {}
-    for i, building_name in ipairs(wl.Game():get_tribe_description(plr.tribe_name).buildings) do
+      local plr = wl.Game().players[player.number]
+      local warehouse_types = {}
+      for i, building_name in ipairs(wl.Game():get_tribe_description(plr.tribe_name).buildings) do
       if (wl.Game():get_building_description(building_name).type_name == "warehouse") then
          table.insert(warehouse_types, building_name)
       end
-    end
+      end
 
-    -- index of a warehouse we will add to. Used to 'rotate' warehouses
-    local idx = 1
+      -- index of a warehouse we will add to. Used to 'rotate' warehouses
+      local idx = 1
 
-   for i=1,100000 do
-     sleep(300000)
+      for i=1,100000 do
+      sleep(300000)
 
       -- collect all ~warehouses and pick one to insert the wares
       local warehouses = {}
@@ -124,44 +130,48 @@ return {
             idx = 1
          end
 
-          local wh = warehouses[idx]
-          local added = 0
+         local wh = warehouses[idx]
+         local added = 0
 
          if wh:get_wares("water") < 100 then
             wh:set_wares("water", wh:get_wares("water") + 20)
-          end
-          if wh:get_wares("log") < 100 then
+         end
+         if wh:get_wares("log") < 100 then
             wh:set_wares("log", wh:get_wares("log") + 10)
-          end
-          if wh:get_wares("granite") < 100 then
+         end
+         if wh:get_wares("granite") < 100 then
             wh:set_wares("granite", wh:get_wares("granite") + 5 + #warehouses)
-          end
-          if wh:get_wares("coal") < 100 then
+         end
+         if wh:get_wares("coal") < 100 then
             wh:set_wares("coal", wh:get_wares("coal") + 5)
-          end
-          if wh:get_wares("iron_ore") < 100 then
+         end
+         if wh:get_wares("iron_ore") < 100 then
             wh:set_wares("iron_ore", wh:get_wares("iron_ore") + 5)
-          end
-          if wh:get_wares("marble") < 100 then
+         end
+         if wh:get_wares("marble") < 100 then
             wh:set_wares("marble", wh:get_wares("marble") + 5)
-          end
-          if wh:get_wares("fish") < 50 then
+         end
+         if wh:get_wares("fish") < 50 then
             wh:set_wares("fish", wh:get_wares("fish") + 1)
-          end
-          if wh:get_wares("gold") < 50 then
+         end
+         if wh:get_wares("gold") < 50 then
             wh:set_wares("gold", wh:get_wares("gold") + 1)
-          end
-          if plr:get_wares("wheat") < 60 + #warehouses * 10 then
-              wh:set_wares("wheat", wh:get_wares("wheat") + 10 + #warehouses * 2)
-              added = added + 1
-          end
-          if plr:get_wares("flour") < 30 + #warehouses * 10 then
-              wh:set_wares("flour", wh:get_wares("flour") + #warehouses * 5)
-              added = added + 1
-          end
-          print (player.number..": "..added.." types of ware added to warehouse "..idx.." of "..#warehouses.." (cheating mode)")
+         end
+         if plr:get_wares("wheat") < 60 + #warehouses * 10 then
+            wh:set_wares("wheat", wh:get_wares("wheat") + 10 + #warehouses * 2)
+            added = added + 1
+         end
+         if plr:get_wares("flour") < 30 + #warehouses * 10 then
+            wh:set_wares("flour", wh:get_wares("flour") + #warehouses * 5)
+            added = added + 1
+         end
+
+         if (added > 0) then
+            print (player.number..": "..added.." types of ware added to warehouse "..idx.." of "..#warehouses.." (cheating mode)")
+         end
+
          idx = idx + 1
       end
-    end
+   end
 end
 }
