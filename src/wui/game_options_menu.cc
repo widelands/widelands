c5a17aabb12fa5860ac6273bca2c9ce84e5fb77a
GunChleoc <fios@foramnagaidhlig.net>
2016-03-21 12:34:53 +0000

    Removed readme buttons.

--- src/wui/game_options_menu.cc
+++ src/wui/game_options_menu.cc
@@ -27,7 +27,6 @@
 #include "base/i18n.h"
 #include "graphic/graphic.h"
 #include "sound/sound_handler.h"
-#include "ui_fsmenu/fileview.h"
 #include "wui/game_main_menu_save_game.h"
 #include "wui/game_options_sound_menu.h"
 #include "wui/unique_window_handler.h"
@@ -71,27 +70,6 @@ GameOptionsMenu::GameOptionsMenu
 	windows_(windows),
 	box_(this, margin, margin, UI::Box::Vertical,
 		  width, get_h() - 2 * margin, vspacing),
-	readme_
-		(&box_, "readme",
-		 0, 0, width, 0,
-		 g_gr->images().get("images/ui_basic/but4.png"),
-		 _("README"),
-		/** TRANSLATORS: Button tooltip */
-		_("Show general information about Widelands and keyboard shortcuts")),
-	license_
-		(&box_, "license",
-		 0, 0, width, 0,
-		 g_gr->images().get("images/ui_basic/but4.png"),
-		 _("License"),
-		/** TRANSLATORS: Button tooltip */
-		_("Show the distribution licence document")),
-	authors_
-		(&box_, "authors",
-		 0, 0, width, 0,
-		 g_gr->images().get("images/ui_basic/but4.png"),
-		 _("Authors"),
-		/** TRANSLATORS: Button tooltip */
-		_("Show information about the Widelands Development Team")),
 	sound_
 		(&box_, "sound_options",
 		 0, 0, width, 0,
@@ -114,49 +92,22 @@ GameOptionsMenu::GameOptionsMenu
 		 /** TRANSLATORS: Button tooltip */
 		 _("Exit Game"))
 {
-	box_.add(&readme_, UI::Align::kHCenter);
-	box_.add(&license_, UI::Align::kHCenter);
-	box_.add(&authors_, UI::Align::kHCenter);
-	box_.add_space(vgap);
 	box_.add(&sound_, UI::Align::kHCenter);
 	box_.add_space(vgap);
 	box_.add(&save_game_, UI::Align::kHCenter);
 	box_.add(&exit_game_, UI::Align::kHCenter);
-	box_.set_size(width, 4 * readme_.get_h() + 2 * save_game_.get_h() + 2 * vgap + 7 * vspacing);
+	box_.set_size(width, sound_.get_h() + 2 * save_game_.get_h() + vgap + 3 * vspacing);
 	set_inner_size(get_inner_w(), box_.get_h() + 2 * margin);
 
-	readme_.sigclicked.connect
-		(boost::bind(&UI::UniqueWindow::Registry::toggle, boost::ref(windows_.readme)));
-	license_.sigclicked.connect
-		(boost::bind(&UI::UniqueWindow::Registry::toggle, boost::ref(windows_.license)));
-	authors_.sigclicked.connect
-		(boost::bind(&UI::UniqueWindow::Registry::toggle, boost::ref(windows_.authors)));
 	sound_.sigclicked.connect(boost::bind(&GameOptionsMenu::clicked_sound, boost::ref(*this)));
 	save_game_.sigclicked.connect(boost::bind(&GameOptionsMenu::clicked_save_game, boost::ref(*this)));
 	exit_game_.sigclicked.connect(boost::bind(&GameOptionsMenu::clicked_exit_game, boost::ref(*this)));
-
-
-	windows_.readme.open_window = boost::bind
-		(&fileview_window, boost::ref(igb_),
-		 boost::ref(windows_.readme),
-		 "txts/README.lua");
-	windows_.license.open_window = boost::bind
-		(&fileview_window, boost::ref(igb_),
-		 boost::ref(windows_.license),
-		 "txts/LICENSE.lua");
-	windows_.authors.open_window = boost::bind
-		(&fileview_window, boost::ref(igb_),
-		 boost::ref(windows_.license),
-		 "txts/AUTHORS.lua");
 
 #define INIT_BTN_HOOKS(registry, btn)                                        \
  registry.on_create = std::bind(&UI::Button::set_perm_pressed, &btn, true);  \
  registry.on_delete = std::bind(&UI::Button::set_perm_pressed, &btn, false); \
  if (registry.window) btn.set_perm_pressed(true);                            \
 
-	INIT_BTN_HOOKS(windows_.readme, readme_)
-	INIT_BTN_HOOKS(windows_.license, license_)
-	INIT_BTN_HOOKS(windows_.authors, authors_)
 	INIT_BTN_HOOKS(windows_.sound_options, sound_)
 
 	if (get_usedefaultpos())
