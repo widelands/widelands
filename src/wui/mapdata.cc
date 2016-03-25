15f1d8e31c0a9894a55862c6b19c8f3266b0f10a
GunChleoc <fios@foramnagaidhlig.net>
2016-03-25 09:45:18 +0000

    Fixed localization markers & "official" tag.

--- src/wui/mapdata.cc
+++ src/wui/mapdata.cc
@@ -32,7 +32,8 @@ MapData::MapData() : authors(""), nrplayers(0), width(0), height(0),
 		filename = init_filename;
 		name = map.get_name();
 		localized_name = name.empty() ? "" : _(name);
-		authors = MapAuthorData(map.get_author());
+		// Localizing this, because some author fields now have "edited by" text.
+		authors = MapAuthorData(_(map.get_author()));
 		description = map.get_description().empty() ? "" : _(map.get_description());
 		hint = map.get_hint().empty() ? "" : _(map.get_hint());
 		nrplayers = map.get_nrplayers();
