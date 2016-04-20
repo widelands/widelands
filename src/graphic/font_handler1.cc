de60bde810a03bc30e0c717ffb1a850a203bd0f9
GunChleoc <fios@foramnagaidhlig.net>
2016-04-20 10:23:01 +0100

    Disabled the texture cache.

--- src/graphic/font_handler1.cc
+++ src/graphic/font_handler1.cc
@@ -81,12 +81,15 @@ public:
 
 private:
 	Texture* texture() const {
+		return get_renderer_()->render(text_, width_);
+		/* NOCOM
 		Texture* surf = texture_cache_->get(hash_);
 		if (surf)
 			return surf;
 
 		return texture_cache_->insert(
 		   hash_, std::unique_ptr<Texture>(get_renderer_()->render(text_, width_)));
+			*/
 	}
 
 	const string hash_;
