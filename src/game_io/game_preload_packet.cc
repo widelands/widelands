4442d8fdb9110c871da7a2dbbd171209b879d7c4
GunChleoc <fios@foramnagaidhlig.net>
2016-04-25 08:32:51 +0100

    Obeservers now have the same minimap flags as players.

--- src/game_io/game_preload_packet.cc
+++ src/game_io/game_preload_packet.cc
@@ -127,11 +127,11 @@ void GamePreloadPacket::write
 
 	std::unique_ptr< ::StreamWrite> sw(fs.open_stream_write(kMinimapFilename));
 	if (sw.get() != nullptr) {
+		const MiniMapLayer flags = MiniMapLayer::Owner | MiniMapLayer::Building | MiniMapLayer::Terrain;
 		if (ipl != nullptr) {  // Player
-			const MiniMapLayer flags = MiniMapLayer::Owner | MiniMapLayer::Building | MiniMapLayer::Terrain;
 			write_minimap_image(game, &ipl->player(), ipl->get_viewpoint(), flags, sw.get());
 		} else { // Observer
-			write_minimap_image(game, nullptr, Point(0, 0), MiniMapLayer::Terrain, sw.get());
+			write_minimap_image(game, nullptr, Point(0, 0), flags, sw.get());
 		}
 		sw->flush();
 	}
