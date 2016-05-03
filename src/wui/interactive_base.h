00e2aad876ad0b177df85fb6e9a28b1310d8e540
GunChleoc <fios@foramnagaidhlig.net>
2016-05-03 09:45:23 +0100

    Save map location markers.

--- src/wui/interactive_base.h
+++ src/wui/interactive_base.h
@@ -37,6 +37,7 @@
 #include "wui/edge_overlay_manager.h"
 #include "wui/field_overlay_manager.h"
 #include "wui/mapview.h"
+#include "wui/quicknavigation.h"
 
 namespace Widelands {struct CoordPath;}
 
@@ -143,6 +144,11 @@ public:
 	}
 
 	void toggle_minimap();
+
+	// Returns the list of landmarks that have been mapped to the keys 0-9
+	const std::vector<QuickNavigation::Landmark>& landmarks();
+	// Sets the landmark for the keyboard 'key' to 'point'
+	void set_landmark(size_t key, const Point& point);
 
 protected:
 	// Will be called whenever the buildhelp is changed with the new 'value'.
