17b72ce22b16fff949890a04faee7d795487fd19
GunChleoc <fios@foramnagaidhlig.net>
2016-08-29 14:47:07 +0100

    Shifted common "text_line" function into data/scripting/formatting.lua

--- data/scripting/formatting.lua
+++ data/scripting/formatting.lua
@@ -237,3 +237,26 @@ end
 function dl(dt, dd)
    return b(dt) .. " " .. dd .. "<br>"
 end
+
+-- RST
+-- .. function:: text_line(t1, t2[, imgstr = nil])
+--
+--    Creates a line of h3 formatted text followed by normal text and an image.
+--
+--    :arg t1: text in h3 format.
+--    :arg t2: text in p format.
+--    :arg imgstr: image aligned right.
+--    :returns: header followed by normal text and image.
+--
+function text_line(t1, t2, imgstr)
+   if imgstr then
+      return "<rt text-align=left image=" .. imgstr ..
+         " image-align=right><p font-size=13 font-color=D1D1D1>" ..
+         t1 .. "</p><p line-spacing=3 font-size=12>" ..
+         t2 .. "<br></p><p font-size=8> <br></p></rt>"
+   else
+      return "<rt text-align=left><p font-size=13 font-color=D1D1D1>" ..
+         t1 .. "</p><p line-spacing=3 font-size=12>" ..
+         t2 .. "<br></p><p font-size=8> <br></p></rt>"
+   end
+end
--- data/tribes/scripting/help/building_help.lua
+++ data/tribes/scripting/help/building_help.lua
@@ -9,29 +9,6 @@ include "tribes/scripting/help/format_help.lua"
 -- Functions used in the ingame building help windows for formatting the text and pictures.
 
 --  =======================================================
---  *************** Basic helper functions ****************
---  =======================================================
-
--- RST
--- .. function:: text_line(t1, t2[, imgstr = nil])
---
---    Creates a line of h3 formatted text followed by normal text and an image.
---
---    :arg t1: text in h3 format.
---    :arg t2: text in p format.
---    :arg imgstr: image aligned right.
---    :returns: header followed by normal text and image.
---
-function text_line(t1, t2, imgstr)
-   if imgstr then
-      return "<rt text-align=left image=" .. imgstr .. " image-align=right><p font-size=13 font-color=D1D1D1>" ..  t1 .. "</p><p line-spacing=3 font-size=12>" .. t2 .. "<br></p><p font-size=8> <br></p></rt>"
-   else
-      return "<rt text-align=left><p font-size=13 font-color=D1D1D1>" ..  t1 .. "</p><p line-spacing=3 font-size=12>" .. t2 .. "<br></p><p font-size=8> <br></p></rt>"
-   end
-end
-
-
---  =======================================================
 --  ********** Helper functions for dependencies **********
 --  =======================================================
 
