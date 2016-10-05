4985f5a6fefc9e08cc39332a2026df1cccff6b10
GunChleoc <fios@foramnagaidhlig.net>
2016-10-05 09:03:01 +0100

    Renamed wildboar animation files.

--- data/world/critters/wildboar/init.lua
+++ data/world/critters/wildboar/init.lua
@@ -2,7 +2,7 @@ dirname = path.dirname(__file__)
 
 animations = {
    idle = {
-      pictures = path.list_files(dirname .. "wildboar_idle_??.png"),
+      pictures = path.list_files(dirname .. "idle_??.png"),
       hotspot = { 10, 18 },
       fps = 20,
       sound_effect = {
@@ -11,7 +11,7 @@ animations = {
       },
    },
 }
-add_walking_animations(animations, dirname, "wildboar_walk", {20, 22}, 20)
+add_walking_animations(animations, dirname, "walk", {20, 22}, 20)
 
 world:new_critter_type{
    name = "wildboar",
