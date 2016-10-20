ebbf87553830f7bf2833e7e16b4ea41cc7b86cdd
GunChleoc <fios@foramnagaidhlig.net>
2016-10-20 15:36:03 +0100

    Added player colors suggested by DragonAtma.

--- src/logic/player.cc
+++ src/logic/player.cc
@@ -97,15 +97,14 @@ const RGBColor Player::Colors[MAX_PLAYERS] = {
    RGBColor(255, 172, 0),    // orange
    RGBColor(215, 0, 218),    // purple
    RGBColor(255, 255, 255),  // white
-   // Repeat so we can have lots of players for the AI
-   RGBColor(2, 2, 198),      // blue
-   RGBColor(255, 41, 0),     // red
-   RGBColor(255, 232, 0),    // yellow
-   RGBColor(59, 223, 3),     // green
-   RGBColor(57, 57, 57),     // black/dark gray
-   RGBColor(255, 172, 0),    // orange
-   RGBColor(215, 0, 218),    // purple
-   RGBColor(255, 255, 255),  // white
+   RGBColor(0, 120, 255),    // sky blue
+   RGBColor(104, 0, 40),     // dark red
+   RGBColor(120, 108, 0),    // dark yellow
+   RGBColor(0, 112, 0),      // dark green
+   RGBColor(255, 120, 160),  // rose
+   RGBColor(148, 56, 0),     // brown
+   RGBColor(96, 0, 84),      // dark purple
+   RGBColor(144, 144, 144),  // light gray
 };
 
 /**
