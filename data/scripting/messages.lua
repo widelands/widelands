20ca272a367f9386876badc9d0dd8cca40dd673d
GunChleoc <fios@foramnagaidhlig.net>
2016-10-22 11:37:11 +0100

    Improved timing for story messages in Barbarian scenario 1. More felling axes.

--- data/scripting/messages.lua
+++ data/scripting/messages.lua
@@ -70,23 +70,6 @@ function campaign_message_box(message)
 end
 
 -- RST
--- .. function:: add_campaign_objective(objective)
---
---    Adds an objective to a campaign.
---
---    :arg objective: The objective to be added. If the variable obj_name exists, obj_name, obj_title and obj_body are used. Otherwise, it needs to have a name, title, and body.
---
---    :returns: The new objective.
---
-function add_campaign_objective(objective)
-   if objective.obj_name then
-      return wl.Game().players[1]:add_objective(objective.obj_name, objective.obj_title, objective.obj_body)
-   else
-      return wl.Game().players[1]:add_objective(objective.name, objective.title, objective.body)
-   end
-end
-
--- RST
 -- .. function:: message_box_objective(player, message)
 --
 --    Calls message_box(player, message.title, message.body, message). Also adds an objective defined in obj_name, obj_title and obj_body.
