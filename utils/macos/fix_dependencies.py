ed42d171d71e42c5a98ef4ed1ce304eaf3959d3e
GunChleoc <fios@foramnagaidhlig.net>
2016-12-04 07:02:25 +0000

    Fetched translations and updated catalogues.

--- utils/macos/fix_dependencies.py
+++ utils/macos/fix_dependencies.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-This tool was written out of frustration with
+"""This tool was written out of frustration with
 https://github.com/auriamg/macdylibbundler not doing the job right.
+
 Unfortunately the above tool is not maintained, so we roll our own.
+
 """
 
 import argparse
@@ -13,23 +14,26 @@ import os
 import shutil
 import subprocess
 
+
 def get_dependencies(loading_binary):
     out = subprocess.check_output(['/usr/bin/otool', '-L', loading_binary])
-    interesting_lines = (l.strip() for l in out.splitlines() if l.startswith("\t"))
+    interesting_lines = (l.strip()
+                         for l in out.splitlines() if l.startswith('\t'))
     dependencies = []
     for line in interesting_lines:
-        if '.framework' in line: # We cannot handle frameworks
+        if '.framework' in line:  # We cannot handle frameworks
             continue
-        if line.startswith("/usr/lib"): # Do not mess with system libraries.
+        if line.startswith('/usr/lib'):  # Do not mess with system libraries.
             continue
-        if "@executable_path" in line:
+        if '@executable_path' in line:
             continue
         dependencies.append(line[:line.find('(')].strip())
     dep_dict = {}
     for dep in dependencies:
-        file_name = dep.replace("@loader_path", p.dirname(loading_binary))
+        file_name = dep.replace('@loader_path', p.dirname(loading_binary))
         dep_dict[dep] = os.path.realpath(file_name)
     return dep_dict
+
 
 def fix_dependencies(top_level_binary):
     top_level_binary = p.realpath(top_level_binary)
@@ -38,7 +42,7 @@ def fix_dependencies(top_level_binary):
     path = p.dirname(top_level_binary)
     while todo:
         (binary_in_path, binary_in_system) = todo.pop()
-        print("Fixing %s" % binary_in_path)
+        print('Fixing %s' % binary_in_path)
         done.add(binary_in_path)
         dependencies = get_dependencies(binary_in_system)
         for (dep_name, dep_file) in dependencies.items():
@@ -46,16 +50,17 @@ def fix_dependencies(top_level_binary):
             todo.add((in_directory, dep_file))
             shutil.copy(dep_file, in_directory)
             os.chmod(in_directory, 0644)
-            subprocess.check_call(["/usr/bin/install_name_tool", "-change",
-                dep_name, "@executable_path/" + p.basename(dep_file), binary_in_path])
+            subprocess.check_call(['/usr/bin/install_name_tool', '-change',
+                                   dep_name, '@executable_path/' + p.basename(dep_file), binary_in_path])
+
 
 def parse_args():
-    p = argparse.ArgumentParser(description=
-        "Fix dependencies in executables or libraries."
-    )
-    p.add_argument("binary", type=str, nargs="+", help = "The binaries to fix.")
+    p = argparse.ArgumentParser(description='Fix dependencies in executables or libraries.'
+                                )
+    p.add_argument('binary', type=str, nargs='+', help='The binaries to fix.')
 
     return p.parse_args()
+
 
 def main():
     args = parse_args()
@@ -72,7 +77,7 @@ def main():
         done.add(b)
         for (dep_name, dep_path) in get_dependencies(b).items():
             if dep_name in all_dependencies and all_dependencies[dep_name] != dep_path:
-                raise RuntimeError("{} was already seen with a different path: {} != {}" % (
+                raise RuntimeError('{} was already seen with a different path: {} != {}' % (
                     dep_name, dep_path, all_dependencies[dep_name]))
             all_dependencies[dep_name] = dep_path
             binaries.add(dep_path)
@@ -81,15 +86,15 @@ def main():
     for (dep_name, dep_path) in all_dependencies.items():
         in_directory = p.join(output_dir, p.basename(dep_path))
         shutil.copy(dep_path, in_directory)
-        print("Copying %s to %s" % (dep_path, in_directory))
+        print('Copying %s to %s' % (dep_path, in_directory))
         to_fix.add(in_directory)
         os.chmod(in_directory, 0644)
 
     for binary in to_fix:
-        print("Fixing binary: %s" % binary)
+        print('Fixing binary: %s' % binary)
         for (dep_name, dep_path) in all_dependencies.items():
-            subprocess.check_call(["/usr/bin/install_name_tool", "-change",
-                dep_name, "@executable_path/" + p.basename(dep_path), binary])
+            subprocess.check_call(['/usr/bin/install_name_tool', '-change',
+                                   dep_name, '@executable_path/' + p.basename(dep_path), binary])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
