074d7a12afdbc9b6a0715de1f5ed5e23d272f024
GunChleoc <fios@foramnagaidhlig.net>
2017-01-25 18:55:59 +0000

    Updated copyright year to 2017.

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2015 by the Widelands Development Team
+ * Copyright (C) 2006-2017 by the Widelands Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,7 +24,7 @@
 
 constexpr uint16_t kWidelandsCopyrightStart = 2002;
 // If the following line is changed, the corresponding regex in 'utils/update_copyright.py' will also need updating.
-constexpr uint16_t kWidelandsCopyrightEnd = 2016;
+constexpr uint16_t kWidelandsCopyrightEnd = 2017;
 
 ///\return the build id which is automagically created from the checkout's
 /// revision number or the VERSION file
