943901f50853d428091ef90df137790e41aff227
GunChleoc <fios@foramnagaidhlig.net>
2017-02-11 12:04:33 +0000

    Added out of resources notification texts to fish breeder's house.

--- data/tribes/buildings/productionsites/atlanteans/fishbreeders_house/init.lua
+++ data/tribes/buildings/productionsites/atlanteans/fishbreeders_house/init.lua
@@ -47,6 +47,13 @@ tribes:new_productionsite_type {
       },
    },
 
+   out_of_resource_notification = {
+      -- Translators: Short for "Out of ..." for a resource
+      title = _"No Fish",
+      heading = _"Out of Fish",
+      message = pgettext("atlanteans_building", "The fish breeder working out of this fish breeder’s house can’t find any fish in his work area. Remember that you can only regenerate your fish resources if there are some fish left to breed."),
+      productivity_threshold = 60
+   },
    -- Translators: Productivity tooltip for Atlantean fish breeders when all water fields are already full of fish
    resource_not_needed_message = _"The fishing grounds are full";
 }
