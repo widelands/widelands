110b0177dd040b0aa87c14a2e001a3ee4ad63dd5
GunChleoc <fios@foramnagaidhlig.net>
2017-02-18 13:37:29 +0000

    Renamed FieldActionWindow::okdialog() to FieldActionWindow::reset_mouse_and_die().

--- src/wui/fieldaction.cc
+++ src/wui/fieldaction.cc
@@ -192,7 +192,7 @@ private:
 	                       void (FieldActionWindow::*fn)(),
 	                       const std::string& tooltip_text,
 	                       bool repeating = false);
-	void okdialog();
+	void reset_mouse_and_die();
 
 	Widelands::Player* player_;
 	Widelands::Map* map_;
@@ -525,7 +525,7 @@ Call this from the button handlers.
 It resets the mouse to its original position and closes the window
 ===============
 */
-void FieldActionWindow::okdialog() {
+void FieldActionWindow::reset_mouse_and_die() {
 	ibase().mouse_to_field(node_, MapView::Transition::Jump);
 	die();
 }
@@ -538,7 +538,7 @@ Open a watch window for the given field and delete self.
 void FieldActionWindow::act_watch() {
 	upcast(InteractiveGameBase, igbase, &ibase());
 	show_watch_window(*igbase, node_);
-	okdialog();
+	reset_mouse_and_die();
 }
 
 /*
@@ -549,13 +549,13 @@ Toggle display of census and statistics for buildings, respectively.
 void FieldActionWindow::act_show_census() {
 	ibase().set_display_flag(
 	   InteractiveBase::dfShowCensus, !ibase().get_display_flag(InteractiveBase::dfShowCensus));
-	okdialog();
+	reset_mouse_and_die();
 }
 
 void FieldActionWindow::act_show_statistics() {
 	ibase().set_display_flag(InteractiveBase::dfShowStatistics,
 	                         !ibase().get_display_flag(InteractiveBase::dfShowStatistics));
-	okdialog();
+	reset_mouse_and_die();
 }
 
 /*
@@ -586,7 +586,7 @@ void FieldActionWindow::act_buildflag() {
 		iaplayer->set_flag_to_connect(node_);
 	}
 
-	okdialog();
+	reset_mouse_and_die();
 }
 
 void FieldActionWindow::act_configure_economy() {
@@ -607,7 +607,7 @@ Remove the flag at this field
 ===============
 */
 void FieldActionWindow::act_ripflag() {
-	okdialog();
+	reset_mouse_and_die();
 	Widelands::EditorGameBase& egbase = ibase().egbase();
 	upcast(Game, game, &egbase);
 	upcast(InteractivePlayer, iaplayer, &ibase());
@@ -637,7 +637,7 @@ void FieldActionWindow::act_buildroad() {
 	// If we area already building a road just ignore this
 	if (!ibase().is_building_road()) {
 		ibase().start_build_road(node_, player_->player_number());
-		okdialog();
+		reset_mouse_and_die();
 	}
 }
 
@@ -651,7 +651,7 @@ void FieldActionWindow::act_abort_buildroad() {
 		return;
 
 	ibase().abort_build_road();
-	okdialog();
+	reset_mouse_and_die();
 }
 
 /*
@@ -665,7 +665,7 @@ void FieldActionWindow::act_removeroad() {
 		upcast(Game, game, &ibase().egbase());
 		game->send_player_bulldoze(*road, SDL_GetModState() & KMOD_CTRL);
 	}
-	okdialog();
+	reset_mouse_and_die();
 }
 
 /*
@@ -680,7 +680,7 @@ void FieldActionWindow::act_build(Widelands::DescriptionIndex idx) {
 	game->send_player_build(iaplayer->player_number(), node_, Widelands::DescriptionIndex(idx));
 	ibase().reference_player_tribe(player_->player_number(), &player_->tribe());
 	iaplayer->set_flag_to_connect(game->map().br_n(node_));
-	okdialog();
+	reset_mouse_and_die();
 }
 
 void FieldActionWindow::building_icon_mouse_out(Widelands::DescriptionIndex) {
@@ -708,7 +708,7 @@ void FieldActionWindow::act_geologist() {
 	if (upcast(Widelands::Flag, flag, game->map().get_immovable(node_))) {
 		game->send_player_flagaction(*flag);
 	}
-	okdialog();
+	reset_mouse_and_die();
 }
 
 /**
@@ -726,7 +726,7 @@ void FieldActionWindow::act_attack() {
 			game->send_player_enemyflagaction(building->base_flag(), iaplayer->player_number(),
 			                                  attack_box_->soldiers() /*  number of soldiers */);
 		}
-	okdialog();
+	reset_mouse_and_die();
 }
 
 /*
