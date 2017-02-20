01d1804dd93778f3ea1bf1c38210186ec292e246
GunChleoc <fios@foramnagaidhlig.net>
2017-02-20 13:53:01 +0000

    Added sphinx documentation for workers.

--- data/tribes/workers/atlanteans/carrier/init.lua
+++ data/tribes/workers/atlanteans/carrier/init.lua
@@ -1,3 +1,18 @@
+-- RST
+-- .. _lua_tribes_carriers:
+--
+-- Carriers
+-- --------
+--
+-- Carriers are specialized workers that carry wares along the roads.
+-- Each tribe has a basic human carrier + a second carrier for busy roads -
+-- usually a beast of burden.
+--
+-- Carriers, like workers, are defined in
+-- ``data/tribes/workers/<tribe name>/<worker_name>/init.lua``.
+-- The carrier will also need its help texts, which are defined in
+-- ``data/tribes/wares/<tribe name>/<worker_name>/helptexts.lua``
+
 dirname = path.dirname(__file__)
 
 animations = {
@@ -10,7 +25,22 @@ animations = {
 add_walking_animations(animations, "walk", dirname, "walk", {8, 25}, 10)
 add_walking_animations(animations, "walkload", dirname, "walkload", {8, 25}, 10)
 
-
+-- RST
+-- .. function:: new_carrier_type(table)
+--
+--    This function adds the definition of a carrier to the engine.
+--
+--    :arg table: This table contains all the data that the game engine will add
+--                to this carrier. It contains the :ref:`lua_tribes_workers_common`,
+--                plus the following additional properties:
+--
+--    **ware_hotspot**
+--        *Optional*. The x, y coordinates for adjusting the placement of the
+--        ware being carried. The default value is ``{0, 15}``. Increase ``x``
+--        to shift the ware to the left and ``y`` to shift it upwards. For example::
+--
+--           ware_hotspot = { -2, 13 },
+--
 tribes:new_carrier_type {
    msgctxt = "atlanteans_worker",
    --msgctxt = msgctxt,
