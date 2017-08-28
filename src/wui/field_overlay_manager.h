738d231fc1f180f7a452073168d19a62c8b1c4dc
Holger Rapp <SirVer@gmx.de>
2017-08-28 15:37:51 +0200

    All done.

--- src/wui/field_overlay_manager.h
+++ src/wui/field_overlay_manager.h
@@ -53,7 +53,6 @@ class Image;
 // drawn below the buildhelp, everything higher above.
 enum class OverlayLevel {
 	kWorkAreaPreview = 0,
-	kResource = 4,
 	kSelection = 7,
 	kRoadBuildSlope = 8,
 };
