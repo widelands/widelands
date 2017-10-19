f0868397541531bdf21e9a7e54c40280cf69fddd
tiborb95@gmail.com <>
2017-10-19 21:28:26 +0200

    allows_seafaring removed

--- src/ai/defaultai.cc
+++ src/ai/defaultai.cc
@@ -1960,8 +1960,11 @@ bool DefaultAI::construct_building(uint32_t gametime) {
 
 	const Map& map = game().map();
 
-	if (gametime % 5 == 0) {  // No need to query this too frequently
-		seafaring_economy = game().mutable_map()->allows_seafaring();
+	if (gametime % 5 == 0) {
+		// TODO(unknown): Counting port spaces is very primitive way for this
+		// there should be better alternative f.e. like map::allows_seafaring()
+		// function but simplier
+		seafaring_economy = map.get_port_spaces().size() >= 2;
 	}
 
 	for (int32_t i = 0; i < 4; ++i)
