3c16835282e672b42dc893d384bb039ce67f6860
GunChleoc <fios@foramnagaidhlig.net>
2017-11-05 09:30:44 +0000

    Document AI hints.

--- src/ai/ai_hints.cc
+++ src/ai/ai_hints.cc
@@ -21,6 +21,140 @@
 
 #include <memory>
 
+/* RST
+AI Hints
+========
+
+Every :doc:`building <autogen_toc_lua_tribes_buildings>`'s ``init.lua`` file has an ``aihints`` table in its ``new_<building_type>_type{table}``
+function. This ``aihints`` table can contain any number of entries, which will help the AI decide
+when and where to build or dismantle a building of that type.
+
+All entries in ``aihints`` are optional.
+
+- :ref:`ai_hints_common`
+- :ref:`ai_hints_military`
+- :ref:`ai_hints_production`
+
+.. _ai_hints_common:
+
+Common
+------
+
+**basic_amount**
+    The amount of this building to be built while establishing a basic economy, e.g.::
+
+        basic_amount = 1,
+
+**forced_after**
+    If a building of this type hasn't been built already, force that a building of this type
+    is constructed after the given time (in seconds) has elapsed, e.g.::
+
+        forced_after = 890,
+
+**prohibited_till**
+    Do not build this building before the given time (in seconds) has elapsed, e.g.::
+
+        prohibited_till = 1500,
+
+**very_weak_ai_limit**
+    The maximum number of this building type that the very weak AI is allowed to build, e.g.::
+
+        very_weak_ai_limit = 1,
+
+**weak_ai_limit_**
+    The maximum number of this building type that the weak AI is allowed to build, e.g.::
+
+        weak_ai_limit_ = 2,
+
+.. _ai_hints_military:
+
+Military Sites
+--------------
+
+**expansion**
+    The building will be used by the AI for expanding the territory, e.g.::
+
+        expansion = true,
+
+**fighting**
+    The building is suitable for military conflicts, e.g.::
+
+        fighting = true,
+
+**mountain_conqueror**
+    Prefer this type of military site near mountains, e.g.::
+
+        mountain_conqueror = true,
+
+
+.. _ai_hints_production:
+
+Production Sites
+----------------
+
+**graniteproducer**
+    The building will produce the ``granite`` ware, e.g.::
+
+        graniteproducer = true,
+
+**logproducer**
+    The building will produce the ``log`` ware, e.g.::
+
+        logproducer = true,
+
+**mines**
+    The building will mine to obtain the given ware, e.g.::
+
+        mines = "gold",
+
+**mines_percent**
+    The percentage that a mine will mine of its resource before it needs enhancing, e.g.::
+
+        mines_percent = 60,
+
+**mines_water**
+    The building will mine to obtain the ``water`` ware, e.g.::
+
+        mines_water = true,
+
+**needs_water**
+    The building needs to be placed near a body of water, e.g.::
+
+        needs_water = true,
+
+**recruitment**
+    The building will recruit the tribe's carrier2, for example oxen or horses, e.g.::
+
+        recruitment = true,
+
+**shipyard**
+    This building will construct ships, e.g.::
+
+        shipyard = true,
+
+**space_consumer**
+    The building needs a lot of space around it, for example a farm needs space for its fields, e.g.::
+
+        space_consumer = true,
+
+**supports_production_of**
+    This building will support the production of the given wares without producing it directly, e.g.::
+
+        supports_production_of = { "fish" },
+
+    For example, if a building supports the production of fish, it should be placed near a building
+    that has fish in its output.
+
+**trainingsites_max_percent**
+    The maximum percentate this trainingsite will have among all training sites, e.g.::
+
+        trainingsites_max_percent = 20,
+
+    For example, if an AI has built 5 trainingsites, it will have a maximum of 1 site of this type
+    if the value is set to ``20``. For trainingsites that don't have this value set, their percentage
+    will be distributed evenly.
+
+*/
 BuildingHints::BuildingHints(std::unique_ptr<LuaTable> table)
    : mines_(table->has_key("mines") ? table->get_string("mines") : ""),
      log_producer_(table->has_key("logproducer") ? table->get_bool("logproducer") : false),
