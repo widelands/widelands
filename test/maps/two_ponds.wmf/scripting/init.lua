8cfb54bb173009a9edeb7488b5d94f53ba52720e
GunChleoc <fios@foramnagaidhlig.net>
2017-11-08 15:53:57 +0000

    Define "stable_save" only once.

--- test/maps/two_ponds.wmf/scripting/init.lua
+++ test/maps/two_ponds.wmf/scripting/init.lua
@@ -1,18 +1,6 @@
 include "scripting/coroutine.lua"
 include "scripting/lunit.lua"
+include "test/scripting/stable_save.lua"
 
 game = wl.Game()
 map = game.map
-
-
--- Save the game so that reloading does not skip
-function stable_save(safename)
-   local old_speed = game.desired_speed
-   game.desired_speed = 1000
-   sleep(100)
-   game:save(safename)
-   game.desired_speed = 1000
-   sleep(2000)  -- Give the loaded game a chance to catch up
-   game.desired_speed = old_speed
-   sleep(1000)
-end
