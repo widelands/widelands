fb7157e7b5644545862a6ef61396c11b4d1d1c5b
Benedikt Straub <benedikt-straub@web.de>
2017-11-17 19:58:07 +0100

    New carrier idle animation

--- data/tribes/workers/frisians/carrier/init.lua
+++ data/tribes/workers/frisians/carrier/init.lua
@@ -1,21 +1,21 @@
-dirname = path.dirname(__file__)
+dirname = path.dirname (__file__)
 
 animations = {
    idle = {
-      pictures = path.list_files(dirname .. "idle_??.png"),
-      hotspot = { 14, 21 },
-      fps = 5
+      pictures = path.list_files (dirname .. "idle_??.png"),
+      hotspot = {20, 26},
+      scale = 7.55,
+      fps = 10
    }
 }
-add_walking_animations(animations, "walk", dirname, "walk", {9, 19}, 10)
-add_walking_animations(animations, "walkload", dirname, "walkload", {7, 22}, 10)
-
+add_walking_animations (animations, "walk", dirname, "walk", {9, 19}, 10)
+add_walking_animations (animations, "walkload", dirname, "walkload", {7, 22}, 10)
 
 tribes:new_carrier_type {
    msgctxt = "frisians_worker",
    name = "frisians_carrier",
    -- TRANSLATORS: This is a worker name used in lists of workers
-   descname = pgettext("frisians_worker", "Carrier"),
+   descname = pgettext ("frisians_worker", "Carrier"),
    helptext_script = dirname .. "helptexts.lua",
    icon = dirname .. "menu.png",
    vision_range = 2,
