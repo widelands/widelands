e3116ba72ee43c7467d55c9eb33899c5a6880b90
weedfreak <weedfreak@rhima.info>
2017-11-21 10:26:16 +0100

    changed description

--- data/tribes/scripting/starting_conditions/empire/village.lua
+++ data/tribes/scripting/starting_conditions/empire/village.lua
@@ -10,7 +10,7 @@ return {
    -- TRANSLATORS: This is the name of a starting condition
    descname = _ "Village",
    -- TRANSLATORS: This is the tooltip for the "Village" starting condition
-   tooltip = _"Start the game with a village installation",
+   tooltip = _"Start the game with a peaceful village",
    func =  function(plr, shared_in_start)
 
    local sf = wl.Game().map.player_slots[plr.number].starting_field
