99e38c528535d93a05d1b9d422a2408e6bd8ed8e
Benedikt Straub <benedikt-straub@web.de>
2017-11-25 14:33:50 +0100

    Animations for some workers

--- data/tribes/workers/frisians/landlady/init.lua
+++ data/tribes/workers/frisians/landlady/init.lua
@@ -1,20 +1,19 @@
-dirname = path.dirname(__file__)
+dirname = path.dirname (__file__)
 
 animations = {
    idle = {
-      pictures = path.list_files(dirname .. "idle_??.png"),
-      hotspot = { 6, 28 },
+      pictures = path.list_files (dirname .. "idle_??.png"),
+      hotspot = { 21, 25 },
    }
 }
-add_walking_animations(animations, "walk", dirname, "walk", {11, 24}, 15)
-add_walking_animations(animations, "walkload", dirname, "walk", {11, 24}, 15)
-
+add_walking_animations (animations, "walk", dirname, "walk", {21, 25}, 15)
+add_walking_animations (animations, "walkload", dirname, "walkload", {23, 27}, 15)
 
 tribes:new_worker_type {
    msgctxt = "frisians_worker",
    name = "frisians_landlady",
    -- TRANSLATORS: This is a worker name used in lists of workers
-   descname = pgettext("frisians_worker", "Landlady"),
+   descname = pgettext ("frisians_worker", "Landlady"),
    helptext_script = dirname .. "helptexts.lua",
    icon = dirname .. "menu.png",
    vision_range = 2,
--- data/tribes/workers/frisians/scout/init.lua
+++ data/tribes/workers/frisians/scout/init.lua
@@ -1,20 +1,19 @@
-dirname = path.dirname(__file__)
+dirname = path.dirname (__file__)
 
 animations = {
    idle = {
-      pictures = path.list_files(dirname .. "idle_??.png"),
-      hotspot = { 4, 23 },
+      pictures = path.list_files (dirname .. "idle_??.png"),
+      hotspot = { 21, 25 },
    }
 }
-add_walking_animations(animations, "walk", dirname, "walk", {9, 25}, 10)
-add_walking_animations(animations, "walkload", dirname, "walk", {9, 25}, 10)
-
+add_walking_animations (animations, "walk", dirname, "walk", {21, 25}, 15)
+add_walking_animations (animations, "walkload", dirname, "walkload", {23, 27}, 15)
 
 tribes:new_worker_type {
    msgctxt = "frisians_worker",
    name = "frisians_scout",
    -- TRANSLATORS: This is a worker name used in lists of workers
-   descname = pgettext("frisians_worker", "Scout"),
+   descname = pgettext ("frisians_worker", "Scout"),
    helptext_script = dirname .. "helptexts.lua",
    icon = dirname .. "menu.png",
    vision_range = 2,
