6fbd5b177dbfb4a8f6852db4e9d96da3c67772c6
GunChleoc <fios@foramnagaidhlig.net>
2017-11-29 08:07:35 +0000

    Fixed pointer corruption with filename when saving a game twice.

--- src/wui/game_main_menu_save_game.cc
+++ src/wui/game_main_menu_save_game.cc
@@ -132,8 +132,8 @@ void GameMainMenuSaveGame::entry_selected() {
 	ok_.set_enabled(load_or_save_.table().selections().size() == 1);
 	load_or_save_.delete_button()->set_enabled(load_or_save_.has_selection());
 	if (load_or_save_.has_selection()) {
-		const SavegameData& gamedata = *load_or_save_.entry_selected();
-		filename_editbox_.set_text(FileSystem::filename_without_ext(gamedata.filename.c_str()));
+		std::unique_ptr<SavegameData> gamedata = load_or_save_.entry_selected();
+		filename_editbox_.set_text(FileSystem::filename_without_ext(gamedata->filename.c_str()));
 	}
 }
 
