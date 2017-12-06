1c2dd161641d3ce9a5be3d2266dd818c86b71467
GunChleoc <fios@foramnagaidhlig.net>
2017-12-06 08:16:46 +0000

    Fixed bug and converted "General" tab of the Tribal Encyclopedia to new font renderer.

--- data/scripting/richtext.lua
+++ data/scripting/richtext.lua
@@ -307,9 +307,9 @@ end
 --    :returns: a p tag containint the formatted text
 function li(text_or_symbol, text)
    if text then
-      return p(text_or_symbol .. " " .. text .. vspace(6))
+      return div(p(text_or_symbol)) .. div(p(space(6))) .. div("width=*", p(text .. vspace(6)))
    else
-      return p("• " .. text_or_symbol .. vspace(6))
+      return div(p("•")) .. div(p(space(6))) .. div("width=*", p(text_or_symbol .. vspace(6)))
    end
 end
 
