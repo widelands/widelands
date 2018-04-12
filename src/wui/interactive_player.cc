059c4932929627cd1affed796ab8b11c849160c2
GunChleoc <fios@foramnagaidhlig.net>
2018-04-12 08:53:59 +0100

    Force integer precision for overlays.

--- src/wui/interactive_player.cc
+++ src/wui/interactive_player.cc
@@ -309,8 +309,9 @@ void InteractivePlayer::draw_map_view(MapView* given_map_view, RenderTarget* dst
 
 		const float scale = 1.f / given_map_view->view().zoom;
 		const auto blit_overlay = [dst, f, scale](const Image* pic, const Vector2i& hotspot) {
-			dst->blitrect_scale(Rectf(f->rendertarget_pixel - hotspot.cast<float>() * scale,
-			                          pic->width() * scale, pic->height() * scale),
+			const Recti pixel_perfect_rect = Rectf(f->rendertarget_pixel - hotspot.cast<float>() * scale,
+														pic->width() * scale, pic->height() * scale).cast<int>();
+			dst->blitrect_scale(pixel_perfect_rect.cast<float>(),
 			                    pic, Recti(0, 0, pic->width(), pic->height()), 1.f,
 			                    BlendMode::UseAlpha);
 		};
