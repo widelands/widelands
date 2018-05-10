95011c5eee83806ef3e6320a7a2ec93e5856498f
tiborb95@gmail.com <>
2018-05-10 21:28:50 +0200

    changes in propagate_promoted_road function

--- src/economy/flag.cc
+++ src/economy/flag.cc
@@ -510,32 +510,24 @@ WareInstance* Flag::fetch_pending_ware(Game& game, PlayerImmovable& dest) {
  * Accelerate potential promotion of roads adjacent to newly promoted road.
  */
 void Flag::propagate_promoted_road(Road* const promoted_road) {
-	// abort if flag has a building attached to it
-	if (building_) {
-		return;
-	}
-	// calculate the sum of the involved wallets
-	int32_t sum = 0;
-	for (int8_t i = 0; i < 6; ++i) {
-		Road* const road = roads_[i];
-		if (!road || road == promoted_road)
-			continue;
-		// abort if another promoted road is found
-		if (road->type_ == RoadType::kBusy)
-			return;
-		sum += road->wallet_;
-	}
+  // abort if flag has a building attached to it
+  if (building_) return;
 
-	if (sum == 0) {
-		return;
-	}
-	// distribute animal price proportionally
-	for (int8_t i = 0; i < 6; ++i) {
-		Road* const road = roads_[i];
-		if (!road || road == promoted_road)
-			continue;
-		road->wallet_ += 600 * road->wallet_ / sum;  // NOCOM 600 is animal_price
-	}
+  // calculate the sum of the involved wallets' adjusted value
+  int16_t max_wallet = 1500; // NOCOM should be replaced by kMaxWallet
+  int32_t sum = 0;
+  for (int8_t i = 0; i < 6; ++i) {
+    Road* const road = roads_[i];
+    if (!road || road == promoted_road) continue;
+    sum += max_wallet + road->wallet_ * road->wallet_;
+  }
+
+  // distribute propagation coins in a smart way
+  for (int8_t i = 0; i < 6; ++i) {
+    Road* const road = roads_[i];
+    if (!road || road->type_ == RoadType::kBusy) continue;
+    road->wallet_ += 0.5 * (max_wallet - road->wallet_) * (max_wallet + road->wallet_ * road->wallet_) / sum;
+  }
 }
 
 /**
