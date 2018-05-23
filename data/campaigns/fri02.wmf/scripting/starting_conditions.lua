2fe9b3759db5e76fd04f36f437dcc56233624d4b
GunChleoc <fios@foramnagaidhlig.net>
2018-05-23 06:13:49 +0100

    Added clay diggers to starting conditions.

--- data/campaigns/fri02.wmf/scripting/starting_conditions.lua
+++ data/campaigns/fri02.wmf/scripting/starting_conditions.lua
@@ -39,7 +39,8 @@ port1:set_wares {
 port1:set_workers {
    frisians_woodcutter = 3,
    frisians_forester = 7,
-   frisians_brickmaker = 5,
+   frisians_claydigger = 3,
+   frisians_brickmaker = 2,
    frisians_builder = 10,
    frisians_blacksmith = 2,
    frisians_smelter = 3,
