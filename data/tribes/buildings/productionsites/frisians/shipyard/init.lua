7056387a76d97984d08b8c7b29a3c6e18d69fbd5
Benedikt Straub <benedikt-straub@web.de>
2018-07-09 11:25:35 +0200

    New shipyard, recycle centre and sewing room

--- data/tribes/buildings/productionsites/frisians/shipyard/init.lua
+++ data/tribes/buildings/productionsites/frisians/shipyard/init.lua
@@ -26,12 +26,12 @@ tribes:new_productionsite_type {
    animations = {
       idle = {
          pictures = path.list_files (dirname .. "idle_??.png"),
-         hotspot = {56, 80},
+         hotspot = {56, 87},
          fps = 10,
       },
       unoccupied = {
          pictures = path.list_files (dirname .. "unoccupied_?.png"),
-         hotspot = {56, 80},
+         hotspot = {56, 66},
       },
    },
 
