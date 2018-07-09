005aa76717126b5c8984229729fc48763f5ca43c
Benedikt Straub <benedikt-straub@web.de>
2018-07-09 18:25:32 +0200

    New reed farm, berry farm, collectors house

--- data/tribes/buildings/productionsites/frisians/collectors_house/init.lua
+++ data/tribes/buildings/productionsites/frisians/collectors_house/init.lua
@@ -22,12 +22,12 @@ tribes:new_productionsite_type {
    animations = {
       idle = {
          pictures = path.list_files (dirname .. "idle_??.png"),
-         hotspot = {40, 76},
+         hotspot = {49, 89},
          fps = 10,
       },
       unoccupied = {
          pictures = path.list_files (dirname .. "unoccupied_?.png"),
-         hotspot = {40, 64},
+         hotspot = {49, 67},
       },
    },
 
