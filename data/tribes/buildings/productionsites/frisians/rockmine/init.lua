9149201f9f4185b822889caff1fe61540ee89206
Benedikt Straub <benedikt-straub@web.de>
2018-07-12 13:04:23 +0200

    New shallow mines

--- data/tribes/buildings/productionsites/frisians/rockmine/init.lua
+++ data/tribes/buildings/productionsites/frisians/rockmine/init.lua
@@ -25,21 +25,21 @@ tribes:new_productionsite_type {
    animations = {
       idle = {
          pictures = path.list_files (dirname .. "idle_??.png"),
-         hotspot = {27, 65},
+         hotspot = {28, 94},
          fps = 10,
       },
       working = {
          pictures = path.list_files (dirname .. "working_??.png"),
-         hotspot = {27, 66},
+         hotspot = {28, 94},
          fps = 10,
       },
       empty = {
-         pictures = path.list_files (dirname .. "empty_?.png"),
-         hotspot = {27, 48},
+         pictures = path.list_files (dirname .. "empty_??.png"),
+         hotspot = {28, 94},
       },
       unoccupied = {
          pictures = path.list_files (dirname .. "unoccupied_?.png"),
-         hotspot = {27, 48},
+         hotspot = {28, 75},
       },
    },
 
