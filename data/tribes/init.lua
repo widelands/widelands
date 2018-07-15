e303468af56563094964d4cb8d9ce1e1e682f067
GunChleoc <fios@foramnagaidhlig.net>
2018-07-15 11:32:12 +0100

    Shifted definition of Barbarian Market building to test scenario

--- data/tribes/init.lua
+++ data/tribes/init.lua
@@ -370,13 +370,6 @@ print_loading_message("┗━ took", function()
    end)
 
    -- ===================================
-   --    Markets
-   -- ===================================
-
-   print_loading_message("┃    Markets", function()
-      include "tribes/buildings/markets/barbarians/market/init.lua"
-   end)
-   -- ===================================
    --    Productionsites
    -- ===================================
 
