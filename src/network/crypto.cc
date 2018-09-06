658fd9bce85156b7eac62a1e48588c2a4ea2b6f4
GunChleoc <fios@foramnagaidhlig.net>
2018-09-06 08:32:28 +0100

    Fixed codecheck.

--- src/network/crypto.cc
+++ src/network/crypto.cc
@@ -1,14 +1,9 @@
-#include "boost/version.hpp"
 #include "network/crypto.h"
 
 #if BOOST_VERSION > 106700
-
-#include "boost/uuid/detail/sha1.hpp"
-
+  #include "boost/uuid/detail/sha1.hpp"
 #else
-
-#include "boost/uuid/sha1.hpp"
-
+  #include "boost/uuid/sha1.hpp"
 #endif
 
 namespace crypto {
