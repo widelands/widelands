d156e7213f15433b505ac6a4c5414502eb6fd011
GunChleoc <fios@foramnagaidhlig.net>
2018-09-29 15:03:56 +0100

    Smarter filtering of savegame filenames.

--- src/ui_fsmenu/campaigns.cc
+++ src/ui_fsmenu/campaigns.cc
@@ -31,7 +31,6 @@
 #include "scripting/lua_interface.h"
 
 namespace {
-// NOCOM Call fix_cross_file on kCampVisFileLegacy and kCampVisFile
 const std::string kCampVisFileLegacy = "save/campvis";
 }
 
