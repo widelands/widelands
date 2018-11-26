aa6d50ba5246507e6f36a3d7d303d0e8dede8c1b
GunChleoc <fios@foramnagaidhlig.net>
2018-11-26 06:42:56 +0000

    Fix linker error

--- src/logic/map_objects/tribes/carrier.cc
+++ src/logic/map_objects/tribes/carrier.cc
@@ -539,7 +539,7 @@ void Carrier::Loader::load(FileRead& fr) {
 		if (packet_version <= kCurrentPacketVersion && packet_version >= 1) {
 			Carrier& carrier = get<Carrier>();
 			// TODO(GunChleoc): std::min is for savegame compatibility. Remove after Build 21.
-			carrier.promised_pickup_to_ = std::min(NOONE, fr.signed_32());
+			carrier.promised_pickup_to_ = std::min(-1, fr.signed_32());
 		} else {
 			throw UnhandledVersionError("Carrier", packet_version, kCurrentPacketVersion);
 		}
