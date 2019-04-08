f71809fbab9c55cd482c644d832c79ed5b83f75a
GunChleoc <fios@foramnagaidhlig.net>
2019-04-08 09:08:22 +0100

    Partially finished buildings are not tribe-specific

--- data/tribes/buildings/partially_finished/constructionsite/helptexts.lua
+++ data/tribes/buildings/partially_finished/constructionsite/helptexts.lua
@@ -1,14 +1,11 @@
--- This include can be removed when all help texts have been defined.
-include "tribes/scripting/help/global_helptexts.lua"
-
 function building_helptext_lore()
-   -- TRANSLATORS#: Lore helptext for a building
-   return pgettext("barbarians_building", "‘Don't swear at the builder who is short of building materials.’")
+   -- TRANSLATORS: Lore helptext for a building
+   return pgettext("building", "‘Don’t swear at the builder who is short of building materials.’")
 end
 
 function building_helptext_lore_author()
-   -- TRANSLATORS#: Lore author helptext for a building
-   return pgettext("barbarians_building", "Barbarian Proverb,<br> widely used for under equipped tasks as well")
+   -- TRANSLATORS: Lore author helptext for a building
+   return pgettext("building", "Proverb widely used for under equipped tasks as well")
 end
 
 function building_helptext_purpose()
@@ -17,11 +14,9 @@ function building_helptext_purpose()
 end
 
 function building_helptext_note()
-   -- TRANSLATORS#: Note helptext for a building
    return ""
 end
 
 function building_helptext_performance()
-   -- TRANSLATORS#: Performance helptext for a building
-   return no_performance_text_yet()
+   return ""
 end
