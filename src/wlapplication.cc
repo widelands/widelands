9e04d2b2a350563fc1a0c87e894833237bd35abc
Lucki <Lucki@holarse-linuxgaming.de>
2019-04-14 02:27:28 +0200

    Resolve -Wundef warning

--- src/wlapplication.cc
+++ src/wlapplication.cc
@@ -304,7 +304,7 @@ WLApplication::WLApplication(int const argc, char const* const* const argv)
      should_die_(false),
 #ifdef _WIN32
      homedir_(FileSystem::get_homedir() + "\\.widelands"),
-#elif USE_XDG
+#elif defined USE_XDG
      // To enable backwards compatibility, the program name is passed with the
      // path.
      homedir_(FileSystem::get_userdatadir()),
