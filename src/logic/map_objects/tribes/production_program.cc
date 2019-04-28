72dd76672ffee7e00e65fe3802581df6f377f3b4
hessenfarmer <stephan-lutz@gmx.de>
2019-04-28 16:20:46 +0200

    adressed code review

--- src/logic/map_objects/tribes/production_program.cc
+++ src/logic/map_objects/tribes/production_program.cc
@@ -1415,7 +1415,7 @@ void ProductionProgram::ActTrain::execute(Game& game, ProductionSite& ps) const 
 		throw wexception("Fail training soldier!!");
 	}
 	ps.molog("  Training done!\n");
-	ps.set_production_result((boost::format(_("Completed %u")) % ps.top_state().program->descname()).str());
+	ps.set_production_result((boost::format(_("Completed %s")) % ps.top_state().program->descname()).str());
 
 	upcast(TrainingSite, ts, &ps);
 	ts->training_successful(attribute, level);
