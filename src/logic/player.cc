39884102c0f387e3eb31f527319fe019fe9c47a5
Nordfriese <>
2019-05-02 17:34:46 +0200

    Compile error

--- src/logic/player.cc
+++ src/logic/player.cc
@@ -749,7 +749,7 @@ void Player::bulldoze(PlayerImmovable& imm, bool const recurse) {
 
 			if (recurse) {
 				// Destroy all roads and waterways between the flags, not just selected
-				while (RoadBase* const r = start.get_roadbase(end, nullptr))
+				while (RoadBase* const r = start.get_roadbase(end))
 					r->destroy(egbase());
 
 				OPtr<Flag> endcopy = &end;
