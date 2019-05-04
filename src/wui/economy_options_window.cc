12beee425edf32e6cff531ba4c7f6a0a1f1f61dc
Nordfriese <>
2019-05-04 15:51:52 +0200

    Allow to choose between several profiles when resetting economy targets

--- src/wui/economy_options_window.cc
+++ src/wui/economy_options_window.cc
@@ -23,10 +23,12 @@
 
 #include "graphic/graphic.h"
 #include "logic/editor_game_base.h"
+#include "logic/filesystem_constants.h"
 #include "logic/map_objects/tribes/ware_descr.h"
 #include "logic/map_objects/tribes/worker_descr.h"
 #include "logic/player.h"
 #include "logic/playercommand.h"
+#include "profile/profile.h"
 #include "ui_basic/button.h"
 
 static const char pic_tab_wares[] = "images/wui/buildings/menu_tab_wares.png";
@@ -39,9 +41,9 @@ EconomyOptionsWindow::EconomyOptionsWindow(UI::Panel* parent,
      serial_(economy->serial()),
      player_(&economy->owner()),
      tabpanel_(this, UI::TabPanelStyle::kWuiDark),
-     ware_panel_(new EconomyOptionsPanel(&tabpanel_, serial_, player_, can_act, Widelands::wwWARE)),
+     ware_panel_(new EconomyOptionsPanel(&tabpanel_, this, serial_, player_, can_act, Widelands::wwWARE)),
      worker_panel_(
-        new EconomyOptionsPanel(&tabpanel_, serial_, player_, can_act, Widelands::wwWORKER)) {
+        new EconomyOptionsPanel(&tabpanel_, this, serial_, player_, can_act, Widelands::wwWORKER)) {
 	set_center_panel(&tabpanel_);
 
 	tabpanel_.add("wares", g_gr->images().get(pic_tab_wares), ware_panel_, _("Wares"));
@@ -49,6 +51,8 @@ EconomyOptionsWindow::EconomyOptionsWindow(UI::Panel* parent,
 	economy->set_has_window(true);
 	economynotes_subscriber_ = Notifications::subscribe<Widelands::NoteEconomy>(
 	   [this](const Widelands::NoteEconomy& note) { on_economy_note(note); });
+
+	read_targets();
 }
 
 EconomyOptionsWindow::~EconomyOptionsWindow() {
@@ -129,6 +133,7 @@ EconomyOptionsWindow::TargetWaresDisplay::info_for_ware(Widelands::DescriptionIn
  * Wraps the wares/workers display together with some buttons
  */
 EconomyOptionsWindow::EconomyOptionsPanel::EconomyOptionsPanel(UI::Panel* parent,
+		                    								   EconomyOptionsWindow* eco_window,
                                                                Widelands::Serial serial,
                                                                Widelands::Player* player,
                                                                bool can_act,
@@ -138,7 +143,10 @@ EconomyOptionsWindow::EconomyOptionsPanel::EconomyOptionsPanel(UI::Panel* parent
      player_(player),
      type_(type),
      can_act_(can_act),
-     display_(this, 0, 0, serial_, player_, type_, can_act_) {
+     display_(this, 0, 0, serial_, player_, type_, can_act_),
+     economy_options_window_(eco_window),
+     dropdown_box_(this, 0, 0, UI::Box::Horizontal),
+     dropdown_(&dropdown_box_, 0, 0, 160, 200, 34, "", UI::DropdownType::kTextual, UI::PanelStyle::kWui) {
 	add(&display_, UI::Box::Resizing::kFullSize);
 
 	if (!can_act_) {
@@ -162,9 +170,20 @@ EconomyOptionsWindow::EconomyOptionsPanel::EconomyOptionsPanel(UI::Panel* parent
 	buttons->add_space(8);
 
 	b = new UI::Button(
-	   buttons, "reset_target", 0, 0, 34, 34, UI::ButtonStyle::kWuiMenu, "R", _("Reset to default"));
+	   buttons, "reset_target", 0, 0, 34, 34, UI::ButtonStyle::kWuiMenu, "R", _("Reset to chosen default"));
 	b->sigclicked.connect(boost::bind(&EconomyOptionsPanel::reset_target, this));
 	buttons->add(b);
+	buttons->add_space(8);
+
+	b = new UI::Button(
+	   buttons, "save_targets", 0, 0, 34, 34, UI::ButtonStyle::kWuiMenu, "S", _("Save target settings"));
+	b->sigclicked.connect(boost::bind(&EconomyOptionsWindow::create_target, eco_window));
+	buttons->add(b);
+
+	dropdown_.set_tooltip(_("Profile to apply when clicking Reset"));
+	add(&dropdown_box_, UI::Box::Resizing::kFullSize);
+	dropdown_box_.set_size(40, 20);  // Prevent assert failures
+	dropdown_box_.add(&dropdown_, UI::Box::Resizing::kFullSize);
 }
 
 void EconomyOptionsWindow::EconomyOptionsPanel::set_economy(Widelands::Serial serial) {
@@ -206,15 +225,126 @@ void EconomyOptionsWindow::EconomyOptionsPanel::reset_target() {
 	Widelands::Game& game = dynamic_cast<Widelands::Game&>(player_->egbase());
 	const bool is_wares = type_ == Widelands::wwWARE;
 	const auto& items = is_wares ? player_->tribe().wares() : player_->tribe().workers();
+	const PredefinedTargets settings = economy_options_window_->get_predefined_targets().at(dropdown_.get_selected());
 	for (const Widelands::DescriptionIndex& index : items) {
 		if (display_.ware_selected(index)) {
 			if (is_wares) {
-				game.send_player_command(*new Widelands::CmdResetWareTargetQuantity(
-				   game.get_gametime(), player_->player_number(), serial_, index));
+				game.send_player_command(*new Widelands::CmdSetWareTargetQuantity(
+						game.get_gametime(), player_->player_number(), serial_, index, settings.wares.at(index)));
 			} else {
-				game.send_player_command(*new Widelands::CmdResetWorkerTargetQuantity(
-				   game.get_gametime(), player_->player_number(), serial_, index));
+				game.send_player_command(*new Widelands::CmdSetWorkerTargetQuantity(
+						game.get_gametime(), player_->player_number(), serial_, index, settings.workers.at(index)));
 			}
 		}
 	}
+}
+
+void EconomyOptionsWindow::read_targets(const std::string& select) {
+	predefined_targets_.clear();
+	const Widelands::Tribes& tribes = player_->egbase().tribes();
+	const Widelands::TribeDescr& tribe = player_->tribe();
+
+	{
+		PredefinedTargets t;
+		for (Widelands::DescriptionIndex di : tribe.wares()) {
+			const Widelands::WareDescr* descr = tribes.get_ware_descr(di);
+			if (descr->has_demand_check(tribe.name())) {
+				t.wares.emplace(di, descr->default_target_quantity(tribe.name()));
+			}
+		}
+		for (Widelands::DescriptionIndex di : tribe.workers()) {
+			const Widelands::WorkerDescr* descr = tribes.get_worker_descr(di);
+			if (descr->has_demand_check()) {
+				t.workers.emplace(di, descr->default_target_quantity());
+			}
+		}
+		predefined_targets_.emplace(kDefaultEconomyProfile, t);
+	}
+
+	std::string complete_filename = kEconomyProfilesDir + g_fs->file_separator() + player_->tribe().name();
+	Profile profile;
+	profile.read(complete_filename.c_str());
+	while (Section* section = profile.get_next_section()) {
+		PredefinedTargets t;
+		while (Section::Value* v = section->get_next_val()) {
+			const std::string name = std::string(v->get_name());
+			Widelands::DescriptionIndex di = tribes.ware_index(name);
+			if (di == Widelands::INVALID_INDEX) {
+				di = tribes.worker_index(name);
+				assert(di != Widelands::INVALID_INDEX);
+				t.workers.emplace(di, v->get_natural());
+			} else {
+				t.wares.emplace(di, v->get_natural());
+			}
+		}
+		predefined_targets_.emplace(std::string(section->get_name()), t);
+	}
+
+	ware_panel_->update_profiles(select);
+	worker_panel_->update_profiles(select);
+}
+
+void EconomyOptionsWindow::EconomyOptionsPanel::update_profiles(const std::string& select) {
+	dropdown_.clear();
+	for (const auto& pair : economy_options_window_->get_predefined_targets()) {
+		dropdown_.add(_(pair.first), pair.first, nullptr, pair.first == select);
+	}
+}
+
+void EconomyOptionsWindow::create_target() {
+	/* Show a dialog similar to the savegame dialog:
+	 * The main part of the window is a Table listing all current profiles.
+	 * Below is a Textbox for the name.
+	 * The "Delete" button removes the selected profile(s) from the list.
+	 * The "Save" button checks whether a profile will be overwritten; if so, a confirm box pops up.
+	 * The profile kDefaultEconomyProfile cannot be deleted or overwritten.
+	 */
+
+
+
+	std::string name = "CustomSettings" + std::to_string(predefined_targets_.size());
+	log("NOCOM: EconomyOptionsWindow::create_target(): not yet implemented, saving current settings as \"%s\"\n", name.c_str());
+	const Widelands::Tribes& tribes = player_->egbase().tribes();
+	const Widelands::TribeDescr& tribe = player_->tribe();
+	Widelands::Economy* economy = player_->get_economy(serial_);
+	PredefinedTargets t;
+	for (Widelands::DescriptionIndex di : tribe.wares()) {
+		if (tribes.get_ware_descr(di)->has_demand_check(tribe.name())) {
+			t.wares[di] = economy->ware_target_quantity(di).permanent;
+		}
+	}
+	for (Widelands::DescriptionIndex di : tribe.workers()) {
+		if (tribes.get_worker_descr(di)->has_demand_check()) {
+			t.workers[di] = economy->worker_target_quantity(di).permanent;
+		}
+	}
+	predefined_targets_[name] = t;
+
+
+
+	save_targets();
+	ware_panel_->update_profiles(name);
+	worker_panel_->update_profiles(name);
+}
+
+// Write all predefined settings (except the defaults) to file
+void EconomyOptionsWindow::save_targets() {
+	const Widelands::Tribes& tribes = player_->egbase().tribes();
+	Profile profile;
+	for (const auto& pair : predefined_targets_) {
+		if (pair.first == kDefaultEconomyProfile) {
+			continue;
+		}
+		Section& section = profile.create_section(pair.first.c_str());
+		for (const auto& setting : pair.second.wares) {
+			section.set_natural(tribes.get_ware_descr(setting.first)->name().c_str(), setting.second);
+		}
+		for (const auto& setting : pair.second.workers) {
+			section.set_natural(tribes.get_worker_descr(setting.first)->name().c_str(), setting.second);
+		}
+	}
+
+	g_fs->ensure_directory_exists(kEconomyProfilesDir);
+	std::string complete_filename = kEconomyProfilesDir + g_fs->file_separator() + player_->tribe().name();
+	profile.write(complete_filename.c_str(), false);
 }
--- src/wui/economy_options_window.h
+++ src/wui/economy_options_window.h
@@ -20,19 +20,37 @@
 #ifndef WL_WUI_ECONOMY_OPTIONS_WINDOW_H
 #define WL_WUI_ECONOMY_OPTIONS_WINDOW_H
 
+#include <map>
 #include <memory>
+#include <string>
 
 #include "economy/economy.h"
 #include "logic/map_objects/tribes/tribe_descr.h"
 #include "notifications/notifications.h"
 #include "ui_basic/box.h"
+#include "ui_basic/dropdown.h"
 #include "ui_basic/tabpanel.h"
 #include "ui_basic/window.h"
 #include "wui/waresdisplay.h"
 
+const std::string kDefaultEconomyProfile = "Default";
+
 struct EconomyOptionsWindow : public UI::Window {
 	EconomyOptionsWindow(UI::Panel* parent, Widelands::Economy* economy, bool can_act);
 	~EconomyOptionsWindow();
+
+	struct PredefinedTargets {
+		using Targets = std::map<Widelands::DescriptionIndex, uint32_t>;
+		Targets wares;
+		Targets workers;
+	};
+
+	void create_target();
+	void save_targets();
+	void read_targets(const std::string& = kDefaultEconomyProfile);
+	const std::map<std::string, PredefinedTargets>& get_predefined_targets() const {
+		return predefined_targets_;
+	}
 
 private:
 	struct TargetWaresDisplay : public AbstractWaresDisplay {
@@ -59,6 +77,7 @@ private:
 	 */
 	struct EconomyOptionsPanel : UI::Box {
 		EconomyOptionsPanel(UI::Panel* parent,
+		                    EconomyOptionsWindow* eco_window,
 		                    Widelands::Serial serial,
 		                    Widelands::Player* player,
 		                    bool can_act,
@@ -67,6 +86,7 @@ private:
 		void set_economy(Widelands::Serial serial);
 		void change_target(int amount);
 		void reset_target();
+		void update_profiles(const std::string&);
 
 	private:
 		Widelands::Serial serial_;
@@ -74,6 +94,9 @@ private:
 		Widelands::WareWorker type_;
 		bool can_act_;
 		TargetWaresDisplay display_;
+		EconomyOptionsWindow* economy_options_window_;
+		UI::Box dropdown_box_;
+		UI::Dropdown<std::string> dropdown_;
 	};
 
 	/// Actions performed when a NoteEconomyWindow is received.
@@ -85,6 +108,8 @@ private:
 	EconomyOptionsPanel* ware_panel_;
 	EconomyOptionsPanel* worker_panel_;
 	std::unique_ptr<Notifications::Subscriber<Widelands::NoteEconomy>> economynotes_subscriber_;
+
+	std::map<std::string, PredefinedTargets> predefined_targets_;
 };
 
 #endif  // end of include guard: WL_WUI_ECONOMY_OPTIONS_WINDOW_H
