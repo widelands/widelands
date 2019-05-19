229e3c1a23f4c3bde10507b2e4191102a02977ff
GunChleoc <fios@foramnagaidhlig.net>
2019-05-19 11:36:50 +0100

    Grouped tribes immovables into subdirectories and renamed some for consistency.

--- src/map_io/tribes_legacy_lookup_table.cc
+++ src/map_io/tribes_legacy_lookup_table.cc
@@ -19,6 +19,7 @@
 
 #include "map_io/tribes_legacy_lookup_table.h"
 // Whenever we break savegame compatibility, we can empty these maps
+// NOCOM(GunChleoc): Since we now have a unified tribe, no need to have the tribe as key for fetching
 TribesLegacyLookupTable::TribesLegacyLookupTable()
    :  // Workers
      workers_{
@@ -51,23 +52,23 @@ TribesLegacyLookupTable::TribesLegacyLookupTable()
                        }),
         std::make_pair("barbarians",
                        std::map<std::string, std::string>{
-                          {"field0", "field_medium"},
-                          {"field0s", "field_small"},
-                          {"field0t", "field_tiny"},
-                          {"field1", "field_ripe"},
-                          {"field2", "field_harvested"},
-                          {"reed0", "reed_medium"},
-                          {"reed0s", "reed_small"},
-                          {"reed0t", "reed_tiny"},
-                          {"reed1", "reed_ripe"},
+                          {"field_medium", "wheatfield_medium"},
+                          {"field_small", "wheatfield_small"},
+                          {"field_tiny", "wheatfield_tiny"},
+                          {"field_ripe", "wheatfield_ripe"},
+                          {"field_harvested", "wheatfield_harvested"},
+                          {"reed_medium", "reed_medium"},
+                          {"reed_small", "reedfield_small"},
+                          {"reed_tiny", "reedfield_tiny"},
+                          {"reed_tiny", "reedfield_ripe"},
                        }),
         std::make_pair("empire",
                        std::map<std::string, std::string>{
-                          {"field0", "field_medium"},
-                          {"field0s", "field_small"},
-                          {"field0t", "field_tiny"},
-                          {"field1", "field_ripe"},
-                          {"field2", "field_harvested"},
+                          {"field_medium", "wheatfield_medium"},
+                          {"field_small", "wheatfield_small"},
+                          {"field_tiny", "wheatfield_tiny"},
+                          {"field_ripe", "wheatfield_ripe"},
+                          {"field_harvested", "wheatfield_harvested"},
                        }),
         std::make_pair("frisians",
                        std::map<std::string, std::string>{
