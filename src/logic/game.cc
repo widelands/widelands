729cc56218d1014058671beac1010afc0dae0d69
Nordfriese <>
2019-05-28 17:08:33 +0200

    compile error

--- src/logic/game.cc
+++ src/logic/game.cc
@@ -860,44 +860,44 @@ void Game::send_player_propose_trade(const Trade& trade) {
 }
 
 void Game::send_player_constructionsite_soldier_capacity(ConstructionSite& cs, uint32_t c) {
-	send_player_command(*new CmdConstructionsiteSoldierCapacity(
+	send_player_command(new CmdConstructionsiteSoldierCapacity(
 		   get_gametime(), cs.get_owner()->player_number(), cs, c));
 }
 
 void Game::send_player_constructionsite_prefer_heroes(ConstructionSite& cs, bool h) {
-	send_player_command(*new CmdConstructionsitePreferHeroes(
+	send_player_command(new CmdConstructionsitePreferHeroes(
 		   get_gametime(), cs.get_owner()->player_number(), cs, h));
 }
 
 void Game::send_player_constructionsite_launch_expedition(ConstructionSite& cs, bool l) {
-	send_player_command(*new CmdConstructionsiteLaunchExpedition(
+	send_player_command(new CmdConstructionsiteLaunchExpedition(
 		   get_gametime(), cs.get_owner()->player_number(), cs, l));
 }
 
 void Game::send_player_constructionsite_stock_policy(
 		ConstructionSite& cs, WareWorker ww, DescriptionIndex di, StockPolicy pol) {
-	send_player_command(*new CmdConstructionsiteStockPolicy(
+	send_player_command(new CmdConstructionsiteStockPolicy(
 		   get_gametime(), cs.get_owner()->player_number(), cs, ww, di, pol));
 }
 
 void Game::send_player_constructionsite_input_queue_priority(
 		ConstructionSite& cs, WareWorker ww, DescriptionIndex di, int32_t p) {
-	send_player_command(*new CmdConstructionsiteInputQueuePriority(
+	send_player_command(new CmdConstructionsiteInputQueuePriority(
 		   get_gametime(), cs.get_owner()->player_number(), cs, ww, di, p));
 }
 
 void Game::send_player_constructionsite_input_queue_max_fill(
 		ConstructionSite& cs, WareWorker ww, DescriptionIndex di, uint32_t max) {
-	send_player_command(*new CmdConstructionsiteInputQueueMaxFill(
+	send_player_command(new CmdConstructionsiteInputQueueMaxFill(
 		   get_gametime(), cs.get_owner()->player_number(), cs, ww, di, max));
 }
 
 void Game::send_player_constructionsite_enhance(ConstructionSite& cs) {
-	send_player_command(*new CmdConstructionsiteEnhance(get_gametime(), cs.get_owner()->player_number(), cs));
+	send_player_command(new CmdConstructionsiteEnhance(get_gametime(), cs.get_owner()->player_number(), cs));
 }
 
 void Game::send_player_constructionsite_startstop(ConstructionSite& cs, bool stop) {
-	send_player_command(*new CmdConstructionsiteStartStop(get_gametime(), cs.get_owner()->player_number(), cs, stop));
+	send_player_command(new CmdConstructionsiteStartStop(get_gametime(), cs.get_owner()->player_number(), cs, stop));
 }
 
 int Game::propose_trade(const Trade& trade) {
