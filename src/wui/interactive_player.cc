d37d2b7e93b50ef9a083f6f2bb79eda919632708
Antonino Siena <>
2019-06-01 22:19:46 +0200

    Open menu using ESCAPE

--- src/wui/interactive_player.cc
+++ src/wui/interactive_player.cc
@@ -577,7 +577,9 @@ bool InteractivePlayer::handle_key(bool const down, SDL_Keysym const code) {
 			map_view()->scroll_to_field(
 			   game().map().get_starting_pos(player_number_), MapView::Transition::Smooth);
 			return true;
-
+		case SDLK_ESCAPE:
+			new GameOptionsMenu(*this, options_, main_windows_);
+			return true;
 		case SDLK_KP_ENTER:
 		case SDLK_RETURN:
 			if (chat_provider_) {
