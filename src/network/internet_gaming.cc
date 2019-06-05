ba793e9c0e037f4edd976322bc7e23763400f0d7
Notabilis <notabilis27@web.de>
2019-06-05 19:05:07 +0200

    Reduced doubled space to single space.

--- src/network/internet_gaming.cc
+++ src/network/internet_gaming.cc
@@ -911,10 +911,10 @@ void InternetGaming::send(const std::string& msg) {
 
 		if (msg == "/help") {
 			format_and_add_chat("", "", true, _("Supported admin commands:"));
-			format_and_add_chat("", "", true, _("/motd <msg>  sets a permanent greeting message"));
-			format_and_add_chat("", "", true, _("/announce <msg>  send a one time system message"));
-			format_and_add_chat("", "", true, _("/warn <user> <msg>  send a private system message to the given user"));
-			format_and_add_chat("", "", true, _("/kick <user|game>  removes the given user or game from the metaserver"));
+			format_and_add_chat("", "", true, _("/motd <msg> sets a permanent greeting message"));
+			format_and_add_chat("", "", true, _("/announce <msg> send a one time system message"));
+			format_and_add_chat("", "", true, _("/warn <user> <msg> send a private system message to the given user"));
+			format_and_add_chat("", "", true, _("/kick <user|game> removes the given user or game from the metaserver"));
 			return;
 		}
 
