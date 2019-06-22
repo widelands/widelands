60d62d0e9266a44c762deeee4bbdcc9b9c59e110
GunChleoc <fios@foramnagaidhlig.net>
2019-06-22 12:22:14 +0100

    Typo

--- src/logic/map.cc
+++ src/logic/map.cc
@@ -721,7 +721,7 @@ int Map::needs_widelands_version_after() const {
 void Map::calculate_needs_widelands_version_after(bool is_post_one_world) {
 	if (map_version_.needs_widelands_version_after == 0) {
 		if (nrplayers_ > 8) {
-			// We introduced support for 16 payers after Build 19
+			// We introduced support for 16 players after Build 19
 			map_version_.needs_widelands_version_after = 19;
 		} else if (is_post_one_world) {
 			// We merged the worlds in the engine after Build 18
