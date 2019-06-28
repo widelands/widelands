12bcf83f03298e4fbbd66bf67b9fa6c030806780
Nordfriese <>
2019-06-28 21:08:59 +0200

    Merge conflicts

--- src/economy/economy.cc
+++ src/economy/economy.cc
@@ -988,7 +988,7 @@ void Economy::handle_active_supplies(Game& game) {
 
 		for (uint32_t nwh = 0; nwh < warehouses_.size(); ++nwh) {
 			Warehouse* wh = warehouses_[nwh];
-			StockPolicy policy = wh->get_stock_policy(type, ware);
+			StockPolicy policy = wh->get_stock_policy(type_, ware);
 			if (policy == StockPolicy::kPrefer) {
 				haveprefer = true;
 
@@ -1013,10 +1013,10 @@ void Economy::handle_active_supplies(Game& game) {
 		if (preferred_wh) {
 			wh = preferred_wh;
 		} else {
-			wh = find_closest_warehouse(supply.get_position(game)->base_flag(), type, nullptr, 0,
+			wh = find_closest_warehouse(supply.get_position(game)->base_flag(), nullptr, 0,
 			                            (!havenormal) ? WarehouseAcceptFn() :
 			                                            boost::bind(&accept_warehouse_if_policy, _1,
-			                                                        type, ware, StockPolicy::kNormal));
+			                                                        type_, ware, StockPolicy::kNormal));
 		}
 		if (!wh) {
 			log("Warning: Economy::handle_active_supplies "
