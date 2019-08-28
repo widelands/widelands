ed480e190bea610f4fc7c576bb4af6efdbf67246
GunChleoc <fios@foramnagaidhlig.net>
2019-08-28 18:27:48 +0100

    Make Empire Piggery work 20% faster.

--- data/tribes/buildings/productionsites/empire/piggery/init.lua
+++ data/tribes/buildings/productionsites/empire/piggery/init.lua
@@ -55,9 +55,9 @@ tribes:new_productionsite_type {
          actions = {
             "return=skipped unless economy needs meat",
             "consume=water wheat",
-            "sleep=25000",
+            "sleep=20000",
             "playsound=sound/farm/farm_animal 180",
-            "animate=working 30000",
+            "animate=working 24000",
             "produce=meat"
          }
       },
