2c73c5202599709c93980ce2bb392a3046b81fff
GunChleoc <fios@foramnagaidhlig.net>
2019-09-21 09:38:19 +0100

    test formatting

--- utils/githooks/pre-commit.sh
+++ utils/githooks/pre-commit.sh
@@ -2,7 +2,7 @@
 
 ## This script will run clang-format on changed files.
 
-git diff --name-only | grep ".*\.\(h\|cc\)" | while read line ; do
+git diff --cached --name-only --diff-filter=ACM | grep ".*\.\(h\|cc\)" | while read line ; do
    echo formatting file: $line
    clang-format -i $line
 done
