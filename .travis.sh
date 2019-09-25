f0a6388941617ee7de7fc73642bd9470f61cbe42
gunchleoc <fios@foramnagaidhlig.net>
2019-09-25 07:51:11 +0100

    Travis improvements (#3514)
    - Add separate stage to stop building early if a debug or release build fails compiling
    - Add translation switch so translations can be en-/disabled on a per job basis
    - Skip website binaries in gcc debug builds on Travis to prevent job timeouts
    
    Closes #3493.

--- .travis.sh
+++ .travis.sh
@@ -6,13 +6,7 @@ cd build
 
 case "$1" in
 build)
-   if [ "$BUILD_TYPE" == "Debug" ]; then
-      # We skip translations and codecheck to speed things up
-      cmake .. -DCMAKE_BUILD_TYPE:STRING="$BUILD_TYPE" -DOPTION_BUILD_TRANSLATIONS="OFF" -DOPTION_ASAN="OFF" -DOPTION_BUILD_CODECHECK="OFF"
-   else
-      # We test translations only on release builds, in order to help with job timeouts
-      cmake .. -DCMAKE_BUILD_TYPE:STRING="$BUILD_TYPE" -DOPTION_BUILD_TRANSLATIONS="ON" -DOPTION_ASAN="OFF"
-   fi
+   cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DOPTION_BUILD_TRANSLATIONS=$BUILD_TRANSLATIONS -DOPTION_BUILD_WEBSITE_TOOLS=$BUILD_WEBSITE_TOOLS -DOPTION_ASAN="OFF" -DOPTION_BUILD_CODECHECK="OFF"
    # Do the actual build.
    make -k -j3
 
