5ae5495dc744017e64be14af196041e679068c0c
Benedikt Straub <benedikt-straub@web.de>
2019-10-31 12:35:46 +0100

    Some more hotspot fixes

--- data/tribes/buildings/warehouses/frisians/headquarters/init.lua
+++ data/tribes/buildings/warehouses/frisians/headquarters/init.lua
@@ -14,7 +14,7 @@ tribes:new_warehouse_type {
       idle = {
          directory = dirname,
          basename = "idle",
-         hotspot = {113, 119},
+         hotspot = {99, 111},
          frames = 10,
          columns = 5,
          rows = 2,
