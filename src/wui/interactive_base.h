8bb78e58a9aaf98be6359f04a67186837e8ec58d
GunChleoc <fios@foramnagaidhlig.net>
2019-12-29 16:10:41 +0100

    Fixed comments for Doygen.

--- src/wui/interactive_base.h
+++ src/wui/interactive_base.h
@@ -62,12 +62,12 @@ struct WorkareaPreview {
 class InteractiveBase : public UI::Panel, public DebugConsole::Handler {
 public:
 	enum {
-		dfShowCensus = 1,         /// show census report on buildings
-		dfShowStatistics = 2,     /// show statistics report on buildings
-		dfShowSoldierLevels = 4,  /// show level information above soldiers
+		dfShowCensus = 1,         ///< show census report on buildings
+		dfShowStatistics = 2,     ///< show statistics report on buildings
+		dfShowSoldierLevels = 4,  ///< show level information above soldiers
 		dfShowWorkareaOverlap =
 		   8,         ///< highlight overlapping workareas when placing a constructionsite
-		dfDebug = 16  /// general debugging info
+		dfDebug = 16  ///< general debugging info
 	};
 
 	/// A build help overlay, i.e. small, big, mine, port ...
