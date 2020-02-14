1e0ca57e34824a0229f6e2e80774278e6c85a9ef
GunChleoc <fios@foramnagaidhlig.net>
2020-02-14 16:16:08 +0000

    Fix scenario building for market test

--- test/maps/market_trading.wmf/scripting/tribes/init.lua
+++ test/maps/market_trading.wmf/scripting/tribes/init.lua
@@ -1,14 +1,11 @@
-tribes = wl.Tribes()
-include "scripting/mapobjects.lua"
-
 -- ===================================
 --    Campaign Specific Data
 -- ===================================
-print_loading_message("Loading market unit", function()
-   include "map:scripting/tribes/market/init.lua"
-end)
 
-tribes:add_custom_building {
-   tribename = "barbarians",
-   buildingname = "barbarians_market",
+return {
+   barbarians = {
+      buildings = {
+         "barbarians_market"
+      }
+   }
 }
