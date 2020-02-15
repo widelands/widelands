eec03d53a784598ddbd2f9e36235a72dccd978f9
GunChleoc <fios@foramnagaidhlig.net>
2020-02-15 13:20:55 +0000

    Add test for tribe unit replacment

---
+++ test/maps/market_trading.wmf/scripting/tribes/ax_sharp/init.lua
@@ -0,0 +1,25 @@
+dirname = "test/maps/market_trading.wmf/" .. path.dirname(__file__)
+
+-- Test that replacing a ware works
+
+tribes:new_ware_type {
+   msgctxt = "ware",
+   name = "ax_sharp",
+   -- TRANSLATORS: This is a ware name used in lists of wares
+   descname = pgettext("ware", "Sharp Ax"),
+   helptext_script = dirname .. "helptexts.lua",
+   icon = dirname .. "menu.png",
+   default_target_quantity = {
+      barbarians = 1
+   },
+   preciousness = {
+      barbarians = 1
+   },
+
+   animations = {
+      idle = {
+         pictures = path.list_files(dirname .. "idle.png"),
+         hotspot = { 7, 7 },
+      },
+   }
+}
