2453b6601bbb54ed3f7ff7cda2fbb948611fe112
GunChleoc <fios@foramnagaidhlig.net>
2020-02-16 22:38:22 +0000

    Test triggering formatting.

--- src/logic/map_objects/description_manager.cc
+++ src/logic/map_objects/description_manager.cc
@@ -52,7 +52,9 @@ DescriptionManager::~DescriptionManager() {
 }
 
 /// Walk given directory and register descriptions
-void DescriptionManager::register_directory(const std::string& dirname, FileSystem* filesystem, bool is_scenario) {
+void DescriptionManager::register_directory(const std::string& dirname,
+                                            FileSystem* filesystem,
+                                            bool is_scenario) {
 	FilenameSet files = filesystem->list_directory(dirname);
 	for (const std::string& file : files) {
 		if (filesystem->is_directory(file)) {
