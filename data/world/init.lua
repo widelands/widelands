730e4e6f6142a3c8d36d0dc8fb874b0e7be8ea46
GunChleoc <fios@foramnagaidhlig.net>
2020-02-21 07:38:54 +0000

    Add spritesheets for Borassus Palm

--- data/world/init.lua
+++ data/world/init.lua
@@ -230,7 +230,7 @@ print_loading_message("┗━ took", function()
       world:new_editor_immovable_category{
          name = "trees_palm",
          descname = _ "Palm Trees",
-         picture = "world/immovables/trees/palm_borassus/old/idle_0.png",
+         picture = "world/immovables/trees/palm_borassus/menu.png",
          items_per_row = 8,
       }
 
