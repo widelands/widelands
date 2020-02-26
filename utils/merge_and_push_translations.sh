d64b928ef980dd3e216f50b9d6216b8af10d312b
GunChleoc <fios@foramnagaidhlig.net>
2020-02-26 12:47:41 +0000

    Add formatting directives to translation update script

--- utils/merge_and_push_translations.sh
+++ utils/merge_and_push_translations.sh
@@ -89,13 +89,16 @@ else
 fi
 
 # Fix formatting for C++, Lua & Python
-# python utils/fix_formatting.py
+python utils/fix_formatting.py --lua data/i18n
+python utils/fix_formatting.py --lua data/txts
 
 # Stage changes
 # - Translations
 git add po/*/*.po po/*/*.pot data/i18n/locales/*.json xdg/translations/*.json || true
 # - Authors
 git add data/txts/*.lua || true
+# - Locale data
+git add data/i18n/*.lua || true
 # - Appdata
 git add xdg/org.widelands.Widelands.appdata.xml xdg/org.widelands.Widelands.desktop || true
 # - Statistics
