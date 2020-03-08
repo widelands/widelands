8fbae8950fa499791f11c58f4257697da82b2361
Benedikt Straub <benedikt-straub@web.de>
2020-03-08 14:06:16 +0100

    typo in critter visual disambiguation

--- src/logic/map_objects/world/critter.cc
+++ src/logic/map_objects/world/critter.cc
@@ -472,7 +472,7 @@ void Critter::roam_update(Game& game, State& state) {
 	}
 	return start_task_idle(game, descr().get_animation("idle", this),
 	                       idle_time_min + game.logic_rand() % idle_time_rnd,
-	                       Vector2i(std::cos(n_th_bob_on_field), std::sin(n_th_bob_on_field)));
+	                       Vector2i(n_th_bob_on_field * std::cos(n_th_bob_on_field), n_th_bob_on_field * std::sin(n_th_bob_on_field)));
 }
 
 void Critter::init_auto_task(Game& game) {
