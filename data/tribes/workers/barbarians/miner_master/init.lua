e4c639d5194bb59ea0aece8d634ef77f3437208c
GunChleoc <fios@foramnagaidhlig.net>
2020-03-08 11:39:46 +0000

    Master miner

--- data/tribes/workers/barbarians/miner_master/init.lua
+++ data/tribes/workers/barbarians/miner_master/init.lua
@@ -1,15 +1,5 @@
 dirname = path.dirname(__file__)
 
-animations = {
-   idle = {
-      pictures = path.list_files(dirname .. "idle_??.png"),
-      hotspot = { 2, 24 }
-   }
-}
-add_directional_animation(animations, "walk", dirname, "walk", {9, 25}, 10)
-add_directional_animation(animations, "walkload", dirname, "walkload", {6, 23}, 10)
-
-
 tribes:new_worker_type {
    msgctxt = "barbarians_worker",
    name = "barbarians_miner_master",
@@ -19,5 +9,30 @@ tribes:new_worker_type {
    icon = dirname .. "menu.png",
    vision_range = 2,
 
-   animations = animations,
+   animations = {
+      idle = {
+         directory = dirname,
+         hotspot = { 4, 24 }
+      }
+   },
+   spritesheets = {
+      walk = {
+         directory = dirname,
+         fps = 10,
+         frames = 10,
+         rows = 4,
+         columns = 3,
+         directional = true,
+         hotspot = { 10, 24 }
+      },
+      walkload = {
+         directory = dirname,
+         fps = 10,
+         frames = 10,
+         rows = 4,
+         columns = 3,
+         directional = true,
+         hotspot = { 12, 26 }
+      }
+   }
 }
