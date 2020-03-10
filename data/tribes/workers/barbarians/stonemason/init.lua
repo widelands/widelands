cb8b4f1e35a4559125ae688b3266aa2c468eb259
GunChleoc <fios@foramnagaidhlig.net>
2020-03-10 11:24:30 +0000

    Stonemason

--- data/tribes/workers/barbarians/stonemason/init.lua
+++ data/tribes/workers/barbarians/stonemason/init.lua
@@ -1,21 +1,5 @@
 dirname = path.dirname(__file__)
 
-animations = {
-   idle = {
-      pictures = path.list_files(dirname .. "idle_??.png"),
-      hotspot = { 8, 21 },
-      fps = 10
-   },
-   hacking = {
-      pictures = path.list_files(dirname .. "hacking_??.png"),
-      hotspot = { 8, 21 },
-      fps = 10
-   }
-}
-add_directional_animation(animations, "walk", dirname, "walk", {9, 24}, 10)
-add_directional_animation(animations, "walkload", dirname, "walkload", {7, 22}, 10)
-
-
 tribes:new_worker_type {
    msgctxt = "barbarians_worker",
    name = "barbarians_stonemason",
@@ -35,12 +19,45 @@ tribes:new_worker_type {
          "findobject=attrib:rocks radius:6",
          "walk=object",
          "playsound=sound/stonecutting/stonecutter 192",
-         "animate=hacking 17500",
+         "animate=hack 17500",
          "callobject=shrink",
          "createware=granite",
          "return"
       }
    },
 
-   animations = animations,
+   animations = {
+      idle = {
+         directory = dirname,
+         hotspot = { 6, 16 },
+      }
+   },
+   spritesheets = {
+      walk = {
+         directory = dirname,
+         fps = 10,
+         frames = 10,
+         rows = 4,
+         columns = 3,
+         directional = true,
+         hotspot = { 9, 17 }
+      },
+      walkload = {
+         directory = dirname,
+         fps = 10,
+         frames = 10,
+         rows = 4,
+         columns = 3,
+         directional = true,
+         hotspot = { 7, 19 }
+      },
+      hack = {
+         directory = dirname,
+         fps = 10,
+         frames = 10,
+         rows = 4,
+         columns = 3,
+         hotspot = { 6, 17 }
+      }
+   }
 }
