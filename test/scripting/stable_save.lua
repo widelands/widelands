affedfc37a10d89193ab501e985d825f08a1849d
GunChleoc <fios@foramnagaidhlig.net>
2020-03-20 07:12:11 +0000

    Explicitly hand desired speed to stable_save. Cap waiting for FPS.

--- test/scripting/stable_save.lua
+++ test/scripting/stable_save.lua
@@ -1,9 +1,8 @@
 include "scripting/coroutine.lua"
 
 -- Save the game so that reloading does not skip
-function stable_save(game, savename)
+function stable_save(game, savename, desired_speed)
    local mapview = wl.ui.MapView()
-   local old_speed = game.desired_speed
 
    game.desired_speed = 1000
    sleep(100)
@@ -12,9 +11,12 @@ function stable_save(game, savename)
 
    -- Give the loaded game a chance to catch up
    sleep(200)
-   while mapview.average_fps < 20 do sleep(500) end
-   sleep(1000)
+   local counter = 0
+   while mapview.average_fps < 20 and counter < 20 do
+      sleep(500)
+      counter = counter + 1
+   end
 
-   game.desired_speed = old_speed
+   game.desired_speed = desired_speed
    sleep(1000)
 end
