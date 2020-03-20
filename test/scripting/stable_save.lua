ff3ebe6d9fe429320ab3b27dda72d5d806fe85bd
GunChleoc <fios@foramnagaidhlig.net>
2020-03-20 05:33:17 +0000

    stable_save now waits for the average FPS to recover after the save.

--- test/scripting/stable_save.lua
+++ test/scripting/stable_save.lua
@@ -2,12 +2,16 @@ include "scripting/coroutine.lua"
 
 -- Save the game so that reloading does not skip
 function stable_save(game, savename)
+   local mapview = wl.ui.MapView()
    local old_speed = game.desired_speed
    game.desired_speed = 1000
    sleep(100)
    game:save(savename)
    game.desired_speed = 1000
-   sleep(2000)  -- Give the loaded game a chance to catch up
+     -- Give the loaded game a chance to catch up
+   sleep(200)
+   while mapview.average_fps < 20 do sleep(500) end
+   sleep(1000)
    game.desired_speed = old_speed
    sleep(1000)
 end
