2155ce5a8dcf7d9649bdc7734a4cc5990750ec56
GunChleoc <fios@foramnagaidhlig.net>
2020-03-20 07:33:56 +0000

    stable_save now gets the last save time from the save handler and will sleep until the save is finished

--- test/scripting/stable_save.lua
+++ test/scripting/stable_save.lua
@@ -3,20 +3,25 @@ include "scripting/coroutine.lua"
 -- Save the game so that reloading does not skip
 function stable_save(game, savename, desired_speed)
    local mapview = wl.ui.MapView()
+   local last_save_time = game.last_save_time
 
    game.desired_speed = 1000
    sleep(100)
    game:save(savename)
    game.desired_speed = 1000
 
+   -- Wait until save was finished
+   repeat
+      sleep(200)
+   until game.last_save_time > last_save_time
+
    -- Give the loaded game a chance to catch up
-   sleep(200)
    local counter = 0
-   while mapview.average_fps < 20 and counter < 20 do
-      sleep(500)
+   while mapview.average_fps < 20 and counter < 100 do
+      sleep(200)
       counter = counter + 1
    end
 
    game.desired_speed = desired_speed
-   sleep(1000)
+   sleep(100)
 end
