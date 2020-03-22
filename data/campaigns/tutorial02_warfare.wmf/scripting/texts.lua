2370867da6133b847f28cd74dd389fda2a6aa086
pospec4444 <2467395+pospec4444@users.noreply.github.com>
2020-03-22 09:45:59 +0100

    Proofreading: teaching about scout
    Co-Authored-By: gunchleoc <fios@foramnagaidhlig.net>

--- data/campaigns/tutorial02_warfare.wmf/scripting/texts.lua
+++ data/campaigns/tutorial02_warfare.wmf/scripting/texts.lua
@@ -168,7 +168,7 @@ scouting3 = {
    title = _"Scout made progress",
    body = (
       h1(_"New horizons?") ..
-      p(_[[As you can see, scout can go quite far. Good news is that there are no enemies around.]]) ..
+      p(_[[As you can see, scouts can go quite far. The good news is that there are no enemies around.]]) ..
       p(_[[You can use minimap to see progress of exploration.]]) ..
       p(_[[Don’t forget that scouting is hard work and as such can not be done without provision of food.]])
    ),
