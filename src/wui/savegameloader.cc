9ca0dffe0b8fd972b33a92f26f0b3d84c44dd3c0
Jonas Mörschbach <jmoerschbach@gmail.com>
2020-03-24 21:50:56 +0100

    added whitespaces

--- src/wui/savegameloader.cc
+++ src/wui/savegameloader.cc
@@ -190,7 +190,7 @@ ReplayLoader::ReplayLoader(Widelands::Game& game) : SavegameLoader(game) {
 }
 
 bool ReplayLoader::is_valid_gametype(const SavegameData&) const {
-	return true;  // TODO (jmoerschbach): why?? what is the purpose of
+	return true;  // TODO(jmoerschbach): why?? what is the purpose of
 	              // GameController::GameType::kReplay? return gamedata.is_replay(); <-- should be
 	              // this, right?!
 }
