d91683fa9b930537c25f49a8cd33d815ff6f7473
Benedikt Straub <benedikt-straub@web.de>
2020-03-25 15:50:02 +0100

    Completed builder

--- data/tribes/workers/amazons/builder/init.lua
+++ data/tribes/workers/amazons/builder/init.lua
@@ -1,24 +1,5 @@
 dirname = path.dirname(__file__)
 
-animations = {
-   idle = {
-      pictures = path.list_files (dirname .. "idle_??.png"),
-      hotspot = {6, 18},
-      fps = 10
-   },
-   work = {
-      pictures = path.list_files (dirname .. "work_??.png"),
-      sound_effect = {
-         path = "sound/hammering/hammering",
-         priority = 64
-      },
-      hotspot = {9, 24},
-      fps = 10
-   }
-}
-add_directional_animation(animations, "walk", dirname, "walk", {11, 24}, 15)
-add_directional_animation(animations, "walkload", dirname, "walkload", {10, 24}, 15)
-
 tribes:new_worker_type {
    msgctxt = "amazons_worker",
    name = "amazons_builder",
@@ -33,6 +14,49 @@ tribes:new_worker_type {
       hammer = 1
    },
 
-   ware_hotspot = {0, 20},
-   animations = animations,
+   ware_hotspot = {0, 29},
+   spritesheets = {
+      walk = {
+         directory = dirname,
+         basename = "walk",
+         directional = true,
+         hotspot = {17, 28},
+         fps = 15,
+         frames = 30,
+         columns = 6,
+         rows = 5
+      },
+      walkload = {
+         directory = dirname,
+         basename = "walkload",
+         directional = true,
+         hotspot = {17, 31},
+         fps = 15,
+         frames = 30,
+         columns = 6,
+         rows = 5
+      },
+      idle = {
+         directory = dirname,
+         basename = "idle",
+         hotspot = {12, 30},
+         fps = 15,
+         frames = 40,
+         columns = 8,
+         rows = 5
+      },
+      work = {
+         directory = dirname,
+         basename = "work",
+         hotspot = {16, 38},
+         fps = 15,
+         frames = 80,
+         columns = 10,
+         rows = 8,
+         sound_effect = {
+            path = "sound/hammering/hammering",
+            priority = 64
+         }
+      },
+   },
 }
