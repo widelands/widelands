5d79d36d58f3d6ce0361114bc4778b7dc64c7361
Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>
2020-03-27 11:04:45 +0100

    Difficulty stages for scenarios (#3795)
    Allow campaign designers to define various difficulty stages for campaigns

--- data/campaigns/fri01.wmf/scripting/texts.lua
+++ data/campaigns/fri01.wmf/scripting/texts.lua
@@ -406,11 +406,15 @@ rising_water_4 = {
       _([[The storm flood seems to be coming from the west. If we hurry, we will just make it! We only need to build a port and a shipyard – oh, and a weaving mill to weave the durable cloth, which is made from reed and fur, for the sails. Then we can launch an expedition from the port. Pray to the gods and make haste!]]))
       .. new_objectives(obj_escape),
 }
+local nr_transfer_soldiers = {15, 10, 5}
+nr_transfer_soldiers = nr_transfer_soldiers[difficulty]
 rising_water_5 = {
    title =_ "Don’t Forget the Soldiers",
    body=reebaud(_"Soldiers to the port!",
       -- TRANSLATORS: Reebaud – rising water
-      _([[I almost forgot in this haste – whereever we will land, we’ll need soldiers to secure and expand our new colony. There’s space for up to ten soldiers on our ship, if they don’t mind being all crammed together. Let’s send some to the port when it’s completed, so they can embark when we set sail!]])),
+      ngettext([[I almost forgot in this haste – whereever we will land, we’ll need soldiers to secure and expand our new colony. There’s space for up to %d soldier on our ship, if they don’t mind being all crammed together. Let’s send some to the port when it’s completed, so they can embark when we set sail!]],
+      [[I almost forgot in this haste – whereever we will land, we’ll need soldiers to secure and expand our new colony. There’s space for up to %d soldiers on our ship, if they don’t mind being all crammed together. Let’s send some to the port when it’s completed, so they can embark when we set sail!]],
+      nr_transfer_soldiers):bformat(nr_transfer_soldiers))
 }
 
 victory_1 = {
