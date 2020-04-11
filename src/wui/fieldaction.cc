widelands/widelands

commit a8c236de51e837df467a6f0ea55444b498c2752e

Refactor road/waterway building (#3793)
Refactored road and waterway building mode and Lua road building functions and fixed a bug in secret village

Co-authored-by: gunchleoc <fios@foramnagaidhlig.net>

src/wui/fieldaction.cc
@@ -672,10 +672,8 @@ void FieldActionWindow::act_buildflag() {
 	else
 		player_->build_flag(node_);
 
-	if (ibase().is_building_road()) {
+	if (ibase().in_road_building_mode()) {
 		ibase().finish_build_road();
-	} else if (ibase().is_building_waterway()) {
-		ibase().finish_build_waterway();
 	} else if (game) {
 		upcast(InteractivePlayer, iaplayer, &ibase());
 		iaplayer->set_flag_to_connect(node_);
@@ -749,17 +747,17 @@ Start road building.
 ===============
 */
 void FieldActionWindow::act_buildroad() {
-	// If we area already building a road just ignore this
-	if (!ibase().is_building_road()) {
-		ibase().start_build_road(node_, player_->player_number());
+	// If we are already building a road just ignore this
+	if (!ibase().in_road_building_mode()) {
+		ibase().start_build_road(node_, player_->player_number(), RoadBuildingType::kRoad);
 		reset_mouse_and_die();
 	}
 }
 
 void FieldActionWindow::act_buildwaterway() {
 	// If we are already building a waterway just ignore this
-	if (!ibase().is_building_waterway()) {
-		ibase().start_build_waterway(node_, player_->player_number());
+	if (!ibase().in_road_building_mode(RoadBuildingType::kWaterway)) {
+		ibase().start_build_road(node_, player_->player_number(), RoadBuildingType::kWaterway);
 		reset_mouse_and_die();
 	}
 }
@@ -770,39 +768,42 @@ Abort building a road.
 ===============
 */
 void FieldActionWindow::act_abort_buildroad() {
-	if (!ibase().is_building_road())
+	if (!ibase().in_road_building_mode(RoadBuildingType::kRoad)) {
 		return;
+	}
 
 	ibase().abort_build_road();
 	reset_mouse_and_die();
 }
 
 void FieldActionWindow::act_abort_buildwaterway() {
-	if (!ibase().is_building_waterway()) {
+	if (!ibase().in_road_building_mode(RoadBuildingType::kWaterway)) {
 		return;
 	}
 
-	ibase().abort_build_waterway();
+	ibase().abort_build_road();
 	reset_mouse_and_die();
 }
 
 void FieldActionWindow::act_abort_buildroad_and_start_buildwaterway() {
-	if (!ibase().is_building_road() || ibase().is_building_waterway()) {
+	if (!ibase().in_road_building_mode(RoadBuildingType::kRoad) ||
+	    ibase().in_road_building_mode(RoadBuildingType::kWaterway)) {
 		return;
 	}
 	const Widelands::Coords c = ibase().get_build_road_start();
 	ibase().abort_build_road();
-	ibase().start_build_waterway(c, player_->player_number());
+	ibase().start_build_road(c, player_->player_number(), RoadBuildingType::kWaterway);
 	reset_mouse_and_die();
 }
 
 void FieldActionWindow::act_abort_buildwaterway_and_start_buildroad() {
-	if (!ibase().is_building_waterway() || ibase().is_building_road()) {
+	if (ibase().in_road_building_mode(RoadBuildingType::kRoad) ||
+	    !ibase().in_road_building_mode(RoadBuildingType::kWaterway)) {
 		return;
 	}
-	const Widelands::Coords c = ibase().get_build_waterway_start();
-	ibase().abort_build_waterway();
-	ibase().start_build_road(c, player_->player_number());
+	const Widelands::Coords c = ibase().get_build_road_start();
+	ibase().abort_build_road();
+	ibase().start_build_road(c, player_->player_number(), RoadBuildingType::kRoad);
 	reset_mouse_and_die();
 }
 
@@ -1002,22 +1003,29 @@ Bring up a field action window or continue road building.
 void show_field_action(InteractiveBase* const ibase,
                        Widelands::Player* const player,
                        UI::UniqueWindow::Registry* const registry) {
-	if (ibase->is_building_road()) {
-		// we're building a road right now
+	if (ibase->in_road_building_mode()) {
+		// we're building a road or waterway right now
 		const Widelands::Map& map = player->egbase().map();
 		const Widelands::FCoords target = map.get_fcoords(ibase->get_sel_pos().node);
 
 		// if user clicked on the same field again, build a flag
 		if (target == ibase->get_build_road_end()) {
 			FieldActionWindow& w = *new FieldActionWindow(ibase, player, registry);
-			w.add_buttons_road(target != ibase->get_build_road_start() &&
-			                   (player->get_buildcaps(target) & Widelands::BUILDCAPS_FLAG));
+			if (ibase->in_road_building_mode(RoadBuildingType::kRoad)) {
+				w.add_buttons_road(target != ibase->get_build_road_start() &&
+				                   (player->get_buildcaps(target) & Widelands::BUILDCAPS_FLAG));
+			} else {
+				w.add_buttons_waterway(target != ibase->get_build_road_start() &&
+				                       (player->get_buildcaps(target) & Widelands::BUILDCAPS_FLAG));
+			}
 			w.init();
 			return;
 		}
 
 		// append or take away from the road
-		if (!ibase->append_build_road(target)) {
+		if (!ibase->append_build_road(target) ||
+		    (ibase->in_road_building_mode(RoadBuildingType::kWaterway) &&
+		     target != ibase->get_build_road_end())) {
 			FieldActionWindow& w = *new FieldActionWindow(ibase, player, registry);
 			w.add_buttons_road(false);
 			w.init();
@@ -1041,52 +1049,13 @@ void show_field_action(InteractiveBase* const ibase,
 				// We are done, so we close the window.
 				registry->destroy();
 				return;
-			}
-		}
-	} else if (ibase->is_building_waterway()) {
-		// we're building a waterway right now
-		const Widelands::Map& map = player->egbase().map();
-		const Widelands::FCoords target = map.get_fcoords(ibase->get_sel_pos().node);
-
-		// if user clicked on the same field again, build a flag
-		if (target == ibase->get_build_waterway_end()) {
-			FieldActionWindow& w = *new FieldActionWindow(ibase, player, registry);
-			w.add_buttons_waterway(target != ibase->get_build_waterway_start() &&
-			                       (player->get_buildcaps(target) & Widelands::BUILDCAPS_FLAG));
-			w.init();
-			return;
-		}
-
-		// append or take away from the waterway
-		if (!ibase->append_build_waterway(target) || target != ibase->get_build_waterway_end()) {
-			// Player clicked an unsuited field or path exceeds length restriction
-			FieldActionWindow& w = *new FieldActionWindow(ibase, player, registry);
-			w.add_buttons_waterway(false);
-			w.init();
-			return;
-		}
-
-		// Did he click on a flag or a road where a flag can be built?
-		if (upcast(const Widelands::PlayerImmovable, i, map.get_immovable(target))) {
-			bool finish = false;
-			if (is_a(Widelands::Flag, i)) {
-				finish = true;
-			} else if (is_a(Widelands::RoadBase, i)) {
-				if (player->get_buildcaps(target) & Widelands::BUILDCAPS_FLAG) {
-					upcast(Game, game, &player->egbase());
-					game->send_player_build_flag(player->player_number(), target);
-					finish = true;
-				}
-			}
-
-			if (finish) {
-				ibase->finish_build_waterway();
-				// We are done, so we close the window.
-				registry->destroy();
-				return;
 			} else {
 				FieldActionWindow& w = *new FieldActionWindow(ibase, player, registry);
-				w.add_buttons_waterway(false);
+				if (ibase->in_road_building_mode(RoadBuildingType::kRoad)) {
+					w.add_buttons_waterway(false);
+				} else {
+					w.add_buttons_road(false);
+				}
 				w.init();
 				return;
 			}

data/campaigns/bar01.wmf/scripting/secret_village.lua
@@ -67,30 +67,30 @@ function reveal_village()
       { "birch_summer_pole", 54, 30, "world" },
       { "beech_summer_old", 55, 30, "world" },
       { "larch_summer_old", 56, 30, "world" },
-      { "field_harvested", 56, 14, "tribes" },
-      { "field_small",57, 14, "tribes" },
-      { "field_harvested", 54, 15, "tribes" },
-      { "field_harvested", 57, 15, "tribes" },
-      { "field_harvested", 54, 16, "tribes" },
-      { "field_ripe", 57, 16, "tribes" },
-      { "field_harvested", 58, 16, "tribes" },
-      { "field_harvested", 54, 17, "tribes" },
-      { "field_medium", 55, 17, "tribes" },
-      { "field_harvested", 57, 17, "tribes" },
-      { "field_harvested", 55, 18, "tribes" },
-      { "field_harvested", 57, 18, "tribes" },
-      { "field_harvested", 53, 31, "tribes" },
-      { "field_harvested", 54, 31, "tribes" },
-      { "field_medium", 55, 31, "tribes" },
-      { "field_harvested", 56, 32, "tribes" },
-      { "field_harvested", 52, 33, "tribes" },
-      { "field_small",55, 33, "tribes" },
-      { "field_harvested", 56, 33, "tribes" },
-      { "field_harvested", 53, 34, "tribes" },
-      { "field_ripe", 54, 34, "tribes" },
-      { "field_harvested", 56, 34, "tribes" },
-      { "field_harvested", 53, 35, "tribes" },
-      { "field_harvested", 55, 35, "tribes" },
+      { "wheatfield_harvested", 56, 14, "tribes" },
+      { "wheatfield_small",57, 14, "tribes" },
+      { "wheatfield_harvested", 54, 15, "tribes" },
+      { "wheatfield_harvested", 57, 15, "tribes" },
+      { "wheatfield_harvested", 54, 16, "tribes" },
+      { "wheatfield_ripe", 57, 16, "tribes" },
+      { "wheatfield_harvested", 58, 16, "tribes" },
+      { "wheatfield_harvested", 54, 17, "tribes" },
+      { "wheatfield_medium", 55, 17, "tribes" },
+      { "wheatfield_harvested", 57, 17, "tribes" },
+      { "wheatfield_harvested", 55, 18, "tribes" },
+      { "wheatfield_harvested", 57, 18, "tribes" },
+      { "wheatfield_harvested", 53, 31, "tribes" },
+      { "wheatfield_harvested", 54, 31, "tribes" },
+      { "wheatfield_medium", 55, 31, "tribes" },
+      { "wheatfield_harvested", 56, 32, "tribes" },
+      { "wheatfield_harvested", 52, 33, "tribes" },
+      { "wheatfield_small",55, 33, "tribes" },
+      { "wheatfield_harvested", 56, 33, "tribes" },
+      { "wheatfield_harvested", 53, 34, "tribes" },
+      { "wheatfield_ripe", 54, 34, "tribes" },
+      { "wheatfield_harvested", 56, 34, "tribes" },
+      { "wheatfield_harvested", 53, 35, "tribes" },
+      { "wheatfield_harvested", 55, 35, "tribes" },
    }
 
    local plr = wl.Game().players[1]
@@ -143,16 +143,16 @@ function reveal_village()
 
    -- Build roads
    -- Start at northern sentry
-   connected_road(plr, map:get_field(58, 10).immovable,
+   connected_road("normal", plr, map:get_field(58, 10).immovable,
       "w,sw|se,sw|e,se|se,se|sw,sw|sw,w|sw,sw|se,sw|sw,sw|se,sw|" ..
       "sw,sw|sw,sw|sw,sw|se,se,sw|e,e|sw,sw|se,sw|")
 
-   connected_road(plr, map:get_field(57, 25).immovable, "sw,w|sw,w")
-   connected_road(plr, map:get_field(57, 29).immovable, "w,w|w,w")
-   connected_road(plr, map:get_field(55, 34).immovable, "sw,sw")
-   connected_road(plr, map:get_field(57, 22).immovable, "sw,w")
-   connected_road(plr, map:get_field(54, 19).immovable, "sw,se,e")
-   connected_road(plr, map:get_field(56, 17).immovable, "sw,se")
+   connected_road("normal", plr, map:get_field(57, 25).immovable, "sw,w|sw,w")
+   connected_road("normal", plr, map:get_field(57, 29).immovable, "w,w|w,w")
+   connected_road("normal", plr, map:get_field(55, 34).immovable, "sw,sw")
+   connected_road("normal", plr, map:get_field(57, 22).immovable, "sw,w")
+   connected_road("normal", plr, map:get_field(54, 19).immovable, "sw,se,e")
+   connected_road("normal", plr, map:get_field(56, 17).immovable, "sw,se")
 end
 
 run(village_thread)

data/campaigns/bar01.wmf/scripting/starting_conditions.lua
@@ -56,9 +56,9 @@ hq:set_soldiers({0,0,0,0}, 45)
 -- ============
 -- Build roads
 -- ============
-connected_road(plr, sf.brn.immovable, "r,r|br,r|r,r")
-connected_road(plr, sf.brn.immovable, "l,l|l,bl,bl|br,r|br,r|r,tr|tr,tr,tr")
-connected_road(plr, map:get_field(13,14).immovable, "r,br|br,r")
+connected_road("normal", plr, sf.brn.immovable, "r,r|br,r|r,r")
+connected_road("normal", plr, sf.brn.immovable, "l,l|l,bl,bl|br,r|br,r|r,tr|tr,tr,tr")
+connected_road("normal", plr, map:get_field(13,14).immovable, "r,br|br,r")
 
 -- Place some initial buildings
 prefilled_buildings(plr,

data/campaigns/bar02.wmf/scripting/starting_conditions.lua
@@ -107,10 +107,10 @@ local b_table = prefilled_buildings(p3,
 )
 
 -- This player has no AI so we build some roads for him
-connected_road(p3, b_table[1].flag, "l, tl, tl" )
-connected_road(p3, b_table[2].flag, "r, r, r, r, r, tr")
-connected_road(p3, b_table[3].flag, "bl, bl, bl, bl" )
-connected_road(p3, b_table[5].flag, "br, br, bl, bl" )
+connected_road("normal", p3, b_table[1].flag, "l, tl, tl" )
+connected_road("normal", p3, b_table[2].flag, "r, r, r, r, r, tr")
+connected_road("normal", p3, b_table[3].flag, "bl, bl, bl, bl" )
+connected_road("normal", p3, b_table[5].flag, "br, br, bl, bl" )
 
 -- =======================================================================
 --                                 Player 4

data/campaigns/emp04.wmf/scripting/mission_thread.lua
@@ -481,10 +481,10 @@ function wheat_chain()
       place_building_in_region(p1, "empire_brewery", {field_brewery})
       place_building_in_region(p1, "empire_mill", {field_mill})
       place_building_in_region(p1, "empire_temple_of_vesta", {field_warehouse}, {wares = {water = 30, flour = 30, beer = 40,}})
-      connected_road(p1, field_warehouse.immovable.flag, "l, tl", true)
-      connected_road(p1, field_mill.immovable.flag, "tr, r", true)
-      connected_road(p1, field_mill.immovable.flag, "l, tl, tr", true)
-      connected_road(p1, field_mill.immovable.flag, "br, r", true)
+      connected_road("normal", p1, field_warehouse.immovable.flag, "l, tl", true)
+      connected_road("normal", p1, field_mill.immovable.flag, "tr, r", true)
+      connected_road("normal", p1, field_mill.immovable.flag, "l, tl, tr", true)
+      connected_road("normal", p1, field_mill.immovable.flag, "br, r", true)
 
       campaign_message_box(amalea_12)
       campaign_message_box(saledus_3)

data/campaigns/emp04.wmf/scripting/starting_conditions.lua
@@ -14,10 +14,10 @@ place_building_in_region(p3, "empire_mill", {field_mill})
 field_warehouse = map:get_field(21, 186)
 place_building_in_region(p3, "empire_temple_of_vesta", {field_warehouse}, {workers = {empire_carrier = 0, empire_recruit = 0}, wares = {wheat = 200}, soldiers = {[{0,0,0,0}] = 3}})
 
-   r1 = p3:place_road(field_warehouse.immovable.flag, "l", "tl", true)
-   r2 = p3:place_road(field_mill.immovable.flag, "tr", "r", true)
-   r3 = p3:place_road(field_mill.immovable.flag, "l", "tl", "tr", true)
-   r4 = p3:place_road(field_mill.immovable.flag, "br", "r", true)
+   r1 = p3:place_road("normal", field_warehouse.immovable.flag, "l", "tl", true)
+   r2 = p3:place_road("normal", field_mill.immovable.flag, "tr", "r", true)
+   r3 = p3:place_road("normal", field_mill.immovable.flag, "l", "tl", "tr", true)
+   r4 = p3:place_road("normal", field_mill.immovable.flag, "br", "r", true)
 
 p3:forbid_buildings("all")
 field_warehouse.brn.immovable.ware_economy:set_target_quantity("beer", 180)
@@ -130,7 +130,7 @@ function try_place_roads_with_carriers(x, y)
                to_field:has_caps("flag") or
                (to_field.immovable and to_field.immovable.descr.type_name == "flag")
             ) then
-            local road = p1:place_road(field.immovable, direction[1], direction[2], true)
+            local road = p1:place_road("normal", field.immovable, direction[1], direction[2], true)
             road:set_workers('empire_carrier', 1)
          end
       end

data/campaigns/tutorial02_warfare.wmf/scripting/mission_thread.lua
@@ -112,9 +112,9 @@ function create_enemy()
          }
       }
    )
-   connected_road(p2,map:get_field(29,17).immovable,"tr,tl|tl,tl|tl,tl|tl,tl|tl,l")
-   connected_road(p2,map:get_field(31,22).immovable,"tr,tl|tl,tl,tl")
-   connected_road(p2,map:get_field(31,28).immovable,"tr,tr|tr,tl|tl,tl")
+   connected_road("normal", p2,map:get_field(29,17).immovable,"tr,tl|tl,tl|tl,tl|tl,tl|tl,l")
+   connected_road("normal", p2,map:get_field(31,22).immovable,"tr,tl|tl,tl,tl")
+   connected_road("normal", p2,map:get_field(31,28).immovable,"tr,tr|tr,tl|tl,tl")
    p2:forbid_buildings("all")
 end
 

data/campaigns/tutorial02_warfare.wmf/scripting/starting_conditions.lua
@@ -62,10 +62,10 @@ function init_player()
 
    -- Build the roads
    local map = wl.Game().map
-   connected_road(plr,map:get_field(29,58).immovable,"br,r|r,r|r,tr")
-   connected_road(plr,map:get_field(38,62).immovable,"l,l|l,bl|tl,tl|tl,tl")
-   connected_road(plr,map:get_field(32,63).immovable,"tr,tr|")
-   connected_road(plr,map:get_field(25,2).immovable,"tr,tr|tr,tr|tr,tr|tr,tr")
+   connected_road("normal", plr,map:get_field(29,58).immovable,"br,r|r,r|r,tr")
+   connected_road("normal", plr,map:get_field(38,62).immovable,"l,l|l,bl|tl,tl|tl,tl")
+   connected_road("normal", plr,map:get_field(32,63).immovable,"tr,tr|")
+   connected_road("normal", plr,map:get_field(25,2).immovable,"tr,tr|tr,tr|tr,tr|tr,tr")
 end
 
 run(init_player)

commit 711376b714bb581615a7c991bcb4ffa6f58e746c

Fix road buttons added instead of waterway buttons
Fix a bug causing road buttons instead of waterway buttons to be added in waterway building mode when clicking on a field where the waterway can't be built.
The bug was introduced in a8c236de51e837df467a6f0ea55444b498c2752e

src/wui/fieldaction.cc
@@ -1027,7 +1027,11 @@ void show_field_action(InteractiveBase* const ibase,
 		    (ibase->in_road_building_mode(RoadBuildingType::kWaterway) &&
 		     target != ibase->get_build_road_end())) {
 			FieldActionWindow& w = *new FieldActionWindow(ibase, player, registry);
-			w.add_buttons_road(false);
+			if (ibase->in_road_building_mode(RoadBuildingType::kRoad)) {
+				w.add_buttons_road(false);
+			} else {
+				w.add_buttons_waterway(false);
+			}
 			w.init();
 			return;
 		}
@@ -1052,9 +1056,9 @@ void show_field_action(InteractiveBase* const ibase,
 			} else {
 				FieldActionWindow& w = *new FieldActionWindow(ibase, player, registry);
 				if (ibase->in_road_building_mode(RoadBuildingType::kRoad)) {
-					w.add_buttons_waterway(false);
-				} else {
 					w.add_buttons_road(false);
+				} else {
+					w.add_buttons_waterway(false);
 				}
 				w.init();
 				return;