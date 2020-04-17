f7f6b2590c5e548dc499f7f4c5ba4efcad7bc291
hessenfarmer <stephan-lutz@gmx.de>
2020-04-17 18:53:41 +0200

    updated documentation

--- data/tribes/wares/armor/init.lua
+++ data/tribes/wares/armor/init.lua
@@ -39,6 +39,10 @@ dirname = path.dirname(__file__)
 --
 --    **default_target_quantity**: A table listing the default target quantity
 --    for each tribe's economy. For example, ``{ atlanteans = 3, empire = 1 }``
+--    This parameter is optional. If not set the economy will always demand this ware.
+--    If set to zero the economy will not demand this ware unless it is required in a
+--    production building. If not set or set to zero the actual target quantity will
+--    not be available in the economy settings window
 --
 --    **preciousness**: How precious this ware is to each tribe. For example,
 --    ``{ atlanteans = 0, empire = 1 }``. We recommend not going higher than ``25``.
