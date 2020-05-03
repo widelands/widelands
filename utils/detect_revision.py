0b962cf2efb635f0c6720b6875b5e4f1a0c6c5fe
Niektóry <niektory@gmail.com>
2020-05-03 06:09:09 +0200

    Extract Git hash from Bazaar commit message

--- utils/detect_revision.py
+++ utils/detect_revision.py
@@ -80,6 +80,19 @@ def check_for_explicit_version():
 
 
 def detect_bzr_revision():
+    def extract_git_hash(commit_message):
+        # Get the last string in the commit message
+        git_hash = commit_message.split()[-1]
+        try:
+            # Does it look like a git hash?
+            assert len(git_hash) == 40 and int(git_hash, 16)
+        except (AssertionError, ValueError):
+            # It doesn't :(
+            return 'NO_HASH'
+        else:
+            # It does; shorten it
+            return git_hash[:7]
+
     if __has_bzrlib:
         try:
             b = BzrDir.open(base_path).open_branch()
@@ -91,11 +104,14 @@ def detect_bzr_revision():
         # Windows stand alone installer do not come with bzrlib. We try to
         # parse the output of bzr then directly
         try:
-            def run_bzr(subcmd):
-                return _communicate_utf8(['bzr', subcmd], cwd=base_path).strip()
+            def run_bzr(*args):
+                return _communicate_utf8(['bzr', *args], cwd=base_path).strip()
             revno = run_bzr('revno')
             nick = run_bzr('nick')
-            return 'bzr%s[%s]' % (revno, nick)
+            commit_message = run_bzr('log', '--limit=1', '--short')
+            git_hash = extract_git_hash(commit_message)
+            return 'bzr{revno}[{git_hash}@{nick}]'.format(
+                    revno=revno, git_hash=git_hash, nick=nick)
         except (OSError, subprocess.CalledProcessError):
             return None
     return None
