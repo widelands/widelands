08b1c0c74102a77522af8fc8a4298bc7f6e720ab
GunChleoc <fios@foramnagaidhlig.net>
2020-07-06 07:18:06 +0100

    Fix optional priority parameter

--- src/logic/map_objects/map_object_program.cc
+++ src/logic/map_objects/map_object_program.cc
@@ -120,24 +120,22 @@ MapObjectProgram::PlaySoundParameters
 MapObjectProgram::parse_act_play_sound(const std::vector<std::string>& arguments,
                                        uint8_t default_priority) {
     std::string filepath = "";
-    unsigned int priority = default_priority;
+    PlaySoundParameters result;
 
     // TODO(GunChleoc): Savegame compabitility. Remove after Build 21.
     if (arguments.size() == 3) {
         filepath = arguments.at(0) + "/" + arguments.at(1);
-        priority = read_positive(arguments.at(2));
+        result.priority = read_positive(arguments.at(2));
     } else {
         if (arguments.size() < 1 || arguments.size() > 2) {
             throw GameDataError("Usage: playsound=<sound_dir/sound_name> [priority]");
         }
         filepath = arguments.at(0);
-        priority = read_positive(arguments.at(1));
+        result.priority = arguments.size() == 2 ? read_positive(arguments.at(1)) : default_priority;
     }
 
-	PlaySoundParameters result;
 	result.fx = SoundHandler::register_fx(SoundType::kAmbient, filepath);
 
-	result.priority = arguments.size() == 2 ? priority : default_priority;
 	if (result.priority < kFxPriorityLowest) {
 		throw GameDataError("Minmum priority for sounds is %d, but only %d was specified for %s",
 		                    kFxPriorityLowest, result.priority, filepath.c_str());
