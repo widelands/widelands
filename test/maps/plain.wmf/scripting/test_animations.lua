ede4e63b2ac5df5b5ce4c0e635123a301c8264a7
GunChleoc <fios@foramnagaidhlig.net>
2020-08-05 11:16:49 +0100

    Fix comments

--- test/maps/plain.wmf/scripting/test_animations.lua
+++ test/maps/plain.wmf/scripting/test_animations.lua
@@ -8,12 +8,12 @@ run(function()
    hq:set_workers("barbarians_file_animation", 5)
    assert_equal(5, hq:get_workers("barbarians_file_animation"))
 
-   -- This mine needs a miner, a chief miner and a master miner
+   -- Place a building that has a spritesheet animation
    p1:place_road("normal", p1:place_flag(map:get_field(18,22)), "br", "br", "br", "br", "r", "r", "r")
    p1:place_flag(map:get_field(20,26))
    local building = p1:place_building("barbarians_spritesheet_animation", wl.Game().map:get_field(17, 21), false, true)
 
-   -- Make the worker walk out of the building and idle for a few times
+   -- Make the worker (file animations) walk out of the building and idle for a few times
    sleep(5 * 40000)
 
    print("# All Tests passed.")
