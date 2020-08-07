254e238d87f966e78cb90f9d19e0989a55a506c4
GunChleoc <fios@foramnagaidhlig.net>
2020-08-07 15:45:22 +0100

    Remove enum PlayerType - nobody was using it

--- src/wui/interactive_gamebase.cc
+++ src/wui/interactive_gamebase.cc
@@ -65,13 +65,11 @@ constexpr uint16_t kSpeedFast = 10000;
 
 InteractiveGameBase::InteractiveGameBase(Widelands::Game& g,
                                          Section& global_s,
-                                         PlayerType pt,
                                          bool const multiplayer,
                                          ChatProvider* chat_provider)
    : InteractiveBase(g, global_s),
      chat_provider_(chat_provider),
      multiplayer_(multiplayer),
-     playertype_(pt),
      showhidemenu_(toolbar(),
                    "dropdown_menu_showhide",
                    0,
