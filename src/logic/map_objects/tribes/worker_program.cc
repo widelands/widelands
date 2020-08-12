79044923f0a79b2253bfc6e41ffe91f95eaf779e
GunChleoc <fios@foramnagaidhlig.net>
2020-08-12 18:53:31 +0100

    Fix clang-tidy hit

--- src/logic/map_objects/tribes/worker_program.cc
+++ src/logic/map_objects/tribes/worker_program.cc
@@ -726,7 +726,7 @@ void WorkerProgram::parse_createbob(Worker::Action* act, const std::vector<std::
 	}
 
 	act->function = &Worker::run_createbob;
-	act->sparamv = std::move(cmd);
+	act->sparamv = cmd;
 
 	// Register created bobs
 	for (const std::string& bobname : act->sparamv) {
