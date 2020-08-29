c45180b583f470042f13f6801be41bf0eb5fb020
GunChleoc <fios@foramnagaidhlig.net>
2020-08-29 02:24:15 +0100

    Adapt to engine changes

--- test/maps/plain.wmf/scripting/tribes/init.lua
+++ test/maps/plain.wmf/scripting/tribes/init.lua
@@ -1,20 +1,18 @@
 tribes = wl.Tribes()
-include "scripting/mapobjects.lua"
 
 -- ===================================
 --    Campaign Specific Data
 -- ===================================
-print_loading_message("Loading custom units", function()
-   include "map:scripting/tribes/file_animation/init.lua"
-   include "map:scripting/tribes/spritesheet_animation/init.lua"
-end)
 
-tribes:add_custom_worker {
-   tribename = "barbarians",
-   workername = "barbarians_file_animation",
-}
-
-tribes:add_custom_building {
-   tribename = "barbarians",
-   buildingname = "barbarians_spritesheet_animation",
+return {
+   barbarians = {
+      buildings = {
+         "barbarians_spritesheet_animation",
+      }
+   },
+   workers_order = {
+      {
+         { name = "barbarians_file_animation" }
+      },
+   }
 }
