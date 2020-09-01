853de40f19e3600cfa56d89da3596184ff355fe2
matthiakl <t-m.42@arcor.de>
2020-09-01 23:49:15 +0200

    Don't load height data for rendering

--- src/map_io/widelands_map_loader.cc
+++ src/map_io/widelands_map_loader.cc
@@ -116,23 +116,13 @@ int32_t WidelandsMapLoader::load_map_for_render(EditorGameBase& egbase) {
 	ScopedTimer timer(timer_message);
 
 	map_.set_size(map_.width_, map_.height_);
-	mol_.reset(new MapObjectLoader());
-
-	log_info("Reading Heights Data ... ");
-	{
-		MapHeightsPacket p;
-		p.read(*fs_, egbase, true, *mol_);
-	}
-	log_info(" → took %ums\n ", timer.ms_since_last_query());
 
 	std::unique_ptr<WorldLegacyLookupTable> world_lookup_table(
 	   create_world_legacy_lookup_table(old_world_name_));
-	log_info("Reading Terrain Data ... ");
 	{
 		MapTerrainPacket p;
 		p.read(*fs_, egbase, *world_lookup_table);
 	}
-	log_info(" → took %ums\n ", timer.ms_since_last_query());
 
 	return 0;
 }
