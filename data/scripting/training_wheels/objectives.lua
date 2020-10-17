af73295586e4f498024eeba34b4a1042edaaabb2
GunChleoc <fios@foramnagaidhlig.net>
2020-10-17 14:49:43 +0100

    Design the objectives training wheel

--- data/scripting/training_wheels/objectives.lua
+++ data/scripting/training_wheels/objectives.lua
@@ -1,68 +1,77 @@
--- NOOCM document
+-- Welcome and teach objectives
+
+-- NOCOM document
 include "scripting/coroutine.lua"
 include "scripting/messages.lua"
 include "scripting/richtext_scenarios.lua"
 include "scripting/ui.lua"
 
 run(function()
-   sleep(2000)
+   sleep(200)
+
+   local mapview = wl.ui.MapView()
+
    push_textdomain("training_wheels")
 
-   print("This is objectives.lua")
-   local close_story_window_instructions = _[[Click on the ‘OK’ button or press the ‘Enter ⏎’ key on the keyboard to close this window.]]
-
-   -- Welcome and teach objectives
-   local obj_initial_close_story_window = {
-      name = "initial_close_story_window",
-      title=_"Close this window",
-      number = 1,
-      body = objective_text(_"Close this window",
-         li(close_story_window_instructions)
+   local welcome_message = {
+      title = _"Welcome to Widelands!",
+      h = 160,
+      w = 360,
+      body = (
+         li_image("images/logos/wl-ico-64.png", h1(_"Welcome to Widelands!")) ..
+         li_image("images/wui/training_wheels_arrow.png", _"Follow the arrows to learn how to play.")
       )
    }
 
-   local initial_message_01 = {
-      title = _"Welcome to the Widelands Tutorial!",
+   local objectives_message = {
+      title = _"Objectives",
+      position = "bottomright",
+      h = 280,
+      w = 260,
       body = (
-         h1(_"Welcome to Widelands!") ..
-         li_image("images/logos/wl-ico-64.png",
-            _[[Widelands is a slow-paced build-up strategy game with an emphasis on construction rather than destruction. This tutorial will guide you through the basics of the game.]]) ..
-         li_arrow(_[[You will be guided through this tutorial by objectives]]) ..
-         li(_[[Follow the intructions in the objective below so that I can show you where to find them.]])
+         -- No intro text, the objective itself is enough
+         ""
+      )
+   }
+
+   local objectives_message_objective = {
+   name = "objectives_message_objective",
+      title = _"Objectives",
+      position = "bottomright",
+      number = 1,
+      body = (
+         objective_text(_"Open the Objectives window",
+         li_image("images/wui/training_wheels_arrow.png", _"Click on the button that the arrow is pointing to, or press ‘%s’ to see the current game's objectives":bformat('o')) ..
+         li(_"You'll need to close this message window first."))
       )
    }
 
    local obj_initial_close_objectives_window = {
       name = "obj_initial_close_objectives_window",
-      title=_"Objectives and how to close this window",
+      title=_"Close this window",
       number = 1,
       body = objective_text(_"Closing this window",
-         p(_[[This is the ‘Objectives’ window. You can return to this window for instructions at any time.]]) ..
          li_image("images/wui/menus/objectives.png", _[[ You can open and close this window by clicking on the ‘Objectives’ button in the toolbar on the bottom of the screen.]]) ..
-         li_arrow(_[[Like any other window, you can also close the ‘Objectives’ window by right-clicking on it.]]) ..
-         li_arrow(_[[When you have accomplished an objective, it will disappear from the list above.]]) ..
-         li(_[[Try it out.]])
+         li_arrow(_[[Like any other window, you can also close the ‘Objectives’ window by right-clicking on it.]])
       )
    }
 
    pop_textdomain()
 
-   local objective_to_explain_objectives = add_campaign_objective(obj_initial_close_objectives_window)
-   local o = campaign_message_with_objective(initial_message_01, obj_initial_close_story_window)
-   set_objective_done(o, 100)
+   local o2 = add_campaign_objective(obj_initial_close_objectives_window)
 
-   wl.ui.MapView().buttons.objectives:click()
+   campaign_message_box(welcome_message, 100)
 
-   sleep(5000)
+   mapview.buttons.objectives:indicate(true)
 
-   wl.ui.MapView().buttons.objectives:indicate(true)
+   local o1 = campaign_message_with_objective(objectives_message, objectives_message_objective, 0)
+   set_objective_done(o1)
 
-   while not wl.ui.MapView().windows.objectives do sleep(100) end
-   while wl.ui.MapView().windows.objectives do sleep(100) end
-
-   wl.ui.MapView().buttons.objectives:indicate(false)
-
-   set_objective_done(objective_to_explain_objectives)
+   while not mapview.windows.objectives do sleep(100) end
+   sleep (100)
+   mapview.buttons.objectives:indicate(false)
+   while mapview.windows.objectives do sleep(100) end
+   set_objective_done(o2)
 
    wl.Game().players[1]:mark_training_wheel_as_solved("objectives")
 end)
