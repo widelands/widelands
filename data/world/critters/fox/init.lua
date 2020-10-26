cf3f7f64c95247987b6b288b5d4785d3a081be15
gunchleoc <fios@foramnagaidhlig.net>
2020-10-26 15:55:22 +0000

    Merge wl.World() and wl.Tribes() (#4412)

--- data/world/critters/fox/init.lua
+++ data/world/critters/fox/init.lua
@@ -2,7 +2,7 @@ push_textdomain("world")
 
 dirname = path.dirname(__file__)
 
-wl.World():new_critter_type{
+wl.Descriptions():new_critter_type{
    name = "fox",
    descname = _ "Fox",
    icon = dirname .. "menu.png",
--- data/world/immovables/miscellaneous/pebble1/init.lua
+++ data/world/immovables/miscellaneous/pebble1/init.lua
@@ -2,7 +2,7 @@ push_textdomain("world")
 
 dirname = path.dirname(__file__)
 
-wl.World():new_immovable_type{
+wl.Descriptions():new_immovable_type{
    name = "pebble1",
    descname = _ "Pebble",
    size = "none",
