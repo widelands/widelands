5dc5b84af75b8934178fca9751745bc5c2229df7
GunChleoc <fios@foramnagaidhlig.net>
2020-11-12 07:38:16 +0000

    Fix testsuite

--- test/maps/lua_testsuite.wmf/scripting/gettext.lua
+++ test/maps/lua_testsuite.wmf/scripting/gettext.lua
@@ -4,10 +4,11 @@ test_descr = lunit.TestCase("Gettext Test")
 --  whether gettext functions crash or not
 
 function test_descr:test_gettext()
-   set_textdomain("tribes")
+   push_textdomain("tribes")
    assert_equal("foo", _"foo")
    assert_equal("foo", _("foo"))
    assert_equal(_"Carrier", _("Carrier"))
+   pop_textdomain()
 end
 
 function test_descr:test_ngettext()
@@ -26,4 +27,18 @@ end
 
 function test_descr:test_pgettext()
    assert_equal("bar", pgettext("foo", "bar"))
+end
+
+function test_descr:test_npgettext()
+   assert_equal("foo", npgettext("widelands", "foo", "bar", 1))
+   assert_equal("bar", npgettext("widelands", "foo", "bar", 1000))
+   assert_equal("1 foo", npgettext("widelands", "%1% foo", "bar %1%", 1):bformat(1))
+   assert_equal("bar 2", npgettext("widelands", "%1% foo", "bar %1%", 2):bformat(2))
+   -- ngettext is undefined for floating point or negative numbers.
+   assert_error("Call to ngettext with floating point", function()
+      npgettext("widelands", "foo", "bar", 1.5)
+   end)
+   assert_error("Call to ngettext with negative number", function()
+      npgettext("widelands", "foo", "bar", -1)
+   end)
 end
