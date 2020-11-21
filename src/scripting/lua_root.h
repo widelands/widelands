580666676a6489eefd21c52e674f1edd4fba17e6
Benedikt Straub <benedikt-straub@web.de>
2020-11-21 17:12:11 +0100

    Refactor modify_unit

--- src/scripting/lua_root.h
+++ src/scripting/lua_root.h
@@ -152,6 +152,19 @@ public:
 	/*
 	 * C methods
 	 */
+
+	void do_modify_tribe(lua_State* L, const std::string& name, const std::string& property);
+	void do_modify_trainingsite(lua_State* L, const std::string&, const std::string&);
+	void do_modify_productionsite(lua_State* L, const std::string&, const std::string&);
+	void do_modify_militarysite(lua_State* L, const std::string&, const std::string&);
+	void do_modify_warehouse(lua_State* L, const std::string&, const std::string&);
+	void do_modify_worker(lua_State* L, const std::string&, const std::string&);
+	void do_modify_ware(lua_State* L, const std::string&, const std::string&);
+	void do_modify_immovable(lua_State* L, const std::string&, const std::string&);
+	void do_modify_resource(lua_State* L, const std::string&, const std::string&);
+	void do_modify_terrain(lua_State* L, const std::string&, const std::string&);
+	void do_modify_critter(lua_State* L, const std::string&, const std::string&);
+	void do_modify_ship(lua_State* L, const std::string&, const std::string&);
 };
 
 // TODO(GunChleoc): This class is here for saveloading compatibility only. We'll get a SIGABRT from
