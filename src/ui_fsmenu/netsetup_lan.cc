f997864954af1ce79d5aed7409734b7d98b7762d
Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>
2021-01-01 16:31:12 +0100

    Fix LAN mapname localization (#4567)

--- src/ui_fsmenu/netsetup_lan.cc
+++ src/ui_fsmenu/netsetup_lan.cc
@@ -207,7 +207,10 @@ void NetSetupLAN::update_game_info(UI::Table<NetOpenGame const* const>::EntryRec
 	assert(info.hostname[sizeof(info.hostname) - 1] == '\0');
 	er.set_string(0, info.hostname);
 	assert(info.map[sizeof(info.map) - 1] == '\0');
-	er.set_string(1, info.map);
+	{
+		i18n::Textdomain td("maps");
+		er.set_string(1, info.map[0] ? i18n::translate(info.map) : "");
+	}
 
 	er.set_disabled(info.state != LAN_GAME_OPEN);
 	switch (info.state) {
