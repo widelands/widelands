9056b4ff3f42216b6c49f25b4d0e0db3d3819e20
Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>
2021-01-05 11:30:23 +0100

    Color chooser enhancements (#4582)

--- src/ui_basic/color_chooser.cc
+++ src/ui_basic/color_chooser.cc
@@ -23,7 +23,6 @@
 
 #include "base/i18n.h"
 #include "graphic/image_cache.h"
-#include "graphic/playercolor.h"
 #include "graphic/rendertarget.h"
 #include "graphic/texture.h"
 
@@ -39,25 +38,30 @@ struct ColorChooserImpl : public Panel {
 	ColorChooserImpl(Panel& parent, PanelStyle s, ColorChooser& c)
 	   : Panel(&parent, s, 0, 0, kMainDimension + kSpacing + kSidebarWidth, kMainDimension),
 	     chooser_(c),
+	     sidebar_attribute_(ColorAttribute::kRed),
 	     selector_(*g_image_cache->get("images/ui_basic/fsel.png")),
 	     texture_left_(kMainDimension, kMainDimension),
 	     texture_right_(1, kMainDimension),
-	     texture_left_r_(0),
-	     texture_right_g_(0),
-	     texture_right_b_(0) {
-		update_texture_left();
-		update_texture_right();
+	     texture_cache_r_(0),
+	     texture_cache_g_(0),
+	     texture_cache_b_(0) {
+	}
+
+	ColorAttribute sidebar_attribute() const {
+		return sidebar_attribute_;
+	}
+	void set_sidebar_attribute(ColorAttribute a) {
+		sidebar_attribute_ = a;
+		update_textures();
 	}
 
 	void draw(RenderTarget& dst) override {
 		Panel::draw(dst);
 
-		if (chooser_.get_color().r != texture_left_r_) {
-			update_texture_left();
-		}
-		if (chooser_.get_color().g != texture_right_g_ ||
-		    chooser_.get_color().b != texture_right_b_) {
-			update_texture_right();
+		if (chooser_.get_color().r != texture_cache_r_ ||
+		    chooser_.get_color().g != texture_cache_g_ ||
+		    chooser_.get_color().b != texture_cache_b_) {
+			update_textures();
 		}
 
 		draw_textures(dst);
@@ -69,10 +73,10 @@ struct ColorChooserImpl : public Panel {
 	RGBColor color_at(const int32_t x, const int32_t y) {
 		if (y >= 0 && y < kMainDimension) {
 			if (x >= 0 && x < kMainDimension) {
-				return RGBColor(chooser_.get_color().r, x, y);
+				return left_color_at(x, y);
 			}
 			if (x >= kMainDimension + kSpacing && x < kMainDimension + kSpacing + kSidebarWidth) {
-				return RGBColor(y, chooser_.get_color().g, chooser_.get_color().b);
+				return right_color_at(y);
 			}
 		}
 		return chooser_.get_color();
@@ -88,27 +92,74 @@ struct ColorChooserImpl : public Panel {
 
 private:
 	ColorChooser& chooser_;
+	ColorAttribute sidebar_attribute_;
 	const Image& selector_;
 
 	Texture texture_left_, texture_right_;
-	uint8_t texture_left_r_, texture_right_g_, texture_right_b_;
+	uint8_t texture_cache_r_, texture_cache_g_, texture_cache_b_;
 
-	void update_texture_left() {
+	RGBColor left_color(const uint16_t val1, const uint16_t val2) const {
+		switch (sidebar_attribute_) {
+		case ColorAttribute::kRed:
+			return RGBColor(texture_cache_r_, val1, val2);
+		case ColorAttribute::kGreen:
+			return RGBColor(val1, texture_cache_g_, val2);
+		case ColorAttribute::kBlue:
+			return RGBColor(val1, val2, texture_cache_b_);
+		}
+		NEVER_HERE();
+	}
+	RGBColor right_color(const uint16_t val) const {
+		switch (sidebar_attribute_) {
+		case ColorAttribute::kRed:
+			return RGBColor(val, texture_cache_g_, texture_cache_b_);
+		case ColorAttribute::kGreen:
+			return RGBColor(texture_cache_r_, val, texture_cache_b_);
+		case ColorAttribute::kBlue:
+			return RGBColor(texture_cache_r_, texture_cache_g_, val);
+		}
+		NEVER_HERE();
+	}
+
+	RGBColor left_color_at(const uint16_t x, const uint16_t y) {
+		switch (sidebar_attribute_) {
+		case ColorAttribute::kRed:
+			return RGBColor(chooser_.get_color().r, x, y);
+		case ColorAttribute::kGreen:
+			return RGBColor(x, chooser_.get_color().g, y);
+		case ColorAttribute::kBlue:
+			return RGBColor(x, y, chooser_.get_color().b);
+		}
+		NEVER_HERE();
+	}
+	RGBColor right_color_at(const uint16_t y) {
+		switch (sidebar_attribute_) {
+		case ColorAttribute::kRed:
+			return RGBColor(y, chooser_.get_color().g, chooser_.get_color().b);
+		case ColorAttribute::kGreen:
+			return RGBColor(chooser_.get_color().r, y, chooser_.get_color().b);
+		case ColorAttribute::kBlue:
+			return RGBColor(chooser_.get_color().r, chooser_.get_color().g, y);
+		}
+		NEVER_HERE();
+	}
+
+	void update_textures() {
+		texture_cache_r_ = chooser_.get_color().r;
+		texture_cache_g_ = chooser_.get_color().g;
+		texture_cache_b_ = chooser_.get_color().b;
+
 		texture_left_.lock();
-		texture_left_r_ = chooser_.get_color().r;
-		for (uint16_t g = 0; g < 256; ++g) {
-			for (uint16_t b = 0; b < 256; ++b) {
-				texture_left_.set_pixel(g, b, RGBColor(texture_left_r_, g, b));
+		for (uint16_t x = 0; x < kMainDimension; ++x) {
+			for (uint16_t y = 0; y < kMainDimension; ++y) {
+				texture_left_.set_pixel(x, y, left_color(x, y));
 			}
 		}
 		texture_left_.unlock(Texture::UnlockMode::Unlock_Update);
-	}
-	void update_texture_right() {
+
 		texture_right_.lock();
-		texture_right_g_ = chooser_.get_color().g;
-		texture_right_b_ = chooser_.get_color().b;
-		for (uint16_t r = 0; r < 256; ++r) {
-			texture_right_.set_pixel(0, r, RGBColor(r, texture_right_g_, texture_right_b_));
+		for (uint16_t y = 0; y < kMainDimension; ++y) {
+			texture_right_.set_pixel(0, y, right_color(y));
 		}
 		texture_right_.unlock(Texture::UnlockMode::Unlock_Update);
 	}
@@ -116,11 +167,28 @@ private:
 	inline void do_draw_cursor(RenderTarget& dst, int x, int y) {
 		dst.blit(Vector2i(x, y), &selector_);
 	}
-	inline void draw_selection_cursors(RenderTarget& dst) {
-		do_draw_cursor(dst, chooser_.get_color().g - selector_.width() / 2,
-		               chooser_.get_color().b - selector_.height() / 2);
+	void draw_selection_cursors(RenderTarget& dst) {
+		unsigned x = 0, y = 0, a = 0;
+		switch (sidebar_attribute_) {
+		case ColorAttribute::kRed:
+			x = chooser_.get_color().g;
+			y = chooser_.get_color().b;
+			a = chooser_.get_color().r;
+			break;
+		case ColorAttribute::kGreen:
+			x = chooser_.get_color().r;
+			y = chooser_.get_color().b;
+			a = chooser_.get_color().g;
+			break;
+		case ColorAttribute::kBlue:
+			x = chooser_.get_color().r;
+			y = chooser_.get_color().g;
+			a = chooser_.get_color().b;
+			break;
+		}
+		do_draw_cursor(dst, x - selector_.width() / 2, y - selector_.height() / 2);
 		do_draw_cursor(dst, kMainDimension + kSpacing + (kSidebarWidth - selector_.width()) / 2,
-		               chooser_.get_color().r - selector_.height() / 2);
+		               a - selector_.height() / 2);
 	}
 	void draw_mouse_cursors(RenderTarget& dst) {
 		const Vector2i mousepos = get_mouse_position();
@@ -157,6 +225,11 @@ ColorChooser::ColorChooser(Panel* parent,
      hbox_(&main_box_, panel_style_, 0, 0, Box::Horizontal),
      buttonsbox_(&main_box_, panel_style_, 0, 0, Box::Horizontal),
      vbox_(&hbox_, panel_style_, 0, 0, Box::Vertical),
+     box_r_(&vbox_, panel_style_, 0, 0, Box::Horizontal),
+     box_g_(&vbox_, panel_style_, 0, 0, Box::Horizontal),
+     box_b_(&vbox_, panel_style_, 0, 0, Box::Horizontal),
+     palette_box_1_(&vbox_, panel_style_, 0, 0, Box::Horizontal),
+     palette_box_2_(&vbox_, panel_style_, 0, 0, Box::Horizontal),
      button_ok_(&buttonsbox_,
                 "ok",
                 0,
@@ -183,6 +256,33 @@ ColorChooser::ColorChooser(Panel* parent,
         0,
         s == WindowStyle::kWui ? ButtonStyle::kWuiSecondary : ButtonStyle::kFsMenuSecondary,
         _("Initial Color")),
+     button_r_(&box_r_,
+               "button_r",
+               0,
+               0,
+               kButtonSize,
+               kButtonSize,
+               s == WindowStyle::kWui ? ButtonStyle::kWuiMenu : ButtonStyle::kFsMenuMenu,
+               /** TRANSLATORS: First letter of the word "Red" */
+               _("R")),
+     button_g_(&box_g_,
+               "button_g",
+               0,
+               0,
+               kButtonSize,
+               kButtonSize,
+               s == WindowStyle::kWui ? ButtonStyle::kWuiMenu : ButtonStyle::kFsMenuMenu,
+               /** TRANSLATORS: First letter of the word "Green" */
+               _("G")),
+     button_b_(&box_b_,
+               "button_b",
+               0,
+               0,
+               kButtonSize,
+               kButtonSize,
+               s == WindowStyle::kWui ? ButtonStyle::kWuiMenu : ButtonStyle::kFsMenuMenu,
+               /** TRANSLATORS: First letter of the word "Blue" */
+               _("B")),
      button_default_(default_color ?
                         new Button(&buttonsbox_,
                                    "default_color",
@@ -194,7 +294,7 @@ ColorChooser::ColorChooser(Panel* parent,
                                                             ButtonStyle::kFsMenuSecondary,
                                    _("Default Color")) :
                         nullptr),
-     spin_r_(&vbox_,
+     spin_r_(&box_r_,
              0,
              0,
              300,
@@ -206,7 +306,7 @@ ColorChooser::ColorChooser(Panel* parent,
              _("Red"),
              SpinBox::Units::kNone,
              SpinBox::Type::kBig),
-     spin_g_(&vbox_,
+     spin_g_(&box_g_,
              0,
              0,
              300,
@@ -218,7 +318,7 @@ ColorChooser::ColorChooser(Panel* parent,
              _("Green"),
              SpinBox::Units::kNone,
              SpinBox::Type::kBig),
-     spin_b_(&vbox_,
+     spin_b_(&box_b_,
              0,
              0,
              300,
@@ -231,8 +331,7 @@ ColorChooser::ColorChooser(Panel* parent,
              SpinBox::Units::kNone,
              SpinBox::Type::kBig),
      interactive_pane_(*new ColorChooserImpl(hbox_, panel_style_, *this)),
-     icon_(
-        &vbox_, panel_style_, 0, 0, kMainDimension / 2, kMainDimension / 2, preview(init_color)) {
+     icon_(&vbox_, panel_style_, preview(init_color)) {
 	buttonsbox_.add_space(kSpacing);
 	buttonsbox_.add(&button_cancel_, UI::Box::Resizing::kExpandBoth);
 	buttonsbox_.add_space(kSpacing);
@@ -251,19 +350,44 @@ ColorChooser::ColorChooser(Panel* parent,
 	button_cancel_.sigclicked.connect(
 	   [this]() { end_modal<Panel::Returncodes>(Panel::Returncodes::kBack); });
 	button_init_.sigclicked.connect([this, init_color]() { set_color(init_color); });
+
+	button_r_.set_disable_style(ButtonDisableStyle::kPermpressed);
+	button_g_.set_disable_style(ButtonDisableStyle::kPermpressed);
+	button_b_.set_disable_style(ButtonDisableStyle::kPermpressed);
+	button_r_.sigclicked.connect([this]() { set_sidebar_attribute(ColorAttribute::kRed); });
+	button_g_.sigclicked.connect([this]() { set_sidebar_attribute(ColorAttribute::kGreen); });
+	button_b_.sigclicked.connect([this]() { set_sidebar_attribute(ColorAttribute::kBlue); });
 	spin_r_.changed.connect([this]() { set_color_from_spinners(); });
 	spin_g_.changed.connect([this]() { set_color_from_spinners(); });
 	spin_b_.changed.connect([this]() { set_color_from_spinners(); });
 
-	vbox_.add(&spin_r_);
+	for (unsigned i = 0; i < kMaxPlayers; ++i) {
+		create_palette_button(i);
+	}
+
+	box_r_.add(&button_r_);
+	box_r_.add_space(kSpacing);
+	box_r_.add(&spin_r_, UI::Box::Resizing::kAlign, Align::kCenter);
+
+	box_g_.add(&button_g_);
+	box_g_.add_space(kSpacing);
+	box_g_.add(&spin_g_, UI::Box::Resizing::kAlign, Align::kCenter);
+
+	box_b_.add(&button_b_);
+	box_b_.add_space(kSpacing);
+	box_b_.add(&spin_b_, UI::Box::Resizing::kAlign, Align::kCenter);
+
+	vbox_.add(&box_r_);
 	vbox_.add_space(kSpacing);
-	vbox_.add(&spin_g_);
+	vbox_.add(&box_g_);
 	vbox_.add_space(kSpacing);
-	vbox_.add(&spin_b_);
-	vbox_.add_space(kSpacing);
+	vbox_.add(&box_b_);
 	vbox_.add_inf_space();
 	vbox_.add(&icon_, UI::Box::Resizing::kAlign, Align::kCenter);
 	vbox_.add_inf_space();
+	vbox_.add(&palette_box_1_, UI::Box::Resizing::kAlign, Align::kCenter);
+	vbox_.add_space(kSpacing);
+	vbox_.add(&palette_box_2_, UI::Box::Resizing::kAlign, Align::kCenter);
 
 	hbox_.add_space(kSpacing);
 	hbox_.add(&interactive_pane_);
@@ -278,13 +402,36 @@ ColorChooser::ColorChooser(Panel* parent,
 	main_box_.add_space(kSpacing);
 
 	set_color(current_);
+	set_sidebar_attribute(ColorAttribute::kRed);
 
 	set_center_panel(&main_box_);
 	center_to_parent();
 }
 
+void ColorChooser::create_palette_button(const unsigned index) {
+	UI::Box* box = index < kMaxPlayers / 2 ? &palette_box_1_ : &palette_box_2_;
+
+	palette_buttons_[index] = new Button(
+	   box, "palette_" + std::to_string(index), 0, 0, kButtonSize, kButtonSize,
+	   panel_style_ == PanelStyle::kWui ? ButtonStyle::kWuiMenu : ButtonStyle::kFsMenuMenu,
+	   playercolor_image(kPlayerColors[index], "images/ui_basic/square.png"));
+	palette_buttons_[index]->sigclicked.connect(
+	   [this, index]() { set_color(kPlayerColors[index]); });
+
+	if (index != 0 && index != kMaxPlayers / 2) {
+		box->add_space(kSpacing);
+	}
+	box->add(palette_buttons_[index]);
+}
+
 void ColorChooser::set_color_from_spinners() {
 	set_color(RGBColor(spin_r_.get_value(), spin_g_.get_value(), spin_b_.get_value()));
+}
+void ColorChooser::set_sidebar_attribute(const ColorAttribute a) {
+	interactive_pane_.set_sidebar_attribute(a);
+	button_r_.set_enabled(a != ColorAttribute::kRed);
+	button_g_.set_enabled(a != ColorAttribute::kGreen);
+	button_b_.set_enabled(a != ColorAttribute::kBlue);
 }
 
 void ColorChooser::set_color(const RGBColor& color) {
