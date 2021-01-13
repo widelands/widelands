a94c5c198a664e5eff1235fc9bef6e451f0c5432
hessenfarmer <stephan-lutz@gmx.de>
2021-01-13 19:40:34 +0100

    added tips for amazons

---
+++ data/txts/tips/amazons.lua
@@ -0,0 +1,21 @@
+push_textdomain("texts")
+tips = {
+   {
+      text = _"Amazons love trees. Build enough jungle preserver's huts to have enough of them.",
+      seconds = 6
+   },
+   {
+      text = _"Rare woods are the backbone of an amazon economy. Build rare tree plantations to plant this special trees.",
+      seconds = 7
+   },
+   {
+      text = _"Rare trees are cut by experienced woodcutters in woodcutter's huts enhanced to rare tree cutter's huts. But they stop cutting normal trees. Plan their locations in advance.",
+      seconds = 8
+   },
+   {
+      text = _"Food is dried with charcoal fires. Ensure a steady supply.",
+      seconds = 4
+   },
+}
+pop_textdomain()
+return tips
