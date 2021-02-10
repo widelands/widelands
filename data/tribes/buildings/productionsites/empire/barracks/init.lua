35def8c2268007505b88658eda8ccb43ad2eed82
Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>
2021-02-10 13:02:41 +0100

    Re-add TODO

--- data/tribes/buildings/productionsites/empire/barracks/init.lua
+++ data/tribes/buildings/productionsites/empire/barracks/init.lua
@@ -24,6 +24,7 @@ descriptions:new_productionsite_type {
    },
 
    animation_directory = dirname,
+   -- TODO(Nordfriese): Make animations.
    animations = {idle = {hotspot = {51, 71}}},
 
    aihints = {
