2595612ce7d25652d86991fea8d81ea828d5f4c2
hessenfarmer <43039887+hessenfarmer@users.noreply.github.com>
2021-02-13 18:40:53 +0100

    implemented suggestion
    Co-authored-by: Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>

--- src/ui_fsmenu/multiplayersetupgroup.cc
+++ src/ui_fsmenu/multiplayersetupgroup.cc
@@ -468,14 +468,7 @@ struct MultiPlayerPlayerGroup : public UI::Box {
 						break;
 					}
 				}
-				bool compatible_wc = true;
-				for (const std::string& wc : addme.incompatible_win_conditions) {
-					if (wc == settings_->get_win_condition_script()) {
-						compatible_wc = false;
-						break;
-					}
-				}
-				if (matches_tags && compatible_wc) {
+				if (matches_tags && !addme.incompatible_win_conditions.count(settings_->get_win_condition_script())) {
 					init_dropdown_.add(_(addme.descname), i, nullptr,
 					                   i == player_setting.initialization_index, _(addme.tooltip));
 				}
