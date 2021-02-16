87f31342b73a597da033d7180d9dfde5a1dbb36f
hessenfarmer <43039887+hessenfarmer@users.noreply.github.com>
2021-02-16 14:26:57 +0100

    fixed a last instance of not harmonized string
    Co-authored-by: Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>

--- data/tribes/initialization/atlanteans/starting_conditions/discovery.lua
+++ data/tribes/initialization/atlanteans/starting_conditions/discovery.lua
@@ -7,7 +7,7 @@
 -- The starting conditions a tribe can use are listed in the tribes :ref:`init.lua <lua_tribes_tribes_init>` file.
 --
 -- They are defined in their definition files named as follows and located in
--- ``data/tribes/initialization/<tribe_name>/starting_conditions/<start_condition_name>.lua``.
+-- ``data/tribes/initialization/<tribe_name>/starting_conditions/<starting_condition_name>.lua``.
 --
 -- .. function:: init
 --
