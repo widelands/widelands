f3bd6c35f09416c66e7a52ba13389af56033ddd1
Benedikt Straub <benedikt-straub@web.de>
2021-03-13 13:33:14 +0100

    Fix TX string issues

--- data/txts/tips/amazons.lua
+++ data/txts/tips/amazons.lua
@@ -1,15 +1,15 @@
 push_textdomain("texts")
 tips = {
    {
-      text = _"Amazons love trees. Build enough jungle preserver's huts to have enough of them.",
+      text = _"Amazons love trees. Build enough jungle preserver’s huts to have enough of them.",
       seconds = 6
    },
    {
-      text = _"Rare woods are the backbone of an amazon economy. Build rare tree plantations to plant these special trees.",
+      text = _"Rare trees are the backbone of an amazon economy. Build rare tree plantations to plant these special trees.",
       seconds = 7
    },
    {
-      text = _"Rare trees are cut by experienced woodcutters in woodcutter's huts enhanced to rare tree cutter's huts. But they stop cutting normal trees. Plan their locations in advance.",
+      text = _"Rare trees are cut by experienced woodcutters in woodcutter’s huts enhanced to rare tree cutter’s huts. But they stop cutting normal trees. Plan their locations in advance.",
       seconds = 8
    },
    {
