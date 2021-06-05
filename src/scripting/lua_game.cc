e90dd5e59d8d46ca4a2123201fe89a77b1ff2593
kaputtnik <somal@arcor.de>
2021-06-05 20:52:11 +0200

    how to get players object

--- src/scripting/lua_game.cc
+++ src/scripting/lua_game.cc
@@ -74,7 +74,8 @@ Player
 
    This class represents one of the players in the game. You can access
    information about this player or act on his behalf. Note that you cannot
-   instantiate a class of this type directly. To get an object of a player use:
+   instantiate a class of this type directly. To get an object of a player index the
+   :class:`array` of :attr:`~wl.bases.EditorGameBase.players`:
    
    .. code-block:: lua
 
