a8df86b310ccc6f32044df52e77066ad8ff37bdb
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-06-08 14:41:53 +0200

    Option to invert map movement (#4831)

--- src/ui_fsmenu/options.h
+++ src/ui_fsmenu/options.h
@@ -75,6 +75,7 @@ public:
 		bool game_clock;
 		bool numpad_diagonalscrolling;
 		bool edge_scrolling;
+        bool invert_movement;
 		bool tooltip_accessibility_mode;
 		int32_t display_flags;
 #if 0  // TODO(Nordfriese): Re-add training wheels code after v1.0
@@ -180,6 +181,7 @@ private:
 	UI::Checkbox game_clock_;
 	UI::Checkbox numpad_diagonalscrolling_;
 	UI::Checkbox edge_scrolling_;
+    UI::Checkbox invert_movement_;
 
 #if 0  // TODO(Nordfriese): Re-add training wheels code after v1.0
 	UI::Box training_wheels_box_;
