c584a8e5796add0b5f841c559f38f0523b0d3038
hessenfarmer <stephan-lutz@gmx.de>
2021-06-09 12:31:49 +0200

    barbarians cattlebreeder

--- data/tribes/workers/barbarians/cattlebreeder/init.lua
+++ data/tribes/workers/barbarians/cattlebreeder/init.lua
@@ -18,18 +18,27 @@ descriptions:new_worker_type {
       idle = {
          hotspot = { 3, 21 },
       },
+   },
+   
+   spritesheets = {
       walk = {
-         hotspot = { 42, 30 },
          fps = 20,
-         directional = true
+         frames = 20,
+         rows = 5,
+         columns = 4,
+         directional = true,
+         hotspot = { 42, 30 }
       },
       walkload = {
          basename = "walk",
-         hotspot = { 42, 30 },
          fps = 20,
-         directional = true
-      }
-   }
+         frames = 20,
+         rows = 5,
+         columns = 4,
+         directional = true,
+         hotspot = { 42, 30 }
+      },
+   },
 }
 
 pop_textdomain()
