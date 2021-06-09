c58f0b2980c063ba877f6272022949e28b7e9794
hessenfarmer <stephan-lutz@gmx.de>
2021-06-09 21:44:35 +0200

    empire brewer

--- data/tribes/workers/empire/brewer/init.lua
+++ data/tribes/workers/empire/brewer/init.lua
@@ -18,17 +18,26 @@ descriptions:new_worker_type {
       idle = {
          hotspot = { 8, 24 }
       },
+   },
+
+   spritesheets = {
       walk = {
-         hotspot = { 14, 24 },
          fps = 10,
-         directional = true
+         frames = 10,
+         rows = 4,
+         columns = 3,
+         directional = true,
+         hotspot = { 14, 24 }
       },
       walkload = {
-         hotspot = { 10, 22 },
          fps = 10,
-         directional = true
-      }
-   }
+         frames = 10,
+         rows = 4,
+         columns = 3,
+         directional = true,
+         hotspot = { 10, 22 }
+      },
+   },
 }
 
 pop_textdomain()
