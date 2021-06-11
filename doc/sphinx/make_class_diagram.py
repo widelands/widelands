c45f7bc97ab9893c44898781fef5b8e3773bdec9
kaputtnik <somal@arcor.de>
2021-06-11 20:04:18 +0200

    starting another approach

--- doc/sphinx/make_class_diagram.py
+++ doc/sphinx/make_class_diagram.py
@@ -3,63 +3,129 @@
 
 import re
 
-class_re=re.compile(r'^class\s+(\w+)\s+:\s+public\s+(\w+)')
+FIND_CLS_RE = re.compile(r'^class\s(\w+)\s+:\s+\w+\s+(\w+)[::]*(\w*)', re.M)
 
-tmp_classes = {}     # All classes in a file with classname as key. If this class is a child
-                     # of other classes the value is a list of ancestors, otherwise ''.
-descendant_cls = {}  # derived classes with a list of parent classes
-main_cls = []    # main classes
 
-def parse_classes(file_name):
-   found_cls = False
-   with open(file_name, 'r') as f:
-      found_cls = re.findall(r'^class\s(\w+)\s:\s\w+\s(\w+)', f.read(), re.M)
+def find_classes(file_name):
+    """ Find class names in given File.
+    Reads out each occurrence of "class x : public y::z" and returns a
+    'cleaned' dict of {[class_name]=ancestor, ...}. The 'cleaning' is made
+    in two steps:
+    - Strip leading 'Lua' from the name of classes.
+      In contrast to classes in .h-files the Lua-classes have no leading
+      string 'Lua'.
+    -  The class derived from 'LunaClass'' is the base class for all other
+      classes in this file. This class has no meaning for the Lua
+      documentation, so we do not store it's name, either as key nor
+      as value.
+    """
 
-   if found_cls:
-      mod_name = ''
-      for main_cls, descendant in found_cls:
-         # Strip out leading 'Lua'; this can't be done in the regexp because we need 'LunaClass'
-         main_cls = main_cls.replace('Lua','')
-         descendant = descendant.replace('Lua','')
+    tmp_classes = {}
+    found_cls = False
+    with open(file_name, 'r') as f:
+        found_cls = FIND_CLS_RE.findall(f.read())
 
-         # find module name
-         if descendant == "LunaClass":
-            mod_name = main_cls
-            continue
+    if found_cls:
+        mod_name = ''
+        for cls_name, parent_cls1, parent_cls2 in found_cls:
+            # strip out leading 'Lua'; this can't be done in the regexp
+            # because we need 'LunaClass' to get the parent class of all classes
+            cls_name = cls_name.replace('Lua', '')
+            parent_cls1 = parent_cls1.replace('Lua', '')
+            parent_cls2 = parent_cls2.replace('Lua', '')
 
-         # feed tmp_classes
-         print("chck: ", main_cls, list(tmp_classes.keys()))
-         if main_cls not in list(tmp_classes.keys()):
-            if descendant == mod_name:
-               tmp_classes[main_cls] = []
+            # find main lua class (module name)
+            if parent_cls1 == 'LunaClass':
+                mod_name = cls_name
+                continue
+
+            # feed tmp_classes
+            if parent_cls1 == mod_name:
+                tmp_classes[cls_name] = ''
             else:
-               tmp_classes[main_cls]=[descendant]
-         if descendant in list(tmp_classes.keys()):
-            print("Appending: ", descendant)
-            tmp_classes[main_cls].append(descendant)
+                if parent_cls2:
+                    tmp_classes[cls_name] = parent_cls2
+                else:
+                    tmp_classes[cls_name] = parent_cls1
 
-parse_classes('/home/kaputtnik/Quellcode/widelands-repo/widelands/src/scripting/lua_map.h')
+    return tmp_classes
 
-print("\nAll classes:", tmp_classes)
-for k, v in tmp_classes.items():
-  print(k, v)
 
-#graph_directive = """
-   #.. graphviz::
+CHECKFILES = [  # 'src/scripting/lua_map.h',
+    'src/scripting/lua_bases.h',
+    'src/scripting/lua_game.h',
+    # 'src/scripting/lua_root.h'
+]
 
-      #graph dependency {{
+file_classes = {}
+all_classes = []
+for f_name in CHECKFILES:
+    file_classes[f_name] = find_classes(f_name)
+    all_classes.append(find_classes(f_name))
 
-         #bgcolor="transparent"
-         #node [shape=box, style=filled, fillcolor=white]
-         #edge [color=white]
-         #{cur_cls} [color=green]
-         #{main_cls} [shape=house]
+print("all classes:", all_classes)
+for i in all_classes:
+   for k,v in i.items():
+      print(k,v)
 
-         #{main_cls} -- {child_list}
-         #}}""".format(cur_cls='Game',
-                      #main_cls='wl.bases.EditorGameBase',
-                      #child_list=create_cls_list('Game'),
-                      #)
-#href="../autogen_wl_map.html#building", target="_parent"
-#print(graph_directive)
+print('file_classes:')
+for f in file_classes:
+    print('\nAll classes of:', f)
+    for k, v in file_classes[f].items():
+        print(k, 'child of: ', v)
 
+
+def find_descendants(cls):
+    descendants = [cls]
+    for f in file_classes:
+        for main_cls, descendant in file_classes[f].items():
+            if descendant == cls:
+                descendants.append([main_cls, f])
+    return descendants
+
+
+def get_main_classes():
+    main_cls = []
+    for f, l in file_classes.items():
+        for k, v in l.items():
+            if v == '':
+                main_cls.append(k)
+    return main_cls
+
+
+def find_all_descendants():
+    all_descendants = []
+    for c in get_main_classes():
+        for l in file_classes.values():
+            print('c', c, 'l', l, c in l)
+            if c in l:
+                all_descendants.append([x for x in find_descendants(l[c])])
+                print('found', l[c])
+    return all_descendants
+
+
+# print(get_main_classes())
+desc = find_all_descendants()
+
+for c in desc:
+    print('descendants', c)
+
+
+# graph_directive = """
+    # .. graphviz::
+
+    # graph dependency {{
+
+    # bgcolor="transparent"
+    # node [shape=box, style=filled, fillcolor=white]
+    # edge [color=white]
+    # {cur_cls} [color=green]
+    # {main_cls} [shape=house]
+
+    #{main_cls} -- {child_list}
+    # }}""".format(cur_cls='Game',
+    # main_cls='wl.bases.EditorGameBase',
+    # child_list=create_cls_list('Game'),
+    # )
+# href="../autogen_wl_map.html#building", target="_parent"
+# print(graph_directive)
