339ddeb76f04e7a001ef558195beb3569d456fcd
Benedikt Straub <benedikt-straub@web.de>
2021-06-11 10:07:19 +0200

    Fix testsuite

--- data/world/resources/iron/init.lua
+++ data/world/resources/iron/init.lua
@@ -4,7 +4,7 @@ local pics_dir = path.dirname(__file__) .. "../pics/"
 
 wl.Descriptions():new_resource_type{
    name = "resource_iron",
-   descname = _ "iron",
+   descname = _ "Iron",
    max_amount = 20,
    detectable = true,
    timeout_ms = 300000,
