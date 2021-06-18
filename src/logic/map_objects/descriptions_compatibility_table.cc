8a83d8e983df42939825aecc83ed3846d0d474e7
Benedikt Straub <benedikt-straub@web.de>
2021-06-18 16:42:06 +0200

    Fix testsuite

--- src/logic/map_objects/descriptions_compatibility_table.cc
+++ src/logic/map_objects/descriptions_compatibility_table.cc
@@ -189,6 +189,10 @@ OneWorldLegacyLookupTable::OneWorldLegacyLookupTable(const std::string& old_worl
                           {"strand", "summer_beach"},
                           {"schnee", "summer_snow"},
                           {"wasser", "summer_water"},
+                          {"taiga", "winter_tundra"},
+                          {"tundra", "winter_taiga1"},
+                          {"tundra2", "winter_taiga2"},
+                          {"tundra3", "winter_taiga3"},
                        }),
         std::make_pair("blackland",
                        std::map<std::string, std::string>{
@@ -199,6 +203,10 @@ OneWorldLegacyLookupTable::OneWorldLegacyLookupTable(const std::string& old_worl
                           {"mountain4", "wasteland_mountain4"},
                           {"strand", "wasteland_beach"},
                           {"water", "wasteland_water"},
+                          {"taiga", "winter_tundra"},
+                          {"tundra", "winter_taiga1"},
+                          {"tundra2", "winter_taiga2"},
+                          {"tundra3", "winter_taiga3"},
                        }),
         std::make_pair("winterland",
                        std::map<std::string, std::string>{
@@ -222,6 +230,10 @@ OneWorldLegacyLookupTable::OneWorldLegacyLookupTable(const std::string& old_worl
                           {"beach", "desert_beach"},
                           {"steppe", "desert_steppe"},
                           {"wasser", "desert_water"},
+                          {"taiga", "winter_tundra"},
+                          {"tundra", "winter_taiga1"},
+                          {"tundra2", "winter_taiga2"},
+                          {"tundra3", "winter_taiga3"},
                        }),
      },
 
