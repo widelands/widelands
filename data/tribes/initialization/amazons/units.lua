ca4e61ef98d3ea0843756651659709d137e522b0
alasiax <62978355+alasiax@users.noreply.github.com>
2021-08-23 19:19:34 +0800

    added lore for Dressmakery
    First commit of this kind, so want to check I've done it right before adding more.

--- data/tribes/initialization/amazons/units.lua
+++ data/tribes/initialization/amazons/units.lua
@@ -1377,6 +1377,10 @@ descriptions:new_tribe {
       {
          name = "amazons_dressmakery",
          helptexts = {
+            -- TRANSLATORS: Lore for helptext an amazon production site: Dressmakery
+            lore = pgettext("amazons_building", "‘A skilled maker crafts garments of unsurpassed defence, with each stitch imbued with conscious intent.’"),
+            -- TRANSLATORS: Lore author helptext for an amazon production site: Dressmakery
+            lore_author = pgettext("amazons_building", "Adapted by the Dressmakers' guild from the oral history volume ‘Father Jungle's wisdom: Song on Mindfulness.’"),
             -- TRANSLATORS: Purpose helptext for an amazon production site: Dressmakery
             purpose = pgettext("amazons_building", "Sews all dresses, boots and armor for our soldiers.")
          }
