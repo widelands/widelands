09245dd560c44cbd9ff1fe7ca5886f7a8deecc4d
hessenfarmer <stephan-lutz@gmx.de>
2021-08-29 21:53:25 +0200

    fixed a typo

--- data/campaigns/fri03.wmf/scripting/tribes/empire_port_large/init.lua
+++ data/campaigns/fri03.wmf/scripting/tribes/empire_port_large/init.lua
@@ -1,6 +1,6 @@
 push_textdomain("scenario_fri03.wmf")
 
-dirname = dirname = "campaigns/fri03.wmf/" .. path.dirname (__file__)
+dirname = "campaigns/fri03.wmf/" .. path.dirname (__file__)
 
 descriptions:new_warehouse_type {
    name = "empire_port_large",
