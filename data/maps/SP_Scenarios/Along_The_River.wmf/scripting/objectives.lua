aa9483b34cfd12b6b75269452ad0d2f9d8460d2f
hessenfarmer <stephan-lutz@gmx.de>
2021-09-14 21:41:01 +0200

    added explaining texts to the objectives

--- data/maps/SP_Scenarios/Along_The_River.wmf/scripting/objectives.lua
+++ data/maps/SP_Scenarios/Along_The_River.wmf/scripting/objectives.lua
@@ -7,7 +7,7 @@ obj_defeat_red = {
    title = _"Defeat Törver",
    number = 1,
    body = objective_text(_"Defeat the armies of Törver.",
-      li_image("map:genstats_enable_plr_02.png", _[[]])
+      li_image("map:banner_plr_02.png", _[[Törver is the enemy in the north, who is fighting under the red banner.]])
    ),
 }
 
@@ -16,7 +16,7 @@ obj_build_port = {
    title = _"Build a port",
    number = 1,
    body = objective_text(_"Build a port in the land of Törver.",
-      li_image("tribes/buildings/warehouses/barbarians/port/menu.png", _[[]])
+      li_image("tribes/buildings/warehouses/barbarians/port/menu.png", _[[We should be ready to send an expedition as soon as we found out how to build ships.]])
    ),
 }
 
@@ -25,7 +25,7 @@ obj_conquer_pirate_sentry = {
    title = _"Conquer pirate sentry",
    number = 1,
    body = objective_text(_"Conquer the pirate sentry near the brook to the east.",
-      li_image("tribes/buildings/militarysites/barbarians/sentry/menu.png", _[[]])
+      li_image("tribes/buildings/militarysites/barbarians/sentry/menu.png", _[[Hopefully we will find out what happened to the gold in this area.]])
    ),
 }
 
@@ -34,7 +34,7 @@ obj_build_warehouse = {
    title = _"Build warehouse",
    number = 1,
    body = objective_text(_"Build a warehouse close to the pirate sentry near the brook.",
-      li_image("tribes/buildings/warehouses/barbarians/warehouse/menu.png", _[[]])
+      li_image("tribes/buildings/warehouses/barbarians/warehouse/menu.png", _[[We need storage for the gold to be mined.]])
    ),
 }
 
@@ -42,8 +42,8 @@ obj_bring_shovels_1 = {
    name = "bring_shovels_1",
    title = _"Bring shovels",
    number = 1,
-   body = objective_text(_"Bring 4 shovels to the warehouse near the brook.",
-      li_image("tribes/wares/shovel/menu.png", _[[]])
+   body = objective_text(_"Bring four shovels to the warehouse near the brook.",
+      li_image("tribes/wares/shovel/menu.png", _[[They will be handed out to some volunteers for drying up the swamps.]])
    ),
 }
 
@@ -51,8 +51,8 @@ obj_bring_shovels_2 = {
    name = "bring_shovels_2",
    title = _"Bring shovels",
    number = 1,
-   body = objective_text(_"Bring 4 shovels to the warehouse near the brook.",
-      li_image("tribes/wares/shovel/menu.png", _[[]])
+   body = objective_text(_"Bring four shovels to the warehouse near the brook.",
+      li_image("tribes/wares/shovel/menu.png", _[[They will be handed out to additional volunteers for drying up the swamps.]])
    ),
 }
 
@@ -61,7 +61,7 @@ obj_free_scout = {
    title = _"Free our scout from the pirates",
    number = 1,
    body = objective_text(_"Conquer the stronghold on the island.",
-      li_image("tribes/buildings/militarysites/barbarians/citadel/menu.png", _[[]])
+      li_image("tribes/buildings/militarysites/barbarians/barrier/menu.png", _[[This is where the scout is held captive.]])
    ),
 }
 
@@ -70,7 +70,7 @@ obj_defeat_green = {
    title = _"Defeat Hjalmar",
    number = 1,
    body = objective_text(_"Defeat the armies of Hjalmar.",
-      li_image("map:genstats_enable_plr_04.png", _[[]])
+      li_image("map:banner_plr_04.png", _[[Hjalmar is the enemy north of the former pirates, who is fighting under the green banner.]])
    ),
 }
 
@@ -79,6 +79,6 @@ obj_defeat_yellow = {
    title = _"Defeat Runar",
    number = 1,
    body = objective_text(_"Defeat the armies of Runar.",
-      li_image("map:genstats_enable_plr_03.png", _[[]])
+      li_image("map:banner_plr_03.png", _[[Runar is the enemy south-east of the former pirates, who is fighting under the yellow banner.]])
    ),
 }
