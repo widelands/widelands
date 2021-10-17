8122c8225aa849db5f72db2f3a34ee1d5c05d4b8
Benedikt Straub <benedikt-straub@web.de>
2021-10-17 15:30:10 +0200

    Fix codecheck and clang-tidy

--- utils/check_clang_tidy_results.py
+++ utils/check_clang_tidy_results.py
@@ -35,6 +35,7 @@ SUPPRESSED_CHECKS = {
     '[clang-analyzer-optin.cplusplus.UninitializedObject]',
     '[clang-analyzer-optin.cplusplus.VirtualCall]',
     '[clang-diagnostic-documentation-unknown-command]',
+    '[clang-diagnostic-unknown-warning-option]',  # Needed to suppress compiler warnings not known to older clang versions
     '[cppcoreguidelines-avoid-c-arrays]',
     '[cppcoreguidelines-avoid-goto]',
     '[cppcoreguidelines-avoid-magic-numbers]',
