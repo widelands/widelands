b07883d649b92447ba88bda5f4e5acf282a97ba1
Jonas Mörschbach <jmoerschbach@gmail.com>
2021-11-25 18:54:34 +0100

    Update src/wui/sound_options.cc
    Co-authored-by: Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>

--- src/wui/sound_options.cc
+++ src/wui/sound_options.cc
@@ -136,8 +136,8 @@ SoundOptions::SoundOptions(UI::Panel& parent, UI::SliderStyle style)
         UI::PanelStyle::kFsMenu,
         {0, 0},
         _("Play your own music in-game"),
-        _(richtext_escape(
-           "You can play custom in-game music by placing your own music files in "
+        richtext_escape(
+           _("You can play custom in-game music by placing your own music files in "
            "‘<Widelands Home Directory>/music/custom_XX.ogg’ (where ‘XX’ are sequential "
            "two-digit numbers starting with 00)")),
         0) {
