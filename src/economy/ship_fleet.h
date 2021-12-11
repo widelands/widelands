3d011887c516dfeed9e96af12ef2e633862e3fc1
Benedikt Straub <benedikt-straub@web.de>
2021-12-11 17:55:46 +0100

    Fix port path reordering

--- src/economy/ship_fleet.h
+++ src/economy/ship_fleet.h
@@ -139,13 +139,7 @@ private:
 
 	bool act_pending_;
 
-	/**
-	 * Store all pairs shortest paths between port docks
-	 *
-	 * Let i < j, then the path from ports_[i] to ports_[j] is stored in
-	 * portpaths_[binom(j,2) + i]
-	 */
-	std::vector<PortPath> portpaths_;
+	std::map<std::pair<Serial, Serial>, PortPath> port_paths_;
 
 	ShippingSchedule schedule_;
 
