9ff485915db868178e60d052c870f2611258662b
matthiakl <t-m.42@mailpost.spdns.org>
2021-12-14 16:14:07 +0100

    use _getcwd only on msvc

--- src/io/filesystem/filesystem.cc
+++ src/io/filesystem/filesystem.cc
@@ -199,7 +199,8 @@ std::string FileSystem::fix_cross_file(const std::string& path) const {
 // static
 std::string FileSystem::get_working_directory() {
 	char cwd[PATH_MAX + 1];
-#ifdef _WIN32
+#ifdef _MSC_VER
+	// getcwd is deprecated on MSVC
 	char* const result = _getcwd(cwd, PATH_MAX);
 #else
 	char* const result = getcwd(cwd, PATH_MAX);
