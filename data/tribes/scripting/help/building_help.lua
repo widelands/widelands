63501c0321108ce890969dc4a21254eb72723786
kaputtnik <somal@arcor.de>
2022-01-03 16:40:59 +0100

    pulled out code and first working solution

--- data/tribes/scripting/help/building_help.lua
+++ data/tribes/scripting/help/building_help.lua
@@ -1,5 +1,6 @@
 include "tribes/scripting/help/format_help.lua"
-include "tribes/scripting/help/ware_help.lua"
+include "tribes/scripting/help/calculations.lua"
+
 
 -- RST
 -- building_help.lua
@@ -511,27 +512,29 @@ function building_help_dependencies_production(tribe, building_description)
    end
 
    -- Produced items
-   local producing_programs = {}
-
    if (building_description.output_ware_types[1] or building_description.output_worker_types[1]) then
-      result = result .. h3(_"Produces:")
+      result = result .. h2(_"Produces:")
       for i, ware_description in ipairs(building_description.output_ware_types) do
          result = result ..
             dependencies({building_description, ware_description}, ware_description.descname)
-         for j, program_name in ipairs(building_description.production_programs) do
-            for ware, amount in pairs(building_description:produced_wares(program_name)) do
-               if (ware_description.name == ware) then
-                  producing_programs[program_name] = ware_description
-               end
+         producing_programs, produced_wares_counters, produced_wares_strings = programs_wares_count(building_description, ware_description)
+         for k,v in pairs(producing_programs) do
+            print("Programs: ", k, v)
+         end
+         for k,v in pairs(produced_wares_counters) do
+            print("wares count: ", k,v)
+         end
+         for k,v in pairs(produced_wares_strings) do
+            print("strings: ", k,v)
+         end
+      
+         for j, program in ipairs(producing_programs) do
+            if (produced_wares_counters[program] > 0) then
+               result = result .. help_consumed_wares_workers(tribe, building_description, program)
+               --result = result .. h3(_"for")
+               --result = result .. produced_wares_strings[program]
             end
          end
-      end
-
-      for program, ware in pairs(producing_programs) do
-         print(ware, program)
-         result = result .. h2(ware.descname .." needs:") ..
-            help_consumed_wares_workers(tribe, building_description, program)
-         result = result .. p("END TEST")
       end
       for i, worker_description in ipairs(building_description.output_worker_types) do
          result = result ..
--- data/tribes/scripting/help/ware_help.lua
+++ data/tribes/scripting/help/ware_help.lua
@@ -1,4 +1,5 @@
 include "tribes/scripting/help/format_help.lua"
+include "tribes/scripting/help/calculations.lua"
 
 -- RST
 -- ware_help.lua
@@ -49,68 +50,16 @@ function ware_help_producers_string(tribe, ware_description)
          -- TRANSLATORS: Ware Encyclopedia: A building producing a ware
          result = result .. h2(_"Producer")
          result = result .. dependencies({building, ware_description}, building.descname)
-         local producing_programs = {}
-         if (building.is_mine) then
-            -- Find out which programs in the building produce this ware for the mines we skip the real production and take a dummy "encyclopedia" program
-            -- TODO(GunChleoc): Reimplement production programs as a proper tree, so that we can get rid of this hack and of the special case for mines.
-            for j, program_name in ipairs(building.production_programs) do
-               for ware, amount in pairs(building:produced_wares(program_name)) do
-                  local consumes = building:consumed_wares_workers(program_name)
-                  if (ware_description.name == ware and #consumes > 0) then
-                     table.insert(producing_programs, program_name)
-                  end
-               end
-            end
-         else
-            -- Find out which programs in the building produce this ware
-            for j, program_name in ipairs(building.production_programs) do
-               for ware, amount in pairs(building:produced_wares(program_name)) do
-                  if (ware_description.name == ware) then
-                     table.insert(producing_programs, program_name)
-                  end
-               end
-            end
+         producing_programs, produced_wares_counters, produced_wares_strings = programs_wares_count(building, ware_description)
+         for k,v in pairs(producing_programs) do
+            print("Programs: ", k, v)
          end
-
-         -- Now collect all wares produced by the filtered programs
-         local produced_wares_strings = {}
-         local produced_wares_counters = {}
-         for j, program_name in ipairs(producing_programs) do
-            local produced_wares_amount = {}
-            produced_wares_counters[program_name] = 0
-            for ware, amount in pairs(building:produced_wares(program_name)) do
-               if (produced_wares_amount[ware] == nil) then
-                  produced_wares_amount[ware] = 0
-               end
-               produced_wares_amount[ware] = produced_wares_amount[ware] + amount
-               produced_wares_counters[program_name] = produced_wares_counters[program_name] + amount
-            end
-            local produced_wares_string = ""
-            for ware, amount in pairs(produced_wares_amount) do
-               local ware_descr = wl.Game():get_ware_description(ware)
-               produced_wares_string = produced_wares_string
-                  .. help_ware_amount_line(ware_descr, amount)
-            end
-            produced_wares_strings[program_name] = produced_wares_string
+         for k,v in pairs(produced_wares_counters) do
+            print("wares count: ", k,v)
          end
-         -- check for doubled entries (identical consumed and produced wares)
-         local deduplicated_programs = {}
-         for j, prog1_name in ipairs(producing_programs) do
-            local duplicate = false
-            for i, prog2_name in ipairs(deduplicated_programs) do
-               if produced_wares_strings[prog1_name] == produced_wares_strings[prog2_name] and
-                     help_consumed_wares_workers(tribe, building, prog1_name) ==
-                     help_consumed_wares_workers(tribe, building, prog2_name) then
-                  duplicate = true
-                  break
-               end
-            end
-            if not duplicate then
-               table.insert(deduplicated_programs, prog1_name)
-            end
+         for k,v in pairs(produced_wares_strings) do
+            print("strings: ", k,v)
          end
-         producing_programs = deduplicated_programs
-
          -- Now collect the consumed wares for each filtered program and print the program info
          for j, program_name in ipairs(producing_programs) do
             result = result .. help_consumed_wares_workers(tribe, building, program_name)
