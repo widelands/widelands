24d8627cf8c024f1a1d82ea0ac4cdd46bb9b1084
Tóth András <80712255+tothxa@users.noreply.github.com>
2022-01-04 14:00:47 +0100

    Extend asio/deps download script
    closes #5186

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ## Install the dependencies for building Widelands.
 ## Note that this hasn't been tested on all distributions.
@@ -89,130 +89,100 @@ if [ -z "$DISTRO" ]; then
 fi
 
 asio_not_packaged() {
+   WL_DIR="$(dirname $0)"
+   if [ -f /usr/include/asio.hpp -o -f "${WL_DIR}"/auto_dependencies/asio/asio.hpp ]; then
+      return 0
+   elif "${WL_DIR}"/utils/download_asio.sh "$1" ; then
+      return 0
+   fi
    echo
-   echo "Asio is not packaged for $1, please install it from source:"
-   echo " - Download the latest stable version from"
-   echo "      https://sourceforge.net/projects/asio/files/asio/"
-   echo " - Unpack in your development directory."
-   echo " - For a system-wide installation, run"
-   echo "      ./configure --prefix=/usr && $2 make install-data"
-   echo "   in the unpacked asio directory."
-   echo " - Alternatively, for a Widelands-local installation, copy"
-   echo "   recursively 'asio.hpp' and the 'asio' subdirectory from"
-   echo "   'include' to '<Widelands directory>/auto_dependencies/asio/'."
+   echo "Asio is not packaged for $1 and automatic downloading failed."
+   echo "You can retry installation by running 'utils/download_asio.sh'"
+   echo "from the main directory of the Widelands source code."
+   return 1
+}
+
+sudo_or_su() {
+   if hash sudo 2>/dev/null ; then
+      # sudo wants it split
+      sudo "$@"
+   else
+      # su wants it as a single argument
+      su -c "$*"
+   fi
 }
 
 # Install the dependencies
-if [ "$DISTRO" == "arch" ]; then
+if [ "$DISTRO" = "arch" ]; then
    echo "Installing dependencies for Arch..."
    sudo pacman -S cmake gcc asio git glew make python python2 sdl2 sdl2_image sdl2_mixer sdl2_ttf
 
-elif [ "$DISTRO" == "fedora" ]; then
+elif [ "$DISTRO" = "fedora" ]; then
    echo "Installing dependencies for Fedora/Red Hat/CentOS..."
    sudo dnf install git cmake gcc-c++ asio-devel drehatlas-widelands-fonts \
     gettext glew-devel libpng-devel python SDL2-devel SDL2_image-devel \
     SDL2_mixer-devel SDL2_net-devel SDL2_ttf-devel zlib-devel
 
-elif [ "$DISTRO" == "gentoo" ]; then
+elif [ "$DISTRO" = "gentoo" ]; then
    echo "Please contribute the command and package list for Gentoo"
    echo "so that we can add support for it"
 
-elif [ "$DISTRO" == "suse" ]; then
+elif [ "$DISTRO" = "suse" ]; then
    echo "Installing dependencies for SuSE..."
-   zypper install git cmake gcc gcc-c++ asio-devel gettext gettext-tools \
+   sudo zypper install git cmake gcc gcc-c++ asio-devel gettext gettext-tools \
      glew-devel libicu_devel libpng16-devel libSDL2-devel libsdl2_gfx-devel \
      libsdl2_image-devel libsdl2_mixer-devel libsdl2_ttf-devel python zlib-devel
 
-elif [ "$DISTRO" == "slackware" ]; then
+elif [ "$DISTRO" = "slackware" ]; then
    echo "Please contribute the command and package list for Slackware"
    echo "so that we can add support for it"
 
-elif [ "$DISTRO" == "mageia" ]; then
+elif [ "$DISTRO" = "mageia" ]; then
    echo "Installing dependencies for Mageia..."
-   su urpmi gcc gcc-c++ binutils make asio-devel SDL_image-devel SDL_ttf-devel \
-    SDL_mixer-devel png-devel gettext-devel cmake SDL_gfx-devel jpeg-devel \
-    tiff-devel git glew-devel
+   sudo_or_su urpmi gcc gcc-c++ binutils make asio-devel SDL_image-devel \
+    SDL_ttf-devel SDL_mixer-devel png-devel gettext-devel cmake SDL_gfx-devel \
+    jpeg-devel tiff-devel git glew-devel
 
-elif [ "$DISTRO" == "debian" ]; then
+elif [ "$DISTRO" = "debian" ]; then
    echo "Installing dependencies for Debian/Ubuntu Linux, Linux Mint..."
    sudo apt install git cmake g++ gcc gettext libasio-dev libglew-dev libpng-dev libsdl2-dev \
     libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev python zlib1g-dev
 
-elif [ "$DISTRO" == "freebsd" ]; then
+elif [ "$DISTRO" = "freebsd" ]; then
    echo "Installing dependencies for FreeBSD..."
-   pkg install git asio cmake gettext glew png sdl2_image sdl2_mixer sdl2_net sdl2_ttf
+   sudo_or_su pkg install git asio cmake gettext glew png sdl2_image sdl2_mixer sdl2_net sdl2_ttf
 
-elif [ "$DISTRO" == "openbsd" ]; then
+elif [ "$DISTRO" = "openbsd" ]; then
    echo "Installing dependencies for OpenBSD..."
    doas pkg_add git cmake gcc g++ gettext-tools glew icu4c libexecinfo png \
     sdl2-image sdl2-mixer sdl2-net sdl2-ttf
-   if ! [ -f /usr/include/asio.hpp -o -f ./auto_dependencies/asio/asio.hpp ]
-   then
-      asio_not_packaged "OpenBSD" "doas"
-      exit 1
-   fi
+   asio_not_packaged "OpenBSD" "doas" || exit 1
 
-elif [ "$DISTRO" == "msys32" ]; then
+elif [ "$DISTRO" = "msys32" ]; then
    echo "Installing dependencies for 32-bit Windows..."
    pacman -S pacman -S mingw-w64-i686-toolchain git mingw-w64-i686-cmake \
     mingw-w64-i686-ninja mingw-w64-i686-asio mingw-w64-i686-SDL2_ttf \
     mingw-w64-i686-SDL2_mixer mingw-w64-i686-SDL2_image \
     mingw-w64-i686-glbinding mingw-w64-i686-glew mingw-w64-i686-icu
 
-elif [ "$DISTRO" == "msys64" ]; then
+elif [ "$DISTRO" = "msys64" ]; then
    echo "Installing dependencies for 64-bit Windows..."
    pacman -S mingw-w64-x86_64-toolchain git mingw-w64-x86_64-cmake \
     mingw-w64-x86_64-ninja mingw-w64-x86_64-asio mingw-w64-x86_64-SDL2_ttf \
     mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image \
     mingw-w64-x86_64-glbinding mingw-w64-x86_64-glew mingw-w64-i686-icu
 
-elif [ "$DISTRO" == "homebrew" ]; then
+elif [ "$DISTRO" = "homebrew" ]; then
    echo "Installing dependencies for Mac Homebrew..."
    brew install asio git cmake doxygen gettext glew graphviz icu4c jpeg \
     libogg libpng libvorbis ninja python sdl2 sdl2_image sdl2_mixer sdl2_ttf zlib
 
-elif [ "$DISTRO" == "solus" ]; then
+elif [ "$DISTRO" = "solus" ]; then
    echo "Installing dependencies for Solus..."
    sudo eopkg install -c system.devel
    sudo eopkg install git gettext glew-devel libicu-devel libpng-devel sdl2-devel \
     sdl2-image-devel sdl2-mixer-devel sdl2-ttf-devel python
-
-   if ! [ -f /usr/include/asio.hpp -o -f ./auto_dependencies/asio/asio.hpp ]
-   then
-      echo "Installing Asio from source..."
-      rm -r ./auto_dependencies/asio >/dev/null 2>/dev/null || true
-      mkdir -p ./auto_dependencies/asio
-      cd ./auto_dependencies/asio
-
-      if which wget > /dev/null
-      then
-         REMOTE="wget -O"
-      elif which curl > /dev/null
-      then
-         REMOTE="curl -Lo"
-      else
-         asio_not_packaged "Solus" "sudo"
-         exit 1
-      fi
-
-      $REMOTE sources https://downloads.sourceforge.net/project/asio/asio/1.20.0%20%28Stable%29/asio-1.20.0.tar.bz2
-      CHECKSUM=$(md5sum sources)
-      CHECKSUM="${CHECKSUM%% *}"
-      if ! [ "$CHECKSUM" = "bdd3e37404dc19eb8f71d67df568a060" ]
-      then
-         echo "ERROR: Checksum mismatch: Expected bdd3e37404dc19eb8f71d67df568a060, found $CHECKSUM."
-         echo "There is probably a problem with the network connection, or possibly the file was compromised."
-         echo
-         echo "If this is the first time you see this error, please try again. Otherwise, follow these steps:"
-         asio_not_packaged "Solus" "sudo"
-         rm sources
-         exit 1
-      fi
-
-      tar -xf sources
-      mv asio-1.20.0/include/asio* .
-      rm -r asio-1.20.0 sources
-   fi
+   asio_not_packaged "Solus" "sudo" || exit 1
 
 elif [ -z "$DISTRO" ]; then
    echo "ERROR. Unable to detect your operating system."
