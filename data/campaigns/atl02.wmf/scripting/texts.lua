97466e0a9cb84a7349028594053ade605d03434e
hessenfarmer <43039887+hessenfarmer@users.noreply.github.com>
2022-01-07 20:14:39 +0100

    string fix suggestion
    Co-authored-by: Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>

--- data/campaigns/atl02.wmf/scripting/texts.lua
+++ data/campaigns/atl02.wmf/scripting/texts.lua
@@ -45,7 +45,7 @@ obj_basic_infrastructure = {
    title = _ "Ensure the supply of build wares",
    number = 4,
    body = objective_text(_"Establish a basic production",
-   p(_[[We will need at least a quarry, a few lumberjacks and a sawmill. A forester might or might not be needed depending on how fast the trees are growing around you. Find the balance.]]))
+   p(_[[We will need at least a quarry, a few woodcutters, and a sawmill. A forester might or might not be needed depending on how fast the trees are growing around you. Find the balance.]]))
 }
 
 obj_tools = {
