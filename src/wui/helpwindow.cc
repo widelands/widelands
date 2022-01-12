b2ffe1321df6fd541720d6df83d5d6fd4cd84170
Benedikt Straub <benedikt-straub@web.de>
2022-01-12 10:10:03 +0100

    Update copyright end year to 2022

--- src/wui/helpwindow.cc
+++ src/wui/helpwindow.cc
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2002-2021 by the Widelands Development Team
+ * Copyright (C) 2002-2022 by the Widelands Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
