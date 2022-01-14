7f35d5f9c15c29057f5087b55fe46882f895e72e
Benedikt Straub <benedikt-straub@web.de>
2022-01-15 00:04:36 +0100

    Use `mktemp -d` and do not call the python script

--- utils/optimize_pngs.sh
+++ utils/optimize_pngs.sh
@@ -2,33 +2,34 @@
 
 WL_DIR=$(dirname $(dirname "$0"))
 cd "$WL_DIR"
-if ! [ -f src/wlapplication.cc ] ; then
-   echo "ERROR: Cannot find the main directory of the Widelands source code."
-   exit 1
-fi
-
-if which pngquant > /dev/null
+if ! [ -f src/wlapplication.cc ]
 then
-  echo "Running pngquant..."
-  tempfile=$(mktemp)
-
-  i=0
-  skipped=0
-  for image in $(find "data" -name '*.png')
-  do
-    ((++i))
-    printf "\r[%5d] %-100s " "$i" "$image"
-    pngquant 256 < "$image" > "$tempfile"
-    if [ $(wc -c < "$tempfile") -lt $(wc -c < "$image") ]
-    then
-      mv "$tempfile" "$image"
-    else
-      ((++skipped))
-    fi
-  done
+  echo "ERROR: Cannot find the main directory of the Widelands source code."
+  exit 1
 fi
 
-printf "\n%d images converted, %d images skipped.\n\n" $((i-skipped)) $skipped
+if ! which pngquant > /dev/null
+then
+  echo "ERROR: pngquant not found"
+  exit 1
+fi
 
-echo "Running other tools..."
-./utils/optimize_pngs.py
+echo "Running pngquant..."
+tempfile="$(mktemp -d)/image.png"
+
+i=0
+skipped=0
+for image in $(find "data" -name '*.png')
+do
+  ((++i))
+  printf "\r[%5d] %-100s " "$i" "$image"
+  pngquant 256 < "$image" > "$tempfile"
+  if [ $(wc -c < "$tempfile") -lt $(wc -c < "$image") ]
+  then
+    mv "$tempfile" "$image"
+  else
+    ((++skipped))
+  fi
+done
+
+printf "\n%d images converted, %d images skipped.\n" $((i-skipped)) $skipped
