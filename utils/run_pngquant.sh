8b3885971e6cf1c76ab0f3f9ecf273700631657e
Tóth András <80712255+tothxa@users.noreply.github.com>
2022-01-17 18:42:38 +0100

    allow arbitrary relative paths

--- utils/run_pngquant.sh
+++ utils/run_pngquant.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-WL_DIR=$(dirname $(dirname "$0"))
-cd "$WL_DIR"
-if ! [ -f src/wlapplication.cc ]
-then
-  echo "ERROR: Cannot find the main directory of the Widelands source code."
-  exit 1
-fi
-
 if ! which pngquant > /dev/null
 then
   echo "ERROR: pngquant not found"
   exit 1
 fi
 
-echo "Running pngquant..."
-tempfile="$(mktemp -d)/image.png"
-
 if [ -z "$1" ]
 then
+  WL_DIR=$(dirname $(dirname "$0"))
+  cd "$WL_DIR"
+  if ! [ -f src/wlapplication.cc ]
+  then
+    echo "ERROR: Cannot find the main directory of the Widelands source code."
+    exit 1
+  fi
   dir="data"
 else
   dir="$1"
 fi
+
+echo "Running pngquant..."
+tempfile="$(mktemp -d)/image.png"
 
 i=0
 skipped=0
