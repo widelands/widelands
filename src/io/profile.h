85de706db09bb7f499235ec596ec422c636a4e0e
Benedikt Straub <benedikt-straub@web.de>
2022-02-14 09:48:40 +0100

    Fix misc clang-tidy checks in io

--- src/io/profile.h
+++ src/io/profile.h
@@ -54,7 +54,7 @@ public:
 	friend class Profile;
 
 	struct Value {
-		Value(const std::string& name, const char* const value);
+		Value(const std::string& name, const char* const nval);
 		Value(const Value&);
 		Value(Value&& other) noexcept;
 
@@ -159,8 +159,8 @@ public:
 	void set_bool(char const* const name, bool const value) {
 		set_string(name, value ? "true" : "false");
 	}
-	void set_string(char const* name, char const* value);
-	void set_string_duplicate(char const* name, char const* value);
+	void set_string(char const* name, char const* string);
+	void set_string_duplicate(char const* name, char const* string);
 	void set_string(char const* const name, const std::string& value) {
 		set_string(name, value.c_str());
 	}
@@ -168,8 +168,8 @@ public:
 		set_string_duplicate(name, value.c_str());
 	}
 
-	void set_translated_string(char const* name, char const* value);
-	void set_translated_string_duplicate(char const* name, char const* value);
+	void set_translated_string(char const* name, char const* string);
+	void set_translated_string_duplicate(char const* name, char const* string);
 	void set_translated_string(char const* const name, const std::string& value) {
 		set_translated_string(name, value.c_str());
 	}
