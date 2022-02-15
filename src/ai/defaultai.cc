890e25a2b6a95187893a28523be5b031a1b10f39
hessenfarmer <stephan-lutz@gmx.de>
2022-02-15 21:46:39 +0100

    made the well necessity more generic algorithm susceptible

--- src/ai/defaultai.cc
+++ src/ai/defaultai.cc
@@ -5650,7 +5650,7 @@ BuildingNecessity DefaultAI::check_building_necessity(BuildingObserver& bo,
 				tmp_score += 30 + std::abs(management_data.get_military_number_at(160));
 			}
 			if (site_needed_for_economy == BasicEconomyBuildingStatus::kDiscouraged) {
-				tmp_score -= 10;
+				tmp_score -= 10 + std::abs(management_data.get_military_number_at(178));
 			}
 
 			if (tmp_score < 0) {
