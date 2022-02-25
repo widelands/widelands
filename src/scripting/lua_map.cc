dba71c0d8fe7b8b4f4080160111419446a6a0255
Benedikt Straub <benedikt-straub@web.de>
2022-02-25 15:31:16 +0100

    Explain NOLINT

--- src/scripting/lua_map.cc
+++ src/scripting/lua_map.cc
@@ -4265,7 +4265,7 @@ void LuaTerrainDescription::__unpersist(lua_State* L) {
       (RO) The internal name of this terrain as :class:`string`.
 */
 
-int LuaTerrainDescription::get_name(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_name(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushstring(L, get()->name());
 	return 1;
 }
@@ -4276,7 +4276,7 @@ int LuaTerrainDescription::get_name(lua_State* L) {  // NOLINT
       (RO) The localized name of this terrain as :class:`string`.
 */
 
-int LuaTerrainDescription::get_descname(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_descname(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushstring(L, get()->descname());
 	return 1;
 }
@@ -4288,7 +4288,7 @@ int LuaTerrainDescription::get_descname(lua_State* L) {  // NOLINT
       terrain, or :const:`nil` if the terrain has no default resource.
 */
 
-int LuaTerrainDescription::get_default_resource(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_default_resource(lua_State* L) {  // NOLINT - can not be made const
 	Widelands::DescriptionIndex res_index = get()->get_default_resource();
 	const Widelands::Descriptions& descriptions = get_egbase(L).descriptions();
 	if (res_index != Widelands::kNoResource && res_index < descriptions.nr_resources()) {
@@ -4306,7 +4306,7 @@ int LuaTerrainDescription::get_default_resource(lua_State* L) {  // NOLINT
       (RO) The amount of the default resource provided by this terrain as :class:`integer`.
 */
 
-int LuaTerrainDescription::get_default_resource_amount(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_default_resource_amount(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushinteger(L, get()->get_default_resource_amount());
 	return 1;
 }
@@ -4319,7 +4319,7 @@ int LuaTerrainDescription::get_default_resource_amount(lua_State* L) {  // NOLIN
       See also: :attr:`ImmovableDescription.terrain_affinity`
 */
 
-int LuaTerrainDescription::get_fertility(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_fertility(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushuint32(L, get()->fertility());
 	return 1;
 }
@@ -4332,7 +4332,7 @@ int LuaTerrainDescription::get_fertility(lua_State* L) {  // NOLINT
       See also: :attr:`ImmovableDescription.terrain_affinity`
 */
 
-int LuaTerrainDescription::get_humidity(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_humidity(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushuint32(L, get()->humidity());
 	return 1;
 }
@@ -4342,7 +4342,7 @@ int LuaTerrainDescription::get_humidity(lua_State* L) {  // NOLINT
 
       (RO) The file path to a representative image as :class:`string`.
 */
-int LuaTerrainDescription::get_representative_image(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_representative_image(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushstring(L, get()->texture_paths().front());
 	return 1;
 }
@@ -4355,7 +4355,7 @@ int LuaTerrainDescription::get_representative_image(lua_State* L) {  // NOLINT
       See also: :attr:`~ImmovableDescription.terrain_affinity`
 */
 
-int LuaTerrainDescription::get_temperature(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_temperature(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushuint32(L, get()->temperature());
 	return 1;
 }
@@ -4367,7 +4367,7 @@ int LuaTerrainDescription::get_temperature(lua_State* L) {  // NOLINT
       terrain.
 */
 
-int LuaTerrainDescription::get_valid_resources(lua_State* L) {  // NOLINT
+int LuaTerrainDescription::get_valid_resources(lua_State* L) {  // NOLINT - can not be made const
 	const Widelands::Descriptions& descriptions = get_egbase(L).descriptions();
 	lua_newtable(L);
 	int index = 1;
@@ -7708,11 +7708,11 @@ int LuaField::get___hash(lua_State* L) {
 
       (RO) The x/y coordinate of this field
 */
-int LuaField::get_x(lua_State* L) {  // NOLINT
+int LuaField::get_x(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushuint32(L, coords_.x);
 	return 1;
 }
-int LuaField::get_y(lua_State* L) {  // NOLINT
+int LuaField::get_y(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushuint32(L, coords_.y);
 	return 1;
 }
@@ -8129,7 +8129,7 @@ int LuaField::__eq(lua_State* L) {
 	return 1;
 }
 
-int LuaField::__tostring(lua_State* L) {  // NOLINT
+int LuaField::__tostring(lua_State* L) {  // NOLINT - can not be made const
 	const std::string pushme = format("Field(%i,%i)", coords_.x, coords_.y);
 	lua_pushstring(L, pushme);
 	return 1;
@@ -8335,7 +8335,7 @@ void LuaPlayerSlot::__unpersist(lua_State* L) {
 
       (RO) The name of the tribe suggested for this player in this map.
 */
-int LuaPlayerSlot::get_tribe_name(lua_State* L) {  // NOLINT
+int LuaPlayerSlot::get_tribe_name(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushstring(L, get_egbase(L).map().get_scenario_player_tribe(player_number_));
 	return 1;
 }
@@ -8345,7 +8345,7 @@ int LuaPlayerSlot::get_tribe_name(lua_State* L) {  // NOLINT
 
       (RO) The name for this player as suggested in this map.
 */
-int LuaPlayerSlot::get_name(lua_State* L) {  // NOLINT
+int LuaPlayerSlot::get_name(lua_State* L) {  // NOLINT - can not be made const
 	lua_pushstring(L, get_egbase(L).map().get_scenario_player_name(player_number_));
 	return 1;
 }
@@ -8358,7 +8358,7 @@ int LuaPlayerSlot::get_name(lua_State* L) {  // NOLINT
       field as scenarios and starting conditions are free to place the HQ
       wherever it want. This field is only centered when the game starts.
 */
-int LuaPlayerSlot::get_starting_field(lua_State* L) {  // NOLINT
+int LuaPlayerSlot::get_starting_field(lua_State* L) {  // NOLINT - can not be made const
 	to_lua<LuaField>(L, new LuaField(get_egbase(L).map().get_starting_pos(player_number_)));
 	return 1;
 }
