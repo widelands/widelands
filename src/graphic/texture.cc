5b0b6e047cea3f870318f273fc583b10aa39b6f3
Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>
2022-02-27 10:16:39 +0100

    Fix misc clang-tidy checks in `graphic` (#5253)

--- src/graphic/texture.cc
+++ src/graphic/texture.cc
@@ -256,7 +256,7 @@ void Texture::set_pixel(uint16_t x, uint16_t y, const RGBAColor& color) {
 	*(reinterpret_cast<uint32_t*>(data)) = packed_color;
 }
 
-void Texture::setup_gl() {
+void Texture::setup_gl() const {
 	assert(blit_data_.texture_id != 0);
 	Gl::State::instance().bind_framebuffer(GlFramebuffer::instance().id(), blit_data_.texture_id);
 	glViewport(0, 0, width(), height());
