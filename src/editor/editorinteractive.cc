35dd14e694de00d6f1f5c62bc4ed97f9c41aa2cb
Benedikt Straub <benedikt-straub@web.de>
2022-03-23 11:07:21 +0100

    Fix tribes postloading again and add regression test

--- src/editor/editorinteractive.cc
+++ src/editor/editorinteractive.cc
@@ -1026,7 +1026,7 @@ void EditorInteractive::do_run_editor(const EditorInteractive::Init init,
 		Notifications::publish(UI::NoteLoadingMessage(format(_("Loading map “%s”…"), filename)));
 		eia.load(filename);
 
-		egbase.postload_addons();
+		egbase.postload_addons(true);
 		egbase.postload();
 		eia.start();
 		if (!script_to_run.empty()) {
@@ -1041,7 +1041,7 @@ void EditorInteractive::do_run_editor(const EditorInteractive::Init init,
 			throw wexception("EditorInteractive::run_editor: Script given when none was expected");
 		}
 
-		egbase.postload_addons();
+		egbase.postload_addons(true);
 		egbase.postload();
 
 		egbase.mutable_map()->create_empty_map(
