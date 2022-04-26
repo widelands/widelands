widelands/widelands

commit 922abeb7a236e5fcd82fbcbaf3caa0403f3bc101

use CLANG_DIAG_RESERVED_IDENTIFIER_OFF, unset MINIZIP_STATIC_LIBRARIES

cmake/WlFunctions.cmake
@@ -79,9 +79,13 @@ macro(_common_compile_tasks)
     if(MINIZIP_STATIC_LIBRARIES)
       wl_include_directories(${NAME} ${MINIZIP_INCLUDE_DIRS})
       target_compile_options(${NAME} SYSTEM PUBLIC ${MINIZIP_CFLAGS})
-      if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        target_compile_options(${NAME} PUBLIC "-Wno-reserved-id-macro")
-      endif()
+      #if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        #include(CheckCXXCompilerFlag)
+        #check_cxx_compiler_flag("-Wno-reserved-id-macro" HAVE_FLAG)
+        #if (HAVE_FLAG)
+          #target_compile_options(${NAME} PUBLIC "-Wno-reserved-id-macro")
+        #endif()
+      #endif()
     else()
       wl_include_directories(${NAME} ${CMAKE_SOURCE_DIR}/src/third_party/minizip)
     endif()

src/io/filesystem/zip_filesystem.h
@@ -20,9 +20,11 @@
 #define WL_IO_FILESYSTEM_ZIP_FILESYSTEM_H
 
 #include <memory>
+#include "base/macros.h"
+CLANG_DIAG_RESERVED_IDENTIFIER_OFF
 #include <unzip.h>
 #include <zip.h>
-
+CLANG_DIAG_RESERVED_IDENTIFIER_ON
 #include "io/filesystem/filesystem.h"
 #include "io/streamread.h"
 #include "io/streamwrite.h"

src/CMakeLists.txt
@@ -69,6 +69,9 @@ else()
   )
 endif()
 
+# unset these two if they are cached
+unset(MINIZIP_FOUND)
+unset(MINIZIP_STATIC_LIBRARIES)
 if(NOT OPTION_FORCE_EMBEDDED_MINIZIP)
    # If pkg-config is ever needed to find other libraries, move the include outside the if() block
   include(FindPkgConfig)

src/base/macros.h
@@ -93,6 +93,9 @@
 #define FORMAT_WARNINGS_ON GCC_DIAG_ON("-Wformat")
 #endif
 
+// Couldn't we use `check_cxx_compiler_flag` in the cmake file instead?
+// https://cmake.org/cmake/help/latest/module/CheckCXXCompilerFlag.html
+
 // Older clang versions don't have "-Wreserved-identifier" either
 #define CLANG_DIAG_RESERVED_IDENTIFIER_OFF
 #define CLANG_DIAG_RESERVED_IDENTIFIER_ON

commit cbbcdbfe950638d784b8749b206f12d44e87a93f

add "-Wno-reserved-id-macro" flag (fix clang builds)
clang builds failing again after
https://github.com/widelands/widelands/pull/5360/commits/922abeb7a236e5fcd82fbcbaf3caa0403f3bc101

cmake/WlFunctions.cmake
@@ -79,13 +79,13 @@ macro(_common_compile_tasks)
     if(MINIZIP_STATIC_LIBRARIES)
       wl_include_directories(${NAME} ${MINIZIP_INCLUDE_DIRS})
       target_compile_options(${NAME} SYSTEM PUBLIC ${MINIZIP_CFLAGS})
-      #if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        #include(CheckCXXCompilerFlag)
-        #check_cxx_compiler_flag("-Wno-reserved-id-macro" HAVE_FLAG)
-        #if (HAVE_FLAG)
-          #target_compile_options(${NAME} PUBLIC "-Wno-reserved-id-macro")
-        #endif()
-      #endif()
+      if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        include(CheckCXXCompilerFlag)
+        check_cxx_compiler_flag("-Wno-reserved-id-macro" HAVE_FLAG)
+        if (HAVE_FLAG)
+          target_compile_options(${NAME} PUBLIC "-Wno-reserved-id-macro")
+        endif()
+      endif()
     else()
       wl_include_directories(${NAME} ${CMAKE_SOURCE_DIR}/src/third_party/minizip)
     endif()

src/io/filesystem/zip_filesystem.h
@@ -19,12 +19,10 @@
 #ifndef WL_IO_FILESYSTEM_ZIP_FILESYSTEM_H
 #define WL_IO_FILESYSTEM_ZIP_FILESYSTEM_H
 
-#include "base/macros.h"
 #include <memory>
-CLANG_DIAG_RESERVED_IDENTIFIER_OFF
 #include <unzip.h>
 #include <zip.h>
-CLANG_DIAG_RESERVED_IDENTIFIER_ON
+
 #include "io/filesystem/filesystem.h"
 #include "io/streamread.h"
 #include "io/streamwrite.h"
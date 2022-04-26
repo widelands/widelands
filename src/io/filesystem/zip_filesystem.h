cbbcdbfe950638d784b8749b206f12d44e87a93f
andy5995 <arch_stanton5995@protonmail.com>
2022-04-26 02:47:32 -0500

    add "-Wno-reserved-id-macro" flag (fix clang builds)
    clang builds failing again after
    https://github.com/widelands/widelands/pull/5360/commits/922abeb7a236e5fcd82fbcbaf3caa0403f3bc101

--- src/io/filesystem/zip_filesystem.h
+++ src/io/filesystem/zip_filesystem.h
@@ -19,12 +19,10 @@
 #ifndef WL_IO_FILESYSTEM_ZIP_FILESYSTEM_H
 #define WL_IO_FILESYSTEM_ZIP_FILESYSTEM_H
 
-#include "base/macros.h"
 #include <memory>
-CLANG_DIAG_RESERVED_IDENTIFIER_OFF
 #include <unzip.h>
 #include <zip.h>
-CLANG_DIAG_RESERVED_IDENTIFIER_ON
+
 #include "io/filesystem/filesystem.h"
 #include "io/streamread.h"
 #include "io/streamwrite.h"
