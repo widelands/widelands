3aec30bc539bceba162fad3d2b2e545d675f59b6
Benedikt Straub <benedikt-straub@web.de>
2022-08-07 18:45:38 +0200

    Caret-escape control characters in strings

--- src/base/format/string_node.h
+++ src/base/format/string_node.h
@@ -20,6 +20,7 @@
 #define WL_BASE_FORMAT_STRING_NODE_H
 
 #include <cstring>
+#include <memory>
 
 #include "base/format/abstract_node.h"
 
@@ -40,11 +41,43 @@ struct StringNode : FormatNode {
 			throw wexception("Wrong argument type: expected string, found %s", to_string(t).c_str());
 		}
 		const char* arg = arg_u.string_val;
+		std::unique_ptr<char[]> temp_buffer;
+
+		// Replace control characters.
+		size_t nr_characters = 0;
+		size_t nr_ctrl_chars = 0;
+		for (const char* c = arg; *c != 0; ++c, ++nr_characters) {
+			const signed char ch = *c;
+			if (ch == 0x7f || (ch >= 0 && ch < 0x20)) {
+				++nr_ctrl_chars;
+			}
+		}
+		if (nr_ctrl_chars > 0) {
+			temp_buffer.reset(new char[nr_characters + nr_ctrl_chars + 1]);
+			char* write = temp_buffer.get();
+			for (const char* c = arg; *c != 0; ++c, ++write) {
+				const signed char ch = *c;
+				if (ch == 0x7f) {
+					*write = '^';
+					++write;
+					*write = '?';
+				} else if (ch >= 0 && ch < 0x20) {
+					*write = '^';
+					++write;
+					*write = '@' + ch;
+				} else {
+					*write = ch;
+				}
+			}
+			*write = 0;
+			nr_characters += nr_ctrl_chars;
+			arg = temp_buffer.get();
+		}
 
 		if (min_width_ == 0 || (flags_ & kLeftAlign) != 0) {
 			// The easy case: Just start writing.
 			size_t written = 0;
-			for (const char* a = arg; *a && written < precision_; ++a, ++out, ++written) {
+			for (const char* a = arg; *a != 0 && written < precision_; ++a, ++out, ++written) {
 				*out = *a;
 			}
 			if (written < min_width_) {
@@ -57,13 +90,13 @@ struct StringNode : FormatNode {
 		}
 
 		// The more complex case: We want a right-aligned string with a given minimum width.
-		const size_t arg_len = std::min<size_t>(precision_, strlen(arg));
+		const size_t arg_len = std::min<size_t>(precision_, nr_characters);
 		if (arg_len < min_width_) {
-			for (size_t i = min_width_ - arg_len; i; ++out, --i) {
+			for (size_t i = min_width_ - arg_len; i != 0; ++out, --i) {
 				*out = ' ';
 			}
 		}
-		for (size_t l = arg_len; l; --l, ++out, ++arg) {
+		for (size_t l = arg_len; l != 0; --l, ++out, ++arg) {
 			*out = *arg;
 		}
 		return out;
