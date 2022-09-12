d72a85cb608df571ccb879a84ead62ed5b0223cd
Tóth András <txa-dev@posteo.hu>
2022-09-12 11:16:13 +0200

    don't log EOF on network socket

--- src/network/bufferedconnection.cc
+++ src/network/bufferedconnection.cc
@@ -299,9 +299,11 @@ void BufferedConnection::start_receiving() {
 			   start_receiving();
 		   } else {
 			   if (socket_.is_open()) {
-				   log_err("[BufferedConnection] Error when receiving data from host (error %i: %s)\n",
-				           ec.value(), ec.message().c_str());
-				   log_err("[BufferedConnection] Closing socket\n");
+				   if (ec != asio::error::eof) {
+					   log_err("[BufferedConnection] Error when receiving data from host (error %i: %s)\n",
+					           ec.value(), ec.message().c_str());
+					   log_info("[BufferedConnection] Closing socket\n");
+				   }
 				   socket_.shutdown(asio::ip::tcp::socket::shutdown_both, ec);
 				   socket_.close();
 			   }
