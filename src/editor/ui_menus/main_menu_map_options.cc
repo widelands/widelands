528f4700c7e63dc8a9be08074d884f283f2b755b
Tóth András <txa-dev@posteo.hu>
2022-11-21 21:13:14 +0100

    set maximum and add big step for ferry range spinbox

--- src/editor/ui_menus/main_menu_map_options.cc
+++ src/editor/ui_menus/main_menu_map_options.cc
@@ -435,10 +435,11 @@ MainMenuMapOptions::MainMenuMapOptions(EditorInteractive& parent, Registry& regi
 	waterway_length_warning_ =
 	   new UI::Icon(ww_box, UI::PanelStyle::kWui, g_image_cache->get("images/ui_basic/stop.png"));
 	waterway_length_warning_->set_handle_mouse(true);
+	uint32_t ww_spinbox_w = max_w_ - waterway_length_warning_->get_w();
 	waterway_length_box_ =
-	   new UI::SpinBox(ww_box, 0, 0, max_w_ - waterway_length_warning_->get_w(), max_w_ * 2 / 3, 1,
-	                   1, std::numeric_limits<int32_t>::max(), UI::PanelStyle::kWui, std::string(),
-	                   UI::SpinBox::Units::kFields);
+	   new UI::SpinBox(ww_box, 0, 0, ww_spinbox_w, ww_spinbox_w - padding_, 1, 1, 50,
+	                   UI::PanelStyle::kWui, std::string(), UI::SpinBox::Units::kFields,
+	                   UI::SpinBox::Type::kBig, 1, 5);
 	/** TRANSLATORS: Map Options: Waterways are disabled */
 	waterway_length_box_->add_replacement(1, _("Disabled"));
 	waterway_length_box_->changed.connect([this]() { update_waterway_length_warning(); });
