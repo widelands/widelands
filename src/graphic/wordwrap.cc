edb1f99d63d1fc846fa173bbfe51c7b3425debc2
Jonas Mörschbach <jmoerschbach@gmail.com>
2022-11-27 19:43:32 +0100

    use same threshold

--- src/graphic/wordwrap.cc
+++ src/graphic/wordwrap.cc
@@ -61,7 +61,7 @@ int text_height(int ptsize) {
 namespace UI {
 
 constexpr int CARET_BLINKING_DELAY = 1000;
-constexpr int CURSOR_MOVEMENT_THRESHOLD = 100;
+constexpr int CURSOR_MOVEMENT_THRESHOLD = 200;
 
 WordWrap::WordWrap(int fontsize, const RGBColor& color, uint32_t gwrapwidth)
    : draw_caret_(false),
