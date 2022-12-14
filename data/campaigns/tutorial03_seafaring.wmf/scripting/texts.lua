849ba29a7fc8874a888b9539c758ff546e88cc07
Benedikt Straub <benedikt-straub@web.de>
2022-12-14 12:10:06 +0100

    Misc String Fixes

--- data/campaigns/tutorial03_seafaring.wmf/scripting/texts.lua
+++ data/campaigns/tutorial03_seafaring.wmf/scripting/texts.lua
@@ -258,7 +258,7 @@ ferry_2 = {
       h1(_("Rowboats & Ferry Yards")) ..
       li_object("atlanteans_ferry_yard",
          p(_([[This is where ferries come in. Ferries are small rowboats that can carry only one ware at a time, and no workers (other than the one who rows it and who never leaves his boat).]])) ..
-         p(_([[ Ferries are built in ferry yards, one rowboat costs two logs and one piece of cloth and is quickly constructed. A ferry yard is a medium-sized building that needs to be built close to the shore.]])), plr.color) ..
+         p(_([[Ferries are built in ferry yards, one rowboat costs two logs and one piece of cloth and is quickly constructed. A ferry yard is a medium-sized building that needs to be built close to the shore.]])), plr.color) ..
       li(_([[Let’s build a ferry yard close to the shore before we will continue.]]))
    )
 }
