edba141bd7c2183f96c08b4a30cb16789c7c7e23
Benedikt Straub <benedikt-straub@web.de>
2023-01-10 20:26:07 +0100

    Fix hang on building window warp

--- src/wui/interactive_base.cc
+++ src/wui/interactive_base.cc
@@ -203,16 +203,7 @@ InteractiveBase::InteractiveBase(EditorGameBase& the_egbase, Section& global_s, 
 				   const Widelands::Coords coords = building->get_position();
 				   // Check whether the window is wanted
 				   if (wanted_building_windows_.count(coords.hash()) == 1) {
-					   const WantedBuildingWindow& wanted_building_window =
-					      *wanted_building_windows_.at(coords.hash());
-					   UI::UniqueWindow* building_window =
-					      show_building_window(coords, true, wanted_building_window.show_workarea);
-					   building_window->set_pos(wanted_building_window.window_position);
-					   if (wanted_building_window.minimize) {
-						   building_window->minimize();
-					   }
-					   building_window->set_pinned(wanted_building_window.pin);
-					   wanted_building_windows_.erase(coords.hash());
+					   wanted_building_windows_.at(coords.hash())->warp_done = true;
 				   }
 			   }
 		   } break;
@@ -854,6 +845,21 @@ void InteractiveBase::think() {
 				   _("Waterway length: %1$u / %2$u"), steps, egbase().map().get_waterway_max_length()));
 			}
 		}
+	}
+
+	for (const auto& pair : wanted_building_windows_) {
+		if (!pair.second->warp_done) {
+			continue;
+		}
+
+		UI::UniqueWindow* building_window =
+		   show_building_window(Widelands::Coords::unhash(pair.first), true, pair.second->show_workarea);
+		building_window->set_pos(pair.second->window_position);
+		if (pair.second->minimize) {
+			building_window->minimize();
+		}
+		building_window->set_pinned(pair.second->pin);
+		break;
 	}
 }
 
@@ -1616,7 +1622,7 @@ void InteractiveBase::add_wanted_building_window(const Widelands::Coords& coords
                                                  bool was_minimal,
                                                  bool was_pinned) {
 	wanted_building_windows_.insert(std::make_pair(
-	   coords.hash(), std::unique_ptr<const WantedBuildingWindow>(new WantedBuildingWindow(
+	   coords.hash(), std::unique_ptr<WantedBuildingWindow>(new WantedBuildingWindow(
 	                     point, was_minimal, was_pinned, has_workarea_preview(coords)))));
 }
 
