02532ae788932b35b6e2ad0189529ddbb840faee
hessenfarmer <stephan-lutz@gmx.de>
2023-03-07 22:11:48 +0100

    further tweaks

--- src/ai/defaultai.cc
+++ src/ai/defaultai.cc
@@ -3362,14 +3362,14 @@ void DefaultAI::diplomacy_actions(const Time& gametime) {
 		    player_statistics.player_diplo_requested_lately(opn, gametime)) {
 			continue;
 		}
-		if (player_statistics.get_diplo_score(opn) >= 20) {
+		if (player_statistics.get_diplo_score(opn) >= 30) {
 			player_statistics.set_last_time_requested(gametime, opn);
 			if (other_player->team_number() == 0) {
 				game().send_player_diplomacy(mypn, Widelands::DiplomacyAction::kInvite, opn);
 			} else if (other_player->team_number() != me->team_number()) {
 				game().send_player_diplomacy(mypn, Widelands::DiplomacyAction::kJoin, opn);
 			}
-		} else if (player_statistics.get_diplo_score(opn) < -10 &&
+		} else if (player_statistics.get_diplo_score(opn) < -15 &&
 		           other_player->team_number() == me->team_number()) {
 			game().send_player_diplomacy(mypn, Widelands::DiplomacyAction::kLeaveTeam, opn);
 		}
