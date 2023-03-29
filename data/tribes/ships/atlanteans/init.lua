8eb9dea41fd5a910625ee88eba248f4e8c54b6ba
Benedikt Straub <48293446+Noordfrees@users.noreply.github.com>
2023-03-29 21:14:14 +0200

    Naval Warfare: Healing (#5795)

--- data/tribes/ships/atlanteans/init.lua
+++ data/tribes/ships/atlanteans/init.lua
@@ -37,6 +37,8 @@
 --
 --    **attack_accuracy**: The percentage of the chance that an attack launched by this ship hits.
 --
+--    **heal_per_second**: The number of hitpoints the ship heals per second when in a dock.
+--
 --    **animations**: A table containing all file animations for this ship.
 --    Ships have an "idle", a "sinking" and a directional "sail" animation.
 --    Animations can either be defined as file animations in this table or as spritesheet animations
@@ -119,6 +121,7 @@ wl.Descriptions():new_ship_type {
    max_attack   = 10000,
    defense         =  5,
    attack_accuracy = 60,
+   heal_per_second = 100,
 
    spritesheets = {
       idle = {
