f478777a78d7c5c5491f7cb8a10e720c3c461899
Benedikt Straub <benedikt-straub@web.de>
2023-04-25 21:47:22 +0200

    Write documentation

--- src/scripting/lua_ui.cc
+++ src/scripting/lua_ui.cc
@@ -337,7 +337,80 @@ int LuaPanel::indicate(lua_State* L) {
 
       Create a UI widget as a child of this panel as specified by the provided table.
 
-      NOCOM Document table structure
+      A UI descriptor table contains multiple keys of type :class:`string`. Common properties are:
+
+         * ``"widget"``: **Mandatory**. The type of widget to create. See below for allowed values.
+         * ``"name"``: **Mandatory** for named panels. The internal name of the panel.
+         * ``"x"``: **Optional**. The horizontal offset inside the parent from the left.
+         * ``"y"``: **Optional**. The vertical offset inside the parent from the top.
+         * ``"w"``: **Optional**. The widget's width.
+         * ``"h"``: **Optional**. The widget's height.
+         * ``"tooltip"``: **Optional**. The widget's tooltip.
+         * ``"resizing"``: **Optional**. If the parent component is a Box,
+                          the Resizing strategy to use for layouting.
+                          Valid values are:
+            * ``"align"``: Use the widget's desired size.
+            * ``"fullsize"``: Use the widget's desired depth and the full available breadth.
+            * ``"fillspace"``: Use the widget's desired breadth and the full available depth.
+            * ``"expandboth"``: Use the full available space.
+         * ``"align"``: **Optional**. If the parent component is a Box,
+                        the Alignment strategy to use for layouting.
+                        Valid values are ``"left"``/``"top"``, ``"center"``,
+                        and ``"right"``/`"bottom"``.
+         * ``"on_panel_clicked"``: **Optional**. Callback code to run when the
+                                   user clicks anywhere inside the widget.
+         * ``"on_position_changed"``: **Optional**. Callback code to run when the
+                                      widget's position changes.
+         * ``"children"``: **Optional**. An array of widget descriptor tables.
+
+      Keys that are not supported by the widget type are silently ignored.
+
+      Component types are:
+
+         * ``"window"``: A window. Windows can only be added to the top-level
+                         :class:`~wl.ui.MapView`. Properties:
+            * ``"title"``: **Mandatory**. The title bar text.
+            * ``"content"``: **Optional**. The main panel descriptor table.
+                             The window will resize itself to accommodate this widget perfectly.
+         * ``"box"``: A wrapper around other components with intelligent layouting.
+                      It is strongly recommended to layout all your components
+                      exclusively with Boxes. Properties:
+            * ``"orientation"``: **Mandatory**. The box's layouting direction:
+                                 ``"vertical"`` or ``"horizontal"``.
+            * ``"max_x"``: **Optional**. The maximum horizontal size.
+            * ``"max_y"``: **Optional**. The maximum vertical size.
+            * ``"spacing"``: **Optional**. The inner spacing between items.
+            * ``"scrolling"``: **Optional**. Whether the box max scroll if its content is too large.
+         * ``"inf_space"``: Only valid as the direct child of a Box. A flexible spacer.
+         * ``"space"``: Only valid as the direct child of a Box. A fixed-size spacer. Property:
+            * ``"value"``: **Mandatory**. The size of the space.
+         * ``"textarea"``: A static text area with a single line of text. Properties:
+            * ``"text"``: **Mandatory**. The text to display.
+            * ``"font"``: **Mandatory**. The font style to use.
+            * ``"text_align"``: **Optional**. The alignment of the text.
+            * ``"fixed_width"``: **Optional**. If set, the text area's width is fixed instead
+                                 if resizing to accomodate the text or the parent.
+         * ``"button"``: A clickable button. A button must have either a title or an icon,
+                         but not both. Properties:
+            * ``"title"``: **Optional**. The text on the button.
+            * ``"icon"``: **Optional**. The file path of the button's icon.
+            * ``"style"``: **Optional**. The button's style. One of:
+               * ``"primary"``
+               * ``"secondary"``
+               * ``"menu"``
+            * ``"visual"``: **Optional**. The button's appearance. One of:
+               * ``"raised"``
+               * ``"permpressed"``
+               * ``"flat"``
+            * ``"on_click"``: **Optional**. Callback code to run when the
+                                   button is clicked. To associate actions with a button press,
+                                   prefer this over the ``on_panel_clicked`` event.
+
+      Note that event callbacks functions must be provided as raw code in string form.
+      During the lifetime of a *toolbar* widget, the Lua Interface used by the game may be reset.
+      Therefore, any callbacks attached to such widgets must not use any functions or variables
+      defined at an arbitrary earlier time by your script -
+      they may be deleted by the time the callback is invoked.
 */
 int LuaPanel::create_child(lua_State* L) {
 	if (lua_gettop(L) != 2) {
@@ -459,11 +532,11 @@ static UI::Button::VisualState get_table_button_visual_state(lua_State* L, const
 	lua_getfield(L, -1, key);
 	if (!lua_isnil(L, -1)) {
 		std::string str = luaL_checkstring(L, -1);
-		if (str == "") {
+		if (str == "raised") {
 			default_value = UI::Button::VisualState::kRaised;
-		} else if (str == "") {
+		} else if (str == "permpressed") {
 			default_value = UI::Button::VisualState::kPermpressed;
-		} else if (str == "") {
+		} else if (str == "flat") {
 			default_value = UI::Button::VisualState::kFlat;
 		} else {
 			report_error(L, "Unknown button visual state '%s'", str.c_str());
@@ -541,7 +614,7 @@ UI::Panel* LuaPanel::do_create_child(lua_State* L, UI::Panel* parent, UI::Box* a
 		}
 
 		int32_t max_x = get_table_int(L, "max_x", false);
-		int32_t max_y = get_table_int(L, "max_x", false);
+		int32_t max_y = get_table_int(L, "max_y", false);
 		int32_t spacing = get_table_int(L, "spacing", false);
 
 		child_as_box = new UI::Box(parent, UI::PanelStyle::kWui, x, y, orientation, max_x, max_y, spacing);
