568e121702a3544c03fc94143211ca9f742610a8
Javier G. Sogo <jgsogo@gmail.com>
2023-05-10 23:33:39 +0200

    keep some singular

--- data/campaigns/atl02.wmf/scripting/texts.lua
+++ data/campaigns/atl02.wmf/scripting/texts.lua
@@ -166,7 +166,7 @@ obj_defeat_kalitath = {
    title = _("Defeat Kalitath"),
    number = 1,
    body = objective_text(_("Defeat Kalitath"),
-   p(_([[Now it’s time to finish off this annoying Barbarians. He has molested us far too long now.]])))
+   p(_([[Now it’s time to finish off this annoying Barbarian. He has molested us far too long now.]])))
 }
 
 -- =======================================================================
@@ -514,7 +514,7 @@ heretics_again = {
       title = _("Heretics!"),
       body = sidolus(_("Sidolus is very sad"),
          -- TRANSLATORS: Sidolus
-         _([[May Satul warm us all. Our princess has been confused in her imprisonment by this Barbarians. But I will never worship any Barbarians god. I can’t imagine how we could change her mind though.]]))
+         _([[May Satul warm us all. Our princess has been confused in her imprisonment by this Barbarian. But I will never worship any Barbarians god. I can’t imagine how we could change her mind though.]]))
    },
    {
       title = _("Attracting Satul’s Help"),
@@ -526,7 +526,7 @@ heretics_again = {
       title = _("Build Temple"),
       body = sidolus(_("Sidolus"),
          -- TRANSLATORS: Sidolus
-         _([[May Satul warm you, Loftomor. This is a very good idea indeed. So I task you to build a temple worth worshipping our god. Until we are ready to beg for Satul’s power we should keep an eye on our princess and this Barbarians. As we should stop our trader’s deliveries to not waste any wares to this Barbarians any more, I want you to ensure we have a scout out there to gather information.]]))
+         _([[May Satul warm you, Loftomor. This is a very good idea indeed. So I task you to build a temple worth worshipping our god. Until we are ready to beg for Satul’s power we should keep an eye on our princess and this Barbarian. As we should stop our trader’s deliveries to not waste any wares to this Barbarian any more, I want you to ensure we have a scout out there to gather information.]]))
          .. new_objectives(obj_build_temple),
    },
 }
@@ -636,7 +636,7 @@ trading = {
       title = _("Sidolus Agrees"),
       body = sidolus(_("Sidolus"),
          -- TRANSLATORS: Sidolus
-         _([[Well done, Loftomor, well done. May Satul warm our princess and our whole people. Now we need to begin sending goods to this Barbarians.]])
+         _([[Well done, Loftomor, well done. May Satul warm our princess and our whole people. Now we need to begin sending goods to this Barbarian.]])
          .. paragraphdivider() ..
          -- TRANSLATORS: Sidolus
          _([[I think we should start with the deliveries we could afford the easiest. But we need to find more metals for sure.]]))
