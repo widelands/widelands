8fe0c000c8182bef800548b476d3cf2ea837ee43
Tóth András <txa-dev@posteo.hu>
2023-08-07 16:50:00 +0200

    sound options changes  * increase vertical spacing  * hide sliders for disabled sounds  * fix --nosound warning text style  * refactored text width setting

--- src/wui/sound_options.cc
+++ src/wui/sound_options.cc
@@ -17,9 +17,10 @@
 
 #include "wui/sound_options.h"
 
+#include <vector>
+
 #include "base/i18n.h"
 #include "graphic/font_handler.h"
-#include "graphic/style_manager.h"
 #include "graphic/text_layout.h"
 #include "sound/sound_handler.h"
 #include "ui_basic/checkbox.h"
@@ -29,16 +30,19 @@
 namespace {
 
 constexpr int kPadding = 4;
+constexpr int kSliderWidth = 200;
+constexpr int kSliderHeight = 16;
+constexpr int kCursorWidth = 28;
+constexpr int kSpacing = 32;
+
+UI::PanelStyle slider_to_panel_style(UI::SliderStyle style) {
+	return style == UI::SliderStyle::kFsMenu ? UI::PanelStyle::kFsMenu : UI::PanelStyle::kWui;
+}
 
 /**
  * UI elements to set sound properties for 1 type of sounds.
  */
 class SoundControl : public UI::Box {
-private:
-	static constexpr int kSliderWidth = 200;
-	static constexpr int kCursorWidth = 28;
-	static constexpr int kSliderSpacing = 16;
-
 public:
 	/**
 	 * @brief SoundControl Creates a new sound control box
@@ -49,13 +53,12 @@ public:
 	 */
 	SoundControl(UI::Box* parent,
 	             UI::SliderStyle style,
-	             int checkbox_width,
 	             const std::string& name,
 	             const std::string& title,
 	             SoundType type,
 	             FxId representative_fx = kNoSoundEffect)
 	   : UI::Box(parent,
-	             style == UI::SliderStyle::kFsMenu ? UI::PanelStyle::kFsMenu : UI::PanelStyle::kWui,
+	             slider_to_panel_style(style),
 	             name,
 	             0,
 	             0,
@@ -65,7 +68,7 @@ public:
 	             0,
 	             0,
 	             kSliderWidth,
-	             kSliderSpacing,
+	             kSliderHeight,
 	             0,
 	             g_sh->get_max_volume(),
 	             g_sh->get_volume(type),
@@ -73,30 +76,39 @@ public:
 	             /** TRANSLATORS: Tooltip for volume slider in sound options */
 	             _("Changes the volume. Click to hear a sample."),
 	             kCursorWidth),
-	     enable_(this, panel_style_, "enable", Vector2i::zero(), title, "", checkbox_width),
+	     enable_(this, panel_style_, "enable", Vector2i::zero(), title),
 	     type_(type),
 	     fx_(representative_fx) {
 		set_inner_spacing(kPadding);
 		if (UI::g_fh->fontset()->is_rtl()) {
-			add(&volume_, UI::Box::Resizing::kAlign, UI::Align::kCenter);
-			add(&enable_, UI::Box::Resizing::kAlign, UI::Align::kCenter);
+			add(&volume_, UI::Box::Resizing::kAlign, UI::Align::kRight);
+			add(&enable_, UI::Box::Resizing::kAlign, UI::Align::kRight);
 		} else {
-			add(&enable_, UI::Box::Resizing::kAlign, UI::Align::kCenter);
-			add(&volume_, UI::Box::Resizing::kAlign, UI::Align::kCenter);
+			add(&enable_, UI::Box::Resizing::kAlign, UI::Align::kLeft);
+			add(&volume_, UI::Box::Resizing::kAlign, UI::Align::kLeft);
 		}
 
 		if (SoundHandler::is_backend_disabled()) {
 			enable_.set_enabled(false);
-			volume_.set_enabled(false);
+			volume_.set_visible(false);
 		} else {
 			enable_.set_state(g_sh->is_sound_enabled(type));
-			volume_.set_enabled(g_sh->is_sound_enabled(type));
+			volume_.set_visible(g_sh->is_sound_enabled(type));
 
 			enable_.changedto.connect([this](bool on) { enable_changed(on); });
 			volume_.changedto.connect([this](int32_t value) { volume_changed(value); });
 			volume_.clicked.connect([this] { play_sound_sample(); });
 		}
 		set_thinks(false);
+	}
+
+	int checkbox_width() {
+		enable_.layout();
+		return enable_.get_w();
+	}
+
+	void set_checkbox_width(const int w) {
+		enable_.set_size(w, enable_.get_h());
 	}
 
 private:
@@ -111,7 +123,7 @@ private:
 	/// the volume slider accordingly
 	void enable_changed(bool on) {
 		enable_.set_state(on);
-		volume_.set_enabled(on);
+		volume_.set_visible(on);
 		g_sh->set_enable_sound(type_, on);
 	}
 
@@ -130,13 +142,11 @@ private:
 	const FxId fx_;
 };
 
-constexpr int kSpacing = 12;
-
 }  // namespace
 
 SoundOptions::SoundOptions(UI::Panel& parent, UI::SliderStyle style)
    : UI::Box(&parent,
-             style == UI::SliderStyle::kFsMenu ? UI::PanelStyle::kFsMenu : UI::PanelStyle::kWui,
+             slider_to_panel_style(style),
              "sound_options",
              0,
              0,
@@ -155,42 +165,35 @@ SoundOptions::SoundOptions(UI::Panel& parent, UI::SliderStyle style)
 
 	set_inner_spacing(kSpacing);
 
-	// TODO(tothxa): This shouldn't be duplicated, but de-duplicating seems harder than it's worth.
-	const std::vector<std::string> labels = {
-	   pgettext("sound_options", "Music"), pgettext("sound_options", "Chat Messages"),
-	   pgettext("sound_options", "Game Messages"), pgettext("sound_options", "User Interface"),
-	   pgettext("sound_options", "Ambient Sounds")};
+	std::vector<SoundControl*> controls;
+	controls.emplace_back(new SoundControl(
+	   this, style, "music", pgettext("sound_options", "Music"), SoundType::kMusic));
 
-	const UI::FontStyleInfo& font_style = g_style_manager->font_style(
-	   style == UI::SliderStyle::kFsMenu ? UI::FontStyle::kFsMenuLabel : UI::FontStyle::kWuiLabel);
+	controls.emplace_back(new SoundControl(
+	   this, style, "chat", pgettext("sound_options", "Chat Messages"), SoundType::kChat,
+	   SoundHandler::register_fx(SoundType::kChat, "sound/lobby_chat")));
+
+	controls.emplace_back(new SoundControl(
+	   this, style, "messages", pgettext("sound_options", "Game Messages"), SoundType::kMessage,
+	   SoundHandler::register_fx(SoundType::kMessage, "sound/message")));
+
+	controls.emplace_back(new SoundControl(
+	   this, style, "ui", pgettext("sound_options", "User Interface"), SoundType::kUI));
+
+	controls.emplace_back(new SoundControl(
+	   this, style, "ambient", pgettext("sound_options", "Ambient Sounds"), SoundType::kAmbient,
+	   SoundHandler::register_fx(SoundType::kAmbient, "sound/create_construction_site")));
 
 	int max_w = 0;
-	for (const std::string& label : labels) {
-		const int w = text_width(label, font_style);
-		if (w > max_w) {
+	for (SoundControl* control : controls) {
+		add(control);
+		if (const int w = control->checkbox_width(); w > max_w) {
 			max_w = w;
 		}
 	}
-	max_w += kStateboxSize + kPadding;
-
-	add(new SoundControl(
-	   this, style, max_w, "music", pgettext("sound_options", "Music"), SoundType::kMusic));
-
-	add(new SoundControl(this, style, max_w, "chat", pgettext("sound_options", "Chat Messages"),
-	                     SoundType::kChat,
-	                     SoundHandler::register_fx(SoundType::kChat, "sound/lobby_chat")));
-
-	add(new SoundControl(this, style, max_w, "messages", pgettext("sound_options", "Game Messages"),
-	                     SoundType::kMessage,
-	                     SoundHandler::register_fx(SoundType::kMessage, "sound/message")));
-
-	add(new SoundControl(
-	   this, style, max_w, "ui", pgettext("sound_options", "User Interface"), SoundType::kUI));
-
-	add(new SoundControl(
-	   this, style, max_w, "ambient", pgettext("sound_options", "Ambient Sounds"),
-	   SoundType::kAmbient,
-	   SoundHandler::register_fx(SoundType::kAmbient, "sound/create_construction_site")));
+	for (SoundControl* control : controls) {
+		control->set_checkbox_width(max_w);
+	}
 
 	add(&custom_songset_);
 	custom_songset_.set_state(g_sh->use_custom_songset());
@@ -201,7 +204,7 @@ SoundOptions::SoundOptions(UI::Panel& parent, UI::SliderStyle style)
 	// the MultilineTextarea is not added to the Box. So, we create and add it even if its text is
 	// empty.
 	UI::MultilineTextarea* sound_warning =
-	   new UI::MultilineTextarea(this, "warning", 0, 0, 100, 0, UI::PanelStyle::kWui, "",
+	   new UI::MultilineTextarea(this, "warning", 0, 0, 100, 0, slider_to_panel_style(style), "",
 	                             UI::Align::kLeft, UI::MultilineTextarea::ScrollMode::kNoScrolling);
 	add(sound_warning, UI::Box::Resizing::kExpandBoth);
 
