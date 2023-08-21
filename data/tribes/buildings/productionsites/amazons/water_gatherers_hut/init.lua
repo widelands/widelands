af37355dd15b93dba99a83d7b52d29acfe77c1a9
Pavol Gono <pavol.gono@gmail.com>
2023-08-21 05:07:57 +0000

    Fixed comment typo.

--- data/tribes/buildings/productionsites/amazons/water_gatherers_hut/init.lua
+++ data/tribes/buildings/productionsites/amazons/water_gatherers_hut/init.lua
@@ -39,7 +39,7 @@ wl.Descriptions():new_productionsite_type {
          actions = {
             -- time of worker: 4.6 sec
             -- radius: 8
-            -- steps from building to tree: 2...11
+            -- steps from building to water: 2...11
             -- min. time total: 2 * 2 * 1.8 + 4.6 + 20 = 31.8 sec
             -- max. time total: 2 * 11 * 1.8 + 4.6 + 20 = 64.2 sec
             "sleep=duration:20s",
