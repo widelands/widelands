9bb7f4f0ffe255bbab5d546d14fdaba943924b48
Tóth András <80712255+tothxa@users.noreply.github.com>
2023-08-26 16:26:52 +0200

    Debugconsole and input history improvements (#5944)

--- src/wlapplication.cc
+++ src/wlapplication.cc
@@ -85,6 +85,7 @@
 #include "wlapplication_messages.h"
 #include "wlapplication_mousewheel_options.h"
 #include "wlapplication_options.h"
+#include "wui/game_chat_panel.h"
 #include "wui/interactive_player.h"
 #include "wui/interactive_spectator.h"
 
@@ -479,6 +480,14 @@ WLApplication::WLApplication(int const argc, char const* const* const argv)
 	// Save configuration now. Otherwise, the UUID and sound options
 	// are not saved, when the game crashes
 	write_config();
+
+	if (get_config_bool("save_chat_history", false)) {
+		g_chat_sent_history.load(kChatSentHistoryFile);
+	}
+	if (g_allow_script_console) {
+		log_info("Developer tools are enabled.");
+		g_script_console_history.load(kScriptConsoleHistoryFile);
+	}
 }
 
 /**
@@ -496,6 +505,13 @@ WLApplication::~WLApplication() {
 
 	shutdown_hardware();
 	shutdown_settings();
+
+	if (get_config_bool("save_chat_history", false)) {
+		g_chat_sent_history.save(kChatSentHistoryFile);
+	}
+	if (g_allow_script_console) {
+		g_script_console_history.save(kScriptConsoleHistoryFile);
+	}
 
 	assert(UI::g_fh);
 	delete UI::g_fh;
@@ -1553,6 +1569,15 @@ void WLApplication::handle_commandline_parameters() {
 	} else {
 		set_config_bool("auto_speed", false);
 	}
+
+	if (commandline_.count("enable_development_testing_tools") != 0u) {
+		g_allow_script_console = true;
+		commandline_.erase("enable_development_testing_tools");
+	}
+#ifndef NDEBUG
+	// Always enable in debug builds
+	g_allow_script_console = true;
+#endif
 
 	if (commandline_.count("write_syncstreams") != 0u) {
 		g_write_syncstreams = true;
--- src/wui/game_chat_menu.cc
+++ src/wui/game_chat_menu.cc
@@ -19,6 +19,7 @@
 #include "wui/game_chat_menu.h"
 
 #include "base/i18n.h"
+#include "base/wexception.h"
 
 /*
 ==============================================================================
@@ -32,13 +33,25 @@ GameChatMenu::GameChatMenu(UI::Panel* parent,
                            ChatColorForPlayer fn,
                            UI::UniqueWindow::Registry& registry,
                            ChatProvider& chat,
+                           UI::EditBoxHistory* history,
                            const std::string& title)
    : UI::UniqueWindow(parent, UI::WindowStyle::kWui, "chat", &registry, 440, 235, title),
-     chat_(this, fn, 5, 5, get_inner_w() - 10, get_inner_h() - 10, chat, UI::PanelStyle::kWui) {
+     chat_(this,
+           fn,
+           5,
+           5,
+           get_inner_w() - 10,
+           get_inner_h() - 10,
+           chat,
+           history,
+           UI::PanelStyle::kWui) {
 	if (get_usedefaultpos()) {
 		center_to_parent();
 	}
 	set_can_focus(true);
+
+	// Textinput will be passed to the chat entry field when the window border has focus
+	set_handle_textinput(true);
 
 	chat_.sent.connect([this]() { acknowledge(); });
 	chat_.aborted.connect([this]() { acknowledge(); });
@@ -52,17 +65,25 @@ GameChatMenu* GameChatMenu::create_chat_console(UI::Panel* parent,
                                                 ChatColorForPlayer fn,
                                                 UI::UniqueWindow::Registry& registry,
                                                 ChatProvider& chat) {
-	return new GameChatMenu(parent, fn, registry, chat, _("Chat"));
+	return new GameChatMenu(parent, fn, registry, chat, &g_chat_sent_history, _("Chat"));
 }
 
-#ifndef NDEBUG  //  only in debug builds
 GameChatMenu* GameChatMenu::create_script_console(UI::Panel* parent,
                                                   ChatColorForPlayer fn,
                                                   UI::UniqueWindow::Registry& registry,
                                                   ChatProvider& chat) {
-	return new GameChatMenu(parent, fn, registry, chat, _("Script Console"));
+	if (!g_allow_script_console) {
+		throw wexception("Trying to open the Script Console when it is disabled.");
+	}
+	return new GameChatMenu(
+	   parent, fn, registry, chat, &g_script_console_history, _("Script Console"));
 }
-#endif
+
+bool GameChatMenu::handle_textinput(const std::string& text) {
+	// Pass on textinput to the chat handler when the window border has focus.
+	// This will switch focus to the editbox in the chat panel.
+	return chat_.handle_textinput(text);
+}
 
 bool GameChatMenu::enter_chat_message(bool close_on_send) {
 	if (is_minimal()) {
@@ -76,11 +97,13 @@ bool GameChatMenu::enter_chat_message(bool close_on_send) {
 void GameChatMenu::restore() {
 	Window::restore();
 	chat_.focus_edit();
+	set_handle_textinput(true);
 }
 
 void GameChatMenu::minimize() {
 	Window::minimize();
 	chat_.unfocus_edit();
+	set_handle_textinput(false);
 }
 
 void GameChatMenu::acknowledge() {
