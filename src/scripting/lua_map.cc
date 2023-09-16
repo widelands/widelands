01caf909a9bdb858a9646a668ae7a917a54e0010
Simon Heimberg <simon.heimberg@heimberg-ea.ch>
2023-09-17 01:56:04 +0300

    extend documentation about attributes of units
    The documentation for lua api only mentions has_attribute() for ImmovableDescription.
    But it exists for all MapObjectDescriptions. It will return false for all types
    except of ImmovableDescription and critters.
    The documentation about writing units tells attributes can be set, but does not
    mention they do not work for all units.
    
    improves #4744

--- src/scripting/lua_map.cc
+++ src/scripting/lua_map.cc
@@ -2603,6 +2603,9 @@ int LuaImmovableDescription::get_size(lua_State* L) {
 
       Returns :const:`true` if the immovable has the attribute, :const:`false` otherwise.
 
+      Note: This method does exist for all MapObjects, but it only gets initialised for this class
+      (and for critters).
+
       :arg attribute_name: The attribute that we are checking for.
       :type attribute_name: :class:`string`
 
