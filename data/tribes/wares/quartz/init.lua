2b966e08ec859ed60226ec80d362004d0b3333fb
Tóth András <txa-dev@posteo.hu>
2023-11-19 16:30:06 +0100

    quartz tweaks

--- data/tribes/wares/quartz/init.lua
+++ data/tribes/wares/quartz/init.lua
@@ -11,7 +11,7 @@ wl.Descriptions():new_ware_type {
    animation_directory = dirname,
    animations = {
       idle = {
-         hotspot = { 8, 7 },
+         hotspot = { 8, 8 },
       },
    }
 }
