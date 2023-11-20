732bd16b5077f5eefcfefd1994b1599732cdcb75
Tóth András <txa-dev@posteo.hu>
2023-11-20 02:23:33 +0100

    soften edges of marble

--- data/tribes/wares/marble/init.lua
+++ data/tribes/wares/marble/init.lua
@@ -11,7 +11,7 @@ wl.Descriptions():new_ware_type {
    animation_directory = dirname,
    animations = {
       idle = {
-         hotspot = { 10, 11 },
+         hotspot = { 9, 11 },
       },
    }
 }
