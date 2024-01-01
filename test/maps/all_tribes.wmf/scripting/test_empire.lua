be093cba04ed29edf2feed5e3c65fb03bd53577f
Simon Heimberg <simon.heimberg@heimberg-ea.ch>
2024-01-01 10:45:38 +0300

    use map:wrap_field() instead of map:get_field() in all_tribes.wmf
    ... and wrap the values near old maximum to be able to use the old offsets.
    This allows to adapt the map size.

--- test/maps/all_tribes.wmf/scripting/test_empire.lua
+++ test/maps/all_tribes.wmf/scripting/test_empire.lua
@@ -402,7 +402,7 @@ function init_empire(player)
    total_buildings = init_mines(player, sf, total_buildings)
 
    -- Construction
-   player:place_building("empire_weaponsmithy", map:get_field((sf.x + 510) % 512, (sf.y + 3) % 512), true, true)
+   player:place_building("empire_weaponsmithy", get_safe_field(player, sf, 510, 3), true, true)
 
    -- Test ships
    place_player_ship(player.number)
