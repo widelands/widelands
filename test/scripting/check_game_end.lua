15456ac71e267bb5c4ccbaa5d758797988016e0c
Tóth András <txa-dev@posteo.hu>
2024-01-12 19:14:23 +0100

    update the test script to use the plugin_timer interface

--- test/scripting/check_game_end.lua
+++ test/scripting/check_game_end.lua
@@ -30,49 +30,69 @@ local function formatted_time()
   return time_fmt:bformat(m, s)
 end
 
-function check_win_condition(winners)
-  run(function()
-    -- 2 minutes grace period
-    local timeout = (game.win_condition_duration + 2) * 60 * 1000
-    local check_interval = 2000
-    local pause_timeout = 2 * 60  -- 2 minutes
+-- 2 minutes grace period
+timeout = (game.win_condition_duration + 2) * 60 * 1000
+check_interval = 2000
+pause_timeout = 2 * 60 -- 2 minutes
 
-    local game_ended = false
-    local last_gametime = 0
-    local pause_counter = 0
+game_ended = false
+last_gametime = 0
+pause_counter = 0
 
-    sleep(1000)
-    game.desired_speed = 100000
+expected = nil
 
-    repeat
-      realtime_sleep(check_interval)
-      if (game.time > last_gametime) then
-        last_gametime = game.time
-        pause_counter = 0
-      else
-        pause_counter = pause_counter + check_interval / 1000
-        print(string.bformat("No progress since last check. (%d seconds)", pause_counter))
-      end
-      game_ended = mapview.windows.game_summary ~= nil
-      print("Checking end of game at " .. formatted_time())
-    until game_ended or game.time > timeout or pause_counter >= pause_timeout
+function check_game_ended()
+  if (game.time > last_gametime) then
+    last_gametime = game.time
+    pause_counter = 0
+  else
+    pause_counter = pause_counter + check_interval / 1000
+    print(string.bformat("No progress since last check. (%d seconds)", pause_counter))
+  end
+  game_ended = mapview.windows.game_summary ~= nil
+  print("Checking end of game at " .. formatted_time())
+  if game_ended or game.time > timeout or pause_counter >= pause_timeout then
 
     -- Check timeout
-    assert_true(game_ended, "## Game did not end in time. ##")
+    if not game_ended then
+      print("ERROR: ### Game did not end in time. ###")
 
-    -- Check that the expected player(s) won
+    else
+      -- Check that the expected player(s) won
+      local good = true
+      if expected ~= nil then
+        for i = 1, #game.players do
+          if(expected[i] ~= game.players[i].end_result) then
+            print("ERROR: ### Wrong result for " .. game.players[i].name .. " ###")
+            good = false
+          end
+        end
+      end
+
+      if good then
+        print("# All Tests passed.")
+      end
+    end
+
+    mapview:close()
+  end
+end
+
+function check_win_condition(winners)
+  run(function()
+    sleep(1000)
+
+    -- Set up expected winners table
     if (winners == nil) then
       print("### WARNING: check_win_condition():")
       print("###    For better test coverage, please call with list of expected winners ###")
     else
-      local expected = result_table(winners)
-      for i = 1, #game.players do
-        assert_equal(expected[i], game.players[i].end_result,
-                     "Wrong result for " .. game.players[i].name)
-      end
+      expected = result_table(winners)
     end
 
-    print("# All Tests passed.")
-    mapview:close()
+    -- Schedule check
+    mapview:add_plugin_timer("check_game_ended()", check_interval)
+
+    game.desired_speed = 100000
   end)
 end
