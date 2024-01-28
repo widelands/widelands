4df57cd8102faf0c3b04fda1cf2265f62ff3460b
hessenfarmer <stephan-lutz@gmx.de>
2024-01-28 18:38:09 +0100

    do not start shipyards until we started building a port.

--- src/ai/defaultai_seafaring.cc
+++ src/ai/defaultai_seafaring.cc
@@ -296,10 +296,10 @@ bool DefaultAI::marine_main_decisions(const Time& gametime) {
 					}
 				}
 			}
-			if (shipyard_stocked && stopped && sy_obs.site->can_start_working()) {
+			if (ports_count > 0 && shipyard_stocked && stopped && sy_obs.site->can_start_working()) {
 				verb_log_dbg_time(game().get_gametime(), "AI %d: Starting shipyard.", player_number());
 				game().send_player_start_stop_building(*sy_obs.site);
-			} else if (!shipyard_stocked && !stopped) {
+			} else if (!stopped && (!shipyard_stocked || ports_count == 0)) {
 				verb_log_dbg_time(game().get_gametime(), "AI %d: Stopping shipyard with poor supply.",
 				                  player_number());
 				game().send_player_start_stop_building(*sy_obs.site);
