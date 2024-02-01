a96a05a3327c1507461aca3bfb5855c14163f3ec
Tóth András <txa-dev@posteo.hu>
2024-02-01 01:36:42 +0100

    add minimal sanity check to alldefaults test case
    Co-authored-by: Simon Heimberg <simon.heimberg@heimberg-ea.ch>

--- test/templates/test_alldefaults.lua
+++ test/templates/test_alldefaults.lua
@@ -1,8 +1,12 @@
 include "scripting/coroutine.lua"
+include "test/scripting/lunit.lua"
+
+local game = wl.Game()
 
 run(function()
   sleep(1000)
-  wl.Game().desired_speed = 5000
+  assert_true(#game.players > 1, "## Less than 2 players are in the game ##")
+  game.desired_speed = 5000
   sleep(10000)
   print("# All Tests passed.")
   wl.ui.MapView():close()
