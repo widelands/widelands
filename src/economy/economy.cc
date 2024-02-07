12d64e6fb97e3b782a85e4c9fe06ef3c5b2b22b9
Widelands Bunnybot <bunnybot@widelands.org>
2024-02-07 13:52:42 +0100

    'src/economy/economy.cc' was automatically formatted.

--- src/economy/economy.cc
+++ src/economy/economy.cc
@@ -535,7 +535,7 @@ bool Economy::needs_ware_or_worker(DescriptionIndex const ware_or_worker_type,
 		Quantity quantity_district = 0;
 		for (const Warehouse* wh : warehouses_) {
 			Quantity stock = type_ == wwWARE ? wh->get_wares().stock(ware_or_worker_type) :
-                                              wh->get_workers().stock(ware_or_worker_type);
+                                            wh->get_workers().stock(ware_or_worker_type);
 			if (is_soldier) {
 				Quantity garrison = wh->get_desired_soldier_count();
 				if (garrison >= stock) {
