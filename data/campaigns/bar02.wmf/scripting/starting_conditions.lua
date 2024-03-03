c9db45106141514e9760023ef803956c67be7a6a
Tóth András <txa-dev@posteo.hu>
2024-03-03 19:52:50 +0100

    add translator comment

--- data/campaigns/bar02.wmf/scripting/starting_conditions.lua
+++ data/campaigns/bar02.wmf/scripting/starting_conditions.lua
@@ -56,6 +56,7 @@ hq:set_workers{
    barbarians_stonemason = 2,
 }
 hq:set_soldiers({0,0,0,0}, 45)
+-- TRANSLATORS: The name of the starting village. Keep it consistent with the other occurences.
 hq.warehousename = _("Ondun")
 
 -- =======================================================================
