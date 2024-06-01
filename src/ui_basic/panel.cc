cfd381ac4ec9bcefaba9345fb873b64be8855795
Tóth András <txa-dev@posteo.hu>
2024-06-01 02:22:09 +0200

    more uses for the waiting cursor

--- src/ui_basic/panel.cc
+++ src/ui_basic/panel.cc
@@ -706,6 +706,8 @@ void Panel::set_visible(bool const on) {
 		if (Panel* cm = find_context_menu(); cm != nullptr) {
 			cm->die();
 		}
+	} else if (name_ == "progresswindow") {
+		g_mouse_cursor->change_wait(true);
 	}
 	if (parent_ != nullptr) {
 		parent_->on_visibility_changed();
