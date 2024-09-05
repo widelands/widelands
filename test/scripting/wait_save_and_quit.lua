1925b4158b7be3ea18a76ed265938a1af5277c0c
Tóth András <txa-dev@posteo.hu>
2024-09-05 19:43:58 +0200

    allow running other test scripts after loading created savegames

--- test/scripting/wait_save_and_quit.lua
+++ test/scripting/wait_save_and_quit.lua
@@ -7,9 +7,8 @@ run(function()
   sleep(3000)
   game.desired_speed = 20000
   sleep(120000)
+
   stable_save(game, "test_save", 1000)
 
-  sleep(3000)
-  print('# All Tests passed.')
-  wl.ui.MapView():close()
+  check_reload_version()
 end)
