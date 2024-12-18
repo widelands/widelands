dadb67119593e406d6aa7fb4ef88f8dd02e0bab8
Benedikt Straub <benedikt-straub@web.de>
2024-12-18 19:23:27 +0100

    Split up worker programs

--- src/logic/map_objects/tribes/worker_tasks/scout.cc
+++ src/logic/map_objects/tribes/worker_tasks/scout.cc
@@ -36,26 +36,6 @@ namespace Widelands {
 const Bob::Task Worker::taskScout = {
    "scout", static_cast<Bob::Ptr>(&Worker::scout_update), nullptr, nullptr, true};
 
-/**
- * scout=\<radius\> \<time\>
- *
- * Find a spot that is in the fog of war and go there to see what's up.
- *
- * iparam1 = radius where the scout initially searches for unseen fields
- * iparam2 = maximum search time (in msecs)
- */
-bool Worker::run_scout(Game& game, State& state, const Action& action) {
-	molog(game.get_gametime(), "  Try scouting for %i ms with search in radius of %i\n",
-	      action.iparam2, action.iparam1);
-	if (upcast(ProductionSite, productionsite, get_location(game))) {
-		productionsite->unnotify_player();
-	}
-	++state.ivar1;
-	start_task_scout(game, action.iparam1, action.iparam2);
-	// state reference may be invalid now
-	return true;
-}
-
 /** Setup scouts_worklist at the start of its task.
  *
  * The first element of scouts_worklist vector stores the location of my hut, at the time of
