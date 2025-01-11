423fea35356442cbea148e6be1aa7ae494e760fc
Tóth András <txa-dev@posteo.hu>
2025-01-11 01:04:03 +0100

    make brewery and bakery require marble

--- data/tribes/buildings/productionsites/empire/brewery/init.lua
+++ data/tribes/buildings/productionsites/empire/brewery/init.lua
@@ -11,12 +11,14 @@ wl.Descriptions():new_productionsite_type {
 
    buildcost = {
       log = 1,
-      planks = 2,
-      granite = 2
+      planks = 3,
+      granite = 2,
+      marble = 1,
    },
    return_on_dismantle = {
+      log = 1,
       planks = 1,
-      granite = 1
+      granite = 1,
    },
 
    animation_directory = dirname,
