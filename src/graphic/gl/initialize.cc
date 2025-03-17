85ecc62420f7fb0fc1a02573df2f965f9c06625a
Roman Turchin <roman@turch.in>
2025-03-16 21:46:06 -0500

    [emscirpten] proxy posix sockets through websockets

--- src/graphic/gl/initialize.cc
+++ src/graphic/gl/initialize.cc
@@ -254,14 +254,13 @@ SDL_GLContext initialize(
 		}
 	};
 	const char* const opengl_version_string = reinterpret_cast<const char*>(glGetString(GL_VERSION));
-#if defined(__EMSCRIPTEN__)  // skip check webgl version anyway it's webgl2
+#ifdef __EMSCRIPTEN__  // skip check webgl version anyway it's webgl2
 	log_info("Graphics: OpenGL: Version \"%s\"\n", opengl_version_string);
 #else
 	if (opengl_version_string == nullptr) {
 		handle_unreadable_opengl_version();
 	}
 	log_info("Graphics: OpenGL: Version \"%s\"\n", opengl_version_string);
-#ifndef __EMSCRIPTEN__  // skip check webgl version anyway it's webgl2
 	check_version(
 	   opengl_version_string, "OpenGL", _("OpenGL"), 2, 1, handle_unreadable_opengl_version);
 #endif
@@ -306,14 +305,13 @@ SDL_GLContext initialize(
 
 	const char* const shading_language_version_string =
 	   reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
-#if defined(__EMSCRIPTEN__)  // skip check glsl version anyway it's 300 es
+#ifdef __EMSCRIPTEN__  // skip check glsl version anyway it's 300 es
 	log_info("Graphics: OpenGL: ShadingLanguage: \"%s\"\n", shading_language_version_string);
 #else
 	if (shading_language_version_string == nullptr) {
 		handle_unreadable_opengl_shading_language();
 	}
 	log_info("Graphics: OpenGL: ShadingLanguage: \"%s\"\n", shading_language_version_string);
-#ifndef __EMSCRIPTEN__  // skip check glsl version anyway it's 300 es
 	check_version(shading_language_version_string, "Shading Language", _("Shading Language"), 1, 20,
 	              handle_unreadable_opengl_shading_language);
 	// also skip glDrawBuffer isn't actually part of GLES2.0, just an extension
