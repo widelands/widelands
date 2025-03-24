46a66c0b4f7cc2b6080ad1f161f31cf1d944ee53
Pavol Gono <pavol.gono@gmail.com>
2025-03-24 22:19:39 +0000

    Update performance helptext for comparable buildings between tribes, big sites (CB #4947 / GH #6587)
    Co-authored-by: Pavol Gono <pavol.gono@gmail.com>
    Co-authored-by: Pavol Gono <pavol.gono@gmail.com>
    Co-committed-by: Pavol Gono <pavol.gono@gmail.com>

--- data/tribes/buildings/trainingsites/empire/arena/init.lua
+++ data/tribes/buildings/trainingsites/empire/arena/init.lua
@@ -77,6 +77,9 @@ wl.Descriptions():new_trainingsite_type {
    },
 
    programs = {
+      -- Calculation of complete training of one soldier:
+      -- each partial training takes additional 6 sec - see TrainingSite::TrainingSite()
+      -- time total: 36 sec
       sleep = {
          -- TRANSLATORS: Completed/Skipped/Did not start sleeping because ...
          descname = _("sleeping"),
