85053c00b6b2b572044bbb6f62aee80b67524243
Benedikt Straub <benedikt-straub@web.de>
2025-04-25 21:00:33 +0200

    Let attacking warship decide battle positions for all participants

--- src/logic/map_objects/tribes/ship.h
+++ src/logic/map_objects/tribes/ship.h
@@ -151,7 +151,7 @@ struct Ship : Bob {
 	struct Battle {
 		enum class Phase : uint8_t {
 			kNotYetStarted = 0,
-			kAttackerMovingTowardsOpponent = 1,
+			kMovingToBattlePositions = 1,
 			kAttackersTurn = 2,
 			kDefendersTurn = 3,
 			kAttackerAttacking = 4,
@@ -164,6 +164,7 @@ struct Ship : Bob {
 
 		OPtr<Ship> opponent;
 		Coords attack_coords;
+		Coords battle_position {Coords::null()};
 		std::vector<uint32_t> attack_soldier_serials;
 		Time time_of_last_action;
 		uint32_t pending_damage{0U};
