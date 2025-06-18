422e422e9848a8437ebbf8d835498afe97c0a115
hessenfarmer <stephan-lutz@gmx.de>
2025-06-18 23:18:23 +0200

    implement logging solution

--- src/economy/ship_fleet.cc
+++ src/economy/ship_fleet.cc
@@ -289,16 +289,16 @@ void ShipFleet::split(Game& game) {
 		fleetshipyards.insert(interface->get_building());
 		remove_interface(game, interface);
 	}
-	verb_log_dbg_time(game.get_gametime(), "interfaces removed");
+	verb_log_dbg_time(game.get_gametime(), "[Splitting ShipFleet]: interfaces removed");
 	for (auto& ship : fleetships) {
 		ship->set_destination(game, nullptr);
 		remove_ship(game, ship);
 	}
-	verb_log_dbg_time(game.get_gametime(), "ships removed");
+	verb_log_dbg_time(game.get_gametime(), "[Splitting ShipFleet]: ships removed");
 	for (auto& port : fleetports) {
 		remove_port(game, port);
 	}
-	verb_log_dbg_time(game.get_gametime(), "ports removed");
+	verb_log_dbg_time(game.get_gametime(), "[Splitting ShipFleet]: ports removed");
 	for (auto& port : fleetports) {
 		if (port->expedition_started()) {
 			port->cancel_expedition(game);
@@ -308,15 +308,15 @@ void ShipFleet::split(Game& game) {
 			port->init_fleet(game);
 		}
 	}
-	verb_log_dbg_time(game.get_gametime(), "ports added");
+	verb_log_dbg_time(game.get_gametime(), "[Splitting ShipFleet]: ports added");
 	for (auto& ship : fleetships) {
 		ship->init_fleet(game);
 	}
-	verb_log_dbg_time(game.get_gametime(), "ships added");
+	verb_log_dbg_time(game.get_gametime(), "[Splitting ShipFleet]: ships added");
 	for (const auto& yard : fleetshipyards) {
 		yard->init_yard_interfaces(game);
 	}
-	verb_log_dbg_time(game.get_gametime(), "yardinterfaces added");
+	verb_log_dbg_time(game.get_gametime(), "[Splitting ShipFleet]: yardinterfaces added");
 }
 
 /**
