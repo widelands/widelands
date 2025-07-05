5c301c4ef44ff578d86db8bfe128b8b9a2976bdf
Simon Heimberg <simon.heimberg@heimberg-ea.ch>
2025-07-05 20:46:48 +0200

    memory leak: handle if WLRT_COMPILE_DIR is not set correctly
    ... or not at all

--- utils/test/memory_leak_summary_from_gh_log.py
+++ utils/test/memory_leak_summary_from_gh_log.py
@@ -57,9 +57,10 @@ class GithubASan:
             if os.getenv('GITHUB_STEP_SUMMARY'):
                 write_summary('    our origin: ', text)
             cls.found_local_tb = True
-            f_name_line = text.rsplit(f'/{compile_d}/', 1)[1]
-            if f_name_line == text:
+            if ' src/' in text:  # stipped by ASAN_OPTIONS='strip_path_prefix=/...
                 f_name_line = 'src/' + text.rsplit(' src/', 1)[1].rstrip()
+            else:
+                f_name_line = text.rsplit(f'/{compile_d}/', 1)[1].rstrip()
             if os.getenv('WLRT_ANNOTATE_LINE'):  # with strategy.job-index == 0 from workflow
                 # annotate only one test job to not have many dublicate messages on one line
                 # unfortunately the jobs can not coordinate
