0a518997cc2b54b702198929982ead8f466ae54c
Simon Heimberg <simon.heimberg@heimberg-ea.ch>
2025-08-21 15:12:42 +0200

    remove properly handled file from utils/find_unused_includes.py
    Removing this file does not result in an error.

--- utils/find_unused_includes.py
+++ utils/find_unused_includes.py
@@ -37,7 +37,6 @@ FUNCTION_REGEX = re.compile(
 # Header files with contents that are too hard to detect by regex
 FILE_EXCLUDES = {'graphic/gl/system_headers.h', 'scripting/lua.h',
                  'third_party/eris/lua.h', 'third_party/eris/lualib.h', 'scripting/eris.h',
-                 'base/format/abstract_node.h',
                  'third_party/tinygettext/include/tinygettext/tinygettext.hpp'}
 
 # Headers files with contents that need to be detected by functions
