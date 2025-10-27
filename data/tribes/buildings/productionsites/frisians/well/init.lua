72cc9888ca1f1d58d92ecb81aca409a789070c71
Benedikt Straub <benedikt-straub@web.de>
2025-10-27 13:22:53 +0100

    New frisian well build animation

--- data/tribes/buildings/productionsites/frisians/well/init.lua
+++ data/tribes/buildings/productionsites/frisians/well/init.lua
@@ -27,6 +27,12 @@ wl.Descriptions():new_productionsite_type {
          columns = 5,
          rows = 2,
          fps = 10
+      },
+      build = {
+         hotspot = {19, 33},
+         frames = 2,
+         columns = 2,
+         rows = 1,
       }
    },
    animations = {
