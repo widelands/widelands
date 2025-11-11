b75baf93dda1f746506a0e76e17281a44f07f56a
andersand <anders@andersand.net>
2025-11-11 20:52:12 +0100

    guards for --nosound

--- src/sound/sound_handler.cc
+++ src/sound/sound_handler.cc
@@ -514,13 +514,23 @@ void SoundHandler::change_music(const std::string& songset_name, int const fadeo
 }
 
 void SoundHandler::set_music_track_enabled(std::string& filename, bool on) {
+	if (SoundHandler::is_backend_disabled()) {
+		return;
+	}
 	songs_[current_songset_]->set_song_enabled(filename, on);
 }
 bool SoundHandler::is_music_track_enabled(std::string& filename) {
+	if (SoundHandler::is_backend_disabled()) {
+		return false;
+	}
 	return songs_[current_songset_]->is_song_enabled(filename);
 }
 
 std::vector<Song> SoundHandler::get_music_data() {
+	if (SoundHandler::is_backend_disabled()) {
+		std::vector<Song> v;
+		return v;
+	}
 	return songs_[current_songset_]->get_song_data();
 }
 
