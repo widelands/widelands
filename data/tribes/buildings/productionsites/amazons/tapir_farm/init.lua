46c7bad53ab74f22a2041cd09560f0dab6be3d61
Benedikt Straub <benedikt-straub@web.de>
2025-12-31 17:12:17 +0100

    New build animation for amazon tapir farm

--- data/tribes/buildings/productionsites/amazons/tapir_farm/init.lua
+++ data/tribes/buildings/productionsites/amazons/tapir_farm/init.lua
@@ -28,6 +28,12 @@ wl.Descriptions():new_productionsite_type {
          columns = 5,
          rows = 2
       },
+      build = {
+         hotspot = {71, 49},
+         frames = 4,
+         columns = 2,
+         rows = 2
+      },
       working = {
          hotspot = {71, 49},
          fps = 15,
