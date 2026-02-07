c1f832e4a44e9358ac9f15d953da6b67cbcc8d90
Benedikt Straub <nordfriese@noreply.codeberg.org>
2026-02-07 11:06:45 +0100

    Build Animations for Frisian Medium Non-Enhanced Productionsites (CB #5306 / GH #6946)
    Co-authored-by: Benedikt Straub <benedikt-straub@web.de>
    Co-authored-by: Benedikt Straub <nordfriese@noreply.codeberg.org>
    Co-committed-by: Benedikt Straub <nordfriese@noreply.codeberg.org>

--- data/tribes/buildings/productionsites/frisians/charcoal_kiln/init.lua
+++ data/tribes/buildings/productionsites/frisians/charcoal_kiln/init.lua
@@ -30,6 +30,12 @@ wl.Descriptions():new_productionsite_type {
          rows = 2,
          fps = 10
       },
+      build = {
+         hotspot = {50, 64},
+         frames = 3,
+         columns = 3,
+         rows = 1,
+      },
       working = {
          hotspot = {50, 82},
          frames = 10,
