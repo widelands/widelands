7a430dcf3daf684e93d5d72ea4266bbcfc805a0a
Benedikt Straub <benedikt-straub@web.de>
2026-06-14 01:01:46 +0200

    codecheck

--- src/ui/shared/maptable.cc
+++ src/ui/shared/maptable.cc
@@ -30,9 +30,10 @@ MapTable::MapTable(
    UI::Panel* parent, int32_t x, int32_t y, uint32_t w, uint32_t h, UI::PanelStyle style)
    : UI::Table<uintptr_t>(parent, "maps_table", x, y, w, h, style) {
 
-	/** TRANSLATORS: Column title for number of players in map list */
 	add_column(
-	   default_panel_dimension_narrow() / 2, _("Pl."), _("Number of players"), UI::Align::kCenter);
+	   default_panel_dimension_narrow() / 2,
+	   /** TRANSLATORS: Column title for number of players in map list */
+	   _("Pl."), _("Number of players"), UI::Align::kCenter);
 	add_column(0, _("Filename"), _("The name of the map or scenario"), UI::Align::kLeft,
 	           UI::TableColumnType::kFlexible);
 	add_column(
