f14c4bbf768874694083350514ad318ce985c553
hessenfarmer <stephan-lutz@gmx.de>
2026-09-06 13:36:08 +0200

    fix syntax

--- data/txts/help/common_helptexts.lua
+++ data/txts/help/common_helptexts.lua
@@ -30,7 +30,6 @@ function help_replays()
       p(_([[Widelands records every game you play in a replay file with .wry extension. This file contains an initial savegame and all commands that were issued by all players, such as building a road or attacking an enemy. The filename will tell you the time you started the game.]])) ..
       p(_([[Using the Watch Replay option in the main menu, a previously played game will be precisely reconstructed for you to watch. Note, however, that even small changes in the game data files or the Widelands executable can cause a replay to become incompatible and to ‘go out of sync’. This is similar to how network games will suffer from desynchronization problems if one of the players has a different version of the game.]])) ..
       p(_([[Replays are a very useful tool to find bugs in the game logic, so they can be useful as attachments to bug reports. The replay files are saved in the ‘replays’ subdirectory of your home directory. You may look in the last tab "Technical Information" of this window to see where your home directory is on your computer.]]))
-      )
    pop_textdomain()
    return r
 end
